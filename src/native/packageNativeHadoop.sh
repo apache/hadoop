32f11812bf344d1b8e476117199da2469d651dba
Doug Cutting <cutting@apache.org>
2006-11-15 17:55:01 +0000

    HADOOP-722.  Fix some issues with HADOOP-538, setting scripts to be executable, and fixing the package target when lib directories don't exist.  Contributed by Arun.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/hadoop/trunk@475331 13f79535-47bb-0310-9956-ffa450edef68

--- src/native/packageNativeHadoop.sh
+++ src/native/packageNativeHadoop.sh
@@ -16,29 +16,36 @@
 TAR='tar -c'
 UNTAR='tar -x'
 
-# Copy the pre-built libraries in $BASE_NATIVE_LIB_DIR 
-for platform in `ls $BASE_NATIVE_LIB_DIR`
-do
-  if test ! -e $DIST_LIB_DIR/$platform
-  then
-    mkdir -p $DIST_LIB_DIR/$platform
-    echo "Created $DIST_LIB_DIR/$platform"
-  fi
-  echo "Copying libraries in $BASE_NATIVE_LIB_DIR/$platform to $DIST_LIB_DIR/$platform/"
-  cd $BASE_NATIVE_LIB_DIR/$platform/
-  $TAR *hadoop* | $UNTAR -C $DIST_LIB_DIR/$platform/
-done  
+# Copy the pre-built libraries in $BASE_NATIVE_LIB_DIR
+if test -e $BASE_NATIVE_LIB_DIR
+then
+  for platform in `ls $BASE_NATIVE_LIB_DIR`
+  do
+    if test ! -e $DIST_LIB_DIR/$platform
+    then
+      mkdir -p $DIST_LIB_DIR/$platform
+      echo "Created $DIST_LIB_DIR/$platform"
+    fi
+    echo "Copying libraries in $BASE_NATIVE_LIB_DIR/$platform to $DIST_LIB_DIR/$platform/"
+    cd $BASE_NATIVE_LIB_DIR/$platform/
+    $TAR *hadoop* | $UNTAR -C $DIST_LIB_DIR/$platform/
+  done
+fi
 
 # Copy the custom-built libraries in $BUILD_DIR
-for platform in `ls $BUILD_NATIVE_DIR`
-do
-  if test ! -e $DIST_LIB_DIR/$platform
-  then
-    mkdir -p $DIST_LIB_DIR/$platform
-    echo "Created $DIST_LIB_DIR/$platform"
-  fi
-  echo "Copying libraries in $BUILD_NATIVE_DIR/$platform/lib to $DIST_LIB_DIR/$platform/"
-  cd $BUILD_NATIVE_DIR/$platform/lib
-  $TAR *hadoop* | $UNTAR -C $DIST_LIB_DIR/$platform/
-done  
+if test -e $BUILD_NATIVE_DIR
+then 
+  for platform in `ls $BUILD_NATIVE_DIR`
+  do
+    if test ! -e $DIST_LIB_DIR/$platform
+    then
+      mkdir -p $DIST_LIB_DIR/$platform
+      echo "Created $DIST_LIB_DIR/$platform"
+    fi
+    echo "Copying libraries in $BUILD_NATIVE_DIR/$platform/lib to $DIST_LIB_DIR/$platform/"
+    cd $BUILD_NATIVE_DIR/$platform/lib
+    $TAR *hadoop* | $UNTAR -C $DIST_LIB_DIR/$platform/
+  done  
+fi
 
+#vim: ts=2: sw=2: et
