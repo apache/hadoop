cb238d625b20c45b689444a816b6068c888b3572
Thomas White <tomwhite@apache.org>
2007-05-22 08:35:10 +0000

    Merge -r 540468:540469 from trunk to 0.13 branch. Fixes: HADOOP-1407.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/hadoop/branches/branch-0.13@540473 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/mapred/JobInProgress.java
+++ src/java/org/apache/hadoop/mapred/JobInProgress.java
@@ -945,8 +945,8 @@ class JobInProgress {
       //
       boolean killJob = 
         tip.isMapTask() ? 
-            (((++failedMapTIPs*100)/numMapTasks) > mapFailuresPercent) :
-            (((++failedReduceTIPs*100)/numReduceTasks) > reduceFailuresPercent);
+            ((++failedMapTIPs*100) > (mapFailuresPercent*numMapTasks)) :
+            ((++failedReduceTIPs*100) > (reduceFailuresPercent*numReduceTasks));
       
       if (killJob) {
         LOG.info("Aborting job " + profile.getJobId());
