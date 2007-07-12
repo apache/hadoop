01550f18e77d78dec4ba31c064dab968db44c26d
Doug Cutting <cutting@apache.org>
2007-07-12 20:20:53 +0000

    HADOOP-1598.  Fix license headers.  Contributed by Enis Soztutar.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/hadoop/trunk@555744 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/filecache/DistributedCache.java
+++ src/java/org/apache/hadoop/filecache/DistributedCache.java
@@ -1,5 +1,4 @@
-/* Copyright 2005 The Apache Software Foundation
- *
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.hadoop.filecache;
 
 import org.apache.commons.logging.*;
