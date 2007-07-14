bda7cc6ad350d979a9d290835ecd6ba217f9a635
Michael Stack <stack@apache.org>
2007-07-14 23:53:39 +0000

    HADOOP-1607 [hbase shell] Clear screen command. A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/ClearCommand.java     Added. M src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/HBaseShell.jj     Add 'clear' command handling.     (clearCommand): Added. M src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/generated/Parser.java M src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/generated/ParserConstants.java M src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/generated/ParserTokenManager.java     Add 'clear' command handling (Generated by javacc). M src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/HelpContents.java     Add 'CLEAR' help. M src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/HelpManager.java     Clear screen before outputting version string.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/hadoop/trunk@556356 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/ClearCommand.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2007 The Apache Software Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.hbase.shell;
+
+import java.io.IOException;
+
+import org.apache.hadoop.hbase.HClient;
+
+public class ClearCommand extends BasicCommand {
+
+  public ReturnMsg execute(HClient client) {
+    clear();
+    return null;
+  }
+
+  static void clear() {
+    String osName = System.getProperty("os.name");
+    if (osName.length() > 7 && osName.subSequence(0, 7).equals("Windows")) {
+      try {
+        Runtime.getRuntime().exec("cls");
+      } catch (IOException e) {
+        System.out.println("Can't clear." + e.toString());
+      }
+    } else {
+      System.out.print("\033c");
+    }
+  }
+
+}
