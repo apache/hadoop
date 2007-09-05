e4d9ded2f42a9bdb4130a9615c87c1ee5ed13941
Michael Stack <stack@apache.org>
2007-09-05 16:12:39 +0000

    HADOOP-1835 Updated Documentation for HBase setup/installation M    hbase/conf/hbase-env.sh     Removed JAVA_HOME references. M    hbase/src/java/org/apache/hadoop/hbase/package.html     Improved setup instruction
    git-svn-id: https://svn.apache.org/repos/asf/lucene/hadoop/trunk@572983 13f79535-47bb-0310-9956-ffa450edef68

--- src/contrib/hbase/conf/hbase-env.sh
+++ src/contrib/hbase/conf/hbase-env.sh
@@ -21,14 +21,6 @@
 
 # Set HBase-specific environment variables here.
 
-# The only required environment variable is JAVA_HOME.  All others are
-# optional.  When running a distributed configuration it is best to
-# set JAVA_HOME in this file, so that it is correctly defined on
-# remote nodes.
-
-# The java implementation to use.  Required.
-# export JAVA_HOME=/usr/lib/j2sdk1.5-sun
-
 # Extra Java CLASSPATH elements.  Optional.
 # export HBASE_CLASSPATH=
 
@@ -38,5 +30,5 @@
 # Extra Java runtime options.  Empty by default.
 # export HBASE_OPTS=-server
 
-# File naming remote slave hosts.  $HADOOP_HOME/conf/slaves by default.
+# File naming hosts on which HRegionServers will run.  $HBASE_HOME/conf/regionservers by default.
 # export HBASE_REGIONSERVERS=${HBASE_HOME}/conf/regionservers
