b683c99315308984beeb7a99ab79f7beabe6f156
Jim Kellerman <jimk@apache.org>
2007-11-03 04:00:12 +0000

    HADOOP-2109 Fix another race condition in processing dead servers
    git-svn-id: https://svn.apache.org/repos/asf/lucene/hadoop/trunk@591549 13f79535-47bb-0310-9956-ffa450edef68

--- src/contrib/hbase/src/java/org/apache/hadoop/hbase/HMaster.java
+++ src/contrib/hbase/src/java/org/apache/hadoop/hbase/HMaster.java
@@ -2087,7 +2087,7 @@ HMasterRegionInterface {
           if (closed.get()) {
             return true;
           }
-          if (!rootScanned ||
+          if (!rootRescanned ||
               numberOfMetaRegions.get() != onlineMetaRegions.size()) {
             // We can't proceed because not all of the meta regions are online.
             // We can't block either because that would prevent the meta region
@@ -2096,7 +2096,7 @@ HMasterRegionInterface {
             
             if (LOG.isDebugEnabled()) {
               LOG.debug("Requeuing shutdown because rootScanned: " +
-                  rootScanned + ", numberOfMetaRegions: " +
+                  rootRescanned + ", numberOfMetaRegions: " +
                   numberOfMetaRegions.get() + ", onlineMetaRegions.size(): " +
                   onlineMetaRegions.size());
             }
