5c0ac7a4b7023176a247d149cf74da775100a6b8
Christopher Douglas <cdouglas@apache.org>
2008-05-01 18:49:49 +0000

    HADOOP-2799. Deprecate o.a.h.io.Closable in favor of java.io.Closable. Contributed by Tsz Wo (Nicholas), SZE.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/core/trunk@652619 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/io/Closeable.java
+++ src/java/org/apache/hadoop/io/Closeable.java
@@ -18,13 +18,7 @@
 
 package org.apache.hadoop.io;
 
-import java.io.IOException;
-
-// NOTE: Replace with java.io.Closeable when we upgrade to Java 1.5.
-
-/** That which can be closed. */
-public interface Closeable {
-  /** Called after the last call to any other method on this object to free
-   * and/or flush resources.  Typical implementations do nothing. */
-  void close() throws IOException;
+/** @deprecated use java.io.Closeable */
+@Deprecated
+public interface Closeable extends java.io.Closeable{
 }
