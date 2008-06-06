a3aa3548c41a0862997631aa14aec7925b3fa3ed
Michael Stack <stack@apache.org>
2008-06-06 23:01:31 +0000

    HBASE-671 New UI page displaying all regions in a table should be sorted
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk@664181 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hbase/client/HTable.java
+++ src/java/org/apache/hadoop/hbase/client/HTable.java
@@ -21,8 +21,6 @@ package org.apache.hadoop.hbase.client;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
