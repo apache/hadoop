20e3a44bee5b53512b81f9cf1f73d87b11603bf4
Konstantin Shvachko <shv@apache.org>
2008-06-06 23:48:28 +0000

    HADOOP-3489. Fix NPE in SafeModeMonitor. Contributed by Lohit Vijayarenu.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/core/trunk@664201 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/dfs/FSNamesystem.java
+++ src/java/org/apache/hadoop/dfs/FSNamesystem.java
@@ -3948,7 +3948,7 @@ class FSNamesystem implements FSConstants, FSNamesystemMBean {
     /**
      */
     public void run() {
-      while (fsRunning && !safeMode.canLeave()) {
+      while (fsRunning && (safeMode != null && !safeMode.canLeave())) {
         try {
           Thread.sleep(recheckInterval);
         } catch (InterruptedException ie) {
