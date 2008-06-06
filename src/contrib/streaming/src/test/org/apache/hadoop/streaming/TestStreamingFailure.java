5e3496db0a0f099367caae65123502927c0fa807
Arun Murthy <acmurthy@apache.org>
2008-06-06 03:11:22 +0000

    HADOOP-3493. Fix TestStreamingFailure to use FileUtil.fullyDelete to ensure correct cleanup. Contributed by Lohit Vijayarenu.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/core/trunk@663812 13f79535-47bb-0310-9956-ffa450edef68

--- src/contrib/streaming/src/test/org/apache/hadoop/streaming/TestStreamingFailure.java
+++ src/contrib/streaming/src/test/org/apache/hadoop/streaming/TestStreamingFailure.java
@@ -23,6 +23,7 @@ import java.io.*;
 import java.util.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
 
 /**
@@ -59,7 +60,7 @@ public class TestStreamingFailure extends TestStreaming
   {
     try {
       try {
-        OUTPUT_DIR.getAbsoluteFile().delete();
+        FileUtil.fullyDelete(OUTPUT_DIR.getAbsoluteFile());
       } catch (Exception e) {
       }
 
