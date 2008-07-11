e60307ea31ab407c18dff465ab9c7fe840e8ba1b
Michael Stack <stack@apache.org>
2008-07-11 22:18:43 +0000

    HBASE-739 HBaseAdmin.createTable() using old HTableDescription doesn't work
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk@676092 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hbase/client/UnmodifyableHTableDescriptor.java
+++ src/java/org/apache/hadoop/hbase/client/UnmodifyableHTableDescriptor.java
@@ -22,9 +22,13 @@ package org.apache.hadoop.hbase.client;
 
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.util.Bytes;
 
-class UnmodifyableHTableDescriptor extends HTableDescriptor {
+/**
+ * Read-only table descriptor.
+ * Returned out of {@link HTable.getTableDescriptor}.
+ */
+public class UnmodifyableHTableDescriptor extends HTableDescriptor {
+
   /*
    * Create an unmodifyable copy of an HTableDescriptor
    * @param desc
