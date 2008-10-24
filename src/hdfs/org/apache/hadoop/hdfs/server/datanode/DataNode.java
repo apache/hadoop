e6cacda2a4a8b62d4de1e4387b936da65bf2d1a1
Owen O'Malley <omalley@apache.org>
2008-10-24 22:47:54 +0000

    HADOOP-4482. Make the JMX monitoring use predictable names for the datanodes to enable Nagios monitoring. (Brian Bockelman via omalley)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/core/trunk@707772 13f79535-47bb-0310-9956-ffa450edef68

--- src/hdfs/org/apache/hadoop/hdfs/server/datanode/DataNode.java
+++ src/hdfs/org/apache/hadoop/hdfs/server/datanode/DataNode.java
@@ -365,7 +365,7 @@ public class DataNode extends Configured
     this.infoServer.start();
     // adjust info port
     this.dnRegistration.setInfoPort(this.infoServer.getPort());
-    myMetrics = new DataNodeMetrics(conf, dnRegistration.getStorageID());
+    myMetrics = new DataNodeMetrics(conf, dnRegistration.getName());
     
     //init ipc server
     InetSocketAddress ipcAddr = NetUtils.createSocketAddr(
