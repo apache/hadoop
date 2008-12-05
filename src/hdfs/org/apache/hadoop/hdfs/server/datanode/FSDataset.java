025363eeafd3bbf54e94712acac09512c5639489
Tsz-wo Sze <szetszwo@apache.org>
2008-12-05 21:43:07 +0000

    HADOOP-4778. Check for zero size block meta file when updating a block. (szetszwo)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/core/branches/branch-0.19@723882 13f79535-47bb-0310-9956-ffa450edef68

--- src/hdfs/org/apache/hadoop/hdfs/server/datanode/FSDataset.java
+++ src/hdfs/org/apache/hadoop/hdfs/server/datanode/FSDataset.java
@@ -1240,6 +1240,9 @@ public class FSDataset implements FSConstants, FSDatasetInterface {
                             " metafile " + meta +
                             " does not exist on disk.");
     }
+    if (meta.length() == 0) {
+      throw new IOException("Block " + b + " metafile " + meta + " is empty.");
+    }
     long stamp = parseGenerationStamp(f, meta);
     if (stamp != b.getGenerationStamp()) {
       throw new IOException("Block " + b + 
