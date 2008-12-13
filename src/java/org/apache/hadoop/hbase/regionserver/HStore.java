0f3838e1d1e5d0b058ec82ab7f71c3ae93e00efa
Michael Stack <stack@apache.org>
2008-12-13 00:40:07 +0000

    HBASE-1059 ConcurrentModificationException in notifyChangedReadersObservers
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk@726152 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hbase/regionserver/HStore.java
+++ src/java/org/apache/hadoop/hbase/regionserver/HStore.java
@@ -122,8 +122,10 @@ public class HStore implements HConstants {
   private final Path compactionDir;
   private final Integer compactLock = new Integer(0);
   private final int compactionThreshold;
+  
+  // All access must be synchronized.
   private final Set<ChangedReadersObserver> changedReaderObservers =
-    Collections.synchronizedSet(new HashSet<ChangedReadersObserver>());
+    new HashSet<ChangedReadersObserver>();
 
   /**
    * An HStore is a set of zero or more MapFiles, which stretch backwards over 
@@ -740,15 +742,19 @@ public class HStore implements HConstants {
    * @param o Observer who wants to know about changes in set of Readers
    */
   void addChangedReaderObserver(ChangedReadersObserver o) {
-    this.changedReaderObservers.add(o);
+    synchronized(this.changedReaderObservers) {
+      this.changedReaderObservers.add(o);
+    }
   }
   
   /*
    * @param o Observer no longer interested in changes in set of Readers.
    */
   void deleteChangedReaderObserver(ChangedReadersObserver o) {
-    if (!this.changedReaderObservers.remove(o)) {
-      LOG.warn("Not in set" + o);
+    synchronized (this.changedReaderObservers) {
+      if (!this.changedReaderObservers.remove(o)) {
+        LOG.warn("Not in set" + o);
+      }
     }
   }
 
