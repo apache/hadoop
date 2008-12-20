6b518b65af57093123e297dc818e572b120bbbac
Christopher Douglas <cdouglas@apache.org>
2008-12-20 01:17:25 +0000

    HADOOP-4894. Correctly parse ps output in Chukwa jettyCollector.sh. Contributed by Ari Rabkin
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/core/branches/branch-0.20@728211 13f79535-47bb-0310-9956-ffa450edef68

--- src/contrib/chukwa/bin/jettyCollector.sh
+++ src/contrib/chukwa/bin/jettyCollector.sh
@@ -25,7 +25,7 @@ trap 'stop; exit 0' 1 2 15
 
 function stop {
   echo -n "Shutting down Collector..."
-  ${JPS} | grep CollectorStub | grep -v grep | cut -f 1 -d" " | xargs kill -TERM >&/dev/null
+  ${JPS} | grep CollectorStub | grep -v grep | grep -o '[^ ].*'| cut -f 1 -d" " | xargs kill -TERM >&/dev/null
   echo "done"
   exit 0
 }
@@ -34,4 +34,4 @@ if [ "X$1" = "Xstop" ]; then
   stop
 fi
 
-${JAVA_HOME}/bin/java -DAPP=collector -Dlog4j.configuration=chukwa-log4j.properties -DCHUKWA_HOME=${CHUKWA_HOME} -DCHUKWA_CONF_DIR=${CHUKWA_CONF_DIR} -DCHUKWA_LOG_DIR=${CHUKWA_LOG_DIR} -classpath ${CLASSPATH}:${CHUKWA_CORE}:${COMMON}:${HADOOP_JAR}:${CHUKWA_CONF_DIR} org.apache.hadoop.chukwa.datacollection.collector.CollectorStub 
+${JAVA_HOME}/bin/java -DAPP=collector -Dlog4j.configuration=chukwa-log4j.properties -DCHUKWA_HOME=${CHUKWA_HOME} -DCHUKWA_CONF_DIR=${CHUKWA_CONF_DIR} -DCHUKWA_LOG_DIR=${CHUKWA_LOG_DIR} -classpath ${CLASSPATH}:${CHUKWA_CORE}:${COMMON}:${HADOOP_JAR}:${CHUKWA_CONF_DIR} org.apache.hadoop.chukwa.datacollection.collector.CollectorStub $@
