82ba0bd2d0c3ac1c9a8cf098b46e8867295cee28
Hairong Kuang <hairong@apache.org>
2009-01-08 18:31:34 +0000

    Merge -r 732776:732777 from trunk to move the change of HADOOP-4910 into branch 0.20
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/core/branches/branch-0.20@732784 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/hadoop/hdfs/MiniDFSCluster.java
+++ src/test/org/apache/hadoop/hdfs/MiniDFSCluster.java
@@ -638,7 +638,7 @@ public class MiniDFSCluster {
   /*
    * Restart a particular datanode
    */
-  synchronized boolean restartDataNode(int i) throws IOException {
+  public synchronized boolean restartDataNode(int i) throws IOException {
     DataNodeProperties dnprop = stopDataNode(i);
     if (dnprop == null) {
       return false;
