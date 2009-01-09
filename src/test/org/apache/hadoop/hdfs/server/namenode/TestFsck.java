c08f767dce602ac921583459d8ea42a551341b81
Konstantin Shvachko <shv@apache.org>
2009-01-09 00:49:51 +0000

    HADOOP-4982. Merge -r 732881:732882 from trunk to branch 0.19.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/core/branches/branch-0.19@732888 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/hadoop/hdfs/server/namenode/TestFsck.java
+++ src/test/org/apache/hadoop/hdfs/server/namenode/TestFsck.java
@@ -146,7 +146,8 @@ public class TestFsck extends TestCase {
       String block = dfsClient.namenode.
                       getBlockLocations(fileNames[0], 0, Long.MAX_VALUE).
                       get(0).getBlock().getBlockName();
-      File baseDir = new File(System.getProperty("test.build.data"),"dfs/data");
+      File baseDir = new File(System.getProperty("test.build.data",
+                                                 "build/test/data"),"dfs/data");
       for (int i=0; i<8; i++) {
         File blockFile = new File(baseDir, "data" +(i+1)+ "/current/" + block);
         if(blockFile.exists()) {
@@ -255,7 +256,8 @@ public class TestFsck extends TestCase {
     assertTrue(outStr.contains("HEALTHY"));
     
     // corrupt replicas 
-    File baseDir = new File(System.getProperty("test.build.data"), "dfs/data");
+    File baseDir = new File(System.getProperty("test.build.data",
+                                               "build/test/data"),"dfs/data");
     for (int i=0; i < 6; i++) {
       File blockFile = new File(baseDir, "data" + (i+1) + "/current/" +
                                 block);
