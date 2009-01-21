66260f8d2d4884967b92506076506ad9dbdd94af
Christopher Douglas <cdouglas@apache.org>
2009-01-21 00:09:39 +0000

    HADOOP-5070. Increment copyright year to 2009, remove assertions of ASF copyright to licensed files. Contributed by Tsz Wo (Nicholas), SZE.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/core/trunk@736174 13f79535-47bb-0310-9956-ffa450edef68

--- src/c++/libhdfs/hdfsJniHelper.c
+++ src/c++/libhdfs/hdfsJniHelper.c
@@ -1,9 +1,11 @@
 /**
- * Copyright 2006 The Apache Software Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #include <string.h> 
 #include <error.h>
 #include "hdfsJniHelper.h"
