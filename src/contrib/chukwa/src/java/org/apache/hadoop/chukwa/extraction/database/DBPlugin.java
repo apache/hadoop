cdbded170b95d730154c93e9611ef99b2292eded
Owen O'Malley <omalley@apache.org>
2009-03-10 18:18:54 +0000

    Moving codebase
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/core/trunk@752204 13f79535-47bb-0310-9956-ffa450edef68

--- src/contrib/chukwa/src/java/org/apache/hadoop/chukwa/extraction/database/DBPlugin.java
+++
@@ -1,9 +0,0 @@
-package org.apache.hadoop.chukwa.extraction.database;
-
-import org.apache.hadoop.io.SequenceFile;
-
-public interface DBPlugin
-{
-	void process(SequenceFile.Reader reader)
-	throws DBException;
-}
--- src/contrib/chukwa/tools/startpacketcapture.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-OUTFILE=/scratch/packets
-rm -f $OUTFILE
-sudo tcpdump -w $OUTFILE &
-CAPTUREPID=$!
-echo $CAPTUREPID > /var/tmp/packetcapturepid
-chmod 644 /var/tmp/packetcapturepid #to avoid stop being a huge security hole
-echo "capture process has PID $CAPTUREPID"
