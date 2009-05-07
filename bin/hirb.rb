59c6ce905d330a5fbdb33379f0fe6cabbed79080
Michael Stack <stack@apache.org>
2009-05-07 04:48:44 +0000

    HBASE-1383 hbase shell needs to warn on deleting multi-region table
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk@772499 13f79535-47bb-0310-9956-ffa450edef68

--- bin/hirb.rb
+++ bin/hirb.rb
@@ -194,7 +194,10 @@ HBASE SHELL COMMANDS:
 
  disable   Disable the named table: e.g. "hbase> disable 't1'"
  
- drop      Drop the named table. Table must first be disabled
+ drop      Drop the named table. Table must first be disabled. If table has
+           more than one region, run a major compaction on .META.:
+
+           hbase> major_compact ".META."
 
  enable    Enable the named table
 
