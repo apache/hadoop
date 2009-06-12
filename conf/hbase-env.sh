d841ab681dc2708950e52aed6d1a932e94ab1764
Michael Stack <stack@apache.org>
2009-06-12 05:59:54 +0000

    HBASE-1507  iCMS as default JVM
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk@783990 13f79535-47bb-0310-9956-ffa450edef68

--- conf/hbase-env.sh
+++ conf/hbase-env.sh
@@ -30,8 +30,10 @@
 # The maximum amount of heap to use, in MB. Default is 1000.
 # export HBASE_HEAPSIZE=1000
 
-# Extra Java runtime options.  Empty by default.
-# export HBASE_OPTS=-server
+# Extra Java runtime options.
+# Below are what we set by default.  For more on why as well as other possible
+# settings, see http://wiki.apache.org/hadoop/PerformanceTuning
+# export HBASE_OPTS="$HBASE_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode"
 
 # File naming hosts on which HRegionServers will run.  $HBASE_HOME/conf/regionservers by default.
 # export HBASE_REGIONSERVERS=${HBASE_HOME}/conf/regionservers
