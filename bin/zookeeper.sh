e62faa3535fe2a9b3560495ef0f9af4a2eb7d321
Michael Stack <stack@apache.org>
2009-06-19 21:39:40 +0000

    HBASE-1549 in zoo.cfg, use localhost instead of 127.0.0.1?
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk@786673 13f79535-47bb-0310-9956-ffa450edef68

--- bin/zookeeper.sh
+++ bin/zookeeper.sh
@@ -53,7 +53,7 @@ if [ "$HBASE_MANAGES_ZK" = "" ]; then
 fi
 
 if [ "$HBASE_MANAGES_ZK" = "true" ]; then
- ssh $HBASE_SSH_OPTS 127.0.0.1 $"${@// /\\ }" 2>&1 | sed "s/^/localhost: /" &
+ ssh $HBASE_SSH_OPTS localhost $"${@// /\\ }" 2>&1 | sed "s/^/localhost: /" &
  if [ "$HBASE_SLAVE_SLEEP" != "" ]; then
    sleep $HBASE_SLAVE_SLEEP
  fi
