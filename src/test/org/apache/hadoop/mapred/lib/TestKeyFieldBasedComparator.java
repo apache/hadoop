64612daf53d0a7d4887bbdf987db234a67fbb081
Devaraj Das <ddas@apache.org>
2009-07-22 14:47:09 +0000

    MAPREDUCE-735. Fixes a problem in the KeyFieldHelper to do with the end index for some inputs. Contributed by Amar Kamat.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.20@796749 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/hadoop/mapred/lib/TestKeyFieldBasedComparator.java
+++ src/test/org/apache/hadoop/mapred/lib/TestKeyFieldBasedComparator.java
@@ -133,6 +133,8 @@ public class TestKeyFieldBasedComparator extends HadoopTestCase {
     configure("-k10,10",2);
     
     localTestWithoutMRJob("-k9,9", 1);
+
+    localTestWithoutMRJob("-k9n", 1);
   }
   
   byte[] line1_bytes = line1.getBytes();
