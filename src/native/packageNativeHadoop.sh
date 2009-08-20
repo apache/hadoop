1f3337996e76fb13595129b0b4f8240539a32b72
Tsz-wo Sze <szetszwo@apache.org>
2009-08-20 21:28:09 +0000

    HADOOP-6173. Change src/native/packageNativeHadoop.sh to package all native library files.  Contributed by Hong Tang
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@806368 13f79535-47bb-0310-9956-ffa450edef68

--- src/native/packageNativeHadoop.sh
+++ src/native/packageNativeHadoop.sh
@@ -42,7 +42,7 @@ then
     fi
     echo "Copying libraries in $BASE_NATIVE_LIB_DIR/$platform to $DIST_LIB_DIR/$platform/"
     cd $BASE_NATIVE_LIB_DIR/$platform/
-    $TAR *hadoop* | (cd $DIST_LIB_DIR/$platform/; $UNTAR)
+    $TAR . | (cd $DIST_LIB_DIR/$platform/; $UNTAR)
   done
 fi
 
@@ -58,7 +58,7 @@ then
     fi
     echo "Copying libraries in $BUILD_NATIVE_DIR/$platform/lib to $DIST_LIB_DIR/$platform/"
     cd $BUILD_NATIVE_DIR/$platform/lib
-    $TAR *hadoop* | (cd $DIST_LIB_DIR/$platform/; $UNTAR)
+    $TAR . | (cd $DIST_LIB_DIR/$platform/; $UNTAR)
   done  
 fi
 
