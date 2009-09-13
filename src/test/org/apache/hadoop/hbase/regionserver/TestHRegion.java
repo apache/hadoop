4479b081abfdcb11dde34ba4843c068bd329f667
Michael Stack <stack@apache.org>
2009-09-13 22:16:36 +0000

    HBASE-1835  Add more delete tests
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk@814398 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/hadoop/hbase/regionserver/TestHRegion.java
+++ src/test/org/apache/hadoop/hbase/regionserver/TestHRegion.java
@@ -314,6 +314,27 @@ public class TestHRegion extends HBaseTestCase {
     get = new Get(row).addColumn(fam, splitB);
     result = region.get(get, null);
     assertEquals(1, result.size());
+
+    // Assert that after a delete, I can put.
+    put = new Put(row);
+    put.add(fam, splitA, Bytes.toBytes("reference_A"));
+    region.put(put);
+    get = new Get(row);
+    result = region.get(get, null);
+    assertEquals(3, result.size());
+    
+    // Now delete all... then test I can add stuff back
+    delete = new Delete(row);
+    region.delete(delete, null, false);
+    assertEquals(0, region.get(get, null).size());
+    try { 
+      Thread.sleep(10);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    region.put(new Put(row).add(fam, splitA, Bytes.toBytes("reference_A")));
+    result = region.get(get, null);
+    assertEquals(1, result.size());
   }
   
   public void testDeleteRowWithFutureTs() throws IOException {
