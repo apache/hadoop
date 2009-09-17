4c6d3ff0d43c427d09b26dc7bc43b12bc5fe8338
Jonathan Gray <jgray@apache.org>
2009-09-17 18:45:39 +0000

    HBASE-1850 moving SampleUploader to mapreduce package
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk@816332 13f79535-47bb-0310-9956-ffa450edef68

--- src/examples/mapreduce/org/apache/hadoop/hbase/mapreduce/SampleUploader.java
+++ src/examples/mapreduce/org/apache/hadoop/hbase/mapreduce/SampleUploader.java
@@ -17,7 +17,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.hbase.mapred;
+package org.apache.hadoop.hbase.mapreduce;
 
 import java.io.IOException;
 
