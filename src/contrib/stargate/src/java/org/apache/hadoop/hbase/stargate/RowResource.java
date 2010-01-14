437d1374141c7e8ff215bc273f96fca0b6604caa
Andrew Kyle Purtell <apurtell@apache.org>
2010-01-14 01:39:27 +0000

    HBASE-2120 [stargate] Unable to delete column families
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk@899022 13f79535-47bb-0310-9956-ffa450edef68

--- src/contrib/stargate/src/java/org/apache/hadoop/hbase/stargate/RowResource.java
+++ src/contrib/stargate/src/java/org/apache/hadoop/hbase/stargate/RowResource.java
@@ -281,13 +281,13 @@ public class RowResource implements Constants {
     for (byte[] column: rowspec.getColumns()) {
       byte[][] split = KeyValue.parseColumn(column);
       if (rowspec.hasTimestamp()) {
-        if (split.length == 2) {
+        if (split.length == 2 && split[1].length != 0) {
           delete.deleteColumns(split[0], split[1], rowspec.getTimestamp());
         } else {
           delete.deleteFamily(split[0], rowspec.getTimestamp());
         }
       } else {
-        if (split.length == 2) {
+        if (split.length == 2 && split[1].length != 0) {
           delete.deleteColumns(split[0], split[1]);
         } else {
           delete.deleteFamily(split[0]);
