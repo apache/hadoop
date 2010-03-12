dd8456b1beae7a84bf394fda33df4bc624c81aed
Michael Stack <stack@apache.org>
2010-03-12 19:59:35 +0000

    HBASE-2316 Need an ability to run shell tests w/o invoking junit
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk@922408 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/ruby/tests_runner.rb
+++ core/src/test/ruby/tests_runner.rb
@@ -1,6 +1,26 @@
 require 'rubygems'
 require 'rake'
 
+unless defined?($TEST_CLUSTER)
+  include Java
+
+  # Set logging level to avoid verboseness
+  org.apache.log4j.Logger.getRootLogger.setLevel(org.apache.log4j.Level::OFF)
+  org.apache.log4j.Logger.getLogger("org.apache.zookeeper").setLevel(org.apache.log4j.Level::OFF)
+  org.apache.log4j.Logger.getLogger("org.apache.hadoop.hdfs").setLevel(org.apache.log4j.Level::OFF)
+  org.apache.log4j.Logger.getLogger("org.apache.hadoop.hbase").setLevel(org.apache.log4j.Level::OFF)
+  org.apache.log4j.Logger.getLogger("org.apache.hadoop.ipc.HBaseServer").setLevel(org.apache.log4j.Level::OFF)
+
+  java_import org.apache.hadoop.hbase.HBaseTestingUtility
+
+  $TEST_CLUSTER = HBaseTestingUtility.new
+  $TEST_CLUSTER.configuration.setInt("hbase.regionserver.msginterval", 100)
+  $TEST_CLUSTER.configuration.setInt("hbase.client.pause", 250)
+  $TEST_CLUSTER.configuration.setInt("hbase.client.retries.number", 6)
+  $TEST_CLUSTER.startMiniCluster
+  @own_cluster = true
+end
+
 require 'test_helper'
 
 puts "Running tests..."
@@ -15,4 +35,10 @@ files.each do |file|
   end
 end
 
-puts "Done with tests!"
+Test::Unit::AutoRunner.run
+
+puts "Done with tests! Shutting down the cluster..."
+if @own_cluster
+  $TEST_CLUSTER.shutdownMiniCluster
+  java.lang.System.exit(0)
+end
