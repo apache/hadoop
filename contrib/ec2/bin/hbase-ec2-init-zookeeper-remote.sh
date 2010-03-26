a03976103d8efe4074542c6f0a1d612e96410788
Andrew Kyle Purtell <apurtell@apache.org>
2010-03-26 22:45:48 +0000

    added copyright notices
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk@928089 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/ec2/bin/hbase-ec2-init-zookeeper-remote.sh
+++ contrib/ec2/bin/hbase-ec2-init-zookeeper-remote.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # ZOOKEEPER_QUORUM set in the environment by the caller
 HBASE_HOME=`ls -d /usr/local/hbase-*`
 
-###############################################################################
 # HBase configuration (Zookeeper)
-###############################################################################
-
 cat > $HBASE_HOME/conf/hbase-site.xml <<EOF
 <?xml version="1.0"?>
 <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
@@ -30,9 +42,7 @@ cat > $HBASE_HOME/conf/hbase-site.xml <<EOF
 </configuration>
 EOF
 
-###############################################################################
 # Start services
-###############################################################################
 
 # up open file descriptor limits
 echo "root soft nofile 32768" >> /etc/security/limits.conf
