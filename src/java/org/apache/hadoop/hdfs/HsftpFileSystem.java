65df338953f24c3f8c5d379316e90b2c435a5582
Tsz-wo Sze <szetszwo@apache.org>
2010-04-06 20:55:28 +0000

    HDFS-482. Move HsftpFileSystem's ssl.client.do.not.authenticate.server configuration setting to ssl-client.xml.  Contributed by Srikanth Sundarrajan
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hdfs/trunk@931325 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hdfs/HsftpFileSystem.java
+++ src/java/org/apache/hadoop/hdfs/HsftpFileSystem.java
@@ -90,7 +90,7 @@ public class HsftpFileSystem extends HftpFileSystem {
         fis = null;
       }
       // initialize default trust manager with truststore file and pass
-      if (conf.getBoolean("ssl.client.do.not.authenticate.server", false)) {
+      if (sslConf.getBoolean("ssl.client.do.not.authenticate.server", false)) {
         // by pass trustmanager validation
         tms = new DummyTrustManager[] { new DummyTrustManager() };
       } else {
