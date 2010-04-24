27a9fce10dbf9ff85ed8bf90b3b7625c6e69d75a
Suresh Srinivas <suresh@apache.org>
2010-04-24 02:04:20 +0000

    HADOOP-6710. Symbolic umask for file creation is not conformant with posix.  Contributed by Suresh Srinivas.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@937577 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/fs/permission/UmaskParser.java
+++ src/java/org/apache/hadoop/fs/permission/UmaskParser.java
@@ -30,7 +30,7 @@ class UmaskParser extends PermissionParser {
   private static Pattern chmodOctalPattern =
     Pattern.compile("^\\s*[+]?()([0-7]{3})\\s*$"); // no leading 1 for sticky bit
   private static Pattern umaskSymbolicPattern =    /* not allow X or t */
-    Pattern.compile("\\G\\s*([ugoa]*)([+=-]+)([rwx]+)([,\\s]*)\\s*");
+    Pattern.compile("\\G\\s*([ugoa]*)([+=-]+)([rwx]*)([,\\s]*)\\s*");
   final short umaskMode;
   
   public UmaskParser(String modeStr) throws IllegalArgumentException {
@@ -39,7 +39,21 @@ class UmaskParser extends PermissionParser {
     umaskMode = (short)combineModes(0, false);
   }
 
+  /**
+   * To be used for file/directory creation only. Symbolic umask is applied
+   * relative to file mode creation mask; the permission op characters '+'
+   * results in clearing the corresponding bit in the mask, '-' results in bits
+   * for indicated permission to be set in the mask.
+   * 
+   * For octal umask, the specified bits are set in the file mode creation mask.
+   * 
+   * @return umask
+   */
   public short getUMask() {
+    if (symbolic) {
+      // Return the complement of octal equivalent of umask that was computed
+      return (short) (~umaskMode & 0777);      
+    }
     return umaskMode;
   }
 }
