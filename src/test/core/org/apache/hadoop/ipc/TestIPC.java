cd85fc46dc4d027a2102e874051d943fab0512ed
Thomas White <tomwhite@apache.org>
2010-04-28 17:34:55 +0000

    HADOOP-6724. IPC doesn't properly handle IOEs thrown by socket factory. Contributed by Todd Lipcon.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@939018 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/core/org/apache/hadoop/ipc/TestIPC.java
+++ src/test/core/org/apache/hadoop/ipc/TestIPC.java
@@ -29,8 +29,10 @@ import java.util.Random;
 import java.io.DataInput;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import javax.net.SocketFactory;
 
 import junit.framework.TestCase;
+import static org.mockito.Mockito.*;
 
 import org.apache.hadoop.conf.Configuration;
 
@@ -264,6 +266,25 @@ public class TestIPC extends TestCase {
       Throwable cause = e.getCause();
       assertTrue(cause instanceof IOException);
       assertEquals(LongErrorWritable.ERR_MSG, cause.getMessage());
+    }
+  }
+
+  /**
+   * Test that, if the socket factory throws an IOE, it properly propagates
+   * to the client.
+   */
+  public void testSocketFactoryException() throws Exception {
+    SocketFactory mockFactory = mock(SocketFactory.class);
+    doThrow(new IOException("Injected fault")).when(mockFactory).createSocket();
+    Client client = new Client(LongWritable.class, conf, mockFactory);
+    
+    InetSocketAddress address = new InetSocketAddress("127.0.0.1", 10);
+    try {
+      client.call(new LongWritable(RANDOM.nextLong()),
+              address, null, null);
+      fail("Expected an exception to have been thrown");
+    } catch (IOException e) {
+      assertTrue(e.getMessage().contains("Injected fault"));
     }
   }
 
