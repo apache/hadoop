7e575a986a13054ac886adc6699476420b41a943
Dhruba Borthakur <dhruba@apache.org>
2010-06-17 01:52:50 +0000

    HDFS-1210. DFSClient should log exception when block recovery fails. (Todd Lipcon via dhruba)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.20-append@955448 13f79535-47bb-0310-9956-ffa450edef68

--- src/hdfs/org/apache/hadoop/hdfs/DFSClient.java
+++ src/hdfs/org/apache/hadoop/hdfs/DFSClient.java
@@ -2635,6 +2635,8 @@ public class DFSClient implements FSConstants, java.io.Closeable {
           primary = createClientDatanodeProtocolProxy(primaryNode, conf);
           newBlock = primary.recoverBlock(block, isAppend, newnodes);
         } catch (IOException e) {
+          LOG.warn("Failed recovery attempt #" + recoveryErrorCount +
+                   " from primary datanode " + primaryNode, e);
           recoveryErrorCount++;
           if (recoveryErrorCount > maxRecoveryErrorCount) {
             if (nodes.length > 1) {
