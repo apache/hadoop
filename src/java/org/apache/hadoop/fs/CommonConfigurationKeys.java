ffbf57866fb2568b2226475cbb0dd1aedf6f5902
Amareshwari Sri Ramadasu <amareshwari@apache.org>
2010-06-18 12:36:57 +0000

    HADOOP-6748. Merge -r 955974:955975 from trunk.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.21@955981 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/fs/CommonConfigurationKeys.java
+++ src/java/org/apache/hadoop/fs/CommonConfigurationKeys.java
@@ -151,10 +151,5 @@ public class CommonConfigurationKeys {
   public static final String  HADOOP_SECURITY_AUTHENTICATION = "hadoop.security.authentication";
   public static final String HADOOP_SECURITY_AUTHORIZATION =
       "hadoop.security.authorization";
-  /**
-   * ACL denoting the administrator ACLs for a hadoop cluster.
-   */
-  public final static String HADOOP_CLUSTER_ADMINISTRATORS_PROPERTY =
-      "hadoop.cluster.administrators";
 }
 
