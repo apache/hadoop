73edb7b39518c3277e116b4b262ae7981c706128
Tsz-wo Sze <szetszwo@apache.org>
2010-06-21 21:26:45 +0000

    MAPREDUCE-1880. Fix BigDecimal.divide(..) in the pi example.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.20-append@956704 13f79535-47bb-0310-9956-ffa450edef68

--- src/examples/org/apache/hadoop/examples/PiEstimator.java
+++ src/examples/org/apache/hadoop/examples/PiEstimator.java
@@ -20,6 +20,7 @@ package org.apache.hadoop.examples;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Iterator;
 
 import org.apache.hadoop.conf.Configured;
@@ -310,10 +311,11 @@ public class PiEstimator extends Configured implements Tool {
       }
 
       //compute estimated value
+      final BigDecimal numTotal
+          = BigDecimal.valueOf(numMaps).multiply(BigDecimal.valueOf(numPoints));
       return BigDecimal.valueOf(4).setScale(20)
           .multiply(BigDecimal.valueOf(numInside.get()))
-          .divide(BigDecimal.valueOf(numMaps))
-          .divide(BigDecimal.valueOf(numPoints));
+          .divide(numTotal, RoundingMode.HALF_UP);
     } finally {
       fs.delete(TMP_DIR, true);
     }
