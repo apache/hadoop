af09891840737cacb1ac1f4917736225dc8691b4
Andrew Kyle Purtell <apurtell@apache.org>
2010-08-09 14:56:38 +0000

    HBASE-2897 RowResultGenerator should handle NoSuchColumnFamilyException; add hunk missed from last commit
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@983667 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/rest/RowResultGenerator.java
+++ src/main/java/org/apache/hadoop/hbase/rest/RowResultGenerator.java
@@ -27,6 +27,7 @@ import java.util.NoSuchElementException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.apache.hadoop.util.StringUtils;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.Get;
