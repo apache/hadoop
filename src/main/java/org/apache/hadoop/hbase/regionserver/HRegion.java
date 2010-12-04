be91a5326f6a289ae9d34437a63c8ba8434fe2b2
Michael Stack <stack@apache.org>
2010-12-04 18:20:10 +0000

    HBASE-3298 Regionserver can close during a split causing double assignment -- fix broken heapsize test -- add reference too
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1042220 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -3281,7 +3281,7 @@ public class HRegion implements HeapSize { // , Writable{
 
   public static final long FIXED_OVERHEAD = ClassSize.align(
       (4 * Bytes.SIZEOF_LONG) + Bytes.SIZEOF_BOOLEAN +
-      (22 * ClassSize.REFERENCE) + ClassSize.OBJECT + Bytes.SIZEOF_INT);
+      (23 * ClassSize.REFERENCE) + ClassSize.OBJECT + Bytes.SIZEOF_INT);
 
   public static final long DEEP_OVERHEAD = ClassSize.align(FIXED_OVERHEAD +
       (ClassSize.OBJECT * 2) + (2 * ClassSize.ATOMIC_BOOLEAN) +
