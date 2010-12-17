1cb79368d36cba4076e37bd5cead7b0b6536ec72
Michael Stack <stack@apache.org>
2010-12-17 05:48:05 +0000

    HBASE-3368 Split message can come in before region opened message; results in 'Region has been PENDING_CLOSE for too long' cycle
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1050279 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/hadoop/hbase/master/CatalogJanitor.java
+++ src/main/java/org/apache/hadoop/hbase/master/CatalogJanitor.java
@@ -160,6 +160,9 @@ class CatalogJanitor extends Chore {
     if (!hasReferencesA && !hasReferencesB) {
       LOG.debug("Deleting region " + parent.getRegionNameAsString() +
         " because daughter splits no longer hold references");
+      // This latter regionOffline should not be necessary but is done for now
+      // until we let go of regionserver to master heartbeats.  See HBASE-3368.
+      this.services.getAssignmentManager().regionOffline(parent);
       FileSystem fs = this.services.getMasterFileSystem().getFileSystem();
       Path rootdir = this.services.getMasterFileSystem().getRootDir();
       HRegion.deleteRegion(fs, rootdir, parent);
