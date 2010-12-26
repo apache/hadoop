38ab39aa61133fc6c3d3fa34757c46133ffc884f
Todd Lipcon <todd@apache.org>
2010-12-26 20:06:27 +0000

    HADOOP-7078. Improve javadocs for RawComparator interface. Contributed by Harsh J Chouraria.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1052933 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/io/RawComparator.java
+++ src/java/org/apache/hadoop/io/RawComparator.java
@@ -36,6 +36,18 @@ import org.apache.hadoop.io.serializer.DeserializerComparator;
 @InterfaceStability.Stable
 public interface RawComparator<T> extends Comparator<T> {
 
+  /**
+   * Compare two objects in binary.
+   * b1[s1:l1] is the first object, and b2[s2:l2] is the second object.
+   * 
+   * @param b1 The first byte array.
+   * @param s1 The position index in b1. The object under comparison's starting index.
+   * @param l1 The length of the object in b1.
+   * @param b2 The second byte array.
+   * @param s2 The position index in b2. The object under comparison's starting index.
+   * @param l2 The length of the object under comparison in b2.
+   * @return An integer result of the comparison.
+   */
   public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2);
 
 }
