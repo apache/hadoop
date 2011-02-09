a805223b4e83792b4edf0514431fc52eec77f023
Todd Lipcon <todd@apache.org>
2011-02-09 01:39:02 +0000

    HADOOP-7129. Complete patch by removing the transitional method name. Contributed by Todd Lipcon
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1068728 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/ipc/ProtocolSignature.java
+++ src/java/org/apache/hadoop/ipc/ProtocolSignature.java
@@ -238,16 +238,4 @@ public class ProtocolSignature implements Writable {
     return ProtocolSignature.getProtocolSignature(
         clientMethodsHash, serverVersion, inter);
   }
-
-  /**
-   * @deprecated Misspelled method name - to be removed after references
-   * to this typo have been fixed in HDFS and MapRed.
-   */
-  @Deprecated
-  public static ProtocolSignature getProtocolSigature(VersionedProtocol server,
-      String protocol,
-      long clientVersion, int clientMethodsHash) throws IOException {
-    return getProtocolSignature(server, protocol, clientVersion,
-        clientMethodsHash);
-  }
 }
