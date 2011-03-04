25727a7eb30ffa771ea35786234616dc56d0603b
Owen O'Malley <omalley@apache.org>
2011-03-04 04:45:05 +0000

    commit 8891c97ad9de98c54e52eece049c35696a936bd9 Author: Devaraj Das <ddas@yahoo-inc.com> Date:   Fri Sep 17 23:04:19 2010 -0700
        Fixes the JobLocalizer to use the jobtoken correctly for creating the taskumbilical
    
    
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.20-security-patches@1077692 13f79535-47bb-0310-9956-ffa450edef68

--- src/mapred/org/apache/hadoop/mapred/JobLocalizer.java
+++ src/mapred/org/apache/hadoop/mapred/JobLocalizer.java
@@ -42,10 +42,12 @@ import org.apache.hadoop.fs.LocalDirAllocator;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.ipc.RPC;
 import org.apache.hadoop.mapred.QueueManager.QueueACL;
 import org.apache.hadoop.mapreduce.JobID;
 import org.apache.hadoop.mapreduce.security.TokenCache;
+import org.apache.hadoop.mapreduce.security.token.JobTokenIdentifier;
 import org.apache.hadoop.security.Credentials;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.security.token.Token;
@@ -494,10 +496,23 @@ public class JobLocalizer {
     for (Token<? extends TokenIdentifier> token : creds.getAllTokens()) {
       ugi.addToken(token);
     }
-    final TaskUmbilicalProtocol taskTracker =
-      (TaskUmbilicalProtocol) RPC.getProxy(TaskUmbilicalProtocol.class,
-                                           TaskUmbilicalProtocol.versionID,
-                                           ttAddr, conf);
+    
+    UserGroupInformation ugiJob = UserGroupInformation.createRemoteUser(jobid);
+    Token<JobTokenIdentifier> jt = TokenCache.getJobToken(creds);
+    jt.setService(new Text(ttAddr.getAddress().getHostAddress() + ":"
+        + ttAddr.getPort()));
+    ugiJob.addToken(jt);
+
+    final TaskUmbilicalProtocol taskTracker = 
+      ugiJob.doAs(new PrivilegedExceptionAction<TaskUmbilicalProtocol>() {
+        public TaskUmbilicalProtocol run() throws IOException {
+          TaskUmbilicalProtocol taskTracker =
+            (TaskUmbilicalProtocol) RPC.getProxy(TaskUmbilicalProtocol.class,
+                TaskUmbilicalProtocol.versionID,
+                ttAddr, conf);
+          return taskTracker;
+        }
+      });
     System.exit(
       ugi.doAs(new PrivilegedExceptionAction<Integer>() {
         public Integer run() {
