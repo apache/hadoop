5d4d16994ccdb4b7f7567a97df4d315dbb4990ed
Owen O'Malley <omalley@apache.org>
2011-03-04 04:53:17 +0000

    commit a9ed8698858303de8176c799e46a8fd333307bc8 Author: Richard King <dking@yahoo-inc.com> Date:   Mon Dec 13 18:19:05 2010 +0000
        .  Removes a syntactically illegal second semicolon from
             src/test/system/java/org/apache/hadoop/hdfs/TestBalancer.java
        
        +++ b/YAHOO-CHANGES.txt
        +    .  Removes a syntactically illegal second semicolon from
        +     src/test/system/java/org/apache/hadoop/hdfs/TestBalancer.java
        +    (dking)
        +
        +Release 0.20.1xx - unreleased
        +
    
    
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.20-security-patches@1077768 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/system/java/org/apache/hadoop/hdfs/TestBalancer.java
+++ src/test/system/java/org/apache/hadoop/hdfs/TestBalancer.java
@@ -41,7 +41,7 @@ import java.util.Set;
 import org.apache.commons.lang.ArrayUtils;
 
 import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;;
+import org.apache.commons.logging.LogFactory;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
