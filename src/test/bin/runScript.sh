b1d148221b3080e68f2cf7422ff85d953df420cd
Owen O'Malley <omalley@apache.org>
2011-03-04 04:08:47 +0000

    commit eadbb408a0b3b1643f18d89c04b0eddaf31d3f6c Author: Jeffrey Bowles <jab@yahoo-inc.com> Date:   Fri Apr 9 10:02:29 2010 -0700
        adding SVN / GIT support.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.20-security-patches@1077375 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/bin/runScript.sh
+++ src/test/bin/runScript.sh
@@ -8,21 +8,30 @@ then
 	echo "No arguments given. Exiting." 1>&2
 	exit 1
 fi
-if [ "$GIT_REPO_NOT_NECESSARY" != true ]
-then
-	if [ -z "$GIT_REPO" ]
+	if [ -z "$GIT_REPO" -a -z "$SVN_REPO" ]
 	then
-		echo "Serious error: GIT_REPO was not specified." 1>&2
+		echo "Serious error: neither SVN_REPO nor GIT_REPO were specified." 1>&2
 		exit 1
 	fi
-	if [ -d hudson ]
+	if [ -n "$GIT_REPO" -a -n "$SVN_REPO" ]
 	then
-	    (cd hudson && git pull)
-	else
-	    # git clone ssh://gwbl2001.blue.ygrid.yahoo.com/grid/0/gs/gridre/hudson.git
-	    git clone $GIT_REPO
+		echo "Serious error: both SVN_REPO and GIT_REPO were specified." 1>&2
+		exit 1
 	fi
-fi
+	if [ -n "$GIT_REPO" ]
+	then
+		if [ -d hudson ]
+		then
+	    		(cd hudson && git pull)
+		else
+	    	# git clone ssh://gwbl2001.blue.ygrid.yahoo.com/grid/0/gs/gridre/hudson.git
+	    	git clone $GIT_REPO
+		fi
+	fi
+	if [ -n "$SVN_REPO" ]
+	then
+		svn co $SVN_REPO
+	fi
 
 echo  ============  hostname = `hostname`
 
