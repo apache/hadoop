f616c0c70964fb4e88c3dc60c59d99b6d1d4759a
Gary Helmling <garyh@apache.org>
2011-04-11 19:22:47 +0000

    HBASE-3762  HTableFactory.releaseHTableInterface() should throw IOException not wrap in RTE (test fix)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1091174 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/hadoop/hbase/client/TestHTablePool.java
+++ src/test/java/org/apache/hadoop/hbase/client/TestHTablePool.java
@@ -52,7 +52,7 @@ public class TestHTablePool  {
   }
 
   @Test
-  public void testTableWithStringName() {
+  public void testTableWithStringName() throws Exception {
     HTablePool pool =
       new HTablePool(TEST_UTIL.getConfiguration(), Integer.MAX_VALUE);
     String tableName = Bytes.toString(TABLENAME);
@@ -86,7 +86,7 @@ public class TestHTablePool  {
   }
 
   @Test
-  public void testTableWithMaxSize() {
+  public void testTableWithMaxSize() throws Exception {
     HTablePool pool = new HTablePool(TEST_UTIL.getConfiguration(), 2);
 
     // Request tables from an empty pool
