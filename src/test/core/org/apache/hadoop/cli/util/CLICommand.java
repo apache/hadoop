98a2b7051f01d87f19948e1a251c3ee0e4b2523c
Konstantin Boudnik <cos@apache.org>
2011-04-16 01:43:30 +0000

    HADOOP-7014. Adding missed files
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1092853 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/core/org/apache/hadoop/cli/util/CLICommand.java
+++ src/test/core/org/apache/hadoop/cli/util/CLICommand.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,23 +17,12 @@
  */
 package org.apache.hadoop.cli.util;
 
-import org.apache.hadoop.fs.FsShell;
-import org.apache.hadoop.util.ToolRunner;
-
-public class CLICommands {
-
-  public static class FSCmdExecutor extends CommandExecutor {
-    protected String namenode = null;
-    protected FsShell shell = null;
-    
-    public FSCmdExecutor(String namenode, FsShell shell) {
-      this.namenode = namenode;
-      this.shell = shell;
-    }
-
-    protected void execute(final String cmd) throws Exception{
-      String[] args = getCommandAsArgs(cmd, "NAMENODE", this.namenode);
-      ToolRunner.run(shell, args);
-    }
-  }
+/**
+ * This interface is to generalize types of test command for upstream projects
+ */
+public interface CLICommand {
+  public CommandExecutor getExecutor(String tag) throws IllegalArgumentException;
+  public CLICommandTypes getType();
+  public String getCmd();
+  public String toString();
 }
--- src/test/core/org/apache/hadoop/cli/util/CLICommandTypes.java
+++ src/test/core/org/apache/hadoop/cli/util/CLICommandTypes.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,21 +17,8 @@
  */
 package org.apache.hadoop.cli.util;
 
-import org.apache.hadoop.fs.FsShell;
-
-public abstract class CmdFactory {
-  public static CommandExecutor getCommandExecutor(CLITestData.TestCmd cmd,
-                                                   String tag)
-    throws IllegalArgumentException {
-    CommandExecutor executor = null;
-    switch (cmd.getType()) {
-      case FS:
-        executor = new CLICommands.FSCmdExecutor(tag, new FsShell());
-        break;
-      default:
-        throw new IllegalArgumentException("Unknown type of Test command:" +
-          cmd.getType());
-    }
-    return executor;
-  }
+/**
+ * This interface is to provide command type for test commands enums
+ */
+public interface CLICommandTypes {
 }
