cb60da7cfe4e207ef50ce7b4571a9eb8edbd0501
Owen O'Malley <omalley@apache.org>
2011-04-22 22:39:06 +0000

    HADOOP-7232. Fix Javadoc warnings. (omalley)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.20-security-203@1096071 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/hadoop/metrics/util/MetricsTimeVaryingInt.java
+++ src/core/org/apache/hadoop/metrics/util/MetricsTimeVaryingInt.java
@@ -34,7 +34,7 @@ import org.apache.commons.logging.LogFactory;
  * Note if one wants a time associated with the metric then use
  * @see org.apache.hadoop.metrics.util.MetricsTimeVaryingRate
  *
- * @deprecated in favor of {@link org.apache.hadoop.metrics2.lib.MutableCounterInt}.
+ * @deprecated in favor of {@link org.apache.hadoop.metrics2.lib.MetricMutableCounterInt}.
  */
 @Deprecated
 @InterfaceAudience.LimitedPrivate({"HDFS", "MapReduce"})
