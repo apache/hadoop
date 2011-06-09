32af4f1cc9c95dfbf1f654744f1b51caf45a320d
Suresh Srinivas <suresh@apache.org>
2011-06-09 21:13:39 +0000

    Merging change r1127697 for HADOOP-7322 from trunk
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/yahoo-merge@1134079 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/core/org/apache/hadoop/fs/TestFileUtil.java
+++ src/test/core/org/apache/hadoop/fs/TestFileUtil.java
@@ -117,6 +117,32 @@ public class TestFileUtil {
     }
   }
 
+  @Test
+  public void testListFiles() throws IOException {
+    setupDirs();
+    //Test existing files case 
+    File[] files = FileUtil.listFiles(partitioned);
+    Assert.assertEquals(2, files.length);
+
+    //Test existing directory with no files case 
+    File newDir = new File(tmp.getPath(),"test");
+    newDir.mkdir();
+    Assert.assertTrue("Failed to create test dir", newDir.exists());
+    files = FileUtil.listFiles(newDir);
+    Assert.assertEquals(0, files.length);
+    newDir.delete();
+    Assert.assertFalse("Failed to delete test dir", newDir.exists());
+    
+    //Test non-existing directory case, this throws 
+    //IOException
+    try {
+      files = FileUtil.listFiles(newDir);
+      Assert.fail("IOException expected on listFiles() for non-existent dir "
+      		+ newDir.toString());
+    } catch(IOException ioe) {
+    	//Expected an IOException
+    }
+  }
 
   @Test
   public void testListAPI() throws IOException {
