a88c867f7192fc9936eb1a4da324f4edecb005a7
Owen O'Malley <omalley@apache.org>
2011-06-16 22:47:16 +0000

    MAPREDUCE-2514. Fix typo in TaskTracker ReinitTrackerAction log message. (Jonathan Eagles via cdouglas)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.20-security-204@1136720 13f79535-47bb-0310-9956-ffa450edef68

--- src/mapred/org/apache/hadoop/mapred/TaskTracker.java
+++ src/mapred/org/apache/hadoop/mapred/TaskTracker.java
@@ -1880,7 +1880,7 @@ public class TaskTracker implements MRConstants, TaskUmbilicalProtocol,
       for (TaskTrackerAction action : actions) {
         if (action.getActionId() == 
             TaskTrackerAction.ActionType.REINIT_TRACKER) {
-          LOG.info("Recieved RenitTrackerAction from JobTracker");
+          LOG.info("Recieved ReinitTrackerAction from JobTracker");
           return true;
         }
       }
