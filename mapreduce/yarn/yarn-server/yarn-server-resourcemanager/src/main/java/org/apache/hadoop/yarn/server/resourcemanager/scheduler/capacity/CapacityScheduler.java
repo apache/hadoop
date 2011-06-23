2d37d37e932a06f8857e95bde25cab6c162f6fa9
Mahadev Konar <mahadev@apache.org>
2011-06-23 06:01:26 +0000

    Fix class cast exception in release reserved containers in capacity scheduler (mahadev)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/MR-279@1138742 13f79535-47bb-0310-9956-ffa450edef68

--- mapreduce/yarn/yarn-server/yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/CapacityScheduler.java
+++ mapreduce/yarn/yarn-server/yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/CapacityScheduler.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -557,7 +558,7 @@ implements ResourceScheduler, CapacitySchedulerContext {
     Map<Priority, Set<NodeInfo>> reservations = application.getAllReservations();
     for (Map.Entry<Priority, Set<NodeInfo>> e : reservations.entrySet()) {
       Priority priority = e.getKey();
-      Set<NodeInfo> reservedNodes = new TreeSet<NodeInfo>(e.getValue());
+      Set<NodeInfo> reservedNodes = new HashSet<NodeInfo>(e.getValue());
       for (NodeInfo node : reservedNodes) {
         Resource allocatedResource = 
           application.getResourceRequest(priority, NodeManagerImpl.ANY).getCapability();
