81001b87c12899d8f354e5b42927213b0c471be8
Eli Collins <eli@apache.org>
2011-07-18 17:23:25 +0000

    HDFS-2157. Improve header comment in o.a.h.hdfs.server.namenode.NameNode. Contributed by Aaron T. Myers
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1147980 13f79535-47bb-0310-9956-ffa450edef68

--- hdfs/src/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
+++ hdfs/src/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
@@ -110,35 +110,38 @@ import org.apache.hadoop.util.StringUtils;
  * NameNode serves as both directory namespace manager and
  * "inode table" for the Hadoop DFS.  There is a single NameNode
  * running in any DFS deployment.  (Well, except when there
- * is a second backup/failover NameNode.)
+ * is a second backup/failover NameNode, or when using federated NameNodes.)
  *
  * The NameNode controls two critical tables:
  *   1)  filename->blocksequence (namespace)
  *   2)  block->machinelist ("inodes")
  *
  * The first table is stored on disk and is very precious.
- * The second table is rebuilt every time the NameNode comes
- * up.
+ * The second table is rebuilt every time the NameNode comes up.
  *
  * 'NameNode' refers to both this class as well as the 'NameNode server'.
  * The 'FSNamesystem' class actually performs most of the filesystem
  * management.  The majority of the 'NameNode' class itself is concerned
- * with exposing the IPC interface and the http server to the outside world,
+ * with exposing the IPC interface and the HTTP server to the outside world,
  * plus some configuration management.
  *
- * NameNode implements the ClientProtocol interface, which allows
- * clients to ask for DFS services.  ClientProtocol is not
- * designed for direct use by authors of DFS client code.  End-users
- * should instead use the org.apache.nutch.hadoop.fs.FileSystem class.
+ * NameNode implements the
+ * {@link org.apache.hadoop.hdfs.protocol.ClientProtocol} interface, which
+ * allows clients to ask for DFS services.
+ * {@link org.apache.hadoop.hdfs.protocol.ClientProtocol} is not designed for
+ * direct use by authors of DFS client code.  End-users should instead use the
+ * {@link org.apache.hadoop.fs.FileSystem} class.
  *
- * NameNode also implements the DatanodeProtocol interface, used by
- * DataNode programs that actually store DFS data blocks.  These
+ * NameNode also implements the
+ * {@link org.apache.hadoop.hdfs.server.protocol.DatanodeProtocol} interface,
+ * used by DataNodes that actually store DFS data blocks.  These
  * methods are invoked repeatedly and automatically by all the
  * DataNodes in a DFS deployment.
  *
- * NameNode also implements the NamenodeProtocol interface, used by
- * secondary namenodes or rebalancing processes to get partial namenode's
- * state, for example partial blocksMap etc.
+ * NameNode also implements the
+ * {@link org.apache.hadoop.hdfs.server.protocol.NamenodeProtocol} interface,
+ * used by secondary namenodes or rebalancing processes to get partial
+ * NameNode state, for example partial blocksMap etc.
  **********************************************************/
 @InterfaceAudience.Private
 public class NameNode implements NamenodeProtocols, FSConstants {
