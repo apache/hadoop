3633f06b508a6d4c7fb38bc3743c9886b42099c7
Zhihong Yu <tedyu@apache.org>
2011-08-04 01:20:49 +0000

    HBASE-3857  Fix TestHFileBlock.testBlockHeapSize test failure (Mikhail)
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1153718 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/hadoop/hbase/io/hfile/TestHFileBlock.java
+++ src/test/java/org/apache/hadoop/hbase/io/hfile/TestHFileBlock.java
@@ -482,12 +482,16 @@ public class TestHFileBlock {
 
   @Test
   public void testBlockHeapSize() {
+    // We have seen multiple possible values for this estimate of the heap size
+    // of a ByteBuffer, presumably depending on the JDK version.
+    assertTrue(HFileBlock.BYTE_BUFFER_HEAP_SIZE == 64 ||
+               HFileBlock.BYTE_BUFFER_HEAP_SIZE == 80);
+
     for (int size : new int[] { 100, 256, 12345 }) {
       byte[] byteArr = new byte[HFileBlock.HEADER_SIZE + size];
       ByteBuffer buf = ByteBuffer.wrap(byteArr, 0, size);
       HFileBlock block = new HFileBlock(BlockType.DATA, size, size, -1, buf,
           true, -1);
-      assertEquals(80, HFileBlock.BYTE_BUFFER_HEAP_SIZE);
       long expected = ClassSize.align(ClassSize.estimateBase(HFileBlock.class,
           true)
           + ClassSize.estimateBase(buf.getClass(), true)
