4d90df82a9e4fcbcebd298665092f7016a2bbb98
Arun Murthy <acmurthy@apache.org>
2011-09-13 18:33:22 +0000

    Merge -r 1170285:1170286 from branch-0.20-security to trunk to fix MAPREDUCE-2549.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1170288 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-mapreduce-project/src/contrib/streaming/src/java/org/apache/hadoop/streaming/Environment.java
+++ hadoop-mapreduce-project/src/contrib/streaming/src/java/org/apache/hadoop/streaming/Environment.java
@@ -23,6 +23,7 @@ import java.net.InetAddress;
 import java.util.*;
 
 import org.apache.hadoop.classification.InterfaceAudience;
+import org.apache.hadoop.io.IOUtils;
 
 /**
  * This is a class used to get the current environment
@@ -62,17 +63,24 @@ public class Environment extends Properties {
 
     Process pid = Runtime.getRuntime().exec(command);
     BufferedReader in = new BufferedReader(new InputStreamReader(pid.getInputStream()));
-    while (true) {
-      String line = in.readLine();
-      if (line == null) break;
-      int p = line.indexOf("=");
-      if (p != -1) {
-        String name = line.substring(0, p);
-        String value = line.substring(p + 1);
-        setProperty(name, value);
+    try {
+      while (true) {
+        String line = in.readLine();
+        if (line == null)
+          break;
+        int p = line.indexOf("=");
+        if (p != -1) {
+          String name = line.substring(0, p);
+          String value = line.substring(p + 1);
+          setProperty(name, value);
+        }
       }
+      in.close();
+      in = null;
+    } finally {
+      IOUtils.closeStream(in);
     }
-    in.close();
+   
     try {
       pid.waitFor();
     } catch (InterruptedException e) {
