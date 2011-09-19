eed08c0356f7f5548f13d0aa232cad3d7e422d9f
Vinod Kumar Vavilapalli <vinodkv@apache.org>
2011-09-19 17:05:06 +0000

    MAPREDUCE-2726. svn merge -c r1172704 --ignore-ancestry ../../trunk/
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.23@1172708 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/webapp/AMWebApp.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/webapp/AMWebApp.java
@@ -32,6 +32,7 @@ public class AMWebApp extends WebApp implements AMParams {
     route("/", AppController.class);
     route("/app", AppController.class);
     route(pajoin("/job", JOB_ID), AppController.class, "job");
+    route(pajoin("/conf", JOB_ID), AppController.class, "conf");
     route(pajoin("/jobcounters", JOB_ID), AppController.class, "jobCounters");
     route(pajoin("/tasks", JOB_ID, TASK_TYPE), AppController.class, "tasks");
     route(pajoin("/attempts", JOB_ID, TASK_TYPE, ATTEMPT_STATE),
