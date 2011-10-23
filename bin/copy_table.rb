329a3681e8d54551cd14e06cc74aae2891ca1973
Michael Stack <stack@apache.org>
2011-10-23 21:18:07 +0000

    HBASE-4647 RAT finds about 40 files missing licenses
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.92@1187974 13f79535-47bb-0310-9956-ffa450edef68

--- bin/copy_table.rb
+++ bin/copy_table.rb
@@ -1,6 +1,4 @@
 #
-# Copyright 2009 The Apache Software Foundation
-#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
