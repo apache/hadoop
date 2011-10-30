2b3fc608bbeb23c6b6688c412b85c57615c74060
Zhihong Yu <tedyu@apache.org>
2011-10-30 21:20:23 +0000

    HBASE-4690  Intermittent TestRegionServerCoprocessorExceptionWithAbort failure
    git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/0.92@1195251 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/hadoop/hbase/HBaseTestingUtility.java
+++ src/test/java/org/apache/hadoop/hbase/HBaseTestingUtility.java
@@ -23,6 +23,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.security.MessageDigest;
@@ -59,6 +60,7 @@ import org.apache.hadoop.hbase.regionserver.ReadWriteConsistencyControl;
 import org.apache.hadoop.hbase.regionserver.Store;
 import org.apache.hadoop.hbase.security.User;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.EnvironmentEdgeManager;
 import org.apache.hadoop.hbase.util.FSUtils;
 import org.apache.hadoop.hbase.util.Threads;
 import org.apache.hadoop.hbase.util.Writables;
@@ -1111,6 +1113,20 @@ public class HBaseTestingUtility {
     byte [] firstrow = metaRows.get(0);
     LOG.debug("FirstRow=" + Bytes.toString(firstrow));
     int index = hbaseCluster.getServerWith(firstrow);
+    long start = EnvironmentEdgeManager.currentTimeMillis();
+    int timeout = 3000;   // 3sec timeout
+    while (index == -1 &&
+        EnvironmentEdgeManager.currentTimeMillis() - start < timeout) {
+      try {
+        // wait for the region to come online
+        Thread.sleep(50);
+      } catch (InterruptedException ie) {
+        IOException t = new InterruptedIOException();
+        t.initCause(ie);
+        throw t;
+      }
+      index = hbaseCluster.getServerWith(firstrow);
+    }
     return hbaseCluster.getRegionServerThreads().get(index).getRegionServer();
   }
 
