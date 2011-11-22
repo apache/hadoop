9d1586e10a6c0d32dfe4f86fa3835d225fd2ca53
Mahadev Konar <mahadev@apache.org>
2011-11-22 22:48:38 +0000

    MAPREDUCE-3411. Performance Upgrade for jQuery (Jonathan Eagles via mahadev) - Merging r1205210 from trunk
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.23@1205213 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-mapreduce-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/webapp/view/JQueryUI.java
+++ hadoop-mapreduce-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/webapp/view/JQueryUI.java
@@ -79,11 +79,11 @@ public class JQueryUI extends HtmlBlock {
   @Override
   protected void render(Block html) {
     html.
-      link(join("https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/themes/",
+      link(join("https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/",
                 getTheme(), "/jquery-ui.css")).
       link(root_url("static/dt-1.7.5/css/jui-dt.css")).
-      script("https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js").
-      script("https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js").
+      script("https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js").
+      script("https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js").
       script(root_url("static/dt-1.7.5/js/jquery.dataTables.min.js")).
       script(root_url("static/yarn.dt.plugins.js")).
       script(root_url("static/themeswitcher.js")).
