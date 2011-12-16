feb3a23c71a5e000f1100e1ca6459c1fade8befc
Vinod Kumar Vavilapalli <vinodkv@apache.org>
2011-12-16 23:41:36 +0000

    MAPREDUCE-3387. Fixed AM's tracking URL to always go through the proxy, even before the job started, so that it works properly with oozie throughout the job execution. Contributed by Robert Joseph Evans.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1215345 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-mapreduce-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmapp/attempt/RMAppAttemptImpl.java
+++ hadoop-mapreduce-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmapp/attempt/RMAppAttemptImpl.java
@@ -273,6 +273,8 @@ public class RMAppAttemptImpl implements RMAppAttempt {
     this.readLock = lock.readLock();
     this.writeLock = lock.writeLock();
 
+    this.proxiedTrackingUrl = generateProxyUriWithoutScheme();
+    
     this.stateMachine = stateMachineFactory.make(this);
   }
 
@@ -358,11 +360,16 @@ public class RMAppAttemptImpl implements RMAppAttempt {
     }    
   }
   
+  private String generateProxyUriWithoutScheme() {
+    return generateProxyUriWithoutScheme(null);
+  }
+  
   private String generateProxyUriWithoutScheme(
       final String trackingUriWithoutScheme) {
     this.readLock.lock();
     try {
-      URI trackingUri = ProxyUriUtils.getUriFromAMUrl(trackingUriWithoutScheme);
+      URI trackingUri = trackingUriWithoutScheme == null ? null : 
+        ProxyUriUtils.getUriFromAMUrl(trackingUriWithoutScheme);
       URI proxyUri = ProxyUriUtils.getUriFromAMUrl(proxy);
       URI result = ProxyUriUtils.getProxyUri(trackingUri, proxyUri, 
           applicationAttemptId.getApplicationId());
