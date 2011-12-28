5836feb4a0103527dfbbe4bece129b65899acefb
Konstantin Shvachko <shv@apache.org>
2011-12-28 20:58:32 +0000

    HDFS-1910. NameNdoe should not save fsimage twice. Contributed by Konstantin Shvachko.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.22@1225333 13f79535-47bb-0310-9956-ffa450edef68

--- hdfs/src/java/org/apache/hadoop/hdfs/server/namenode/FSImage.java
+++ hdfs/src/java/org/apache/hadoop/hdfs/server/namenode/FSImage.java
@@ -1421,7 +1421,7 @@ public class FSImage extends Storage {
   void rollFSImage(CheckpointSignature sig, 
       boolean renewCheckpointTime) throws IOException {
     sig.validateStorageInfo(this);
-    rollFSImage(true);
+    rollFSImage(renewCheckpointTime);
   }
 
   private void rollFSImage(boolean renewCheckpointTime)
