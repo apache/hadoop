57dda44906bb8d309ebc1d30365c96259dbfa230
Eli Collins <eli@apache.org>
2012-02-24 21:18:23 +0000

    HDFS-3008. Negative caching of local addrs doesn't work. Contributed by Eli Collins
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-1@1293422 13f79535-47bb-0310-9956-ffa450edef68

--- src/hdfs/org/apache/hadoop/hdfs/DFSClient.java
+++ src/hdfs/org/apache/hadoop/hdfs/DFSClient.java
@@ -371,11 +371,12 @@ public class DFSClient implements FSConstants, java.io.Closeable {
   private static boolean isLocalAddress(InetSocketAddress targetAddr) {
     InetAddress addr = targetAddr.getAddress();
     Boolean cached = localAddrMap.get(addr.getHostAddress());
-    if (cached != null && cached) {
+    if (cached != null) {
       if (LOG.isTraceEnabled()) {
-        LOG.trace("Address " + targetAddr + " is local");
+        LOG.trace("Address " + targetAddr +
+                  (cached ? " is local" : " is not local"));
       }
-      return true;
+      return cached;
     }
 
     // Check if the address is any local or loop back
@@ -390,7 +391,8 @@ public class DFSClient implements FSConstants, java.io.Closeable {
       }
     }
     if (LOG.isTraceEnabled()) {
-      LOG.trace("Address " + targetAddr + " is local");
+      LOG.trace("Address " + targetAddr +
+                (local ? " is local" : " is not local"));
     }
     localAddrMap.put(addr.getHostAddress(), local);
     return local;
