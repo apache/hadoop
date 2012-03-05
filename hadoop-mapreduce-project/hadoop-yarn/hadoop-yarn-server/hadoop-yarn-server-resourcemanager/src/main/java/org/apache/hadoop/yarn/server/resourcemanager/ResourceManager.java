68e54b192d09e82f1f037cf1241520ad54ca1531
Robert Joseph Evans <bobby@apache.org>
2012-03-05 22:05:00 +0000

    svn merge -c 1297248 from trunk to branch-0.23 FIXES MAPREDUCE-3964. ResourceManager does not have JVM metrics (Jason Lowe via bobby)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.23@1297250 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-mapreduce-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/ResourceManager.java
+++ hadoop-mapreduce-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/ResourceManager.java
@@ -27,7 +27,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.classification.InterfaceAudience.Private;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.metrics2.MetricsSystem;
 import org.apache.hadoop.metrics2.lib.DefaultMetricsSystem;
+import org.apache.hadoop.metrics2.source.JvmMetrics;
 import org.apache.hadoop.security.SecurityUtil;
 import org.apache.hadoop.util.ReflectionUtils;
 import org.apache.hadoop.util.StringUtils;
@@ -449,7 +451,8 @@ public class ResourceManager extends CompositeService implements Recoverable {
     }
 
     startWepApp();
-    DefaultMetricsSystem.initialize("ResourceManager");
+    MetricsSystem ms = DefaultMetricsSystem.initialize("ResourceManager");
+    JvmMetrics.create("ResourceManager", null, ms);
     try {
       rmDTSecretManager.startThreads();
     } catch(IOException ie) {
