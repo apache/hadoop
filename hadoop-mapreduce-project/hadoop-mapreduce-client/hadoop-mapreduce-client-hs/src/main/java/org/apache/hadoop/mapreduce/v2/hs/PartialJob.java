cb3d6c3d980de7b003f0e9acd6ff1251e30b08db
Thomas White <tomwhite@apache.org>
2012-04-12 16:31:15 +0000

    Merge -r 1325351:1325352 from trunk to branch-2. Fixes: MAPREDUCE-4140
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1325359 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-hs/src/main/java/org/apache/hadoop/mapreduce/v2/hs/PartialJob.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-hs/src/main/java/org/apache/hadoop/mapreduce/v2/hs/PartialJob.java
@@ -21,6 +21,8 @@ package org.apache.hadoop.mapreduce.v2.hs;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Counters;
 import org.apache.hadoop.mapreduce.JobACL;
@@ -37,9 +39,9 @@ import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.security.authorize.AccessControlList;
 import org.apache.hadoop.yarn.factory.providers.RecordFactoryProvider;
 
-import clover.org.apache.log4j.Logger;
 
 public class PartialJob implements org.apache.hadoop.mapreduce.v2.app.job.Job {
+  private static final Log LOG = LogFactory.getLog(PartialJob.class);
 
   private JobIndexInfo jobIndexInfo = null;
   private JobId jobId = null;
@@ -78,8 +80,7 @@ public class PartialJob implements org.apache.hadoop.mapreduce.v2.app.job.Job {
     } catch (Exception e) {
       // Meant for use by the display UI. Exception would prevent it from being
       // rendered.e Defaulting to KILLED
-      Logger.getLogger(this.getClass().getName()).warn(
-          "Exception while parsing job state. Defaulting to KILLED", e);
+      LOG.warn("Exception while parsing job state. Defaulting to KILLED", e);
       js = JobState.KILLED;
     }
     return js;
