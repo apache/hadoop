c21bd72a2f1ff9de49dad102e43788f2667e9c49
Aaron Myers <atm@apache.org>
2012-04-17 17:06:38 +0000

    HADOOP-8280. Move VersionUtil/TestVersionUtil and GenericTestUtils from HDFS into Common. Contributed by Ahmed Radwan.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1327182 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/util/TestVersionUtil.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/util/TestVersionUtil.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.hdfs.util;
+package org.apache.hadoop.util;
 
 import static org.junit.Assert.*;
 
