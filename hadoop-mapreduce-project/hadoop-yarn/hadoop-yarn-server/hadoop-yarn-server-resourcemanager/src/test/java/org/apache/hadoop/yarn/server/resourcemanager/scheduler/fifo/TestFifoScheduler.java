02a703fac2eedbdb1be3439288de001768c00e9c
Thomas Graves <tgraves@apache.org>
2012-05-24 13:17:08 +0000

    merge -r 1342243:1342244  from trunk. FIXES: MAPREDUCE-4224
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1342245 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-mapreduce-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fifo/TestFifoScheduler.java
+++ hadoop-mapreduce-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fifo/TestFifoScheduler.java
@@ -64,6 +64,7 @@ public class TestFifoScheduler {
 
   @After
   public void tearDown() throws Exception {
+    resourceManager.stop();
   }
   
   private org.apache.hadoop.yarn.server.resourcemanager.NodeManager
