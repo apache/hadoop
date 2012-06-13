cd72aad1253536e681471f3b304001a1e5d5d04d
Eli Collins <eli@apache.org>
2012-06-13 05:37:27 +0000

    HADOOP-8433. Don't set HADOOP_LOG_DIR in hadoop-env.sh. Contributed by Brahma Reddy Battula
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1349641 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-common-project/hadoop-common/src/main/conf/hadoop-env.sh
+++ hadoop-common-project/hadoop-common/src/main/conf/hadoop-env.sh
@@ -61,7 +61,7 @@ export HADOOP_CLIENT_OPTS="-Xmx128m $HADOOP_CLIENT_OPTS"
 export HADOOP_SECURE_DN_USER=${HADOOP_SECURE_DN_USER}
 
 # Where log files are stored.  $HADOOP_HOME/logs by default.
-export HADOOP_LOG_DIR=${HADOOP_LOG_DIR}/$USER
+#export HADOOP_LOG_DIR=${HADOOP_LOG_DIR}/$USER
 
 # Where log files are stored in the secure data environment.
 export HADOOP_SECURE_DN_LOG_DIR=${HADOOP_LOG_DIR}/${HADOOP_HDFS_USER}
