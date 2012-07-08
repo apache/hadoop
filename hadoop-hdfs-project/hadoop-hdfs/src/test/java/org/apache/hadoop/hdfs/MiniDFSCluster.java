5952c7bb62854384fe2774c248625fe1bd7263c5
Eli Collins <eli@apache.org>
2012-07-08 19:53:14 +0000

    HDFS-3614. Revert unused MiniDFSCluster constructor from HDFS-3049. Contributed by Arun Murthy
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1358825 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/MiniDFSCluster.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/MiniDFSCluster.java
@@ -392,7 +392,7 @@ public class MiniDFSCluster {
   public MiniDFSCluster(Configuration conf,
                         int numDataNodes,
                         StartupOption nameNodeOperation) throws IOException {
-    this(0, conf, numDataNodes, false, false, false, false,  nameNodeOperation, 
+    this(0, conf, numDataNodes, false, false, false,  nameNodeOperation, 
           null, null, null);
   }
   
@@ -414,7 +414,7 @@ public class MiniDFSCluster {
                         int numDataNodes,
                         boolean format,
                         String[] racks) throws IOException {
-    this(0, conf, numDataNodes, format, true, true, true, null,
+    this(0, conf, numDataNodes, format, true, true, null,
         racks, null, null);
   }
   
@@ -437,7 +437,7 @@ public class MiniDFSCluster {
                         int numDataNodes,
                         boolean format,
                         String[] racks, String[] hosts) throws IOException {
-    this(0, conf, numDataNodes, format, true, true, true, null,
+    this(0, conf, numDataNodes, format, true, true, null,
         racks, hosts, null);
   }
   
@@ -472,7 +472,7 @@ public class MiniDFSCluster {
                         StartupOption operation,
                         String[] racks) throws IOException {
     this(nameNodePort, conf, numDataNodes, format, manageDfsDirs,
-        manageDfsDirs, manageDfsDirs, operation, racks, null, null);
+        manageDfsDirs, operation, racks, null, null);
   }
 
   /**
@@ -505,7 +505,7 @@ public class MiniDFSCluster {
                         StartupOption operation,
                         String[] racks,
                         long[] simulatedCapacities) throws IOException {
-    this(nameNodePort, conf, numDataNodes, format, manageDfsDirs, manageDfsDirs,
+    this(nameNodePort, conf, numDataNodes, format, manageDfsDirs, 
         manageDfsDirs, operation, racks, null, simulatedCapacities);
   }
   
@@ -540,14 +540,13 @@ public class MiniDFSCluster {
                         int numDataNodes,
                         boolean format,
                         boolean manageNameDfsDirs,
-                        boolean enableManagedDfsDirsRedundancy,
                         boolean manageDataDfsDirs,
                         StartupOption operation,
                         String[] racks, String hosts[],
                         long[] simulatedCapacities) throws IOException {
     this.nameNodes = new NameNodeInfo[1]; // Single namenode in the cluster
     initMiniDFSCluster(conf, numDataNodes, format,
-        manageNameDfsDirs, true, enableManagedDfsDirsRedundancy, manageDataDfsDirs,
+        manageNameDfsDirs, true, manageDataDfsDirs, manageDataDfsDirs,
         operation, racks, hosts,
         simulatedCapacities, null, true, false,
         MiniDFSNNTopology.simpleSingleNN(nameNodePort, 0));
