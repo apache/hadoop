d07d0f1a5171f78d71e6cc684e76d6d89ff3f616
Aaron Myers <atm@apache.org>
2012-07-09 18:32:38 +0000

    HDFS-3067. NPE in DFSInputStream.readBuffer if read is repeated on corrupted block. Contributed by Henry Robinson.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1359330 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSInputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSInputStream.java
@@ -623,7 +623,9 @@ public class DFSInputStream extends FSInputStream implements ByteBufferReadable 
       int retries = 2;
       while (retries > 0) {
         try {
-          if (pos > blockEnd) {
+          // currentNode can be left as null if previous read had a checksum
+          // error on the same block. See HDFS-3067
+          if (pos > blockEnd || currentNode == null) {
             currentNode = blockSeekTo(pos);
           }
           int realLen = (int) Math.min(len, (blockEnd - pos + 1L));
