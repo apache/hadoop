6df36b322b9b566887a10d4674f6975d3c5c9a94
Harsh J <harsh@apache.org>
2012-07-10 21:15:21 +0000

    Backport of HDFS-3629 from trunk. svn merge -c 1359905 ../../trunk (harsh)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1359906 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NNStorage.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NNStorage.java
@@ -1103,7 +1103,7 @@ public class NNStorage extends Storage implements Closeable {
     }
     if (multipleLV) {            
       throw new IOException(
-          "Storage directories containe multiple layout versions: "
+          "Storage directories contain multiple layout versions: "
               + layoutVersions);
     }
     // If the storage directories are with the new layout version
