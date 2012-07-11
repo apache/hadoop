6ff0e1fc3a1286d0927768433d0f9266629ce805
Harsh J <harsh@apache.org>
2012-07-11 03:37:55 +0000

    Backport of HDFS-3611 from trunk. svn merge -c 1360001 ../../trunk (harsh)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1360002 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/EditLogFileInputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/EditLogFileInputStream.java
@@ -164,9 +164,11 @@ public class EditLogFileInputStream extends EditLogInputStream {
           //
           long skipAmt = file.length() - tracker.getPos();
           if (skipAmt > 0) {
-            LOG.warn("skipping " + skipAmt + " bytes at the end " +
-              "of edit log  '" + getName() + "': reached txid " + txId +
-              " out of " + lastTxId);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("skipping " + skipAmt + " bytes at the end " +
+                  "of edit log  '" + getName() + "': reached txid " + txId +
+                  " out of " + lastTxId);
+            }
             tracker.clearLimit();
             IOUtils.skipFully(tracker, skipAmt);
           }
