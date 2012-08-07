faf54e5b2aac182951da8a0c5bc066f326435b56
Alejandro Abdelnur <tucu@apache.org>
2012-08-07 17:46:24 +0000

    Merge -r 1370386:1370387 from trunk to branch. FIXES: HDFS-3291
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1370392 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs-httpfs/src/test/java/org/apache/hadoop/fs/http/client/TestHttpFSFWithWebhdfsFileSystem.java
+++ hadoop-hdfs-project/hadoop-hdfs-httpfs/src/test/java/org/apache/hadoop/fs/http/client/TestHttpFSFWithWebhdfsFileSystem.java
@@ -18,20 +18,15 @@
 
 package org.apache.hadoop.fs.http.client;
 
-import java.net.URI;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.hdfs.web.WebHdfsFileSystem;
-import org.apache.hadoop.test.TestJettyHelper;
-import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 @RunWith(value = Parameterized.class)
-public class TestWebhdfsFileSystem extends TestHttpFSFileSystem {
+public class TestHttpFSFWithWebhdfsFileSystem
+  extends TestHttpFSWithHttpFSFileSystem {
 
-  public TestWebhdfsFileSystem(TestHttpFSFileSystem.Operation operation) {
+  public TestHttpFSFWithWebhdfsFileSystem(Operation operation) {
     super(operation);
   }
 
