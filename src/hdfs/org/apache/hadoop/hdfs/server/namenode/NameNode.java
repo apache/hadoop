e788d6c9f8dfa27c21c902421b2adfcd13f8511b
Owen O'Malley <omalley@apache.org>
2012-08-31 22:42:01 +0000

    HDFS-3466. Get HTTP kerberos principal from the web authentication keytab. (omalley)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-1.1@1379655 13f79535-47bb-0310-9956-ffa450edef68

--- src/hdfs/org/apache/hadoop/hdfs/server/namenode/NameNode.java
+++ src/hdfs/org/apache/hadoop/hdfs/server/namenode/NameNode.java
@@ -372,7 +372,12 @@ public class NameNode implements ClientProtocol, DatanodeProtocol,
                       SecurityUtil.getServerPrincipal(principalInConf,
                           serverAddress.getHostName()));
                 }
-                String httpKeytab = conf.get(DFSConfigKeys.DFS_NAMENODE_KEYTAB_FILE_KEY);
+                String httpKeytab = conf.get(
+                  DFSConfigKeys.DFS_WEB_AUTHENTICATION_KERBEROS_KEYTAB_KEY);
+                if (httpKeytab == null) {
+                  httpKeytab = 
+                    conf.get(DFSConfigKeys.DFS_NAMENODE_KEYTAB_FILE_KEY);
+                }
                 if (httpKeytab != null && !httpKeytab.isEmpty()) {
                   params.put("kerberos.keytab", httpKeytab);
                 }
