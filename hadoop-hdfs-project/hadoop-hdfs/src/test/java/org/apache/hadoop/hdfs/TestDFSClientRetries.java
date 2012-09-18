d1e2d368a485b36b873794dd36fce728865da5b2
Eli Collins <eli@apache.org>
2012-09-18 17:08:56 +0000

    HDFS-3936. MiniDFSCluster shutdown races with BlocksMap usage. Contributed by Eli Collins
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1387264 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/TestDFSClientRetries.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/TestDFSClientRetries.java
@@ -778,7 +778,7 @@ public class TestDFSClientRetries {
   }
 
   /** Test client retry with namenode restarting. */
-  @Test
+  @Test(timeout=300000)
   public void testNamenodeRestart() throws Exception {
     namenodeRestartTest(new Configuration(), false);
   }
