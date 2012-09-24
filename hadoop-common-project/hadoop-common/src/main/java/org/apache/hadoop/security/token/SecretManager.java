38ec8babda50215f984e5f5532ae70918af77378
Owen O'Malley <omalley@apache.org>
2012-09-24 03:47:25 +0000

    Fix the length of the secret key.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1389195 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/token/SecretManager.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/token/SecretManager.java
@@ -93,7 +93,7 @@ public abstract class SecretManager<T extends TokenIdentifier> {
   /**
    * The length of the random keys to use.
    */
-  private static final int KEY_LENGTH = 20;
+  private static final int KEY_LENGTH = 64;
 
   /**
    * A thread local store for the Macs.
