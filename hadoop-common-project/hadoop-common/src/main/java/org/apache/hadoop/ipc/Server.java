d1d1165a7d923f59f453ce18890d67e4eda68901
Robert Joseph Evans <bobby@apache.org>
2012-10-30 15:51:29 +0000

    svn merge -c 1403745 FIXES: HADOOP-8986. Server$Call object is never released after it is sent (bobby)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1403747 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
@@ -933,6 +933,8 @@ public abstract class Server {
             return true;
           }
           if (!call.rpcResponse.hasRemaining()) {
+            //Clear out the response buffer so it can be collected
+            call.rpcResponse = null;
             call.connection.decRpcCount();
             if (numElements == 1) {    // last call fully processes.
               done = true;             // no more data for this channel.
