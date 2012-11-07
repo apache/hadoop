11acd4f87ec10b34850299b84f36a19f64740613
Suresh Srinivas <suresh@apache.org>
2012-11-07 22:55:07 +0000

    HDFS-2815. Merging r1378664 from branch-1
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-1.1@1406854 13f79535-47bb-0310-9956-ffa450edef68

--- src/hdfs/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ src/hdfs/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -2081,7 +2081,6 @@ public class FSNamesystem implements FSConstants, FSNamesystemMBean,
    */
   private boolean deleteInternal(String src,
       boolean enforcePermission) throws IOException {
-    boolean deleteNow = false;
     ArrayList<Block> collectedBlocks = new ArrayList<Block>();
     synchronized (this) {
       if (isInSafeMode()) {
@@ -2094,17 +2093,11 @@ public class FSNamesystem implements FSConstants, FSNamesystemMBean,
       if (!dir.delete(src, collectedBlocks)) {
         return false;
       }
-      deleteNow = collectedBlocks.size() <= BLOCK_DELETION_INCREMENT;
-      if (deleteNow) { // Perform small deletes right away
-        removeBlocks(collectedBlocks);
-      }
     }
     
     // Log directory deletion to editlog
     getEditLog().logSync();
-    if (!deleteNow) {
-      removeBlocks(collectedBlocks); // Incremental deletion of blocks
-    }
+    removeBlocks(collectedBlocks); // Incremental deletion of blocks
     collectedBlocks.clear();
     if (NameNode.stateChangeLog.isDebugEnabled()) {
       NameNode.stateChangeLog.debug("DIR* Namesystem.delete: " + src
