3e6592363d3e5286d537f5a0abff52e9d4027de7
Eric Yang <eyang@apache.org>
2012-12-02 00:32:59 +0000

    MAPREDUCE-4798. Updated TestJobHistoryServer test case for startup race conditions.  (Sam Liu via eyang)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-1.0@1416089 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/hadoop/mapred/TestJobHistoryServer.java
+++ src/test/org/apache/hadoop/mapred/TestJobHistoryServer.java
@@ -72,6 +72,9 @@ public class TestJobHistoryServer extends TestCase {
     } catch (IOException e) {
       LOG.error("Failure running test", e);
       Assert.fail(e.getMessage());
+    } catch (InterruptedException e) {
+      LOG.error("Exit due to being interrupted");
+      Assert.fail(e.getMessage());
     } finally {
       if (mrCluster != null) mrCluster.shutdown();
     }
@@ -110,6 +113,9 @@ public class TestJobHistoryServer extends TestCase {
 
     } catch (IOException e) {
       LOG.error("Failure running test", e);
+      Assert.fail(e.getMessage());
+    } catch (InterruptedException e) {
+      LOG.error("Exit due to being interrupted");
       Assert.fail(e.getMessage());
     } finally {
       if (mrCluster != null) mrCluster.shutdown();
@@ -150,12 +156,28 @@ public class TestJobHistoryServer extends TestCase {
     return JobClient.runJob(conf);
   }
 
-  private String getRedirectUrl(String jobUrl) throws IOException {
+  private String getRedirectUrl(String jobUrl) throws IOException, InterruptedException {
     HttpClient client = new HttpClient();
     GetMethod method = new GetMethod(jobUrl);
     method.setFollowRedirects(false);
     try {
       int status = client.executeMethod(method);
+      if(status!=HttpURLConnection.HTTP_MOVED_TEMP) {
+        int retryTimes = 4;
+        for(int i = 1; i < retryTimes + 1; i++) {
+          try {
+            // Wait i sec
+            Thread.sleep(i * 1000);
+          } catch (InterruptedException e) {
+            throw new InterruptedException("Exit due to being interrupted");
+          }
+          // Get the latest status
+          status = client.executeMethod(method);
+          if(status == HttpURLConnection.HTTP_MOVED_TEMP)
+            break;
+        }
+      }
+
       Assert.assertEquals(status, HttpURLConnection.HTTP_MOVED_TEMP);
 
       LOG.info("Location: " + method.getResponseHeader("Location"));
