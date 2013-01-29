64c6dd8f5c3a93ebe8f000c38b9eaad121502513
Kihwal Lee <kihwal@apache.org>
2013-01-29 22:49:17 +0000

    merge -r 1440191:1440192 from trunk to branch-2 to fix HDFS-4288
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1440194 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/DatanodeDescriptor.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/DatanodeDescriptor.java
@@ -551,6 +551,7 @@ public class DatanodeDescriptor extends DatanodeInfo {
   @Override
   public void updateRegInfo(DatanodeID nodeReg) {
     super.updateRegInfo(nodeReg);
+    firstBlockReport = true; // must re-process IBR after re-registration
   }
 
   /**
