d540d9d161e3f030e2a57be5f9ef41f0c764c397
Thomas Graves <tgraves@apache.org>
2013-02-01 22:43:52 +0000

    HDFS-4444. Add space between total transaction time and number of transactions in FSEditLog#printStatistics. (Stephen Chu via tgraves)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.23@1441652 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSEditLog.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSEditLog.java
@@ -476,7 +476,7 @@ public class FSEditLog  {
     buf.append(numTransactions);
     buf.append(" Total time for transactions(ms): ");
     buf.append(totalTimeTransactions);
-    buf.append("Number of transactions batched in Syncs: ");
+    buf.append(" Number of transactions batched in Syncs: ");
     buf.append(numTransactionsBatchedInSync);
     buf.append(" Number of syncs: ");
     for (JournalAndStream jas : journals) {
