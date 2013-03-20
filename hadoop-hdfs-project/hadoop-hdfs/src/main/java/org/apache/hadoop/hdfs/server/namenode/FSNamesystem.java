c92b59fd8db89d78b877ac52d40d4354655cc652
Aaron Myers <atm@apache.org>
2013-03-20 00:36:13 +0000

    HDFS-4614. FSNamesystem#getContentSummary should use getPermissionChecker helper method. Contributed by Aaron T. Myers.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1458593 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -3054,8 +3054,7 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
 
   ContentSummary getContentSummary(String src) throws AccessControlException,
       FileNotFoundException, UnresolvedLinkException, StandbyException {
-    FSPermissionChecker pc = new FSPermissionChecker(fsOwnerShortUserName,
-        supergroup);
+    FSPermissionChecker pc = getPermissionChecker();
     checkOperation(OperationCategory.READ);
     readLock();
     try {
