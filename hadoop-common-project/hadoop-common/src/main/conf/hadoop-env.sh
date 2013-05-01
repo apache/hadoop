07f634819222b44f10e71a7f00a2cbd881522d7d
Suresh Srinivas <suresh@apache.org>
2013-05-01 16:13:37 +0000

    HADOOP-9455. Merge change r1478067 from trunk
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1478071 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-common-project/hadoop-common/src/main/conf/hadoop-env.sh
+++ hadoop-common-project/hadoop-common/src/main/conf/hadoop-env.sh
@@ -45,7 +45,7 @@ done
 #export HADOOP_NAMENODE_INIT_HEAPSIZE=""
 
 # Extra Java runtime options.  Empty by default.
-export HADOOP_OPTS="-Djava.net.preferIPv4Stack=true $HADOOP_CLIENT_OPTS"
+export HADOOP_OPTS="-Djava.net.preferIPv4Stack=true"
 
 # Command specific options appended to HADOOP_OPTS when specified
 export HADOOP_NAMENODE_OPTS="-Dhadoop.security.logger=${HADOOP_SECURITY_LOGGER:-INFO,RFAS} -Dhdfs.audit.logger=${HDFS_AUDIT_LOGGER:-INFO,NullAppender} $HADOOP_NAMENODE_OPTS"
