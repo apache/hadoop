945cb2ecaa110e7815ebdefd50dc8c8353f30cb3
Aaron Myers <atm@apache.org>
2013-05-10 21:45:32 +0000

    HADOOP-9485. No default value in the code for hadoop.rpc.socket.factory.class.default. Contributed by Colin Patrick McCabe.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1481200 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ipc/TestSocketFactory.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ipc/TestSocketFactory.java
@@ -27,6 +27,7 @@ import javax.net.SocketFactory;
 import junit.framework.Assert;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.CommonConfigurationKeys;
 import org.apache.hadoop.net.NetUtils;
 import org.apache.hadoop.net.StandardSocketFactory;
 import org.junit.Test;
@@ -39,13 +40,13 @@ public class TestSocketFactory {
     int toBeCached1 = 1;
     int toBeCached2 = 2;
     Configuration conf = new Configuration();
-    conf.set("hadoop.rpc.socket.factory.class.default",
+    conf.set(CommonConfigurationKeys.HADOOP_RPC_SOCKET_FACTORY_CLASS_DEFAULT_KEY,
         "org.apache.hadoop.ipc.TestSocketFactory$DummySocketFactory");
     final SocketFactory dummySocketFactory = NetUtils
         .getDefaultSocketFactory(conf);
     dummyCache.put(dummySocketFactory, toBeCached1);
 
-    conf.set("hadoop.rpc.socket.factory.class.default",
+    conf.set(CommonConfigurationKeys.HADOOP_RPC_SOCKET_FACTORY_CLASS_DEFAULT_KEY,
         "org.apache.hadoop.net.StandardSocketFactory");
     final SocketFactory defaultSocketFactory = NetUtils
         .getDefaultSocketFactory(conf);
