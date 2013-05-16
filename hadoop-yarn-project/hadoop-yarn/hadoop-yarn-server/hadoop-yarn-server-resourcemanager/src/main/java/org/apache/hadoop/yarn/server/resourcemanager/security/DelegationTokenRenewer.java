3641f938f6c394f0f877a9b351ec17d63ce1a166
Robert Joseph Evans <bobby@apache.org>
2013-05-16 21:41:49 +0000

    YARN-690. RM exits on token cancel/renew problems (daryn via bobby)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1483578 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/security/DelegationTokenRenewer.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/security/DelegationTokenRenewer.java
@@ -241,6 +241,9 @@ public class DelegationTokenRenewer extends AbstractService {
         } catch (IOException e) {
           LOG.warn("Failed to cancel token " + tokenWithConf.token + " " +  
               StringUtils.stringifyException(e));
+        } catch (RuntimeException e) {
+          LOG.warn("Failed to cancel token " + tokenWithConf.token + " " +  
+              StringUtils.stringifyException(e));
         } catch (InterruptedException ie) {
           return;
         } catch (Throwable t) {
