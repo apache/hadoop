3f9b3d49a3aeb77e10dcd595e81c3a8f4b7c26ea
Kihwal Lee <kihwal@apache.org>
2013-05-23 21:37:32 +0000

    svn merge -c 1485861 Merging from trunk to branch-2 to fix HDFS-4807.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1485863 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSOutputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSOutputStream.java
@@ -1293,7 +1293,7 @@ public class DFSOutputStream extends FSOutputSummer implements Syncable {
     final InetSocketAddress isa = NetUtils.createSocketAddr(dnAddr);
     final Socket sock = client.socketFactory.createSocket();
     final int timeout = client.getDatanodeReadTimeout(length);
-    NetUtils.connect(sock, isa, client.getRandomLocalInterfaceAddr(), timeout);
+    NetUtils.connect(sock, isa, client.getRandomLocalInterfaceAddr(), client.getConf().socketTimeout);
     sock.setSoTimeout(timeout);
     sock.setSendBufferSize(HdfsConstants.DEFAULT_DATA_SOCKET_SIZE);
     if(DFSClient.LOG.isDebugEnabled()) {
