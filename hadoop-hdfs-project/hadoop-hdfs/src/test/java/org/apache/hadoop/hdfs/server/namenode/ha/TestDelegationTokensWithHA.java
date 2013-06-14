7a434f3b3122774ae90676ace64b1d0f454f5f88
Chris Nauroth <cnauroth@apache.org>
2013-06-14 16:52:40 +0000

    HDFS-4783. Merging change r1493151 from branch-2 to branch-2.1-beta
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2.1-beta@1493153 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/ha/TestDelegationTokensWithHA.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/ha/TestDelegationTokensWithHA.java
@@ -207,8 +207,10 @@ public class TestDelegationTokensWithHA {
     ugi.addToken(token);
 
     Collection<InetSocketAddress> nnAddrs = new HashSet<InetSocketAddress>();
-    nnAddrs.add(nn0.getNameNodeAddress());
-    nnAddrs.add(nn1.getNameNodeAddress());
+    nnAddrs.add(new InetSocketAddress("localhost",
+      nn0.getNameNodeAddress().getPort()));
+    nnAddrs.add(new InetSocketAddress("localhost",
+      nn1.getNameNodeAddress().getPort()));
     HAUtil.cloneDelegationTokenForLogicalUri(ugi, haUri, nnAddrs);
     
     Collection<Token<? extends TokenIdentifier>> tokens = ugi.getTokens();
