04bea5f2fd713c18dc9ed6f46e16b71b5d408314
Vinod Kumar Vavilapalli <vinodkv@apache.org>
2013-06-17 06:52:39 +0000

    YARN-833. Moved Graph and VisualizeStateMachine into yarn.state package. Contributed by Zhijie Shen. svn merge --ignore-ancestry -c 1493654 ../../trunk/
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1493655 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/state/Graph.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/state/Graph.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.yarn.util;
+package org.apache.hadoop.yarn.state;
 
 import java.io.FileWriter;
 import java.io.IOException;
