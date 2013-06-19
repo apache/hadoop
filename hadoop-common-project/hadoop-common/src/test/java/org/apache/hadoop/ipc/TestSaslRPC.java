8d517e60af1662a8b08421b3e5d6d393328d2c3c
Aaron Myers <atm@apache.org>
2013-06-19 22:36:20 +0000

    Fix up a little test failure.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.23@1494792 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ipc/TestSaslRPC.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ipc/TestSaslRPC.java
@@ -91,6 +91,9 @@ public class TestSaslRPC {
   public void setup() {
     conf = new Configuration();
     conf.set(HADOOP_SECURITY_AUTHENTICATION, "kerberos");
+    conf.setBoolean(
+        CommonConfigurationKeys.IPC_CLIENT_FALLBACK_TO_SIMPLE_AUTH_ALLOWED_KEY,
+        true);
     UserGroupInformation.setConfiguration(conf);
   }
 
