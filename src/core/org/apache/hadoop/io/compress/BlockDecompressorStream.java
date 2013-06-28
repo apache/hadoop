7f5fe8223f205e802c4900c5c7ebc89caedeaa28
Arun Murthy <acmurthy@apache.org>
2013-06-28 21:40:43 +0000

    Merge -c 1497938 from branch-1 to branch-1.2 to fix HADOOP-9665. Fixed BlockDecompressorStream#decompress to return -1 rather than throw EOF at end of file. Contributed by Zhijie Shen.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-1.2@1497939 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/hadoop/io/compress/BlockDecompressorStream.java
+++ src/core/org/apache/hadoop/io/compress/BlockDecompressorStream.java
@@ -81,7 +81,13 @@ public class BlockDecompressorStream extends DecompressorStream {
         }
       }
       if (decompressor.needsInput()) {
-        int m = getCompressedData();
+        int m;
+        try {
+          m = getCompressedData();
+        } catch (EOFException e) {
+          eof = true;
+          return -1;
+        }
         // Send the read data to the decompressor
         decompressor.setInput(buffer, 0, m);
       }
