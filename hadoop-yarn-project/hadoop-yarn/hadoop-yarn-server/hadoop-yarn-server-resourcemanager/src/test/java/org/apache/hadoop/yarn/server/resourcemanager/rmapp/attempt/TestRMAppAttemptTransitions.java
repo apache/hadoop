645845b4e9fbd19e2386af44a3cf328237f6ff45
Vinod Kumar Vavilapalli <vinodkv@apache.org>
2013-10-09 22:50:18 +0000

    YARN-1283. Fixed RM to give a fully-qualified proxy URL for an application so that clients don't need to do scheme-mangling. Contributed by Omkar Vinit Joshi. svn merge --ignore-ancestry -c 1530819 ../../trunk/
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2.2@1530821 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/rmapp/attempt/TestRMAppAttemptTransitions.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/rmapp/attempt/TestRMAppAttemptTransitions.java
@@ -41,7 +41,6 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.http.HttpConfig;
 import org.apache.hadoop.security.SecurityUtil;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.security.UserGroupInformation.AuthenticationMethod;
@@ -277,8 +276,7 @@ public class TestRMAppAttemptTransitions {
       URI proxyUri = ProxyUriUtils.getUriFromAMUrl(proxy);
       URI result = ProxyUriUtils.getProxyUri(trackingUri, proxyUri,
           appAttempt.getAppAttemptId().getApplicationId());
-      url = result.toASCIIString().substring(
-          HttpConfig.getSchemePrefix().length());
+      url = result.toASCIIString();
     } catch (URISyntaxException ex) {
       Assert.fail();
     }
