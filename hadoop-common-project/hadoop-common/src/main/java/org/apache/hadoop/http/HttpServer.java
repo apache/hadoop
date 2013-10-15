98b29a36218878559e1d757146b584781c3cbdbe
Andrew Wang <wang@apache.org>
2013-10-15 18:35:49 +0000

    HADOOP-100046. Print a log message when SSL is enabled (David S. Wang via wang)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1532477 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/http/HttpServer.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/http/HttpServer.java
@@ -341,6 +341,7 @@ public class HttpServer implements FilterContainer {
       }
       listener.setHost(bindAddress);
       listener.setPort(port);
+      LOG.info("SSL is enabled on " + toString());
     } else {
       listenerStartedExternally = true;
       listener = connector;
