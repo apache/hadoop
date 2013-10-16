5ab998107fe99b9fb7760b27821fd457d46ebdc9
Jonathan Turner Eagles <jeagles@apache.org>
2013-10-16 20:32:14 +0000

    MAPREDUCE-5586. TestCopyMapper#testCopyFailOnBlockSizeDifference fails when run from hadoop-tools/hadoop-distcp directory (jeagles)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.23@1532896 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-tools/hadoop-distcp/src/test/java/org/apache/hadoop/tools/mapred/TestCopyMapper.java
+++ hadoop-tools/hadoop-distcp/src/test/java/org/apache/hadoop/tools/mapred/TestCopyMapper.java
@@ -76,6 +76,7 @@ public class TestCopyMapper {
     Configuration configuration = new Configuration();
     System.setProperty("test.build.data", "target/tmp/build/TEST_COPY_MAPPER/data");
     configuration.set("hadoop.log.dir", "target/tmp");
+    configuration.set("dfs.namenode.fs-limits.min-block-size", "0");
     LOG.debug("fs.default.name  == " + configuration.get("fs.default.name"));
     LOG.debug("dfs.http.address == " + configuration.get("dfs.http.address"));
     return configuration;
