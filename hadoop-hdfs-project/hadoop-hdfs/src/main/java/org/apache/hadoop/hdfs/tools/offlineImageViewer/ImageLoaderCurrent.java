7e8d85ebb3071baf16c4c264433f3f6acd01b769
Kihwal Lee <kihwal@apache.org>
2014-02-19 15:28:57 +0000

    HDFS-5961. OIV cannot load fsimages containing a symbolic link. Contributed by Kihwal Lee.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1569789 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/tools/offlineImageViewer/ImageLoaderCurrent.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/tools/offlineImageViewer/ImageLoaderCurrent.java
@@ -737,6 +737,7 @@ class ImageLoaderCurrent implements ImageLoader {
       processPermission(in, v);
     } else if (numBlocks == -2) {
       v.visit(ImageElement.SYMLINK, Text.readString(in));
+      processPermission(in, v);
     } else if (numBlocks == -3) { // reference node
       final boolean isWithName = in.readBoolean();
       int snapshotId = in.readInt();
