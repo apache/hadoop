914c8712965f694770d591c393e8b22aa61cc3c8
Vinod Kumar Vavilapalli <vinodkv@apache.org>
2014-05-11 00:05:31 +0000

    YARN-1982. Renamed the daemon name to be TimelineServer instead of History Server and deprecated the old usage. Contributed by Zhijie Shen. svn merge --ignore-ancestry -c 1593748 ../../trunk
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1593749 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-yarn-project/hadoop-yarn/conf/yarn-env.sh
+++ hadoop-yarn-project/hadoop-yarn/conf/yarn-env.sh
@@ -54,14 +54,14 @@ fi
 # or JAVA_HEAP_MAX with YARN_HEAPMAX as the preferred option of the two.
 #export YARN_RESOURCEMANAGER_HEAPSIZE=1000
 
-# Specify the max Heapsize for the HistoryManager using a numerical value
+# Specify the max Heapsize for the timeline server using a numerical value
 # in the scale of MB. For example, to specify an jvm option of -Xmx1000m, set
 # the value to 1000.
 # This value will be overridden by an Xmx setting specified in either YARN_OPTS
-# and/or YARN_HISTORYSERVER_OPTS.
+# and/or YARN_TIMELINESERVER_OPTS.
 # If not specified, the default value will be picked from either YARN_HEAPMAX
 # or JAVA_HEAP_MAX with YARN_HEAPMAX as the preferred option of the two.
-#export YARN_HISTORYSERVER_HEAPSIZE=1000
+#export YARN_TIMELINESERVER_HEAPSIZE=1000
 
 # Specify the JVM options to be used when starting the ResourceManager.
 # These options will be appended to the options specified as YARN_OPTS
