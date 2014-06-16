50cd2a6bed7521d517e69c38f3249c26b328fa8f
Andrew Wang <wang@apache.org>
2014-06-16 23:23:33 +0000

    HADOOP-10666. Remove Copyright /d/d/d/d Apache Software Foundation from the source files license header. Contributed by Henry Saputra.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1603025 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-common-project/hadoop-common/src/main/conf/hadoop-env.sh
+++ hadoop-common-project/hadoop-common/src/main/conf/hadoop-env.sh
@@ -1,5 +1,3 @@
-# Copyright 2011 The Apache Software Foundation
-# 
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
