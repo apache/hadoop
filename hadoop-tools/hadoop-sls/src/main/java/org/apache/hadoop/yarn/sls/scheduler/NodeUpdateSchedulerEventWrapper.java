913833abaf572c0127b327ef19f358fa1bf69cf0
Karthik Kambatla <kasha@apache.org>
2014-07-25 17:33:42 +0000

    YARN-2335. Annotate all hadoop-sls APIs as @Private. (Wei Yan via kasha)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1613480 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-tools/hadoop-sls/src/main/java/org/apache/hadoop/yarn/sls/scheduler/NodeUpdateSchedulerEventWrapper.java
+++ hadoop-tools/hadoop-sls/src/main/java/org/apache/hadoop/yarn/sls/scheduler/NodeUpdateSchedulerEventWrapper.java
@@ -18,9 +18,13 @@
 
 package org.apache.hadoop.yarn.sls.scheduler;
 
+import org.apache.hadoop.classification.InterfaceAudience.Private;
+import org.apache.hadoop.classification.InterfaceStability.Unstable;
 import org.apache.hadoop.yarn.server.resourcemanager.scheduler.event
         .NodeUpdateSchedulerEvent;
 
+@Private
+@Unstable
 public class NodeUpdateSchedulerEventWrapper extends NodeUpdateSchedulerEvent {
   
   public NodeUpdateSchedulerEventWrapper(NodeUpdateSchedulerEvent event) {
