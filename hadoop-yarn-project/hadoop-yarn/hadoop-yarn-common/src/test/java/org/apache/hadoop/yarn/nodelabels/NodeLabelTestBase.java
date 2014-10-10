db7f1653198b950e89567c06898d64f6b930a0ee
Vinod Kumar Vavilapalli <vinodkv@apache.org>
2014-10-10 11:44:21 -0700

    YARN-2494. Added NodeLabels Manager internal API and implementation. Contributed by Wangda Tan.

---
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/test/java/org/apache/hadoop/yarn/nodelabels/NodeLabelTestBase.java
@@ -0,0 +1,76 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.yarn.nodelabels;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.hadoop.yarn.api.records.NodeId;
+import org.junit.Assert;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Sets;
+
+public class NodeLabelTestBase {
+  public static void assertMapEquals(Map<NodeId, Set<String>> m1,
+      ImmutableMap<NodeId, Set<String>> m2) {
+    Assert.assertEquals(m1.size(), m2.size());
+    for (NodeId k : m1.keySet()) {
+      Assert.assertTrue(m2.containsKey(k));
+      assertCollectionEquals(m1.get(k), m2.get(k));
+    }
+  }
+
+  public static void assertMapContains(Map<NodeId, Set<String>> m1,
+      ImmutableMap<NodeId, Set<String>> m2) {
+    for (NodeId k : m2.keySet()) {
+      Assert.assertTrue(m1.containsKey(k));
+      assertCollectionEquals(m1.get(k), m2.get(k));
+    }
+  }
+
+  public static void assertCollectionEquals(Collection<String> c1,
+      Collection<String> c2) {
+    Assert.assertEquals(c1.size(), c2.size());
+    Iterator<String> i1 = c1.iterator();
+    Iterator<String> i2 = c2.iterator();
+    while (i1.hasNext()) {
+      Assert.assertEquals(i1.next(), i2.next());
+    }
+  }
+
+  public static <E> Set<E> toSet(E... elements) {
+    Set<E> set = Sets.newHashSet(elements);
+    return set;
+  }
+  
+  public NodeId toNodeId(String str) {
+    if (str.contains(":")) {
+      int idx = str.indexOf(':');
+      NodeId id =
+          NodeId.newInstance(str.substring(0, idx),
+              Integer.valueOf(str.substring(idx + 1)));
+      return id;
+    } else {
+      return NodeId.newInstance(str, CommonNodeLabelsManager.WILDCARD_PORT);
+    }
+  }
+}
