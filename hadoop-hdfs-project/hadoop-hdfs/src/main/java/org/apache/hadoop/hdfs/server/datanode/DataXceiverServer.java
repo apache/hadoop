b3d8a642a938da9de680b479585a7c2014b8965c
Haohui Mai <wheat9@apache.org>
2014-10-24 10:50:16 -0700

    HDFS-7283. Bump DataNode OOM log from WARN to ERROR. Contributed by Stephen Chu.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DataXceiverServer.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DataXceiverServer.java
@@ -161,7 +161,7 @@ class DataXceiverServer implements Runnable {
         // DataNode can run out of memory if there is too many transfers.
         // Log the event, Sleep for 30 seconds, other transfers may complete by
         // then.
-        LOG.warn("DataNode is out of memory. Will retry in 30 seconds.", ie);
+        LOG.error("DataNode is out of memory. Will retry in 30 seconds.", ie);
         try {
           Thread.sleep(30 * 1000);
         } catch (InterruptedException e) {
