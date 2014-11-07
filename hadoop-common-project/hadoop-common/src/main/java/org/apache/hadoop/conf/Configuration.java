7a91ea32e1085e075de232a3d5060f54cdcb75de
Jing Zhao <jing9@apache.org>
2014-11-06 18:27:59 -0800

    Revert "HADOOP-11274. ConcurrentModificationException in Configuration Copy Constructor. Contributed by Junping Du."
    This reverts commit 16b34824673f5a50d464727b8fad98470e5e984a.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/conf/Configuration.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/conf/Configuration.java
@@ -690,26 +690,26 @@ public class Configuration implements Iterable<Map.Entry<String,String>>,
    */
   @SuppressWarnings("unchecked")
   public Configuration(Configuration other) {
-    synchronized(other) {
-      this.resources = (ArrayList<Resource>) other.resources.clone();
-      if (other.properties != null) {
-        this.properties = (Properties)other.properties.clone();
-      }
+   this.resources = (ArrayList<Resource>) other.resources.clone();
+   synchronized(other) {
+     if (other.properties != null) {
+       this.properties = (Properties)other.properties.clone();
+     }
 
-      if (other.overlay!=null) {
-        this.overlay = (Properties)other.overlay.clone();
-      }
+     if (other.overlay!=null) {
+       this.overlay = (Properties)other.overlay.clone();
+     }
 
-      this.updatingResource = new HashMap<String, String[]>(other.updatingResource);
-      this.finalParameters = new HashSet<String>(other.finalParameters);
-
-      this.classLoader = other.classLoader;
-      this.loadDefaults = other.loadDefaults;
-      setQuietMode(other.getQuietMode());
-    }
+     this.updatingResource = new HashMap<String, String[]>(other.updatingResource);
+     this.finalParameters = new HashSet<String>(other.finalParameters);
+   }
+   
     synchronized(Configuration.class) {
       REGISTRY.put(this, null);
     }
+    this.classLoader = other.classLoader;
+    this.loadDefaults = other.loadDefaults;
+    setQuietMode(other.getQuietMode());
   }
   
   /**
@@ -1019,28 +1019,26 @@ public class Configuration implements Iterable<Map.Entry<String,String>>,
     getProps().setProperty(name, value);
     String newSource = (source == null ? "programatically" : source);
 
-    synchronized (this) {
-      if (!isDeprecated(name)) {
-        updatingResource.put(name, new String[] {newSource});
-        String[] altNames = getAlternativeNames(name);
-        if(altNames != null) {
-          for(String n: altNames) {
-            if(!n.equals(name)) {
-              getOverlay().setProperty(n, value);
-              getProps().setProperty(n, value);
-              updatingResource.put(n, new String[] {newSource});
-            }
+    if (!isDeprecated(name)) {
+      updatingResource.put(name, new String[] {newSource});
+      String[] altNames = getAlternativeNames(name);
+      if(altNames != null) {
+        for(String n: altNames) {
+          if(!n.equals(name)) {
+            getOverlay().setProperty(n, value);
+            getProps().setProperty(n, value);
+            updatingResource.put(n, new String[] {newSource});
           }
         }
       }
-      else {
-        String[] names = handleDeprecation(deprecationContext.get(), name);
-        String altSource = "because " + name + " is deprecated";
-        for(String n : names) {
-          getOverlay().setProperty(n, value);
-          getProps().setProperty(n, value);
-          updatingResource.put(n, new String[] {altSource});
-        }
+    }
+    else {
+      String[] names = handleDeprecation(deprecationContext.get(), name);
+      String altSource = "because " + name + " is deprecated";
+      for(String n : names) {
+        getOverlay().setProperty(n, value);
+        getProps().setProperty(n, value);
+        updatingResource.put(n, new String[] {altSource});
       }
     }
   }
@@ -2271,7 +2269,7 @@ public class Configuration implements Iterable<Map.Entry<String,String>>,
    *
    * @return final parameter set.
    */
-  public synchronized Set<String> getFinalParameters() {
+  public Set<String> getFinalParameters() {
     return new HashSet<String>(finalParameters);
   }
 
@@ -2534,18 +2532,14 @@ public class Configuration implements Iterable<Map.Entry<String,String>>,
     if (value != null) {
       if (!finalParameters.contains(attr)) {
         properties.setProperty(attr, value);
-        synchronized(this) {
-          updatingResource.put(attr, source);
-        }
+        updatingResource.put(attr, source);
       } else if (!value.equals(properties.getProperty(attr))) {
         LOG.warn(name+":an attempt to override final parameter: "+attr
             +";  Ignoring.");
       }
     }
     if (finalParameter) {
-      synchronized(this) {
-        finalParameters.add(attr);
-      }
+      finalParameters.add(attr);
     }
   }
 
@@ -2739,7 +2733,7 @@ public class Configuration implements Iterable<Map.Entry<String,String>>,
   }
 
   @Override
-  public synchronized void readFields(DataInput in) throws IOException {
+  public void readFields(DataInput in) throws IOException {
     clear();
     int size = WritableUtils.readVInt(in);
     for(int i=0; i < size; ++i) {
@@ -2751,8 +2745,9 @@ public class Configuration implements Iterable<Map.Entry<String,String>>,
     }
   }
 
+  //@Override
   @Override
-  public synchronized void write(DataOutput out) throws IOException {
+  public void write(DataOutput out) throws IOException {
     Properties props = getProps();
     WritableUtils.writeVInt(out, props.size());
     for(Map.Entry<Object, Object> item: props.entrySet()) {
