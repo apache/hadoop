6dfdcf094d196fbf376129ec31a889632c3a30ae
Jian He <jianhe@apache.org>
2014-12-19 16:56:30 -0800

    YARN-2952. Fixed incorrect version check in StateStore. Contributed by Rohith Sharmaks (cherry picked from commit 808cba3821d5bc4267f69d14220757f01cd55715)
    (cherry picked from commit 9180d11b3bbb2a49127d5d25f53b38c5113bf7ea)

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-shuffle/src/main/java/org/apache/hadoop/mapred/ShuffleHandler.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-shuffle/src/main/java/org/apache/hadoop/mapred/ShuffleHandler.java
@@ -493,9 +493,9 @@ public class ShuffleHandler extends AuxiliaryService {
   @VisibleForTesting
   Version loadVersion() throws IOException {
     byte[] data = stateDb.get(bytes(STATE_DB_SCHEMA_VERSION_KEY));
-    // if version is not stored previously, treat it as 1.0.
+    // if version is not stored previously, treat it as CURRENT_VERSION_INFO.
     if (data == null || data.length == 0) {
-      return Version.newInstance(1, 0);
+      return getCurrentVersion();
     }
     Version version =
         new VersionPBImpl(VersionProto.parseFrom(data));
