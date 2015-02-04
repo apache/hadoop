42548f4dc2b2e8ace891b970c2c6712c02bb43ea
Akira Ajisaka <aajisaka@apache.org>
2015-02-04 04:17:14 -0800

    HADOOP-11544. Remove unused configuration keys for tracing. Contributed by Masatake Iwasaki.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/CommonConfigurationKeys.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/CommonConfigurationKeys.java
@@ -286,7 +286,4 @@ public class CommonConfigurationKeys extends CommonConfigurationKeysPublic {
   public static final String NFS_EXPORTS_ALLOWED_HOSTS_SEPARATOR = ";";
   public static final String NFS_EXPORTS_ALLOWED_HOSTS_KEY = "nfs.exports.allowed.hosts";
   public static final String NFS_EXPORTS_ALLOWED_HOSTS_KEY_DEFAULT = "* rw";
-
-  public static final String  HADOOP_TRACE_SAMPLER = "hadoop.htrace.sampler";
-  public static final String  HADOOP_TRACE_SAMPLER_DEFAULT = "NeverSampler";
 }
