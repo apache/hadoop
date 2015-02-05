d2f66c0dc8f1fff9495bae3053b110e9d9311f04
Akira Ajisaka <aajisaka@apache.org>
2015-02-04 17:44:46 -0800

    MAPREDUCE-5988. Fix dead links to the javadocs in mapreduce project. (aajisaka)

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-common/src/main/java/org/apache/hadoop/mapreduce/v2/api/protocolrecords/package-info.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-common/src/main/java/org/apache/hadoop/mapreduce/v2/api/protocolrecords/package-info.java
@@ -15,6 +15,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@InterfaceAudience.Private
 package org.apache.hadoop.mapreduce.v2.api.protocolrecords;
-import org.apache.hadoop.classification.InterfaceAudience;
