92acb0e4f46ccb0b20c64e0fe6878bf4d4c4b8b6
Haohui Mai <wheat9@apache.org>
2015-02-23 16:37:48 -0800

    HADOOP-10478. Fix new findbugs warnings in hadoop-maven-plugins. Contributed by Li Lu.

--- hadoop-maven-plugins/src/main/java/org/apache/hadoop/maven/plugin/util/Exec.java
+++ hadoop-maven-plugins/src/main/java/org/apache/hadoop/maven/plugin/util/Exec.java
@@ -19,6 +19,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -87,7 +88,11 @@ public class Exec {
     public OutputBufferThread(InputStream is) {
       this.setDaemon(true);
       output = new ArrayList<String>();
-      reader = new BufferedReader(new InputStreamReader(is));
+      try {
+        reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+      } catch (UnsupportedEncodingException e) {
+        throw new RuntimeException("Unsupported encoding " + e.toString());
+      }
     }
 
     @Override
