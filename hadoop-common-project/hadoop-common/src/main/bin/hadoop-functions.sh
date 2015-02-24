5c3f97d06d24c86f951d34777db7ceabd5ade006
Elliott Clark <elliott@fb.com>
2015-02-24 13:08:28 -0800

    HADOOP-11630 Allow HDFS to bind to ipv6 conditionally
    Signed-off-by: Elliott Clark <elliott@fb.com>

--- hadoop-common-project/hadoop-common/src/main/bin/hadoop-functions.sh
+++ hadoop-common-project/hadoop-common/src/main/bin/hadoop-functions.sh
@@ -269,7 +269,11 @@ function hadoop_bootstrap
   export HADOOP_OS_TYPE=${HADOOP_OS_TYPE:-$(uname -s)}
 
   # defaults
-  export HADOOP_OPTS=${HADOOP_OPTS:-"-Djava.net.preferIPv4Stack=true"}
+  if [[ "${HADOOP_ALLOW_IPV6}" -ne "yes" ]]; then
+    export HADOOP_OPTS=${HADOOP_OPTS:-"-Djava.net.preferIPv4Stack=true"}
+  else
+    export HADOOP_OPTS=${HADOOP_OPTS:-""}
+  fi
   hadoop_debug "Initial HADOOP_OPTS=${HADOOP_OPTS}"
 }
 
