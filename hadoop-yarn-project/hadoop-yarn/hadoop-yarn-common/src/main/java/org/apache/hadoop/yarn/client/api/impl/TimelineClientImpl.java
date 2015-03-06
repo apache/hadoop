a57ada6c1f0c921df910c8b69cddbddd55a4f1b0
Xuan <xgong@apache.org>
2015-03-06 13:32:05 -0800

    YARN-3227. Timeline renew delegation token fails when RM user's TGT is expired. Contributed by Zhijie Shen
    (cherry picked from commit d1abc5d4fc00bb1b226066684556ba16ace71744)
    (cherry picked from commit 56c2050ab7c04e9741bcba9504b71e5a54d09eea)
    (cherry picked from commit 780a9b1a98827a692e0ea9fbc92f9d1ab979e3e0)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/client/api/impl/TimelineClientImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/client/api/impl/TimelineClientImpl.java
@@ -425,6 +425,7 @@ public class TimelineClientImpl extends TimelineClient {
         UserGroupInformation callerUGI = isProxyAccess ?
             UserGroupInformation.getCurrentUser().getRealUser()
             : UserGroupInformation.getCurrentUser();
+        callerUGI.checkTGTAndReloginFromKeytab();
         try {
           return callerUGI.doAs(action);
         } catch (UndeclaredThrowableException e) {
@@ -474,6 +475,7 @@ public class TimelineClientImpl extends TimelineClient {
           : UserGroupInformation.getCurrentUser();
       final String doAsUser = isProxyAccess ?
           UserGroupInformation.getCurrentUser().getShortUserName() : null;
+      callerUGI.checkTGTAndReloginFromKeytab();
       try {
         return callerUGI.doAs(new PrivilegedExceptionAction<HttpURLConnection>() {
           @Override
