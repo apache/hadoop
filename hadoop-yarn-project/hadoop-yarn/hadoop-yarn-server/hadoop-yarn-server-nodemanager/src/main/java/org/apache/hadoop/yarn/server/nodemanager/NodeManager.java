cf4154676b892a36fe977c115bac52f9dabcc128
Jason Lowe <jlowe@apache.org>
2015-04-24 22:02:53 +0000

    YARN-3537. NPE when NodeManager.serviceInit fails and stopRecoveryStore invoked. Contributed by Brahma Reddy Battula (cherry picked from commit 5e093f0d400f82f67d9b2d24253c79e4a5abacf9)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/NodeManager.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/NodeManager.java
@@ -177,18 +177,20 @@ public class NodeManager extends CompositeService
   }
 
   private void stopRecoveryStore() throws IOException {
-    nmStore.stop();
-    if (null != context) {
-      if (context.getDecommissioned() && nmStore.canRecover()) {
-        LOG.info("Removing state store due to decommission");
-        Configuration conf = getConfig();
-        Path recoveryRoot =
-            new Path(conf.get(YarnConfiguration.NM_RECOVERY_DIR));
-        LOG.info("Removing state store at " + recoveryRoot
-            + " due to decommission");
-        FileSystem recoveryFs = FileSystem.getLocal(conf);
-        if (!recoveryFs.delete(recoveryRoot, true)) {
-          LOG.warn("Unable to delete " + recoveryRoot);
+    if (null != nmStore) {
+      nmStore.stop();
+      if (null != context) {
+        if (context.getDecommissioned() && nmStore.canRecover()) {
+          LOG.info("Removing state store due to decommission");
+          Configuration conf = getConfig();
+          Path recoveryRoot =
+              new Path(conf.get(YarnConfiguration.NM_RECOVERY_DIR));
+          LOG.info("Removing state store at " + recoveryRoot
+              + " due to decommission");
+          FileSystem recoveryFs = FileSystem.getLocal(conf);
+          if (!recoveryFs.delete(recoveryRoot, true)) {
+            LOG.warn("Unable to delete " + recoveryRoot);
+          }
         }
       }
     }
