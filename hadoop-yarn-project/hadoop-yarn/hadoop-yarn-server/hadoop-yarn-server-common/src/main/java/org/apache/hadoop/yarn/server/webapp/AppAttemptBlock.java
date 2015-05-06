f8488b5aeb89449f749d8b8c13b3d3a2ce65a1a9
Jian He <jianhe@apache.org>
2015-05-06 13:45:00 -0700

    YARN-3301. Fixed the format issue of the new RM attempt web page. Contributed by Xuan Gong (cherry picked from commit 0d3188fd25f31b83caf16b77773620f4ee237409)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-common/src/main/java/org/apache/hadoop/yarn/server/webapp/AppAttemptBlock.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-common/src/main/java/org/apache/hadoop/yarn/server/webapp/AppAttemptBlock.java
@@ -149,6 +149,8 @@ public class AppAttemptBlock extends HtmlBlock {
     createAttemptHeadRoomTable(html);
     html._(InfoBlock.class);
 
+    createTablesForAttemptMetrics(html);
+
     // Container Table
     TBODY<TABLE<Hamlet>> tbody =
         html.table("#containers").thead().tr().th(".id", "Container ID")
@@ -235,5 +237,9 @@ public class AppAttemptBlock extends HtmlBlock {
 
   protected void createAttemptHeadRoomTable(Block html) {
     
+  }
+
+  protected void createTablesForAttemptMetrics(Block html) {
+
   }
 }
