cd6b26cce7457d08346b9d90a5f2f333ba4202d8
Arpit Agarwal <arp@apache.org>
2015-05-08 10:04:11 -0700

    HDFS-8274. NFS configuration nfs.dump.dir not working (Contributed by Ajith S)

--- hadoop-hdfs-project/hadoop-hdfs-nfs/src/main/java/org/apache/hadoop/hdfs/nfs/conf/NfsConfigKeys.java
+++ hadoop-hdfs-project/hadoop-hdfs-nfs/src/main/java/org/apache/hadoop/hdfs/nfs/conf/NfsConfigKeys.java
@@ -28,7 +28,7 @@ public class NfsConfigKeys {
   
   public static final String DFS_NFS_FILE_DUMP_KEY = "nfs.file.dump";
   public static final boolean DFS_NFS_FILE_DUMP_DEFAULT = true;
-  public static final String DFS_NFS_FILE_DUMP_DIR_KEY = "nfs.file.dump.dir";
+  public static final String DFS_NFS_FILE_DUMP_DIR_KEY = "nfs.dump.dir";
   public static final String DFS_NFS_FILE_DUMP_DIR_DEFAULT = "/tmp/.hdfs-nfs";
   
   public static final String DFS_NFS_MAX_READ_TRANSFER_SIZE_KEY = "nfs.rtmax";
