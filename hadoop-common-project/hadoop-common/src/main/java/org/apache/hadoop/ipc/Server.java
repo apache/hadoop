8011d4688fa77f123de14ad4e1722cda23536888
Haohui Mai <wheat9@apache.org>
2015-05-12 10:30:32 -0700

    HADOOP-11962. Sasl message with MD5 challenge text shouldn't be LOG out even in debug level. Contributed by Junping Du.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
@@ -1448,9 +1448,6 @@ public abstract class Server {
     }
     
     private void doSaslReply(Message message) throws IOException {
-      if (LOG.isDebugEnabled()) {
-        LOG.debug("Sending sasl message "+message);
-      }
       setupResponse(saslResponse, saslCall,
           RpcStatusProto.SUCCESS, null,
           new RpcResponseWrapper(message), null, null);
