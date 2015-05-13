780d4f416e3cac3b9e8188c658c6c8438c6a865b
Tsuyoshi Ozawa <ozawa@apache.org>
2015-05-13 16:44:37 +0900

    MAPREDUCE-6366. mapreduce.terasort.final.sync configuration in TeraSort doesn't work. Contributed by Takuya Fukudome.
    (cherry picked from commit e82067bfe680ce04acc0153693cce3cd385e5567)

--- hadoop-mapreduce-project/hadoop-mapreduce-examples/src/main/java/org/apache/hadoop/examples/terasort/TeraSortConfigKeys.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-examples/src/main/java/org/apache/hadoop/examples/terasort/TeraSortConfigKeys.java
@@ -70,7 +70,7 @@ public enum TeraSortConfigKeys {
   public static final long DEFAULT_NUM_ROWS = 0L;
   public static final int DEFAULT_NUM_PARTITIONS = 10;
   public static final long DEFAULT_SAMPLE_SIZE = 100000L;
-  public static final boolean DEFAULT_FINAL_SYNC_ATTRIBUTE = false;
+  public static final boolean DEFAULT_FINAL_SYNC_ATTRIBUTE = true;
   public static final boolean DEFAULT_USE_TERA_SCHEDULER = true;
   public static final boolean DEFAULT_USE_SIMPLE_PARTITIONER = false;
   public static final int DEFAULT_OUTPUT_REPLICATION = 1;
