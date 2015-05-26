fb57a1aac81139a4a1032dd91da83e14fdeea43d
Wangda Tan <wangda@apache.org>
2015-05-26 15:58:47 -0700

    YARN-3686. CapacityScheduler should trim default_node_label_expression. (Sunil G via wangda)
    (cherry picked from commit cdbd66be111c93c85a409d47284e588c453ecae9)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/api/records/impl/pb/ResourceRequestPBImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/api/records/impl/pb/ResourceRequestPBImpl.java
@@ -193,7 +193,7 @@ public class ResourceRequestPBImpl extends  ResourceRequest {
     if (!p.hasNodeLabelExpression()) {
       return null;
     }
-    return (p.getNodeLabelExpression());
+    return (p.getNodeLabelExpression().trim());
   }
 
   @Override
