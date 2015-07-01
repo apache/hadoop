fad478e7ce3763f02ecd97341c624a09f3f1f7e9
Lei Xu <lei@apache.org>
2015-06-30 17:26:49 -0700

    HADOOP-12164. Fix TestMove and TestFsShellReturnCode failed to get command name using reflection. (Lei Xu)
    (cherry picked from commit 47fab573bcb7b1129648499571b518f7b9773f89)
    
    Conflicts:
    	hadoop-common-project/hadoop-common/CHANGES.txt
    	hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/TestFsShellReturnCode.java

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/shell/TestMove.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/shell/TestMove.java
@@ -114,6 +114,7 @@ public class TestMove {
   }
     
   private static class InstrumentedRenameCommand extends MoveCommands.Rename {
+    public static String NAME = "InstrumentedRename";
     private Exception error = null;
     @Override
     public void displayError(Exception e) {
