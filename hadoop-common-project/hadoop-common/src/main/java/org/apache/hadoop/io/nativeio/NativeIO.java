1b3bceb58c8e536a75fa3f99cc3ceeaba91a07de
Colin Patrick Mccabe <cmccabe@cloudera.com>
2015-07-22 11:11:38 -0700

    HADOOP-12184. Remove unused Linux-specific constants in NativeIO (Martin Walsh via Colin P. McCabe)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/nativeio/NativeIO.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/nativeio/NativeIO.java
@@ -67,9 +67,6 @@ public class NativeIO {
     public static final int O_APPEND   = 02000;
     public static final int O_NONBLOCK = 04000;
     public static final int O_SYNC   =  010000;
-    public static final int O_ASYNC  =  020000;
-    public static final int O_FSYNC = O_SYNC;
-    public static final int O_NDELAY = O_NONBLOCK;
 
     // Flags for posix_fadvise() from bits/fcntl.h
     /* No further special treatment.  */
@@ -356,7 +353,6 @@ public class NativeIO {
       public static final int   S_IFREG  = 0100000;  /* regular */
       public static final int   S_IFLNK  = 0120000;  /* symbolic link */
       public static final int   S_IFSOCK = 0140000;  /* socket */
-      public static final int   S_IFWHT  = 0160000;  /* whiteout */
       public static final int S_ISUID = 0004000;  /* set user id on execution */
       public static final int S_ISGID = 0002000;  /* set group id on execution */
       public static final int S_ISVTX = 0001000;  /* save swapped text even after use */
