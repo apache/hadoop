3007622955ba25dc4aba5b449dba5b3e80ade349
Junping Du <junping_du@apache.org>
2015-08-11 16:59:21 -0700

    YARN-3906. Split the application table from the entity table. Contributed by Sangjin Lee.
    (cherry picked from commit bcd755eba9466ce277d3c14192c31da6462c4ab3)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timelineservice/src/main/java/org/apache/hadoop/yarn/server/timelineservice/storage/application/ApplicationTable.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timelineservice/src/main/java/org/apache/hadoop/yarn/server/timelineservice/storage/application/ApplicationTable.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.yarn.server.timelineservice.storage.entity;
+package org.apache.hadoop.yarn.server.timelineservice.storage.application;
 
 import java.io.IOException;
 
@@ -32,35 +32,34 @@ import org.apache.hadoop.yarn.server.timelineservice.storage.common.BaseTable;
 import org.apache.hadoop.yarn.server.timelineservice.storage.common.TimelineHBaseSchemaConstants;
 
 /**
- * The entity table as column families info, config and metrics. Info stores
- * information about a timeline entity object config stores configuration data
- * of a timeline entity object metrics stores the metrics of a timeline entity
- * object
+ * The application table as column families info, config and metrics. Info
+ * stores information about a YARN application entity, config stores
+ * configuration data of a YARN application, metrics stores the metrics of a
+ * YARN application. This table is entirely analogous to the entity table but
+ * created for better performance.
  *
- * Example entity table record:
+ * Example application table record:
  *
  * <pre>
  * |-------------------------------------------------------------------------|
  * |  Row       | Column Family                | Column Family| Column Family|
  * |  key       | info                         | metrics      | config       |
  * |-------------------------------------------------------------------------|
- * | userName!  | id:entityId                  | metricId1:   | configKey1:  |
- * | clusterId! |                              | metricValue1 | configValue1 |
- * | flowId!    | type:entityType              | @timestamp1  |              |
- * | flowRunId! |                              |              | configKey2:  |
- * | AppId!     | created_time:                | metriciD1:   | configValue2 |
- * | entityType!| 1392993084018                | metricValue2 |              |
- * | entityId   |                              | @timestamp2  |              |
- * |            | modified_time:               |              |              |
- * |            | 1392995081012                | metricId2:   |              |
- * |            |                              | metricValue1 |              |
- * |            | i!infoKey:                   | @timestamp2  |              |
- * |            | infoValue                    |              |              |
+ * | clusterId! | id:appId                     | metricId1:   | configKey1:  |
+ * | userName!  |                              | metricValue1 | configValue1 |
+ * | flowId!    | created_time:                | @timestamp1  |              |
+ * | flowRunId! | 1392993084018                |              | configKey2:  |
+ * | AppId      |                              | metriciD1:   | configValue2 |
+ * |            | modified_time:               | metricValue2 |              |
+ * |            | 1392995081012                | @timestamp2  |              |
  * |            |                              |              |              |
+ * |            | i!infoKey:                   | metricId2:   |              |
+ * |            | infoValue                    | metricValue1 |              |
+ * |            |                              | @timestamp2  |              |
  * |            | r!relatesToKey:              |              |              |
  * |            | id3?id4?id5                  |              |              |
  * |            |                              |              |              |
- * |            | s!isRelatedToKey             |              |              |
+ * |            | s!isRelatedToKey:            |              |              |
  * |            | id7?id9?id6                  |              |              |
  * |            |                              |              |              |
  * |            | e!eventId?timestamp?infoKey: |              |              |
@@ -71,23 +70,24 @@ import org.apache.hadoop.yarn.server.timelineservice.storage.common.TimelineHBas
  * |-------------------------------------------------------------------------|
  * </pre>
  */
-public class EntityTable extends BaseTable<EntityTable> {
-  /** entity prefix */
+public class ApplicationTable extends BaseTable<ApplicationTable> {
+  /** application prefix */
   private static final String PREFIX =
-      YarnConfiguration.TIMELINE_SERVICE_PREFIX + "entity";
+      YarnConfiguration.TIMELINE_SERVICE_PREFIX + ".application";
 
-  /** config param name that specifies the entity table name */
+  /** config param name that specifies the application table name */
   public static final String TABLE_NAME_CONF_NAME = PREFIX + ".table.name";
 
   /**
    * config param name that specifies the TTL for metrics column family in
-   * entity table
+   * application table
    */
   private static final String METRICS_TTL_CONF_NAME = PREFIX
       + ".table.metrics.ttl";
 
-  /** default value for entity table name */
-  private static final String DEFAULT_TABLE_NAME = "timelineservice.entity";
+  /** default value for application table name */
+  private static final String DEFAULT_TABLE_NAME =
+      "timelineservice.application";
 
   /** default TTL is 30 days for metrics timeseries */
   private static final int DEFAULT_METRICS_TTL = 2592000;
@@ -95,9 +95,9 @@ public class EntityTable extends BaseTable<EntityTable> {
   /** default max number of versions */
   private static final int DEFAULT_METRICS_MAX_VERSIONS = 1000;
 
-  private static final Log LOG = LogFactory.getLog(EntityTable.class);
+  private static final Log LOG = LogFactory.getLog(ApplicationTable.class);
 
-  public EntityTable() {
+  public ApplicationTable() {
     super(TABLE_NAME_CONF_NAME, DEFAULT_TABLE_NAME);
   }
 
@@ -121,40 +121,40 @@ public class EntityTable extends BaseTable<EntityTable> {
           + " already exists.");
     }
 
-    HTableDescriptor entityTableDescp = new HTableDescriptor(table);
+    HTableDescriptor applicationTableDescp = new HTableDescriptor(table);
     HColumnDescriptor infoCF =
-        new HColumnDescriptor(EntityColumnFamily.INFO.getBytes());
+        new HColumnDescriptor(ApplicationColumnFamily.INFO.getBytes());
     infoCF.setBloomFilterType(BloomType.ROWCOL);
-    entityTableDescp.addFamily(infoCF);
+    applicationTableDescp.addFamily(infoCF);
 
     HColumnDescriptor configCF =
-        new HColumnDescriptor(EntityColumnFamily.CONFIGS.getBytes());
+        new HColumnDescriptor(ApplicationColumnFamily.CONFIGS.getBytes());
     configCF.setBloomFilterType(BloomType.ROWCOL);
     configCF.setBlockCacheEnabled(true);
-    entityTableDescp.addFamily(configCF);
+    applicationTableDescp.addFamily(configCF);
 
     HColumnDescriptor metricsCF =
-        new HColumnDescriptor(EntityColumnFamily.METRICS.getBytes());
-    entityTableDescp.addFamily(metricsCF);
+        new HColumnDescriptor(ApplicationColumnFamily.METRICS.getBytes());
+    applicationTableDescp.addFamily(metricsCF);
     metricsCF.setBlockCacheEnabled(true);
     // always keep 1 version (the latest)
     metricsCF.setMinVersions(1);
     metricsCF.setMaxVersions(DEFAULT_METRICS_MAX_VERSIONS);
     metricsCF.setTimeToLive(hbaseConf.getInt(METRICS_TTL_CONF_NAME,
         DEFAULT_METRICS_TTL));
-    entityTableDescp
+    applicationTableDescp
         .setRegionSplitPolicyClassName("org.apache.hadoop.hbase.regionserver.KeyPrefixRegionSplitPolicy");
-    entityTableDescp.setValue("KeyPrefixRegionSplitPolicy.prefix_length",
+    applicationTableDescp.setValue("KeyPrefixRegionSplitPolicy.prefix_length",
         TimelineHBaseSchemaConstants.USERNAME_SPLIT_KEY_PREFIX_LENGTH);
-    admin.createTable(entityTableDescp,
+    admin.createTable(applicationTableDescp,
         TimelineHBaseSchemaConstants.getUsernameSplits());
     LOG.info("Status of table creation for " + table.getNameAsString() + "="
         + admin.tableExists(table));
   }
 
   /**
-   * @param metricsTTL time to live parameter for the metricss in this table.
-   * @param hbaseConf configururation in which to set the metrics TTL config
+   * @param metricsTTL time to live parameter for the metrics in this table.
+   * @param hbaseConf configuration in which to set the metrics TTL config
    *          variable.
    */
   public void setMetricsTTL(int metricsTTL, Configuration hbaseConf) {
