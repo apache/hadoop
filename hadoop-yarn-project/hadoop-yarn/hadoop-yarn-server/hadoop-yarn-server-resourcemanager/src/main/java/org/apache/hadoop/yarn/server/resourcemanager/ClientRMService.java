2b526ba75780ac61ba6d0bcb36044c6f7d9b6633
Jian He <jianhe@apache.org>
2015-08-13 16:02:57 -0700

    YARN-4047. ClientRMService getApplications has high scheduler lock contention. Contributed by Jason Lowe (cherry picked from commit 7a445fcfabcf9c6aae219051f65d3f6cb8feb87c)
    (cherry picked from commit 703fa1b141a98449746bd6fb3b144e74d964d1f5)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/ClientRMService.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/ClientRMService.java
@@ -738,12 +738,8 @@ public class ClientRMService extends AbstractService implements
       RMApp application = appsIter.next();
 
       // Check if current application falls under the specified scope
-      boolean allowAccess = checkAccess(callerUGI, application.getUser(),
-          ApplicationAccessType.VIEW_APP, application);
       if (scope == ApplicationsRequestScope.OWN &&
           !callerUGI.getUserName().equals(application.getUser())) {
-        continue;
-      } else if (scope == ApplicationsRequestScope.VIEWABLE && !allowAccess) {
         continue;
       }
 
@@ -791,6 +787,13 @@ public class ClientRMService extends AbstractService implements
         if (!match) {
           continue;
         }
+      }
+
+      // checkAccess can grab the scheduler lock so call it last
+      boolean allowAccess = checkAccess(callerUGI, application.getUser(),
+          ApplicationAccessType.VIEW_APP, application);
+      if (scope == ApplicationsRequestScope.VIEWABLE && !allowAccess) {
+        continue;
       }
 
       reports.add(application.createAndGetApplicationReport(
