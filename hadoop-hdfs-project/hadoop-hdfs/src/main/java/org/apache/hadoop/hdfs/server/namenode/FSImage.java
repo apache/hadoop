f638ff90420768d54bcbad0ac4a1850ce001611d
Jing Zhao <jing9@apache.org>
2015-09-02 15:19:04 -0700

    HDFS-9003. ForkJoin thread pool leaks. Contributed by Kihwal Lee.
    (cherry picked from commit de928d566a119f0b7fa5f171719642cd86be0af7)
    (cherry picked from commit 1d56325a80cdac5820079ac04fa18a7e5126f8ef)
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/CHANGES.txt
    (cherry picked from commit 879567d24a6e3b625f639a88137fb1654c54c18f)
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/CHANGES.txt

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSImage.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSImage.java
@@ -871,6 +871,7 @@ public class FSImage implements Closeable {
     RecursiveAction task = new InitQuotaTask(root, counts);
     p.execute(task);
     task.join();
+    p.shutdown();
     LOG.info("Quota initialization completed in " + (Time.now() - start) +
         " milliseconds\n" + counts);
   }
