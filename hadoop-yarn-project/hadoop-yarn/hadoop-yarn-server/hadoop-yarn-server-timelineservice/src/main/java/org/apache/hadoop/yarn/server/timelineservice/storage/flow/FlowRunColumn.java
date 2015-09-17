bf01ef1ea6dfeea534b9fb9c6801ff3625f3f1f8
Sangjin Lee <sjlee@apache.org>
2015-09-17 10:34:52 -0700

    YARN-3901. Populate flow run data in the flow_run & flow activity tables (Vrushali C via sjlee)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timelineservice/src/main/java/org/apache/hadoop/yarn/server/timelineservice/storage/flow/FlowRunColumn.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timelineservice/src/main/java/org/apache/hadoop/yarn/server/timelineservice/storage/flow/FlowRunColumn.java
@@ -15,8 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.yarn.server.timelineservice.storage.apptoflow;
+package org.apache.hadoop.yarn.server.timelineservice.storage.flow;
 
+import java.io.IOException;
 
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -24,40 +25,48 @@ import org.apache.hadoop.yarn.server.timelineservice.storage.common.Column;
 import org.apache.hadoop.yarn.server.timelineservice.storage.common.ColumnFamily;
 import org.apache.hadoop.yarn.server.timelineservice.storage.common.ColumnHelper;
 import org.apache.hadoop.yarn.server.timelineservice.storage.common.Separator;
+import org.apache.hadoop.yarn.server.timelineservice.storage.common.TimelineWriterUtils;
 import org.apache.hadoop.yarn.server.timelineservice.storage.common.TypedBufferedMutator;
-import org.apache.hadoop.yarn.server.timelineservice.storage.flow.Attribute;
-
-import java.io.IOException;
-import java.util.Map;
 
 /**
- * Identifies fully qualified columns for the {@link AppToFlowTable}.
+ * Identifies fully qualified columns for the {@link FlowRunTable}.
  */
-public enum AppToFlowColumn implements Column<AppToFlowTable> {
+public enum FlowRunColumn implements Column<FlowRunTable> {
 
   /**
-   * The flow ID
+   * When the flow was started. This is the minimum of currently known
+   * application start times.
    */
-  FLOW_ID(AppToFlowColumnFamily.MAPPING, "flow_id"),
+  MIN_START_TIME(FlowRunColumnFamily.INFO, "min_start_time",
+      AggregationOperation.MIN),
 
   /**
-   * The flow run ID
+   * When the flow ended. This is the maximum of currently known application end
+   * times.
    */
-  FLOW_RUN_ID(AppToFlowColumnFamily.MAPPING, "flow_run_id");
+  MAX_END_TIME(FlowRunColumnFamily.INFO, "max_end_time",
+      AggregationOperation.MAX),
 
-  private final ColumnHelper<AppToFlowTable> column;
-  private final ColumnFamily<AppToFlowTable> columnFamily;
+  /**
+   * The version of the flow that this flow belongs to.
+   */
+  FLOW_VERSION(FlowRunColumnFamily.INFO, "flow_version", null);
+
+  private final ColumnHelper<FlowRunTable> column;
+  private final ColumnFamily<FlowRunTable> columnFamily;
   private final String columnQualifier;
   private final byte[] columnQualifierBytes;
+  private final AggregationOperation aggOp;
 
-  AppToFlowColumn(ColumnFamily<AppToFlowTable> columnFamily,
-      String columnQualifier) {
+  private FlowRunColumn(ColumnFamily<FlowRunTable> columnFamily,
+      String columnQualifier, AggregationOperation aggOp) {
     this.columnFamily = columnFamily;
     this.columnQualifier = columnQualifier;
+    this.aggOp = aggOp;
     // Future-proof by ensuring the right column prefix hygiene.
-    this.columnQualifierBytes =
-        Bytes.toBytes(Separator.SPACE.encode(columnQualifier));
-    this.column = new ColumnHelper<AppToFlowTable>(columnFamily);
+    this.columnQualifierBytes = Bytes.toBytes(Separator.SPACE
+        .encode(columnQualifier));
+    this.column = new ColumnHelper<FlowRunTable>(columnFamily);
   }
 
   /**
@@ -67,11 +76,31 @@ public enum AppToFlowColumn implements Column<AppToFlowTable> {
     return columnQualifier;
   }
 
+  public byte[] getColumnQualifierBytes() {
+    return columnQualifierBytes.clone();
+  }
+
+  public AggregationOperation getAggregationOperation() {
+    return aggOp;
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * org.apache.hadoop.yarn.server.timelineservice.storage.common.Column#store
+   * (byte[], org.apache.hadoop.yarn.server.timelineservice.storage.common.
+   * TypedBufferedMutator, java.lang.Long, java.lang.Object,
+   * org.apache.hadoop.yarn.server.timelineservice.storage.flow.Attribute[])
+   */
   public void store(byte[] rowKey,
-      TypedBufferedMutator<AppToFlowTable> tableMutator, Long timestamp,
+      TypedBufferedMutator<FlowRunTable> tableMutator, Long timestamp,
       Object inputValue, Attribute... attributes) throws IOException {
+
+    Attribute[] combinedAttributes = TimelineWriterUtils.combineAttributes(
+        attributes, aggOp);
     column.store(rowKey, tableMutator, columnQualifierBytes, timestamp,
-        inputValue, attributes);
+        inputValue, combinedAttributes);
   }
 
   public Object readResult(Result result) throws IOException {
@@ -79,17 +108,18 @@ public enum AppToFlowColumn implements Column<AppToFlowTable> {
   }
 
   /**
-   * Retrieve an {@link AppToFlowColumn} given a name, or null if there is no
+   * Retrieve an {@link FlowRunColumn} given a name, or null if there is no
    * match. The following holds true: {@code columnFor(x) == columnFor(y)} if
    * and only if {@code x.equals(y)} or {@code (x == y == null)}
    *
-   * @param columnQualifier Name of the column to retrieve
-   * @return the corresponding {@link AppToFlowColumn} or null
+   * @param columnQualifier
+   *          Name of the column to retrieve
+   * @return the corresponding {@link FlowRunColumn} or null
    */
-  public static final AppToFlowColumn columnFor(String columnQualifier) {
+  public static final FlowRunColumn columnFor(String columnQualifier) {
 
     // Match column based on value, assume column family matches.
-    for (AppToFlowColumn ec : AppToFlowColumn.values()) {
+    for (FlowRunColumn ec : FlowRunColumn.values()) {
       // Find a match based only on name.
       if (ec.getColumnQualifier().equals(columnQualifier)) {
         return ec;
@@ -101,20 +131,22 @@ public enum AppToFlowColumn implements Column<AppToFlowTable> {
   }
 
   /**
-   * Retrieve an {@link AppToFlowColumn} given a name, or null if there is no
+   * Retrieve an {@link FlowRunColumn} given a name, or null if there is no
    * match. The following holds true: {@code columnFor(a,x) == columnFor(b,y)}
    * if and only if {@code a.equals(b) & x.equals(y)} or
    * {@code (x == y == null)}
    *
-   * @param columnFamily The columnFamily for which to retrieve the column.
-   * @param name Name of the column to retrieve
-   * @return the corresponding {@link AppToFlowColumn} or null if both arguments
+   * @param columnFamily
+   *          The columnFamily for which to retrieve the column.
+   * @param name
+   *          Name of the column to retrieve
+   * @return the corresponding {@link FlowRunColumn} or null if both arguments
    *         don't match.
    */
-  public static final AppToFlowColumn columnFor(
-      AppToFlowColumnFamily columnFamily, String name) {
+  public static final FlowRunColumn columnFor(FlowRunColumnFamily columnFamily,
+      String name) {
 
-    for (AppToFlowColumn ec : AppToFlowColumn.values()) {
+    for (FlowRunColumn ec : FlowRunColumn.values()) {
       // Find a match based column family and on name.
       if (ec.columnFamily.equals(columnFamily)
           && ec.getColumnQualifier().equals(name)) {
@@ -125,4 +157,5 @@ public enum AppToFlowColumn implements Column<AppToFlowTable> {
     // Default to null
     return null;
   }
+
 }
