ea6b183a1a649ad2874050ade8856286728c654c
Tsuyoshi Ozawa <ozawa@apache.org>
2015-10-27 10:57:45 +0900

    HADOOP-12457. [JDK8] Fix a failure of compiling common by javadoc. Contributed by Akira AJISAKA.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/conf/Configuration.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/conf/Configuration.java
@@ -179,7 +179,7 @@ import com.google.common.base.Preconditions;
  * <p>When <tt>conf.get("otherdir")</tt> is called, then <tt>${<i>env.BASE_DIR</i>}</tt>
  * will be resolved to the value of the <tt>${<i>BASE_DIR</i>}</tt> environment variable.
  * It supports <tt>${<i>env.NAME:-default</i>}</tt> and <tt>${<i>env.NAME-default</i>}</tt> notations.
- * The former is resolved to “default” if <tt>${<i>NAME</i>}</tt> environment variable is undefined
+ * The former is resolved to "default" if <tt>${<i>NAME</i>}</tt> environment variable is undefined
  * or its value is empty.
  * The latter behaves the same way only if <tt>${<i>NAME</i>}</tt> is undefined.
  * <p>By default, warnings will be given to any deprecated configuration 
