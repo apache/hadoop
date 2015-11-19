bd166f0eed058795dd70b2ee019356c55422173c
Tsuyoshi Ozawa <ozawa@apache.org>
2015-11-19 17:26:23 +0900

    HADOOP-12582. Using BytesWritable's getLength() and getBytes() instead of get() and getSize(). Contributed by Akira AJISAKA.

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/io/file/tfile/KeySampler.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/io/file/tfile/KeySampler.java
@@ -45,9 +45,9 @@ class KeySampler {
   
   public void next(BytesWritable key) {
     key.setSize(Math.max(MIN_KEY_LEN, keyLenRNG.nextInt()));
-    random.nextBytes(key.get());
+    random.nextBytes(key.getBytes());
     int n = random.nextInt(max - min) + min;
-    byte[] b = key.get();
+    byte[] b = key.getBytes();
     b[0] = (byte) (n >> 24);
     b[1] = (byte) (n >> 16);
     b[2] = (byte) (n >> 8);
