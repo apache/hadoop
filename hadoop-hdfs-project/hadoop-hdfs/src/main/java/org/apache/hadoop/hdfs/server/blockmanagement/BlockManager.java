d2518fd5627220736e691b4a55638fc1f57d612e
Tsz-Wo Nicholas Sze <szetszwo@hortonworks.com>
2015-11-24 14:34:51 -0800

    HDFS-9434. Recommission a datanode with 500k blocks may pause NN for 30 seconds for printing info log messags.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManager.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/BlockManager.java
@@ -2853,11 +2853,11 @@ public class BlockManager {
     for(DatanodeStorageInfo storage : blocksMap.getStorages(block, State.NORMAL)) {
       final DatanodeDescriptor cur = storage.getDatanodeDescriptor();
       if (storage.areBlockContentsStale()) {
-        LOG.info("BLOCK* processOverReplicatedBlock: " +
-            "Postponing processing of over-replicated " +
-            block + " since storage + " + storage
-            + "datanode " + cur + " does not yet have up-to-date " +
-            "block information.");
+        if (LOG.isTraceEnabled()) {
+          LOG.trace("BLOCK* processOverReplicatedBlock: Postponing " + block
+              + " since storage " + storage
+              + " does not yet have up-to-date information.");
+        }
         postponeBlock(block);
         return;
       }
