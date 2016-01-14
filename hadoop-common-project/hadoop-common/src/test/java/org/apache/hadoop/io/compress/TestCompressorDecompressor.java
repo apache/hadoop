b3fa190297cf81d909adb2aeca2868b4fc77c9e9
Akira Ajisaka <aajisaka@apache.org>
2016-01-15 00:31:24 +0900

    HADOOP-12700. Remove unused import in TestCompressorDecompressor.java. Contributed by John Zhuge.
    (cherry picked from commit f977366d9522ee31080e16db90972729fcfe256f)
    (cherry picked from commit 722d9e569ee6891e71274e758474063f25f5babf)

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/io/compress/TestCompressorDecompressor.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/io/compress/TestCompressorDecompressor.java
@@ -17,7 +17,6 @@
  */
 package org.apache.hadoop.io.compress;
 
-import static org.junit.Assert.fail;
 import java.util.Random;
 import org.apache.hadoop.io.compress.CompressDecompressTester.CompressionTestStrategy;
 import org.apache.hadoop.io.compress.lz4.Lz4Compressor;
