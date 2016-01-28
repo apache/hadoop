ef343be82b4268ebd52f6a11e1a1ce53a5d232a9
Rohith Sharma K S <rohithsharmaks@apache.org>
2016-01-28 21:53:45 +0530

    YARN-4633. Fix random test failure in TestRMRestart#testRMRestartAfterPreemption. (Bibin A Chundatt via rohithsharmaks)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/TestRMRestart.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/TestRMRestart.java
@@ -2359,6 +2359,7 @@ public class TestRMRestart extends ParameterizedSchedulerTestBase {
       // kill app0-attempt
       cs.killPreemptedContainer(schedulerAppAttempt.getRMContainer(
           app0.getCurrentAppAttempt().getMasterContainer().getId()));
+      am0.waitForState(RMAppAttemptState.FAILED);
     }
     am0 = MockRM.launchAM(app0, rm1, nm1);
     am0.registerAppAttempt();
