a4492dc6ac67d1234853fb3731c6d1a7bce0e409
Kihwal Lee <kihwal@apache.org>
2016-02-17 09:18:55 -0600

    HDFS-8845. DiskChecker should not traverse the entire tree (Chang Li via Colin P. McCabe)
    (cherry picked from commit ec183faadcf7edaf432aca3b25d24215d505c2ec)
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/CHANGES.txt

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/BlockPoolSlice.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/BlockPoolSlice.java
@@ -301,7 +301,7 @@ class BlockPoolSlice {
   }
 
   void checkDirs() throws DiskErrorException {
-    DiskChecker.checkDirs(finalizedDir);
+    DiskChecker.checkDir(finalizedDir);
     DiskChecker.checkDir(tmpDir);
     DiskChecker.checkDir(rbwDir);
   }
