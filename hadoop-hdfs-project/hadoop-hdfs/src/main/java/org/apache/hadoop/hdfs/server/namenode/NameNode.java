c183f235f1ae94d5b890bf010f7c9b7fa8cf92d2
Akira Ajisaka <aajisaka@apache.org>
2016-02-19 01:35:06 +0900

    HDFS-6832. Fix the usage of 'hdfs namenode' command. Contributed by Manjunath Ballur.
    (cherry picked from commit e78d8e66f748c68d2d88ee748facbfa35817d5e3)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
@@ -282,8 +282,7 @@ public class NameNode implements NameNodeStatusMXBean {
       + StartupOption.BOOTSTRAPSTANDBY.getName() + "] | \n\t["
       + StartupOption.RECOVER.getName() + " [ "
       + StartupOption.FORCE.getName() + "] ] | \n\t["
-      + StartupOption.METADATAVERSION.getName() + " ] "
-      + " ]";
+      + StartupOption.METADATAVERSION.getName() + " ]";
 
   
   public long getProtocolVersion(String protocol, 
