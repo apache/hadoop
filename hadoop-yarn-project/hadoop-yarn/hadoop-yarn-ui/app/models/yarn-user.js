588dee0d8ff412ac9967997fed86701031d20164
Wangda Tan <wangda@apache.org>
2016-03-21 14:03:13 -0700

    YARN-4849. [YARN-3368] cleanup code base, integrate web UI related build to mvn, and fix licenses. (wangda)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-ui/app/models/yarn-user.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  queueName: DS.attr('string'),
-  usedMemoryMB: DS.attr('number'),
-  usedVCore: DS.attr('number')
-})
--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-ui/app/routes/yarn-apps.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model() {
-    var apps = this.store.findAll('yarnApp');
-    return apps;
-  }
-});
