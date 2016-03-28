cb795337772d559027887309f55347079127395f
Andrew Wang <wang@apache.org>
2016-03-28 14:13:48 -0700

    HADOOP-10965. Print fully qualified path in CommandWithDestination error messages. Contributed by John Zhuge.
    (cherry picked from commit 8bfaa80037365c0790083313a905d1e7d88b0682)
    (cherry picked from commit a61a90c5621f7cbc28d150aeaa1fbd92ad76e498)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/PathIOException.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/PathIOException.java
@@ -33,6 +33,7 @@ public class PathIOException extends IOException {
   // uris with no authority
   private String operation;
   private String path;
+  private String fullyQualifiedPath;
   private String targetPath;
 
   /**
@@ -68,6 +69,11 @@ public class PathIOException extends IOException {
     this.path = path;
   }
 
+  public PathIOException withFullyQualifiedPath(String fqPath) {
+    fullyQualifiedPath = fqPath;
+    return this;
+  }
+
   /** Format:
    * cmd: {operation} `path' {to `target'}: error string
    */
@@ -84,6 +90,9 @@ public class PathIOException extends IOException {
     message.append(": " + super.getMessage());
     if (getCause() != null) {
       message.append(": " + getCause().getMessage());
+    }
+    if (fullyQualifiedPath != null && !fullyQualifiedPath.equals(path)) {
+      message.append(": ").append(formatPath(fullyQualifiedPath));
     }
     return message.toString();
   }
