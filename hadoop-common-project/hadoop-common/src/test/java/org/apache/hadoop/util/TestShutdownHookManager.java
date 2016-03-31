aac4d65bf9c6d68f53610e5fe9997a391e3fa053
Xiaoyu Yao <xyao@apache.org>
2016-03-31 15:20:09 -0700

    HADOOP-12950. ShutdownHookManager should have a timeout for each of the Registered shutdown hook. Contributed by Xiaoyu Yao.

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/util/TestShutdownHookManager.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/util/TestShutdownHookManager.java
@@ -17,10 +17,19 @@
  */
 package org.apache.hadoop.util;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.slf4j.LoggerFactory;
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
 
 public class TestShutdownHookManager {
+  static final Logger LOG =
+      LoggerFactory.getLogger(TestShutdownHookManager.class.getName());
 
   @Test
   public void shutdownHookManager() {
@@ -30,18 +39,48 @@ public class TestShutdownHookManager {
     Runnable hook1 = new Runnable() {
       @Override
       public void run() {
+        LOG.info("Shutdown hook1 complete.");
       }
     };
     Runnable hook2 = new Runnable() {
       @Override
       public void run() {
+        LOG.info("Shutdown hook2 complete.");
+      }
+    };
+
+    Runnable hook3 = new Runnable() {
+      @Override
+      public void run() {
+        try {
+          sleep(3000);
+          LOG.info("Shutdown hook3 complete.");
+        } catch (InterruptedException ex) {
+          LOG.info("Shutdown hook3 interrupted exception:",
+              ExceptionUtils.getStackTrace(ex));
+          Assert.fail("Hook 3 should not timeout.");
+        }
+      }
+    };
+
+    Runnable hook4 = new Runnable() {
+      @Override
+      public void run() {
+        try {
+          sleep(3500);
+          LOG.info("Shutdown hook4 complete.");
+          Assert.fail("Hook 4 should timeout");
+        } catch (InterruptedException ex) {
+          LOG.info("Shutdown hook4 interrupted exception:",
+              ExceptionUtils.getStackTrace(ex));
+        }
       }
     };
 
     mgr.addShutdownHook(hook1, 0);
     Assert.assertTrue(mgr.hasShutdownHook(hook1));
     Assert.assertEquals(1, mgr.getShutdownHooksInOrder().size());
-    Assert.assertEquals(hook1, mgr.getShutdownHooksInOrder().get(0));
+    Assert.assertEquals(hook1, mgr.getShutdownHooksInOrder().get(0).getHook());
     mgr.removeShutdownHook(hook1);
     Assert.assertFalse(mgr.hasShutdownHook(hook1));
 
@@ -55,8 +94,20 @@ public class TestShutdownHookManager {
     Assert.assertTrue(mgr.hasShutdownHook(hook1));
     Assert.assertTrue(mgr.hasShutdownHook(hook2));
     Assert.assertEquals(2, mgr.getShutdownHooksInOrder().size());
-    Assert.assertEquals(hook2, mgr.getShutdownHooksInOrder().get(0));
-    Assert.assertEquals(hook1, mgr.getShutdownHooksInOrder().get(1));
+    Assert.assertEquals(hook2, mgr.getShutdownHooksInOrder().get(0).getHook());
+    Assert.assertEquals(hook1, mgr.getShutdownHooksInOrder().get(1).getHook());
 
+    // Test hook finish without timeout
+    mgr.addShutdownHook(hook3, 2, 4, TimeUnit.SECONDS);
+    Assert.assertTrue(mgr.hasShutdownHook(hook3));
+    Assert.assertEquals(hook3, mgr.getShutdownHooksInOrder().get(0).getHook());
+    Assert.assertEquals(4, mgr.getShutdownHooksInOrder().get(0).getTimeout());
+
+    // Test hook finish with timeout
+    mgr.addShutdownHook(hook4, 3, 2, TimeUnit.SECONDS);
+    Assert.assertTrue(mgr.hasShutdownHook(hook4));
+    Assert.assertEquals(hook4, mgr.getShutdownHooksInOrder().get(0).getHook());
+    Assert.assertEquals(2, mgr.getShutdownHooksInOrder().get(0).getTimeout());
+    LOG.info("Shutdown starts here");
   }
 }
