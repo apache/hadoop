ef0870ad038a1b72ea14ce550e34139b81eb901b
Masatake Iwasaki <iwasakims@apache.org>
2016-04-29 20:25:52 +0900

    HADOOP-12378. Fix findbugs warnings in hadoop-tools module. Contributed by Akira AJISAKA.

--- hadoop-tools/hadoop-ant/src/main/java/org/apache/hadoop/ant/condition/DfsZeroLen.java
+++ hadoop-tools/hadoop-ant/src/main/java/org/apache/hadoop/ant/condition/DfsZeroLen.java
@@ -19,6 +19,6 @@
 package org.apache.hadoop.ant.condition;
 
 public class DfsZeroLen extends DfsBaseConditional {
-  protected final char flag = 'z';
+  protected static final char flag = 'z';
   protected char getFlag() { return flag; }
 }
