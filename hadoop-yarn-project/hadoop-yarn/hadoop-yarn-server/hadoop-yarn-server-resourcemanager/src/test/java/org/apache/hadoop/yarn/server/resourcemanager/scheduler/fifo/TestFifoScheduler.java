1217c8f6b49e3afd8ca008ffddcf6615f0accfc5
Eric Payne <epayne@apache.org>
2016-05-16 20:27:07 +0000

    YARN-5069. TestFifoScheduler.testResourceOverCommit race condition. Contributed by Eric Badger.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fifo/TestFifoScheduler.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fifo/TestFifoScheduler.java
@@ -1140,7 +1140,8 @@ public class TestFifoScheduler {
     waitCount = 0;
     while (waitCount++ != 20) {
       report_nm1 = rm.getResourceScheduler().getNodeReport(nm1.getNodeId());
-      if (report_nm1.getAvailableResource().getMemory() != 0) {
+      if (null != report_nm1 &&
+          report_nm1.getAvailableResource().getMemory() != 0) {
         break;
       }
       LOG.info("Waiting for RMNodeResourceUpdateEvent to be handled... Tried "
