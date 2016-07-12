a290a98b6ab2424ae9b7faab0ce9496d09ca46f3
Lei Xu <lei@apache.org>
2016-07-12 16:49:32 -0700

    HADOOP-13315. FileContext#umask is not initialized properly. (John Zhuge via lei)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/FileContext.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/FileContext.java
@@ -228,7 +228,7 @@ public class FileContext {
   private FileContext(final AbstractFileSystem defFs,
     final FsPermission theUmask, final Configuration aConf) {
     defaultFS = defFs;
-    umask = FsPermission.getUMask(aConf);
+    umask = theUmask;
     conf = aConf;
     tracer = FsTracer.get(aConf);
     try {
