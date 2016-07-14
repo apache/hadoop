356c09a50908b450465497f276746002c1b43278
Rohith Sharma K S <rohithsharmaks@apache.org>
2016-07-14 11:48:10 +0530

    YARN-5299. Log Docker run command when container fails. Contributed by Varun Vasudev.
    (cherry picked from commit dbe97aa768e2987209811c407969fea47641418c)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/linux/runtime/DockerLinuxContainerRuntime.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/linux/runtime/DockerLinuxContainerRuntime.java
@@ -416,6 +416,10 @@ public class DockerLinuxContainerRuntime implements LinuxContainerRuntime {
     if (tcCommandFile != null) {
       launchOp.appendArgs(tcCommandFile);
     }
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("Launching container with cmd: " + runCommand
+          .getCommandWithArguments());
+    }
 
     try {
       privilegedOperationExecutor.executePrivilegedOperation(null,
@@ -423,6 +427,7 @@ public class DockerLinuxContainerRuntime implements LinuxContainerRuntime {
           false, false);
     } catch (PrivilegedOperationException e) {
       LOG.warn("Launch container failed. Exception: ", e);
+      LOG.info("Docker command used: " + runCommand.getCommandWithArguments());
 
       throw new ContainerExecutionException("Launch container failed", e
           .getExitCode(), e.getOutput(), e.getErrorOutput());
