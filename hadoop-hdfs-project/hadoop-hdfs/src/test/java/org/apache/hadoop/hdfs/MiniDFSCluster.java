1222889f1a3b45df30c48a80d8c2bec7ad45ee43
Akira Ajisaka <aajisaka@apache.org>
2016-07-21 11:25:28 -0700

    HDFS-10287. MiniDFSCluster should implement AutoCloseable. Contributed by Andras Bokor.
    (cherry picked from commit fcde6940e0cbdedb1105007e4857137ecdfa1284)

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/MiniDFSCluster.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/MiniDFSCluster.java
@@ -136,7 +136,7 @@ import com.google.common.collect.Sets;
  */
 @InterfaceAudience.LimitedPrivate({"HBase", "HDFS", "Hive", "MapReduce", "Pig"})
 @InterfaceStability.Unstable
-public class MiniDFSCluster {
+public class MiniDFSCluster implements AutoCloseable {
 
   private static final String NAMESERVICE_ID_PREFIX = "nameserviceId";
   private static final Log LOG = LogFactory.getLog(MiniDFSCluster.class);
@@ -2970,5 +2970,10 @@ public class MiniDFSCluster {
     } finally {
       writer.close();
     }
+  }
+
+  @Override
+  public void close() {
+    shutdown();
   }
 }
