99380102642da6ced784ed705a5c21d3e7be5c4c
Jason Lowe <jlowe@apache.org>
2016-07-21 18:17:29 +0000

    YARN-3707. RM Web UI queue filter doesn't work. Contributed by Wangda Tan (cherry picked from commit 446d51591e6e99cc60a85c4b9fbac379a8caa49d) (cherry picked from commit 03af057a477b2a31b66831d66f8a38cff42f2201)
    Conflicts:
    
    	hadoop-yarn-project/CHANGES.txt

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/webapp/CapacitySchedulerPage.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/webapp/CapacitySchedulerPage.java
@@ -303,7 +303,7 @@ class CapacitySchedulerPage extends RmView {
             a(_Q).$style(width(Q_MAX_WIDTH)).
               span().$style(join(width(used), ";left:0%;",
                   used > 1 ? Q_OVER : Q_UNDER))._(".")._().
-              span(".q", "root")._().
+              span(".q", "Queue: root")._().
             span().$class("qstats").$style(left(Q_STATS_POS)).
               _(join(percent(used), " used"))._().
             _(QueueBlock.class)._();
@@ -377,8 +377,8 @@ class CapacitySchedulerPage extends RmView {
           "  });",
           "  $('#cs').bind('select_node.jstree', function(e, data) {",
           "    var q = $('.q', data.rslt.obj).first().text();",
-          "    if (q == 'root') q = '';",
-          "    else q = '^' + q.substr(q.lastIndexOf('.') + 1) + '$';",
+          "    if (q == 'Queue: root') q = '';",
+          "    else q = '^' + q.substr(q.lastIndexOf(':') + 2) + '$';",
           "    $('#apps').dataTable().fnFilter(q, 4, true);",
           "  });",
           "  $('#cs').show();",
