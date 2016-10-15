6516f6e5e0561a96d3d302bcefcc5e82e517af3d
Sangjin Lee <sjlee@apache.org>
2016-10-15 13:58:40 -0700

    YARN-5699. Retrospect yarn entity fields which are publishing in events info fields. Contributed by Rohith Sharma K S.
    (cherry picked from commit 787d5942003a5441ba455a79fa08da28f60d5777)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-common/src/main/java/org/apache/hadoop/yarn/server/metrics/AppAttemptMetricsConstants.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-common/src/main/java/org/apache/hadoop/yarn/server/metrics/AppAttemptMetricsConstants.java
@@ -37,28 +37,28 @@ public class AppAttemptMetricsConstants {
   public static final String PARENT_PRIMARY_FILTER =
       "YARN_APPLICATION_ATTEMPT_PARENT";
       
-  public static final String TRACKING_URL_EVENT_INFO =
+  public static final String TRACKING_URL_INFO =
       "YARN_APPLICATION_ATTEMPT_TRACKING_URL";
 
-  public static final String ORIGINAL_TRACKING_URL_EVENT_INFO =
+  public static final String ORIGINAL_TRACKING_URL_INFO =
       "YARN_APPLICATION_ATTEMPT_ORIGINAL_TRACKING_URL";
 
-  public static final String HOST_EVENT_INFO =
+  public static final String HOST_INFO =
       "YARN_APPLICATION_ATTEMPT_HOST";
 
-  public static final String RPC_PORT_EVENT_INFO =
+  public static final String RPC_PORT_INFO =
       "YARN_APPLICATION_ATTEMPT_RPC_PORT";
 
-  public static final String MASTER_CONTAINER_EVENT_INFO =
+  public static final String MASTER_CONTAINER_INFO =
       "YARN_APPLICATION_ATTEMPT_MASTER_CONTAINER";
 
-  public static final String DIAGNOSTICS_INFO_EVENT_INFO =
+  public static final String DIAGNOSTICS_INFO =
       "YARN_APPLICATION_ATTEMPT_DIAGNOSTICS_INFO";
 
-  public static final String FINAL_STATUS_EVENT_INFO =
+  public static final String FINAL_STATUS_INFO =
       "YARN_APPLICATION_ATTEMPT_FINAL_STATUS";
 
-  public static final String STATE_EVENT_INFO =
+  public static final String STATE_INFO =
       "YARN_APPLICATION_ATTEMPT_STATE";
 
 }
