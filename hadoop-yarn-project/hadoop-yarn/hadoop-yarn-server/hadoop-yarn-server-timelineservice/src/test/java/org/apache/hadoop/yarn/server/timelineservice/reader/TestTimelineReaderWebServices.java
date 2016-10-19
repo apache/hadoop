e9c4616b5e47e9c616799abc532269572ab24e6e
Sangjin Lee <sjlee@apache.org>
2016-10-19 09:45:23 -0700

    YARN-5561. [Atsv2] : Support for ability to retrieve apps/app-attempt/containers and entities via REST. Contributed by Rohith Sharma K S.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timelineservice/src/test/java/org/apache/hadoop/yarn/server/timelineservice/reader/TestTimelineReaderWebServices.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timelineservice/src/test/java/org/apache/hadoop/yarn/server/timelineservice/reader/TestTimelineReaderWebServices.java
@@ -37,6 +37,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.yarn.api.records.timeline.TimelineAbout;
 import org.apache.hadoop.yarn.api.records.timelineservice.TimelineEntity;
+import org.apache.hadoop.yarn.api.records.timelineservice.TimelineEntityType;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.server.timelineservice.storage.FileSystemTimelineReaderImpl;
 import org.apache.hadoop.yarn.server.timelineservice.storage.TestFileSystemTimelineReaderImpl;
@@ -563,6 +564,190 @@ public class TestTimelineReaderWebServices {
       uri = URI.create("http://localhost:" + serverPort + "/ws/v2/timeline/" +
           "clusters/cluster1/apps/app1/entities/app?limit=#$561av");
       verifyHttpResponse(client, uri, Status.BAD_REQUEST);
+    } finally {
+      client.destroy();
+    }
+  }
+
+  @Test
+  public void testGetAppAttempts() throws Exception {
+    Client client = createClient();
+    try {
+      URI uri = URI.create("http://localhost:" + serverPort + "/ws/v2/"
+          + "timeline/clusters/cluster1/apps/app1/"
+          + "entities/YARN_APPLICATION_ATTEMPT");
+      ClientResponse resp = getResponse(client, uri);
+      Set<TimelineEntity> entities =
+          resp.getEntity(new GenericType<Set<TimelineEntity>>() {
+          });
+      assertEquals(MediaType.APPLICATION_JSON_TYPE, resp.getType());
+      assertNotNull(entities);
+      int totalEntities = entities.size();
+      assertEquals(2, totalEntities);
+      assertTrue(
+          "Entity with app-attempt-2 should have been present in response.",
+          entities.contains(
+              newEntity(TimelineEntityType.YARN_APPLICATION_ATTEMPT.toString(),
+                  "app-attempt-1")));
+      assertTrue(
+          "Entity with app-attempt-2 should have been present in response.",
+          entities.contains(
+              newEntity(TimelineEntityType.YARN_APPLICATION_ATTEMPT.toString(),
+                  "app-attempt-2")));
+
+      uri = URI.create("http://localhost:" + serverPort + "/ws/v2/"
+          + "timeline/clusters/cluster1/apps/app1/appattempts");
+      resp = getResponse(client, uri);
+      entities = resp.getEntity(new GenericType<Set<TimelineEntity>>() {
+      });
+      assertEquals(MediaType.APPLICATION_JSON_TYPE, resp.getType());
+      assertNotNull(entities);
+      int retrievedEntity = entities.size();
+      assertEquals(2, retrievedEntity);
+      assertTrue(
+          "Entity with app-attempt-2 should have been present in response.",
+          entities.contains(
+              newEntity(TimelineEntityType.YARN_APPLICATION_ATTEMPT.toString(),
+                  "app-attempt-1")));
+      assertTrue(
+          "Entity with app-attempt-2 should have been present in response.",
+          entities.contains(
+              newEntity(TimelineEntityType.YARN_APPLICATION_ATTEMPT.toString(),
+                  "app-attempt-2")));
+
+      assertEquals(totalEntities, retrievedEntity);
+
+    } finally {
+      client.destroy();
+    }
+  }
+
+  @Test
+  public void testGetAppAttempt() throws Exception {
+    Client client = createClient();
+    try {
+      URI uri = URI.create("http://localhost:" + serverPort + "/ws/v2/"
+          + "timeline/clusters/cluster1/apps/app1/entities/"
+          + "YARN_APPLICATION_ATTEMPT/app-attempt-1");
+      ClientResponse resp = getResponse(client, uri);
+      TimelineEntity entities1 =
+          resp.getEntity(new GenericType<TimelineEntity>() {
+          });
+      assertEquals(MediaType.APPLICATION_JSON_TYPE, resp.getType());
+      assertNotNull(entities1);
+
+      uri = URI.create("http://localhost:" + serverPort + "/ws/v2/"
+          + "timeline/clusters/cluster1/apps/app1/appattempts/app-attempt-1");
+      resp = getResponse(client, uri);
+      TimelineEntity entities2 =
+          resp.getEntity(new GenericType<TimelineEntity>() {
+          });
+      assertEquals(MediaType.APPLICATION_JSON_TYPE, resp.getType());
+      assertNotNull(entities2);
+
+      assertEquals(entities1, entities2);
+
+    } finally {
+      client.destroy();
+    }
+  }
+
+  @Test
+  public void testGetContainers() throws Exception {
+    Client client = createClient();
+    try {
+      // total 3 containers in a application.
+      URI uri = URI.create("http://localhost:" + serverPort + "/ws/v2/"
+          + "timeline/clusters/cluster1/apps/app1/entities/YARN_CONTAINER");
+      ClientResponse resp = getResponse(client, uri);
+      Set<TimelineEntity> entities =
+          resp.getEntity(new GenericType<Set<TimelineEntity>>() {
+          });
+      assertEquals(MediaType.APPLICATION_JSON_TYPE, resp.getType());
+      assertNotNull(entities);
+      int totalEntities = entities.size();
+      assertEquals(3, totalEntities);
+      assertTrue(
+          "Entity with container_1_1 should have been present in response.",
+          entities.contains(newEntity(
+              TimelineEntityType.YARN_CONTAINER.toString(), "container_1_1")));
+      assertTrue(
+          "Entity with container_2_1 should have been present in response.",
+          entities.contains(newEntity(
+              TimelineEntityType.YARN_CONTAINER.toString(), "container_2_1")));
+      assertTrue(
+          "Entity with container_2_2 should have been present in response.",
+          entities.contains(newEntity(
+              TimelineEntityType.YARN_CONTAINER.toString(), "container_2_2")));
+
+      // for app-attempt1 1 container has run
+      uri = URI.create("http://localhost:" + serverPort + "/ws/v2/"
+          + "timeline/clusters/cluster1/apps/app1/"
+          + "appattempts/app-attempt-1/containers");
+      resp = getResponse(client, uri);
+      entities = resp.getEntity(new GenericType<Set<TimelineEntity>>() {
+      });
+      assertEquals(MediaType.APPLICATION_JSON_TYPE, resp.getType());
+      assertNotNull(entities);
+      int retrievedEntity = entities.size();
+      assertEquals(1, retrievedEntity);
+      assertTrue(
+          "Entity with container_1_1 should have been present in response.",
+          entities.contains(newEntity(
+              TimelineEntityType.YARN_CONTAINER.toString(), "container_1_1")));
+
+      // for app-attempt2 2 containers has run
+      uri = URI.create("http://localhost:" + serverPort + "/ws/v2/"
+          + "timeline/clusters/cluster1/apps/app1/"
+          + "appattempts/app-attempt-2/containers");
+      resp = getResponse(client, uri);
+      entities = resp.getEntity(new GenericType<Set<TimelineEntity>>() {
+      });
+      assertEquals(MediaType.APPLICATION_JSON_TYPE, resp.getType());
+      assertNotNull(entities);
+      retrievedEntity += entities.size();
+      assertEquals(2, entities.size());
+      assertTrue(
+          "Entity with container_2_1 should have been present in response.",
+          entities.contains(newEntity(
+              TimelineEntityType.YARN_CONTAINER.toString(), "container_2_1")));
+      assertTrue(
+          "Entity with container_2_2 should have been present in response.",
+          entities.contains(newEntity(
+              TimelineEntityType.YARN_CONTAINER.toString(), "container_2_2")));
+
+      assertEquals(totalEntities, retrievedEntity);
+
+    } finally {
+      client.destroy();
+    }
+  }
+
+  @Test
+  public void testGetContainer() throws Exception {
+    Client client = createClient();
+    try {
+      URI uri = URI.create("http://localhost:" + serverPort + "/ws/v2/"
+          + "timeline/clusters/cluster1/apps/app1/"
+          + "entities/YARN_CONTAINER/container_2_2");
+      ClientResponse resp = getResponse(client, uri);
+      TimelineEntity entities1 =
+          resp.getEntity(new GenericType<TimelineEntity>() {
+          });
+      assertEquals(MediaType.APPLICATION_JSON_TYPE, resp.getType());
+      assertNotNull(entities1);
+
+      uri = URI.create("http://localhost:" + serverPort + "/ws/v2/"
+          + "timeline/clusters/cluster1/apps/app1/containers/container_2_2");
+      resp = getResponse(client, uri);
+      TimelineEntity entities2 =
+          resp.getEntity(new GenericType<TimelineEntity>() {
+          });
+      assertEquals(MediaType.APPLICATION_JSON_TYPE, resp.getType());
+      assertNotNull(entities2);
+
+      assertEquals(entities1, entities2);
+
     } finally {
       client.destroy();
     }
