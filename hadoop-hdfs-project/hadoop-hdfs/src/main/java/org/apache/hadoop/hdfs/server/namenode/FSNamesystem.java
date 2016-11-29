92ee5e2077c4a1b2d2795dc4f0afbf81dea374f3
Robert Kanter <rkanter@cloudera.com>
2016-11-28 18:08:09 -0800

    HADOOP-13838. KMSTokenRenewer should close providers (xiaochen via rkanter)
    (cherry picked from commit 47ca9e26fba4a639e43bee5bfc001ffc4b42330d)
    (cherry picked from commit 285edf691db42a3e17138d960f8560c62d8d0dde)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -5253,6 +5253,13 @@ public class FSNamesystem implements Namesystem, FSNamesystemMBean,
     if (blockManager != null) {
       blockManager.shutdown();
     }
+    if (provider != null) {
+      try {
+        provider.close();
+      } catch (IOException e) {
+        LOG.error("Failed to close provider.", e);
+      }
+    }
   }
 
   @Override // FSNamesystemMBean
