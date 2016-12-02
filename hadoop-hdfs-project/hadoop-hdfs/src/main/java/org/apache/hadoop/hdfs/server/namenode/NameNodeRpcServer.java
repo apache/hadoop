5025a898e13b0d442aa8c7a3433a90367e013a42
Akira Ajisaka <aajisaka@apache.org>
2016-12-02 11:34:05 +0900

    HDFS-11180. Intermittent deadlock in NameNode when failover happens.
    (cherry picked from commit 5a7941a4fc193259ab4a306f7fe4f68bf101d0e0)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNodeRpcServer.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNodeRpcServer.java
@@ -1198,7 +1198,7 @@ public class NameNodeRpcServer implements NamenodeProtocols {
     checkNNStartup();
     namesystem.checkOperation(OperationCategory.UNCHECKED);
     namesystem.checkSuperuserPrivilege();
-    return namesystem.getFSImage().getLastAppliedOrWrittenTxId();
+    return namesystem.getFSImage().getCorrectLastAppliedOrWrittenTxId();
   }
   
   @Override // NamenodeProtocol
