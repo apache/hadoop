1a7b2ab9bb596dcb26c264f354f42cc0aa8a1bfe
Andrew Wang <wang@apache.org>
2016-12-05 23:09:30 -0800

    Revert "HDFS-11156. Add new op GETFILEBLOCKLOCATIONS to WebHDFS REST API. Contributed by Weiwei Yang"
    This reverts commit 3b80424d4f2ff073310c9628f4645aab6cfb6d4d.

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/web/WebHdfsFileSystem.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/web/WebHdfsFileSystem.java
@@ -1573,20 +1573,13 @@ public class WebHdfsFileSystem extends FileSystem
     statistics.incrementReadOps(1);
     storageStatistics.incrementOpCounter(OpType.GET_FILE_BLOCK_LOCATIONS);
 
-    final HttpOpParam.Op op = GetOpParam.Op.GETFILEBLOCKLOCATIONS;
+    final HttpOpParam.Op op = GetOpParam.Op.GET_BLOCK_LOCATIONS;
     return new FsPathResponseRunner<BlockLocation[]>(op, p,
         new OffsetParam(offset), new LengthParam(length)) {
       @Override
-      @SuppressWarnings("unchecked")
       BlockLocation[] decodeResponse(Map<?,?> json) throws IOException {
-        List<?> list = JsonUtilClient.getList(json, "BlockLocations");
-        BlockLocation[] locations = new BlockLocation[list.size()];
-        for(int i=0; i<locations.length; i++) {
-          BlockLocation bl = JsonUtilClient.
-              toBlockLocation((Map<String, Object>) list.get(i));
-          locations[i] = bl;
-        }
-        return locations;
+        return DFSUtilClient.locatedBlocks2Locations(
+            JsonUtilClient.toLocatedBlocks(json));
       }
     }.run();
   }
