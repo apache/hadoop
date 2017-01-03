b31e1951e044b2c6f6e88a007a8c175941ddd674
Brahma Reddy Battula <brahma@apache.org>
2017-01-03 19:19:29 +0530

    Revert "HDFS-11280. Allow WebHDFS to reuse HTTP connections to NN. Contributed by Zheng Shao."
    This reverts commit b811a1c14d00ab236158ab75fad1fe41364045a4.

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/web/WebHdfsFileSystem.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/web/WebHdfsFileSystem.java
@@ -659,9 +659,7 @@ public class WebHdfsFileSystem extends FileSystem
           url = new URL(conn.getHeaderField("Location"));
           redirectHost = url.getHost() + ":" + url.getPort();
         } finally {
-          // Don't call conn.disconnect() to allow connection reuse
-          // See http://tinyurl.com/java7-http-keepalive
-          conn.getInputStream().close();
+          conn.disconnect();
         }
       }
       try {
@@ -893,9 +891,7 @@ public class WebHdfsFileSystem extends FileSystem
         LOG.debug("Response decoding failure.", e);
         throw ioe;
       } finally {
-        // Don't call conn.disconnect() to allow connection reuse
-        // See http://tinyurl.com/java7-http-keepalive
-        conn.getInputStream().close();
+        conn.disconnect();
       }
     }
 
@@ -942,9 +938,6 @@ public class WebHdfsFileSystem extends FileSystem
             try {
               validateResponse(op, conn, true);
             } finally {
-              // This is a connection to DataNode.  Let's disconnect since
-              // there is little chance that the connection will be reused
-              // any time soonl
               conn.disconnect();
             }
           }
