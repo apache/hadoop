e792f1671df5c5a6f57eec9a399cf2d4dbb1ee31
John Zhuge <jzhuge@apache.org>
2017-03-20 21:29:17 -0700

    HDFS-11132. Allow AccessControlException in contract tests when getFileStatus on subdirectory of existing files. Contributed by Vishwajeet Dusane
    (cherry picked from commit 19f373a46b2abb7a575f7884a9c7443b8ed67cd3)
    
    Conflicts:
    	hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/FileSystemContractBaseTest.java

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/FileSystemContractBaseTest.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/FileSystemContractBaseTest.java
@@ -28,6 +28,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.security.AccessControlException;
 
 /**
  * <p>
@@ -137,8 +138,14 @@ public abstract class FileSystemContractBaseTest extends TestCase {
     } catch (IOException e) {
       // expected
     }
-    assertFalse(fs.exists(testSubDir));
-    
+
+    try {
+      assertFalse(fs.exists(testSubDir));
+    } catch (AccessControlException e) {
+      // Expected : HDFS-11132 Checks on paths under file may be rejected by
+      // file missing execute permission.
+    }
+
     Path testDeepSubDir = path("/test/hadoop/file/deep/sub/dir");
     try {
       fs.mkdirs(testDeepSubDir);
@@ -146,8 +153,14 @@ public abstract class FileSystemContractBaseTest extends TestCase {
     } catch (IOException e) {
       // expected
     }
-    assertFalse(fs.exists(testDeepSubDir));
-    
+
+    try {
+      assertFalse(fs.exists(testDeepSubDir));
+    } catch (AccessControlException e) {
+      // Expected : HDFS-11132 Checks on paths under file may be rejected by
+      // file missing execute permission.
+    }
+
   }
 
   public void testMkdirsWithUmask() throws Exception {
