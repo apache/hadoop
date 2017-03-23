a5a4867f3b193a137a6260d539da7e21f02ffab3
Steve Loughran <stevel@apache.org>
2017-03-23 12:54:54 +0000

    HADOOP-14120 needless S3AFileSystem.setOptionalPutRequestParameters in S3ABlockOutputStream putObject(). Contributed by Yuanbo Liu
    (cherry picked from commit 20878d052cebc715c2494a97362fdff08885a77f)

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java
@@ -1870,7 +1870,7 @@ public class S3AFileSystem extends FileSystem {
     }
   }
 
-  protected void setOptionalPutRequestParameters(PutObjectRequest request) {
+  private void setOptionalPutRequestParameters(PutObjectRequest request) {
     switch (serverSideEncryptionAlgorithm) {
     case SSE_KMS:
       request.setSSEAwsKeyManagementParams(generateSSEAwsKeyParams());
