6d53224afcfabcc6ed25a5e8d514bf230498be98
Subru Krishnan <subru@apache.org>
2017-05-01 18:48:36 -0700

    YARN-5331. Extend RLESparseResourceAllocation with period for supporting recurring reservations in YARN ReservationSystem. (Sangeetha Abdu Jyothi via Subru).
    (cherry picked from commit 6bf42e48ef658bf6dd86ebd706562ce7cc06216a)

---
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/reservation/PeriodicRLESparseResourceAllocation.java
@@ -0,0 +1,167 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.yarn.server.resourcemanager.reservation;
+
+import org.apache.hadoop.yarn.api.records.Resource;
+import org.apache.hadoop.yarn.util.resource.Resources;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This data structure stores a periodic RLESparseResourceAllocation.
+ * Default period is 1 day (86400000ms).
+ */
+public class PeriodicRLESparseResourceAllocation extends
+    RLESparseResourceAllocation {
+
+  // Log
+  private static final Logger LOG = LoggerFactory
+      .getLogger(PeriodicRLESparseResourceAllocation.class);
+
+  private long timePeriod;
+
+  /**
+   * Constructor.
+   *
+   * @param rleVector {@link RLESparseResourceAllocation} with the run-length
+              encoded data.
+   * @param timePeriod Time period in milliseconds.
+   */
+  public PeriodicRLESparseResourceAllocation(
+      RLESparseResourceAllocation rleVector, Long timePeriod) {
+    super(rleVector.getCumulative(), rleVector.getResourceCalculator());
+    this.timePeriod = timePeriod;
+  }
+
+  /**
+   * Constructor. Default time period set to 1 day.
+   *
+   * @param rleVector {@link RLESparseResourceAllocation} with the run-length
+              encoded data.
+   */
+  public PeriodicRLESparseResourceAllocation(
+      RLESparseResourceAllocation rleVector) {
+    this(rleVector, 86400000L);
+  }
+
+  /**
+   * Get capacity at time based on periodic repetition.
+   *
+   * @param tick UTC time for which the allocated {@link Resource} is queried.
+   * @return {@link Resource} allocated at specified time
+   */
+  public Resource getCapacityAtTime(long tick) {
+    long convertedTime = (tick % timePeriod);
+    return super.getCapacityAtTime(convertedTime);
+  }
+
+  /**
+   * Add resource for the specified interval. This function will be used by
+   * {@link InMemoryPlan} while placing reservations between 0 and timePeriod.
+   * The interval may include 0, but the end time must be strictly less than
+   * timePeriod.
+   *
+   * @param interval {@link ReservationInterval} to which the specified
+   *          resource is to be added.
+   * @param resource {@link Resource} to be added to the interval specified.
+   * @return true if addition is successful, false otherwise
+   */
+  public boolean addInterval(ReservationInterval interval,
+      Resource resource) {
+    long startTime = interval.getStartTime();
+    long endTime = interval.getEndTime();
+    if (startTime >= 0 && endTime > startTime && endTime <= timePeriod) {
+      return super.addInterval(interval, resource);
+    } else {
+      LOG.info("Cannot set capacity beyond end time: " + timePeriod);
+      return false;
+    }
+  }
+
+   /**
+   * Removes a resource for the specified interval.
+   *
+   * @param interval the {@link ReservationInterval} for which the resource is
+   *          to be removed.
+   * @param resource the {@link Resource} to be removed.
+   * @return true if removal is successful, false otherwise
+   */
+  public boolean removeInterval(
+      ReservationInterval interval, Resource resource) {
+    long startTime = interval.getStartTime();
+    long endTime = interval.getEndTime();
+    // If the resource to be subtracted is less than the minimum resource in
+    // the range, abort removal to avoid negative capacity.
+    if (!Resources.fitsIn(
+        resource, super.getMinimumCapacityInInterval(interval))) {
+      LOG.info("Request to remove more resources than what is available");
+      return false;
+    }
+    if (startTime >= 0 && endTime > startTime && endTime <= timePeriod) {
+      return super.removeInterval(interval, resource);
+    } else {
+      LOG.info("Interval extends beyond the end time " + timePeriod);
+      return false;
+    }
+  }
+
+  /**
+   * Get maximum capacity at periodic offsets from the specified time.
+   *
+   * @param tick UTC time base from which offsets are specified for finding
+   *          the maximum capacity.
+   * @param period periodic offset at which capacities are evaluted.
+   * @return the maximum {@link Resource} across the specified time instants.
+   * @return true if removal is successful, false otherwise
+   */
+  public Resource getMaximumPeriodicCapacity(long tick, long period) {
+    Resource maxResource;
+    if (period < timePeriod) {
+      maxResource =
+          super.getMaximumPeriodicCapacity(tick % timePeriod, period);
+    } else {
+      // if period is greater than the length of PeriodicRLESparseAllocation,
+      // only a single value exists in this interval.
+      maxResource = super.getCapacityAtTime(tick % timePeriod);
+    }
+    return maxResource;
+  }
+
+  /**
+   * Get time period of PeriodicRLESparseResourceAllocation.
+   *
+   * @return timePeriod time period represented in ms.
+   */
+  public long getTimePeriod() {
+    return this.timePeriod;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder ret = new StringBuilder();
+    ret.append("Period: ").append(timePeriod).append("\n")
+        .append(super.toString());
+    if (super.isEmpty()) {
+      ret.append(" no allocations\n");
+    }
+    return ret.toString();
+  }
+
+}
