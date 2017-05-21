25e6378f14a9f8f04b8a65afa4ebb902d8405464
John Zhuge <jzhuge@apache.org>
2017-05-21 00:18:35 -0700

    HDFS-11861. ipc.Client.Connection#sendRpcRequest should log request name. Contributed by John Zhuge.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Client.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Client.java
@@ -1109,7 +1109,8 @@ public class Client implements AutoCloseable {
                   return;
                 }
                 if (LOG.isDebugEnabled()) {
-                  LOG.debug(getName() + " sending #" + call.id);
+                  LOG.debug(getName() + " sending #" + call.id
+                      + " " + call.rpcRequest);
                 }
                 // RpcRequestHeader + RpcRequest
                 ipcStreams.sendRequest(buf.toByteArray());
