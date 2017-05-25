6c50341ea88320dccdc861ae4f281adb67aec05c
Wei-Chiu Chuang <weichiu@apache.org>
2017-05-24 18:11:42 -0700

    Revert "HDFS-11515. -du throws ConcurrentModificationException. Contributed by Istvan Fajth, Wei-Chiu Chuang."
    This reverts commit f9d3bb3b8335889b30691baca4331f6f9ed28f69.

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/snapshot/TestRenameWithSnapshots.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/snapshot/TestRenameWithSnapshots.java
@@ -26,7 +26,6 @@ import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
-import static org.apache.hadoop.test.GenericTestUtils.getTestDir;
 
 import java.io.File;
 import java.io.IOException;
@@ -2428,7 +2427,7 @@ public class TestRenameWithSnapshots {
    */
   @Test (timeout=300000)
   public void testDu() throws Exception {
-    File tempFile = File.createTempFile("testDu-", ".tmp", getTestDir());
+    File tempFile = File.createTempFile("testDu-", ".tmp");
     tempFile.deleteOnExit();
 
     final FileSystem localfs = FileSystem.getLocal(conf);
@@ -2538,8 +2537,7 @@ public class TestRenameWithSnapshots {
    */
   @Test (timeout=300000)
   public void testDuMultipleDirs() throws Exception {
-    File tempFile = File.createTempFile("testDuMultipleDirs-", ".tmp",
-        getTestDir());
+    File tempFile = File.createTempFile("testDuMultipleDirs-", "" + ".tmp");
     tempFile.deleteOnExit();
 
     final FileSystem localfs = FileSystem.getLocal(conf);
