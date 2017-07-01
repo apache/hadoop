4b63e3e504e9e226acda67f9d880ab923da2ea97
Weiwei Yang <wwei@apache.org>
2017-07-01 15:22:22 +0800

    HDFS-12002. Ozone : SCM cli misc fixes/improvements. Contributed by Chen Liang.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/ozone/scm/cli/container/DeleteContainerHandler.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/ozone/scm/cli/container/DeleteContainerHandler.java
@@ -30,14 +30,16 @@ import org.apache.hadoop.scm.container.common.helpers.Pipeline;
 import java.io.IOException;
 
 import static org.apache.hadoop.ozone.scm.cli.SCMCLI.CMD_WIDTH;
+import static org.apache.hadoop.ozone.scm.cli.SCMCLI.HELP_OP;
 
 /**
  * This is the handler that process delete container command.
  */
 public class DeleteContainerHandler extends OzoneCommandHandler {
 
-  protected static final String CONTAINER_DELETE = "del";
+  protected static final String CONTAINER_DELETE = "delete";
   protected static final String OPT_FORCE = "f";
+  protected static final String OPT_CONTAINER_NAME = "c";
 
   public DeleteContainerHandler(ScmClient scmClient) {
     super(scmClient);
@@ -46,9 +48,17 @@ public class DeleteContainerHandler extends OzoneCommandHandler {
   @Override
   public void execute(CommandLine cmd) throws IOException {
     Preconditions.checkArgument(cmd.hasOption(CONTAINER_DELETE),
-        "Expecting command del");
+        "Expecting command delete");
+    if (!cmd.hasOption(OPT_CONTAINER_NAME)) {
+      displayHelp();
+      if (!cmd.hasOption(HELP_OP)) {
+        throw new IOException("Expecting container name");
+      } else {
+        return;
+      }
+    }
 
-    String containerName = cmd.getOptionValue(CONTAINER_DELETE);
+    String containerName = cmd.getOptionValue(OPT_CONTAINER_NAME);
 
     Pipeline pipeline = getScmClient().getContainer(containerName);
     if (pipeline == null) {
@@ -61,11 +71,12 @@ public class DeleteContainerHandler extends OzoneCommandHandler {
     logOut("Container %s deleted.", containerName);
   }
 
-  @Override public void displayHelp() {
+  @Override
+  public void displayHelp() {
     Options options = new Options();
     addOptions(options);
     HelpFormatter helpFormatter = new HelpFormatter();
-    helpFormatter.printHelp(CMD_WIDTH, "hdfs scm -container -del <option>",
+    helpFormatter.printHelp(CMD_WIDTH, "hdfs scm -container -delete <option>",
         "where <option> is", options, "");
   }
 
@@ -74,5 +85,8 @@ public class DeleteContainerHandler extends OzoneCommandHandler {
         false,
         "forcibly delete a container");
     options.addOption(forceOpt);
+    Option containerNameOpt = new Option(OPT_CONTAINER_NAME,
+        true, "Specify container name");
+    options.addOption(containerNameOpt);
   }
 }
