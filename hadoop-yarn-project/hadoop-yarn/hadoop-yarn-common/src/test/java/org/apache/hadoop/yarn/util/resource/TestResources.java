049d0fb1ff64bd27891010ffd65b54d8f32b622e
qinchen <qinchen@microsoft.com>
2017-09-05 19:34:14 -0700

    new file:   hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/test/java/org/apache/hadoop/yarn/util/resource/TestResources.java 	modified:   hadoop-common-project/hadoop-common/src/main/native/native.vcxproj 	modified:   hadoop-common-project/hadoop-common/src/main/native/native.vcxproj.filters 	modified:   hadoop-common-project/hadoop-common/src/main/winutils/libwinutils.vcxproj 	modified:   hadoop-common-project/hadoop-common/src/main/winutils/winutils.sln 	modified:   hadoop-common-project/hadoop-common/src/main/winutils/winutils.vcxproj 	modified:   hadoop-hdfs-project/hadoop-hdfs/pom.xml 	modified:   hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/util/resource/Resources.java 	modified:   hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/test/java/org/apache/hadoop/yarn/logaggregation/TestAggregatedLogFormat.java 	modified:   hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/test/java/org/apache/hadoop/yarn/util/resource/TestResourceCalculator.java 	modified:   hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/SchedulerNode.java 	modified:   hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/FSAppAttempt.java

---
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/test/java/org/apache/hadoop/yarn/util/resource/TestResources.java
@@ -0,0 +1,54 @@
+package org.apache.hadoop.yarn.util.resource;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.apache.hadoop.yarn.api.records.Resource;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestResources {
+	
+	@Test	
+	 public void GpuResourcesAllocated()
+	 {
+		
+		Resource clusterResource = Resource.newInstance(0, 0, 0);
+
+	    // For lhs == rhs
+	    Resource lhs = Resource.newInstance(2, 2, 8, 0xFF);
+	    Resource rhs = Resource.newInstance(1, 1, 2, 3);
+	   
+	    Resource ret = Resources.subtract(lhs, rhs);
+	    Assert.assertTrue(ret.equalsWithGPUAttribute(Resource.newInstance(1, 1, 6, 0xFC)));
+	    
+	    Assert.assertTrue(Resources.fitsIn(rhs, lhs));
+	    	 
+	    int allcatedGPU = Resources.allocateGPUs(rhs, lhs);
+	    Assert.assertEquals(allcatedGPU, 3);
+	    
+	    ret = Resources.add(ret, rhs);
+	    Assert.assertTrue(ret.equalsWithGPUAttribute(lhs));
+	    
+	    lhs = Resource.newInstance(2, 2, 4, 0x33);
+	    rhs = Resource.newInstance(1, 1, 4, 0x33);
+	    
+	    ret = Resources.subtract(lhs, rhs);
+	    Assert.assertTrue(Resources.fitsIn(rhs, lhs));
+	    
+	    Assert.assertTrue(ret.equalsWithGPUAttribute(Resource.newInstance(1, 1, 0, 0)));
+	        
+	    ret = Resources.add(ret, rhs);
+	    Assert.assertTrue(ret.equalsWithGPUAttribute(lhs));
+	    
+	    allcatedGPU = Resources.allocateGPUs(rhs, lhs);
+	    Assert.assertEquals(allcatedGPU, 0x33);
+	    
+	    lhs = Resource.newInstance(2, 2, 4, 0x33);
+	    rhs = Resource.newInstance(1, 1, 2, 0);
+	    
+	    allcatedGPU = Resources.allocateGPUs(rhs, lhs);
+	    Assert.assertEquals(allcatedGPU, 0x30);
+	 }   
+}
+    
