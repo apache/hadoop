b127ecd1bac63ceb6152ec2e8c48e93478465c1a
Chen Liang <cliang@apache.org>
2017-09-15 10:18:48 -0700

    Revert "HDFS-12268. Ozone: Add metrics for pending storage container requests. Contributed by Yiqun Lin." as it uses class not part of JRE.
    This reverts commit c29aff44cfc78a2839f15e8cde3b80f0a31c80dd.

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/scm/XceiverClientHandler.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/scm/XceiverClientHandler.java
@@ -18,8 +18,6 @@
 package org.apache.hadoop.scm;
 
 import com.google.common.base.Preconditions;
-import com.sun.tools.javac.util.Pair;
-
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -29,7 +27,6 @@ import org.apache.hadoop.hdfs.ozone.protocol.proto.ContainerProtos
     .ContainerCommandResponseProto;
 
 import org.apache.hadoop.scm.container.common.helpers.Pipeline;
-import org.apache.hadoop.util.Time;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,6 +36,7 @@ import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+
 /**
  * Netty client handler.
  */
@@ -47,13 +45,11 @@ public class XceiverClientHandler extends
 
   static final Logger LOG = LoggerFactory.getLogger(XceiverClientHandler.class);
   private final ConcurrentMap<String,
-      Pair<CompletableFuture<ContainerCommandResponseProto>, Long>> responses =
+      CompletableFuture<ContainerCommandResponseProto>> responses =
       new ConcurrentHashMap<>();
 
   private final Pipeline pipeline;
   private volatile Channel channel;
-
-  private XceiverClientMetrics metrics;
 
   /**
    * Constructs a client that can communicate to a container server.
@@ -61,7 +57,6 @@ public class XceiverClientHandler extends
   public XceiverClientHandler(Pipeline pipeline) {
     super(false);
     this.pipeline = pipeline;
-    this.metrics = XceiverClientManager.getXceiverClientMetrics();
   }
 
   /**
@@ -81,18 +76,11 @@ public class XceiverClientHandler extends
       ContainerProtos.ContainerCommandResponseProto msg)
       throws Exception {
     Preconditions.checkNotNull(msg);
-    metrics.decrPendingContainerOpsMetrics(msg.getCmdType());
-
     String key = msg.getTraceID();
-    Pair<CompletableFuture<ContainerCommandResponseProto>, Long> future =
+    CompletableFuture<ContainerCommandResponseProto> future =
         responses.remove(key);
-
     if (future != null) {
-      future.fst.complete(msg);
-
-      long requestTime = future.snd;
-      metrics.addContainerOpsLatency(msg.getCmdType(),
-          Time.monotonicNowNanos() - requestTime);
+      future.complete(msg);
     } else {
       LOG.error("A reply received for message that was not queued. trace " +
           "ID: {}", msg.getTraceID());
@@ -142,14 +130,11 @@ public class XceiverClientHandler extends
     if(StringUtils.isEmpty(request.getTraceID())) {
       throw new IllegalArgumentException("Invalid trace ID");
     }
-    metrics.incrPendingContainerOpsMetrics(request.getCmdType());
 
-    CompletableFuture<ContainerCommandResponseProto> future
-        = new CompletableFuture<>();
-    Pair<CompletableFuture<ContainerCommandResponseProto>, Long> response =
-        new Pair<CompletableFuture<ContainerCommandResponseProto>,
-        Long>(future, Time.monotonicNowNanos());
-    Pair<CompletableFuture<ContainerCommandResponseProto>, Long> previous =
+    CompletableFuture<ContainerCommandResponseProto> response =
+        new CompletableFuture<>();
+
+    CompletableFuture<ContainerCommandResponseProto> previous =
         responses.putIfAbsent(request.getTraceID(), response);
 
     if (previous != null) {
@@ -162,6 +147,6 @@ public class XceiverClientHandler extends
     }
 
     channel.writeAndFlush(request);
-    return response.fst;
+    return response;
   }
 }
