179d8dc83c1a26adff178c32f1e286b40b1b6a88
Wangda Tan <wangda@apache.org>
2017-09-25 21:41:00 -0700

    YARN-7118. AHS REST API can return NullPointerException. (Billie Rinaldi via wangda)
    Change-Id: I16ca93385ab80e3680d4d173b8d1b79929ea7174

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-applicationhistoryservice/src/test/java/org/apache/hadoop/yarn/server/applicationhistoryservice/webapp/TestAHSWebServices.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-applicationhistoryservice/src/test/java/org/apache/hadoop/yarn/server/applicationhistoryservice/webapp/TestAHSWebServices.java
@@ -98,7 +98,7 @@ public class TestAHSWebServices extends JerseyTestBase {
   private static ApplicationHistoryClientService historyClientService;
   private static AHSWebServices ahsWebservice;
   private static final String[] USERS = new String[] { "foo" , "bar" };
-  private static final int MAX_APPS = 5;
+  private static final int MAX_APPS = 6;
   private static Configuration conf;
   private static FileSystem fs;
   private static final String remoteLogRootDir = "target/logs/";
@@ -361,7 +361,26 @@ public class TestAHSWebServices extends JerseyTestBase {
     JSONObject apps = json.getJSONObject("apps");
     assertEquals("incorrect number of elements", 1, apps.length());
     JSONArray array = apps.getJSONArray("app");
-    assertEquals("incorrect number of elements", 5, array.length());
+    assertEquals("incorrect number of elements", MAX_APPS, array.length());
+  }
+
+  @Test
+  public void testQueueQuery() throws Exception {
+    WebResource r = resource();
+    ClientResponse response =
+        r.path("ws").path("v1").path("applicationhistory").path("apps")
+            .queryParam("queue", "test queue")
+            .queryParam("user.name", USERS[round])
+            .accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+    assertEquals(Status.OK, response.getClientResponseStatus());
+    assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getType());
+    JSONObject json = response.getEntity(JSONObject.class);
+    assertEquals("incorrect number of elements", 1, json.length());
+    JSONObject apps = json.getJSONObject("apps");
+    assertEquals("incorrect number of elements", 1, apps.length());
+    JSONArray array = apps.getJSONArray("app");
+    assertEquals("incorrect number of elements", MAX_APPS - 1,
+        array.length());
   }
 
   @Test
@@ -410,7 +429,7 @@ public class TestAHSWebServices extends JerseyTestBase {
     JSONObject appAttempts = json.getJSONObject("appAttempts");
     assertEquals("incorrect number of elements", 1, appAttempts.length());
     JSONArray array = appAttempts.getJSONArray("appAttempt");
-    assertEquals("incorrect number of elements", 5, array.length());
+    assertEquals("incorrect number of elements", MAX_APPS, array.length());
   }
 
   @Test
@@ -467,7 +486,7 @@ public class TestAHSWebServices extends JerseyTestBase {
     JSONObject containers = json.getJSONObject("containers");
     assertEquals("incorrect number of elements", 1, containers.length());
     JSONArray array = containers.getJSONArray("container");
-    assertEquals("incorrect number of elements", 5, array.length());
+    assertEquals("incorrect number of elements", MAX_APPS, array.length());
   }
 
   @Test
