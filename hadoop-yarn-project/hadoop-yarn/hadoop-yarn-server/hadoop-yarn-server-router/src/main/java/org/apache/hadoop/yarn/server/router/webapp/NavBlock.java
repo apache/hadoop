1fbc72704e3c9bab8cbbc23fcddda6d36f09c59d
Carlo Curino <curino@apache.org>
2017-10-02 13:03:32 -0700

    YARN-3661. Basic Federation UI. (Contributed by Inigo Goiri via curino)
    (cherry picked from commit ceca9694f9a0c78d07cab2c382036f175183e67b)

---
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-router/src/main/java/org/apache/hadoop/yarn/server/router/webapp/NavBlock.java
@@ -0,0 +1,46 @@
+/**
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.apache.hadoop.yarn.server.router.webapp;
+
+import org.apache.hadoop.yarn.webapp.view.HtmlBlock;
+
+/**
+ * Navigation block for the Router Web UI.
+ */
+public class NavBlock extends HtmlBlock {
+
+  @Override
+  public void render(Block html) {
+    html.
+      div("#nav").
+        h3("Cluster").
+        ul().
+          li().a(url(""), "About").__().
+          li().a(url("federation"), "Federation").__().
+          li().a(url("nodes"), "Nodes").__().
+          li().a(url("apps"), "Applications").__().
+      __().
+      h3("Tools").
+      ul().
+        li().a("/conf", "Configuration").__().
+        li().a("/logs", "Local logs").__().
+        li().a("/stacks", "Server stacks").__().
+        li().a("/jmx?qry=Hadoop:*", "Server metrics").__().__().__();
+  }
+}
