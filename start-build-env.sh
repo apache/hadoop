f5ffd36e78be66d9b075050f948d8af67cf53b56
Addison Higham <addisonj@gmail.com>
2017-12-06 23:07:27 -0700

    [HADOOP-15096] Don't create user with lastlog
    This fixes a problem where in certain cases, the useradd command can create a very large diff that can blow up the host disk size.
    
    The reason for this is that lastlog is a sparse file, but AUFS under docker apparently doesn't deal with those well and creates a very large file.

--- start-build-env.sh
+++ start-build-env.sh
@@ -34,7 +34,7 @@ fi
 docker build -t "hadoop-build-${USER_ID}" - <<UserSpecificDocker
 FROM hadoop-build
 RUN groupadd --non-unique -g ${GROUP_ID} ${USER_NAME}
-RUN useradd -g ${GROUP_ID} -u ${USER_ID} -k /root -m ${USER_NAME}
+RUN useradd -g ${GROUP_ID} -u ${USER_ID} -l -k /root -m ${USER_NAME}
 RUN echo "${USER_NAME}\tALL=NOPASSWD: ALL" > "/etc/sudoers.d/hadoop-build-${USER_ID}"
 ENV HOME /home/${USER_NAME}
 
