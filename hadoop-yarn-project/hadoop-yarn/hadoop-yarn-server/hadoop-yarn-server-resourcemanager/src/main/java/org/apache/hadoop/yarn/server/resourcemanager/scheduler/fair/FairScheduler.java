9a57e457141cdc7cb19115d896ae4e0d9cad388a
Haibo Chen <haibochen@apache.org>
2018-01-09 16:45:35 -0800

    YARN-7665. Allow FS scheduler state dump to be turned on/off separately from FS debug log. (Wilfred Spiegelenburg via Haibo Chen)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/FairScheduler.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/FairScheduler.java
@@ -307,10 +307,12 @@ public class FairScheduler extends
   private void dumpSchedulerState() {
     FSQueue rootQueue = queueMgr.getRootQueue();
     Resource clusterResource = getClusterResource();
-    LOG.debug("FairScheduler state: Cluster Capacity: " + clusterResource +
+    STATE_DUMP_LOG.debug(
+        "FairScheduler state: Cluster Capacity: " + clusterResource +
         "  Allocations: " + rootMetrics.getAllocatedResources() +
         "  Availability: " + Resource.newInstance(
-        rootMetrics.getAvailableMB(), rootMetrics.getAvailableVirtualCores()) +
+            rootMetrics.getAvailableMB(),
+            rootMetrics.getAvailableVirtualCores()) +
         "  Demand: " + rootQueue.getDemand());
 
     STATE_DUMP_LOG.debug(rootQueue.dumpState());
@@ -351,7 +353,7 @@ public class FairScheduler extends
       }
 
       // Log debug information
-      if (LOG.isDebugEnabled()) {
+      if (STATE_DUMP_LOG.isDebugEnabled()) {
         if (--updatesToSkipForDebug < 0) {
           updatesToSkipForDebug = UPDATE_DEBUG_FREQUENCY;
           dumpSchedulerState();
