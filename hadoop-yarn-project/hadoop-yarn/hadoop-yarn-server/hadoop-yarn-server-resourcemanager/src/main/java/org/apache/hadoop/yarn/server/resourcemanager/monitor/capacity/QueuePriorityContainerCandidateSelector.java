09d1aba038642e2a648561cb1bfe00779e45efbd
Sunil G <sunilg@apache.org>
2018-01-12 15:41:19 +0530

    YARN-7727. Incorrect log levels in few logs with QueuePriorityContainerCandidateSelector. Contributed by Prabhu Joseph.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/monitor/capacity/QueuePriorityContainerCandidateSelector.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/monitor/capacity/QueuePriorityContainerCandidateSelector.java
@@ -118,8 +118,9 @@ public class QueuePriorityContainerCandidateSelector
   }
 
   private void intializePriorityDigraph() {
-    LOG.info("Initializing priority preemption directed graph:");
-
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("Initializing priority preemption directed graph:");
+    }
     // Make sure we iterate all leaf queue combinations
     for (String q1 : preemptionContext.getLeafQueueNames()) {
       for (String q2 : preemptionContext.getLeafQueueNames()) {
@@ -148,12 +149,12 @@ public class QueuePriorityContainerCandidateSelector
           if (p1 < p2) {
             priorityDigraph.put(q2, q1, true);
             if (LOG.isDebugEnabled()) {
-              LOG.info("- Added priority ordering edge: " + q2 + " >> " + q1);
+              LOG.debug("- Added priority ordering edge: " + q2 + " >> " + q1);
             }
           } else if (p2 < p1) {
             priorityDigraph.put(q1, q2, true);
             if (LOG.isDebugEnabled()) {
-              LOG.info("- Added priority ordering edge: " + q1 + " >> " + q2);
+              LOG.debug("- Added priority ordering edge: " + q1 + " >> " + q2);
             }
           }
         }
