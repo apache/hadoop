04f7abc7cd1afc5fed55e6f457f73e3a98d0b73d
qinchen <qinchen@microsoft.com>
2018-01-15 01:24:19 -0800

    message

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/ResourceTrackerService.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/ResourceTrackerService.java
@@ -412,7 +412,7 @@ public class ResourceTrackerService extends AbstractService implements
     String message =
         "NodeManager from node " + host + "(cmPort: " + cmPort + " httpPort: "
             + httpPort + ") " + "registered with capability: " + capability
-            + ", assigned nodeId " + nodeId;
+            + ", assigned nodeId " + nodeId + ", localUsedPorts: " + localUsedPorts;
     LOG.info(message);
     response.setNodeAction(NodeAction.NORMAL);
     response.setRMIdentifier(ResourceManager.getClusterTimeStamp());
