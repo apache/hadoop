da777a5498e73f9a44e810dc6771e5c8fe37b6f6
Xiaoyu Yao <xyao@apache.org>
2018-03-15 09:45:02 -0700

    HDFS-13251. Avoid using hard coded datanode data dirs in unit tests(addendum). Contributed by Ajay Kumar.

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/tools/TestDFSAdmin.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/tools/TestDFSAdmin.java
@@ -351,11 +351,11 @@ public class TestDFSAdmin {
     }
     File dnDir0 = cluster.getInstanceStorageDir(0, 0);
     File dnDir1 = cluster.getInstanceStorageDir(0, 1);
-    assertThat(outs.get(offset + 1),
-        is(allOf(containsString("From:"), containsString(dnDir0.getName()),
-            containsString(dnDir1.getName()))));
-    assertThat(outs.get(offset + 2), is(not(
-        anyOf(containsString(dnDir0.getName()),
+    assertThat(outs.get(offset + 1), is(allOf(containsString("From:"),
+                containsString(dnDir0.getName()),
+                containsString(dnDir1.getName()))));
+    assertThat(outs.get(offset + 2),
+        is(not(anyOf(containsString(dnDir0.getName()),
             containsString(dnDir1.getName())))));
     assertThat(outs.get(offset + 2),
         is(allOf(containsString("To"), containsString("data_new"))));
