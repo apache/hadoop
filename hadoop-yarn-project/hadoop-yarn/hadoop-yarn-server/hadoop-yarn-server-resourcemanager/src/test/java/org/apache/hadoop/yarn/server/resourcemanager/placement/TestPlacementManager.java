e74075ec3591c62f293d7abd7772f2cc196c153d
Wangda Tan <wangda@apache.org>
2018-03-23 16:43:40 -0700

    YARN-8016. Refine PlacementRule interface and add a app-name queue mapping rule as an example. (Zian Chen via wangda)
    Change-Id: I35caf1480e0f76f5f3a53528af09312e39414bbb

---
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/placement/TestPlacementManager.java
@@ -0,0 +1,112 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.yarn.server.resourcemanager.placement;
+
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.apache.hadoop.yarn.api.records.ApplicationSubmissionContext;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.server.resourcemanager.MockRM;
+import org.apache.hadoop.yarn.server.resourcemanager.scheduler.ResourceScheduler;
+import org.apache.hadoop.yarn.server.resourcemanager.scheduler.capacity.CapacityScheduler;
+import org.apache.hadoop.yarn.server.resourcemanager.scheduler.capacity.CapacitySchedulerConfiguration;
+import org.apache.hadoop.yarn.util.Records;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.apache.hadoop.yarn.server.resourcemanager.scheduler.capacity.CapacitySchedulerConfiguration.DOT;
+import static org.apache.hadoop.yarn.server.resourcemanager.scheduler.capacity.TestCapacitySchedulerAutoCreatedQueueBase.setupQueueConfiguration;
+
+public class TestPlacementManager {
+
+  public static final String USER = "user_";
+  public static final String APP_ID1 = "1";
+  public static final String USER1 = USER + APP_ID1;
+  public static final String APP_ID2 = "2";
+  public static final String USER2 = USER + APP_ID2;
+  public static final String PARENT_QUEUE = "c";
+
+  private MockRM mockRM = null;
+
+  private static final long CLUSTER_TIMESTAMP = System.currentTimeMillis();
+
+  private String getQueueMapping(String parentQueue, String leafQueue) {
+    return parentQueue + DOT + leafQueue;
+  }
+
+  @Test
+  public void testPlaceApplicationWithPlacementRuleChain() throws Exception {
+    CapacitySchedulerConfiguration conf = new CapacitySchedulerConfiguration();
+    setupQueueConfiguration(conf);
+    conf.setClass(YarnConfiguration.RM_SCHEDULER, CapacityScheduler.class,
+        ResourceScheduler.class);
+
+    mockRM = new MockRM(conf);
+    CapacityScheduler cs = (CapacityScheduler) mockRM.getResourceScheduler();
+    mockRM.start();
+    cs.start();
+
+    PlacementManager pm = cs.getRMContext()
+        .getQueuePlacementManager();
+
+    List<PlacementRule> queuePlacementRules = new ArrayList<>();
+    UserGroupMappingPlacementRule.QueueMapping userQueueMapping =
+        new UserGroupMappingPlacementRule.QueueMapping(
+            UserGroupMappingPlacementRule.QueueMapping.MappingType.USER,
+            USER1,
+            getQueueMapping(PARENT_QUEUE, USER1));
+
+    UserGroupMappingPlacementRule ugRule = new UserGroupMappingPlacementRule(
+        false, Arrays.asList(userQueueMapping), null);
+    queuePlacementRules.add(ugRule);
+    pm.updateRules(queuePlacementRules);
+
+    ApplicationSubmissionContext asc = Records.newRecord(
+        ApplicationSubmissionContext.class);
+    ApplicationId appId = ApplicationId.newInstance(CLUSTER_TIMESTAMP,
+        Integer.parseInt(APP_ID1));
+    asc.setApplicationId(appId);
+
+    boolean caughtException = false;
+    try{
+      pm.placeApplication(asc, USER2);
+    } catch (Exception e) {
+      caughtException = true;
+    }
+    Assert.assertTrue(caughtException);
+
+    QueueMappingEntity queueMappingEntity = new QueueMappingEntity(APP_ID1,
+        USER1, PARENT_QUEUE);
+
+    AppNameMappingPlacementRule anRule = new AppNameMappingPlacementRule(false,
+        Arrays.asList(queueMappingEntity));
+    queuePlacementRules.add(anRule);
+    pm.updateRules(queuePlacementRules);
+    try{
+      pm.placeApplication(asc, USER2);
+    } catch (Exception e) {
+      caughtException = false;
+    }
+    Assert.assertFalse(caughtException);
+  }
+
+}
