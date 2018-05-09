9acfaee1f080245744f191a7fd080f91413249a6
Rohith Sharma K S <rohithsharmaks@apache.org>
2018-05-09 12:04:31 +0530

    YARN-8264. [UI2] GPU Info tab disappears if we click any sub link under List of Applications or List of Containers. Contributed by Sunil G.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-ui/src/main/webapp/app/routes/yarn-node-container.js
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-ui/src/main/webapp/app/routes/yarn-node-container.js
@@ -26,6 +26,7 @@ export default AbstractRoute.extend({
     return Ember.RSVP.hash({
       nodeContainer: this.store.queryRecord('yarn-node-container',
           { nodeHttpAddr: param.node_addr, containerId: param.container_id }),
+      nmGpuInfo: this.store.findRecord('yarn-nm-gpu', address, {reload:true}),
       nodeInfo: { id: param.node_id, addr: param.node_addr, containerId: param.container_id }
     });
   },
