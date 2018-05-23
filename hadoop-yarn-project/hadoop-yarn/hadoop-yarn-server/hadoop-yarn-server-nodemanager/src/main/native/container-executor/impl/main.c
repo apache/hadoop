7be1bc52bfb95768dd97374ccbe0769c1a732b6b
Akira Ajisaka <aajisaka@apache.org>
2018-05-23 17:34:28 +0900

    Addendum fix in container executor. Contributed by Wilfred Spiegelenburg.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/native/container-executor/impl/main.c
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/native/container-executor/impl/main.c
@@ -129,7 +129,7 @@ int main(int argc, char **argv) {
   }
 
   char *orig_conf_file = HADOOP_CONF_DIR "/" CONF_FILENAME;
-  char *conf_file = resolve_config_path(orig_conf_file, argv[0]);
+  char *conf_file = resolve_config_path(orig_conf_file, executable_file);
   char *local_dirs, *log_dirs;
   char *resources, *resources_key, *resources_value;
 
