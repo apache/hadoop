8fe4062bfc20ee181106eb68635d3404c50c9238
Xiaoyu Yao <xyao@apache.org>
2018-09-10 08:45:49 -0700

    HDDS-403. Fix createdOn and modifiedOn timestamp for volume, bucket, key. Contributed by Dinesh Chitlangia.

--- hadoop-hdds/client/src/main/java/org/apache/hadoop/hdds/scm/client/HddsClientUtils.java
+++ hadoop-hdds/client/src/main/java/org/apache/hadoop/hdds/scm/client/HddsClientUtils.java
@@ -74,7 +74,7 @@ public final class HddsClientUtils {
    */
   public static String formatDateTime(long millis) {
     ZonedDateTime dateTime = ZonedDateTime.ofInstant(
-        Instant.ofEpochSecond(millis), DATE_FORMAT.get().getZone());
+        Instant.ofEpochMilli(millis), DATE_FORMAT.get().getZone());
     return DATE_FORMAT.get().format(dateTime);
   }
 
@@ -85,7 +85,7 @@ public final class HddsClientUtils {
   public static long formatDateTime(String date) throws ParseException {
     Preconditions.checkNotNull(date, "Date string should not be null.");
     return ZonedDateTime.parse(date, DATE_FORMAT.get())
-        .toInstant().getEpochSecond();
+        .toInstant().toEpochMilli();
   }
 
 
