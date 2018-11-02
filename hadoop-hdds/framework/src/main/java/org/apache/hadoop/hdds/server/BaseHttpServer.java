d16d5f730e9d139d3e026805f21ac2c9b0bbb98b
Yiqun Lin <yqlin@apache.org>
2018-11-02 10:50:32 +0800

    HDDS-751. Replace usage of Guava Optional with Java Optional.

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/server/BaseHttpServer.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/server/BaseHttpServer.java
@@ -17,7 +17,6 @@
 
 package org.apache.hadoop.hdds.server;
 
-import com.google.common.base.Optional;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hdfs.DFSConfigKeys;
 import org.apache.hadoop.hdfs.DFSUtil;
@@ -32,6 +31,7 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Optional;
 
 import static org.apache.hadoop.hdds.HddsUtils.getHostNameFromConfigKeys;
 import static org.apache.hadoop.hdds.HddsUtils.getPortNumberFromConfigKeys;
@@ -118,10 +118,13 @@ public abstract class BaseHttpServer {
     final Optional<String> addresHost =
         getHostNameFromConfigKeys(conf, addressKey);
 
-    String hostName = bindHost.or(addresHost).or(bindHostDefault);
+    String hostName = bindHost.orElse(addresHost.get());
+    if (hostName == null || hostName.isEmpty()) {
+      hostName = bindHostDefault;
+    }
 
     return NetUtils.createSocketAddr(
-        hostName + ":" + addressPort.or(bindPortdefault));
+        hostName + ":" + addressPort.orElse(bindPortdefault));
   }
 
   /**
