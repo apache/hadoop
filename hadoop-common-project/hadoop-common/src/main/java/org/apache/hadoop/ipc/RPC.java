eb9ea3933369549e58be371a6d0fd1427f2fa868
Konstantin V Shvachko <shv@apache.org>
2018-12-24 09:39:20 -0800

    HDFS-14170. [SBN read] Fix checkstyle warnings related to SBN reads. Contributed by Konstantin V Shvachko.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/RPC.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/RPC.java
@@ -591,7 +591,7 @@ public class RPC {
 
   /**
    * Get a protocol proxy that contains a proxy connection to a remote server
-   * and a set of methods that are supported by the server
+   * and a set of methods that are supported by the server.
    *
    * @param protocol protocol
    * @param clientVersion client's version
