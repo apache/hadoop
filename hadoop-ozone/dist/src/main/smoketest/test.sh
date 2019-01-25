8ff9578126cb97bb4958fde78a7f08c6e2b30f4b
Nanda kumar <nandakumar131@gmail.com>
2019-01-25 12:14:10 +0530

    HDDS-1007. Add robot test for AuditParser. Contributed by Dinesh Chitlangia.

--- hadoop-ozone/dist/src/main/smoketest/test.sh
+++ hadoop-ozone/dist/src/main/smoketest/test.sh
@@ -140,6 +140,8 @@ if [ "$RUN_ALL" = true ]; then
 #
 # We select the test suites and execute them on multiple type of clusters
 #
+   DEFAULT_TESTS=("auditparser")
+   execute_tests auditparser "${DEFAULT_TESTS[@]}"
    DEFAULT_TESTS=("security")
    execute_tests ozonesecure "${DEFAULT_TESTS[@]}"
    DEFAULT_TESTS=("basic")
