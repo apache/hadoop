b8e24805f43cc77aa15124d01251a554dbad404d
Steve Loughran <stevel@apache.org>
2019-02-07 21:56:43 +0000

    Revert "HADOOP-15954. ABFS: Enable owner and group conversion for MSI and login user using OAuth."
    (accidentally mixed in two patches)
    
    This reverts commit fa8cd1bf28f5b81849ba351a2d7225fbc580350d.

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/oauth2/package-info.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/oauth2/package-info.java
@@ -15,10 +15,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-@InterfaceAudience.Private
-@InterfaceStability.Evolving
 package org.apache.hadoop.fs.azurebfs.oauth2;
-
-import org.apache.hadoop.classification.InterfaceAudience;
-import org.apache.hadoop.classification.InterfaceStability;
