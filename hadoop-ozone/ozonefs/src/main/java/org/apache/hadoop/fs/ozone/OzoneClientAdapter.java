7d44223dbbccb0762a296974331f7ae6b529e59b
Ajay Yadav <7813154+ajayydv@users.noreply.github.com>
2019-03-01 16:17:07 -0800

    Revert "HDDS-1183. Override getDelegationToken API for OzoneFileSystem. Contributed by Xiaoyu Yao."
    This reverts commit 6fef6fc5ee5d9b72ba659d6a4289f571e1eabf1c.

--- hadoop-ozone/ozonefs/src/main/java/org/apache/hadoop/fs/ozone/OzoneClientAdapter.java
+++ hadoop-ozone/ozonefs/src/main/java/org/apache/hadoop/fs/ozone/OzoneClientAdapter.java
@@ -17,9 +17,6 @@
  */
 package org.apache.hadoop.fs.ozone;
 
-import org.apache.hadoop.ozone.security.OzoneTokenIdentifier;
-import org.apache.hadoop.security.token.Token;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
@@ -55,6 +52,4 @@ public interface OzoneClientAdapter {
 
   Iterator<BasicKeyInfo> listKeys(String pathKey);
 
-  Token<OzoneTokenIdentifier> getDelegationToken(String renewer)
-      throws IOException;
 }
