1689046e02c7c5d617059f46735739c79d05e605
Ajay Kumar <ajay@apache.org>
2019-03-22 15:53:12 -0700

    HDDS-1310. In datanode once a container becomes unhealthy, datanode restart fails. Contributed by  Sandeep Nemuri.

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/keyvalue/KeyValueContainer.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/keyvalue/KeyValueContainer.java
@@ -623,6 +623,9 @@ public class KeyValueContainer implements Container<KeyValueContainerData> {
     case CLOSED:
       state = ContainerReplicaProto.State.CLOSED;
       break;
+    case UNHEALTHY:
+      state = ContainerReplicaProto.State.UNHEALTHY;
+      break;
     default:
       throw new StorageContainerException("Invalid Container state found: " +
           containerData.getContainerID(), INVALID_CONTAINER_STATE);
