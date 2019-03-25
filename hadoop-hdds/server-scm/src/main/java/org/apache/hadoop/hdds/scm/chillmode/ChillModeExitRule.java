71024c84d66572b260d58c4a5fbf7691073c906d
Bharat Viswanadham <bharat@apache.org>
2019-03-25 13:11:03 -0700

    HDDS-1217. Refactor ChillMode rules and chillmode manager. (#558)

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/chillmode/ChillModeExitRule.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/chillmode/ChillModeExitRule.java
@@ -17,16 +17,94 @@
  */
 package org.apache.hadoop.hdds.scm.chillmode;
 
+import org.apache.hadoop.hdds.server.events.EventHandler;
+import org.apache.hadoop.hdds.server.events.EventPublisher;
+import org.apache.hadoop.hdds.server.events.EventQueue;
+import org.apache.hadoop.hdds.server.events.TypedEvent;
+
 /**
- * Interface for defining chill mode exit rules.
+ * Abstract class for ChillModeExitRules. When a new rule is added, the new
+ * rule should extend this abstract class.
+ *
+ * Each rule Should do:
+ * 1. Should add a handler for the event it is looking for during the
+ * initialization of the rule.
+ * 2. Add the rule in ScmChillModeManager to list of the rules.
+ *
  *
  * @param <T>
  */
-public interface ChillModeExitRule<T> {
+public abstract class ChillModeExitRule<T> implements EventHandler<T> {
 
-  boolean validate();
+  private final SCMChillModeManager chillModeManager;
+  private final String ruleName;
 
-  void process(T report);
+  public ChillModeExitRule(SCMChillModeManager chillModeManager,
+      String ruleName, EventQueue eventQueue) {
+    this.chillModeManager = chillModeManager;
+    this.ruleName = ruleName;
+    eventQueue.addHandler(getEventType(), this);
+  }
 
-  void cleanup();
+  /**
+   * Return's the name of this ChillModeExit Rule.
+   * @return ruleName
+   */
+  public String getRuleName() {
+    return ruleName;
+  }
+
+  /**
+   * Return's the event type this chillMode exit rule handles.
+   * @return TypedEvent
+   */
+  protected abstract TypedEvent<T> getEventType();
+
+  /**
+   * Validate's this rule. If this rule condition is met, returns true, else
+   * returns false.
+   * @return boolean
+   */
+  protected abstract boolean validate();
+
+  /**
+   * Actual processing logic for this rule.
+   * @param report
+   */
+  protected abstract void process(T report);
+
+  /**
+   * Cleanup action's need to be done, once this rule is satisfied.
+   */
+  protected abstract void cleanup();
+
+  @Override
+  public final void onMessage(T report, EventPublisher publisher) {
+
+    // TODO: when we have remove handlers, we can remove getInChillmode check
+
+    if (scmInChillMode()) {
+      if (validate()) {
+        chillModeManager.validateChillModeExitRules(ruleName, publisher);
+        cleanup();
+        return;
+      }
+
+      process(report);
+
+      if (validate()) {
+        chillModeManager.validateChillModeExitRules(ruleName, publisher);
+        cleanup();
+      }
+    }
+  }
+
+  /**
+   * Return true if SCM is in chill mode, else false.
+   * @return boolean
+   */
+  protected boolean scmInChillMode() {
+    return chillModeManager.getInChillMode();
+  }
+
 }
