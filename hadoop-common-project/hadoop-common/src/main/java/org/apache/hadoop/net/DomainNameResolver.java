7b5b783f66f32012c00bef7593851392dd8cf2d5
Inigo Goiri <inigoiri@apache.org>
2019-04-03 16:11:13 -0700

    HDFS-14327. Using FQDN instead of IP to access servers with DNS resolving. Contributed by Fengnan Li.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/net/DomainNameResolver.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/net/DomainNameResolver.java
@@ -36,4 +36,27 @@ public interface DomainNameResolver {
    */
   InetAddress[] getAllByDomainName(String domainName)
       throws UnknownHostException;
+
+  /**
+   * Reverse lookup an IP address and get the fully qualified domain name(fqdn).
+   *
+   * @param address
+   * @return fully qualified domain name
+   */
+  String getHostnameByIP(InetAddress address);
+
+  /**
+   * This function combines getAllByDomainName and getHostnameByIP, for a single
+   * domain name, it will first do a forward lookup to get all of IP addresses,
+   * then for each IP address, it will do a reverse lookup to get the fqdn.
+   * This function is necessary in secure environment since Kerberos uses fqdn
+   * in the service principal instead of IP.
+   *
+   * @param domainName
+   * @return all fully qualified domain names belonging to the IPs resolved from
+   * the input domainName
+   * @throws UnknownHostException
+   */
+   String[] getAllResolvedHostnameByDomainName(
+       String domainName, boolean useFQDN) throws UnknownHostException;
 }
