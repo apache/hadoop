dce76e0355089496b9caa8c805f0ba1b958ca41b
Chen Liang <cliang@apache.org>
2019-04-05 11:27:37 -0700

    HDFS-14415. Backport HDFS-13799 to branch-2. Contributed by Chao Sun.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/ha/EditLogTailer.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/ha/EditLogTailer.java
@@ -202,7 +202,6 @@ public class EditLogTailer {
   }
 
   public void stop() throws IOException {
-    rollEditsRpcExecutor.shutdown();
     tailerThread.setShouldRun(false);
     tailerThread.interrupt();
     try {
@@ -210,6 +209,8 @@ public class EditLogTailer {
     } catch (InterruptedException e) {
       LOG.warn("Edit log tailer thread exited with an exception");
       throw new IOException(e);
+    } finally {
+      rollEditsRpcExecutor.shutdown();
     }
   }
 
