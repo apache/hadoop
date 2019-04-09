35b46e1662de35dbe1b06de38035424f5b3c3751
Márton Elek <elek@apache.org>
2019-04-09 17:35:04 +0200

    HDDS-1383. Create project skeleton with CLI interface for In-place upgrade.
    Closes #694

---
+++ hadoop-ozone/upgrade/src/main/java/org/apache/hadoop/ozone/upgrade/Balance.java
@@ -0,0 +1,38 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.ozone.upgrade;
+
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine.Command;
+
+/**
+ * Command to move blocks between HDFS datanodes.
+ */
+@Command(name = "balance",
+    description = "Move the HDFS blocks for a better distribution "
+        + "usage.")
+public class Balance implements Callable<Void> {
+
+  @Override
+  public Void call() throws Exception {
+    System.err.println("[In-Place upgrade : balance] is not yet supported.");
+    return null;
+  }
+
+}
