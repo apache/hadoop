e029e5bdabc2f69a0afc16a19db587ba98d92a00
Márton Elek <elek@apache.org>
2019-05-29 14:19:13 +0200

    fix test

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/TestOzoneConfigurationFields.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/TestOzoneConfigurationFields.java
@@ -19,7 +19,6 @@ package org.apache.hadoop.ozone;
 
 import org.apache.hadoop.conf.TestConfigurationFieldsBase;
 import org.apache.hadoop.hdds.HddsConfigKeys;
-import org.apache.hadoop.ozone.csi.CsiConfigurationValues;
 import org.apache.hadoop.ozone.om.OMConfigKeys;
 import org.apache.hadoop.hdds.scm.ScmConfigKeys;
 import org.apache.hadoop.ozone.recon.ReconServerConfigKeys;
@@ -36,7 +35,6 @@ public class TestOzoneConfigurationFields extends TestConfigurationFieldsBase {
     configurationClasses =
         new Class[] {OzoneConfigKeys.class, ScmConfigKeys.class,
             OMConfigKeys.class, HddsConfigKeys.class,
-            CsiConfigurationValues.class,
             ReconServerConfigKeys.class,
             S3GatewayConfigKeys.class};
     errorIfMissingConfigProps = true;
