86aece539fbe53453229f95e323f4c2fc66f974d
Shashikant Banerjee <shashikant@apache.org>
2019-06-03 21:35:51 +0530

    Fixed compliation issue.

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/impl/HddsDispatcher.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/impl/HddsDispatcher.java
@@ -66,7 +66,6 @@ import org.apache.hadoop.ozone.container.common.interfaces.ContainerDispatcher;
 import io.opentracing.Scope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.jvm.hotspot.utilities.Assert;
 
 import java.io.IOException;
 import java.util.Map;
