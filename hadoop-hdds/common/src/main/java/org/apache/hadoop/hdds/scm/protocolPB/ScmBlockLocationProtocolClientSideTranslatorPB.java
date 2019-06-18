d6ff0f7a99e30d331b3fa9547b3b8e5b5a4b9710
S O'Donnell <sodonnell@cloudera.com>
2019-06-18 16:01:51 +0100

    Fix bug causing deleteblocks to fail

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/scm/protocolPB/ScmBlockLocationProtocolClientSideTranslatorPB.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/scm/protocolPB/ScmBlockLocationProtocolClientSideTranslatorPB.java
@@ -162,7 +162,7 @@ public final class ScmBlockLocationProtocolClientSideTranslatorPB
         .build();
 
     SCMBlockLocationRequest wrapper = createSCMBlockRequest(
-        Type.AllocateScmBlock)
+        Type.DeleteScmKeyBlocks)
         .setDeleteScmKeyBlocksRequest(request)
         .build();
 
