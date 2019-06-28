045bf0d66a88b0447765afa208ae9beb7c0049e9
Weiwei Yang <wwei@apache.org>
2019-06-29 00:29:17 +0800

    Revert "YARN-9655. AllocateResponse in FederationInterceptor lost applicationPriority. (hunshenshi via wwei) closes apache/hadoop#1023"
    This reverts commit 5e7caf128719aac7d16d0efc8334b3b5a4b01e89.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/amrmproxy/FederationInterceptor.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/amrmproxy/FederationInterceptor.java
@@ -1446,11 +1446,6 @@ public class FederationInterceptor extends AbstractRequestInterceptor {
       }
     }
 
-    if (otherResponse.getApplicationPriority() != null) {
-      homeResponse.setApplicationPriority(
-          otherResponse.getApplicationPriority());
-    }
-
     homeResponse.setNumClusterNodes(
         homeResponse.getNumClusterNodes() + otherResponse.getNumClusterNodes());
 
