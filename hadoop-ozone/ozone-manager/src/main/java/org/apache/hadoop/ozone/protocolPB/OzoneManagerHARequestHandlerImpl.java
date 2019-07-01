1e727cf2a17d677a5c1091397a38dad4fc9c085f
Bharat Viswanadham <bharat@apache.org>
2019-06-30 22:38:53 -0700

    HDDS-1730. Implement File CreateDirectory Request to use Cache and Do… (#1026)

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/protocolPB/OzoneManagerHARequestHandlerImpl.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/protocolPB/OzoneManagerHARequestHandlerImpl.java
@@ -108,6 +108,7 @@ public class OzoneManagerHARequestHandlerImpl
     case CommitKey:
     case DeleteKey:
     case RenameKey:
+    case CreateDirectory:
       //TODO: We don't need to pass transactionID, this will be removed when
       // complete write requests is changed to new model. And also we can
       // return OMClientResponse, then adding to doubleBuffer can be taken
