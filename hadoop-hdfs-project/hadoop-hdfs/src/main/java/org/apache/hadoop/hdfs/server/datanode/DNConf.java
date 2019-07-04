e61e52805fd8461e2a309bb705a60e2092b989e2
Masatake Iwasaki <iwasakims@apache.org>
2019-07-04 23:20:01 +0900

    HDFS-14629. Property value Hard Coded in DNConf.java. Contributed by hemanthboyina.
    (cherry picked from commit 1c254a8ec78c8dab9111f3ba2709cef20794b73a)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DNConf.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DNConf.java
@@ -199,9 +199,9 @@ public class DNConf {
         DFS_BLOCKREPORT_INITIAL_DELAY_DEFAULT) * 1000L;
     if (initBRDelay >= blockReportInterval) {
       initBRDelay = 0;
-      DataNode.LOG.info("dfs.blockreport.initialDelay is "
-          + "greater than or equal to" + "dfs.blockreport.intervalMsec."
-          + " Setting initial delay to 0 msec:");
+      DataNode.LOG.info(DFS_BLOCKREPORT_INITIAL_DELAY_KEY + " is "
+          + "greater than or equal to" + DFS_BLOCKREPORT_INTERVAL_MSEC_KEY
+          + ".  Setting initial delay to 0 msec:");
     }
     initialBlockReportDelayMs = initBRDelay;
     
