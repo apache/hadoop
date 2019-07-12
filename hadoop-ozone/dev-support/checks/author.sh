51e7dc32215f1d9e70c427584a8575b0965a9627
Márton Elek <elek@apache.org>
2019-07-12 11:35:34 +0200

    make author check yetus safe

--- hadoop-ozone/dev-support/checks/author.sh
+++ hadoop-ozone/dev-support/checks/author.sh
@@ -16,8 +16,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR/../../.." || exit 1
 
-grep -r --include="*.java" "@author" .
-if grep -r --include="*.java" "@author" .; then
+#hide this tring to not confuse yetus
+AUTHOR="uthor"
+AUTHOR="@a${AUTHOR}"
+
+grep -r --include="*.java" "$AUTHOR" .
+if grep -r --include="*.java" "$AUTHOR" .; then
   exit 0
 else
   exit 1
