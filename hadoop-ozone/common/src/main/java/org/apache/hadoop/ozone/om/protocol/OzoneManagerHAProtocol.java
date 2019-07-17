0541322c9acc0000b63bd90feda9e4eef8dde5f0
Bharat Viswanadham <bharat@apache.org>
2019-07-17 14:13:06 -0700

    HDDS-1675. Cleanup Volume Request 2 phase old code. (#964)

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/protocol/OzoneManagerHAProtocol.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/protocol/OzoneManagerHAProtocol.java
@@ -18,13 +18,8 @@
 
 package org.apache.hadoop.ozone.om.protocol;
 
-import org.apache.hadoop.ozone.om.helpers.OmDeleteVolumeResponse;
 import org.apache.hadoop.ozone.om.helpers.OmKeyArgs;
 import org.apache.hadoop.ozone.om.helpers.OmMultipartInfo;
-import org.apache.hadoop.ozone.om.helpers.OmVolumeArgs;
-import org.apache.hadoop.ozone.om.helpers.OmVolumeOwnerChangeResponse;
-import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
-    .VolumeList;
 
 import java.io.IOException;
 
@@ -54,76 +49,4 @@ public interface OzoneManagerHAProtocol {
   OmMultipartInfo applyInitiateMultipartUpload(OmKeyArgs omKeyArgs,
       String multipartUploadID) throws IOException;
 
-  /**
-   * Start Create Volume Transaction.
-   * @param omVolumeArgs
-   * @return VolumeList
-   * @throws IOException
-   */
-  VolumeList startCreateVolume(OmVolumeArgs omVolumeArgs) throws IOException;
-
-  /**
-   * Apply Create Volume changes to OM DB.
-   * @param omVolumeArgs
-   * @param volumeList
-   * @throws IOException
-   */
-  void applyCreateVolume(OmVolumeArgs omVolumeArgs,
-      VolumeList volumeList) throws IOException;
-
-  /**
-   * Start setOwner Transaction.
-   * @param volume
-   * @param owner
-   * @return OmVolumeOwnerChangeResponse
-   * @throws IOException
-   */
-  OmVolumeOwnerChangeResponse startSetOwner(String volume,
-      String owner) throws IOException;
-
-  /**
-   * Apply Set Quota changes to OM DB.
-   * @param oldOwner
-   * @param oldOwnerVolumeList
-   * @param newOwnerVolumeList
-   * @param newOwnerVolumeArgs
-   * @throws IOException
-   */
-  void applySetOwner(String oldOwner, VolumeList oldOwnerVolumeList,
-      VolumeList newOwnerVolumeList, OmVolumeArgs newOwnerVolumeArgs)
-      throws IOException;
-
-  /**
-   * Start Set Quota Transaction.
-   * @param volume
-   * @param quota
-   * @return OmVolumeArgs
-   * @throws IOException
-   */
-  OmVolumeArgs startSetQuota(String volume, long quota) throws IOException;
-
-  /**
-   * Apply Set Quota Changes to OM DB.
-   * @param omVolumeArgs
-   * @throws IOException
-   */
-  void applySetQuota(OmVolumeArgs omVolumeArgs) throws IOException;
-
-  /**
-   * Start Delete Volume Transaction.
-   * @param volume
-   * @return OmDeleteVolumeResponse
-   * @throws IOException
-   */
-  OmDeleteVolumeResponse startDeleteVolume(String volume) throws IOException;
-
-  /**
-   * Apply Delete Volume changes to OM DB.
-   * @param volume
-   * @param owner
-   * @param newVolumeList
-   * @throws IOException
-   */
-  void applyDeleteVolume(String volume, String owner,
-      VolumeList newVolumeList) throws IOException;
 }
--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/protocolPB/OzoneManagerHARequestHandlerImpl.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/protocolPB/OzoneManagerHARequestHandlerImpl.java
@@ -17,41 +17,19 @@
 
 package org.apache.hadoop.ozone.protocolPB;
 
-import java.io.IOException;
-
 import org.apache.hadoop.ozone.om.OzoneManager;
-import org.apache.hadoop.ozone.om.helpers.OmDeleteVolumeResponse;
-import org.apache.hadoop.ozone.om.helpers.OmVolumeArgs;
-import org.apache.hadoop.ozone.om.helpers.OmVolumeOwnerChangeResponse;
 import org.apache.hadoop.ozone.om.ratis.OzoneManagerDoubleBuffer;
 import org.apache.hadoop.ozone.om.ratis.utils.OzoneManagerRatisUtils;
 import org.apache.hadoop.ozone.om.request.OMClientRequest;
 import org.apache.hadoop.ozone.om.response.OMClientResponse;
-import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos;
-import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
-    .CreateVolumeRequest;
-import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
-    .CreateVolumeResponse;
-import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
-    .DeleteVolumeRequest;
-import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
-    .DeleteVolumeResponse;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
     .OMRequest;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
     .OMResponse;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
-    .SetVolumePropertyRequest;
-import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
-    .SetVolumePropertyResponse;
-import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
     .Status;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
     .Type;
-import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
-    .VolumeInfo;
-import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos
-    .VolumeList;
 
 /**
  * Command Handler for OM requests. OM State Machine calls this handler for
@@ -66,28 +44,6 @@ public class OzoneManagerHARequestHandlerImpl
       OzoneManagerDoubleBuffer ozoneManagerDoubleBuffer) {
     super(om);
     this.ozoneManagerDoubleBuffer = ozoneManagerDoubleBuffer;
-  }
-
-  @Override
-  public OMRequest handleStartTransaction(OMRequest omRequest)
-      throws IOException {
-    LOG.debug("Received OMRequest: {}, ", omRequest);
-    Type cmdType = omRequest.getCmdType();
-    OMRequest newOmRequest = null;
-    switch (cmdType) {
-    case CreateVolume:
-      newOmRequest = handleCreateVolumeStart(omRequest);
-      break;
-    case SetVolumeProperty:
-      newOmRequest = handleSetVolumePropertyStart(omRequest);
-      break;
-    case DeleteVolume:
-      newOmRequest = handleDeleteVolumeStart(omRequest);
-      break;
-    default:
-      throw new IOException("Unrecognized Command Type:" + cmdType);
-    }
-    return newOmRequest;
   }
 
 
@@ -134,129 +90,5 @@ public class OzoneManagerHARequestHandlerImpl
       // here.
       return handle(omRequest);
     }
-  }
-
-
-  private OMRequest handleCreateVolumeStart(OMRequest omRequest)
-      throws IOException {
-    VolumeInfo volumeInfo = omRequest.getCreateVolumeRequest().getVolumeInfo();
-    OzoneManagerProtocolProtos.VolumeList volumeList =
-        getOzoneManager().startCreateVolume(
-            OmVolumeArgs.getFromProtobuf(volumeInfo));
-
-    CreateVolumeRequest createVolumeRequest =
-        CreateVolumeRequest.newBuilder().setVolumeInfo(volumeInfo)
-            .setVolumeList(volumeList).build();
-    return omRequest.toBuilder().setCreateVolumeRequest(createVolumeRequest)
-        .build();
-  }
-
-  private CreateVolumeResponse handleCreateVolumeApply(OMRequest omRequest)
-      throws IOException {
-    OzoneManagerProtocolProtos.VolumeInfo volumeInfo =
-        omRequest.getCreateVolumeRequest().getVolumeInfo();
-    VolumeList volumeList =
-        omRequest.getCreateVolumeRequest().getVolumeList();
-    getOzoneManager().applyCreateVolume(
-        OmVolumeArgs.getFromProtobuf(volumeInfo),
-        volumeList);
-    return CreateVolumeResponse.newBuilder().build();
-  }
-
-  private OMRequest handleSetVolumePropertyStart(OMRequest omRequest)
-      throws IOException {
-    SetVolumePropertyRequest setVolumePropertyRequest =
-        omRequest.getSetVolumePropertyRequest();
-    String volume = setVolumePropertyRequest.getVolumeName();
-    OMRequest newOmRequest = null;
-    if (setVolumePropertyRequest.hasQuotaInBytes()) {
-      long quota = setVolumePropertyRequest.getQuotaInBytes();
-      OmVolumeArgs omVolumeArgs =
-          getOzoneManager().startSetQuota(volume, quota);
-      SetVolumePropertyRequest newSetVolumePropertyRequest =
-          SetVolumePropertyRequest.newBuilder().setVolumeName(volume)
-              .setVolumeInfo(omVolumeArgs.getProtobuf()).build();
-      newOmRequest =
-          omRequest.toBuilder().setSetVolumePropertyRequest(
-              newSetVolumePropertyRequest).build();
-    } else {
-      String owner = setVolumePropertyRequest.getOwnerName();
-      OmVolumeOwnerChangeResponse omVolumeOwnerChangeResponse =
-          getOzoneManager().startSetOwner(volume, owner);
-      // If volumeLists become large and as ratis writes the request to disk we
-      // might take more space if the lists become very big in size. We might
-      // need to revisit this if it becomes problem
-      SetVolumePropertyRequest newSetVolumePropertyRequest =
-          SetVolumePropertyRequest.newBuilder().setVolumeName(volume)
-              .setOwnerName(owner)
-              .setOriginalOwner(omVolumeOwnerChangeResponse.getOriginalOwner())
-              .setNewOwnerVolumeList(
-                  omVolumeOwnerChangeResponse.getNewOwnerVolumeList())
-              .setOldOwnerVolumeList(
-                  omVolumeOwnerChangeResponse.getOriginalOwnerVolumeList())
-              .setVolumeInfo(
-                  omVolumeOwnerChangeResponse.getNewOwnerVolumeArgs()
-                      .getProtobuf()).build();
-      newOmRequest =
-          omRequest.toBuilder().setSetVolumePropertyRequest(
-              newSetVolumePropertyRequest).build();
-    }
-    return newOmRequest;
-  }
-
-
-  private SetVolumePropertyResponse handleSetVolumePropertyApply(
-      OMRequest omRequest) throws IOException {
-    SetVolumePropertyRequest setVolumePropertyRequest =
-        omRequest.getSetVolumePropertyRequest();
-
-    if (setVolumePropertyRequest.hasQuotaInBytes()) {
-      getOzoneManager().applySetQuota(
-          OmVolumeArgs.getFromProtobuf(
-              setVolumePropertyRequest.getVolumeInfo()));
-    } else {
-      getOzoneManager().applySetOwner(
-          setVolumePropertyRequest.getOriginalOwner(),
-          setVolumePropertyRequest.getOldOwnerVolumeList(),
-          setVolumePropertyRequest.getNewOwnerVolumeList(),
-          OmVolumeArgs.getFromProtobuf(
-              setVolumePropertyRequest.getVolumeInfo()));
-    }
-    return SetVolumePropertyResponse.newBuilder().build();
-  }
-
-  private OMRequest handleDeleteVolumeStart(OMRequest omRequest)
-      throws IOException {
-    DeleteVolumeRequest deleteVolumeRequest =
-        omRequest.getDeleteVolumeRequest();
-
-    String volume = deleteVolumeRequest.getVolumeName();
-
-    OmDeleteVolumeResponse omDeleteVolumeResponse =
-        getOzoneManager().startDeleteVolume(volume);
-
-    DeleteVolumeRequest newDeleteVolumeRequest =
-        DeleteVolumeRequest.newBuilder().setVolumeList(
-            omDeleteVolumeResponse.getUpdatedVolumeList())
-            .setVolumeName(omDeleteVolumeResponse.getVolume())
-            .setOwner(omDeleteVolumeResponse.getOwner()).build();
-
-    return omRequest.toBuilder().setDeleteVolumeRequest(
-        newDeleteVolumeRequest).build();
-
-  }
-
-
-  private DeleteVolumeResponse handleDeleteVolumeApply(OMRequest omRequest)
-      throws IOException {
-
-    DeleteVolumeRequest deleteVolumeRequest =
-        omRequest.getDeleteVolumeRequest();
-
-    getOzoneManager().applyDeleteVolume(
-        deleteVolumeRequest.getVolumeName(), deleteVolumeRequest.getOwner(),
-        deleteVolumeRequest.getVolumeList());
-
-    return DeleteVolumeResponse.newBuilder().build();
   }
 }
