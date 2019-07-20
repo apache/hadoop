4aa76e327dcb8ef81ab653b864b18a59f3ef1612
Bharat Viswanadham <bharat@apache.org>
2019-07-20 08:11:36 -0700

    HDDS-1805. Implement S3 Initiate MPU request to use Cache and DoubleBuffer. (#1108)

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/protocolPB/OzoneManagerHARequestHandlerImpl.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/protocolPB/OzoneManagerHARequestHandlerImpl.java
@@ -69,6 +69,7 @@ public class OzoneManagerHARequestHandlerImpl
     case PurgeKeys:
     case CreateS3Bucket:
     case DeleteS3Bucket:
+    case InitiateMultiPartUpload:
       //TODO: We don't need to pass transactionID, this will be removed when
       // complete write requests is changed to new model. And also we can
       // return OMClientResponse, then adding to doubleBuffer can be taken
