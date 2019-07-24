e162e29db6fefb3ac2565cc1a6e06fa0b27d6f9f
sunlisheng <sunlisheng@xiaomi.com>
2019-07-24 09:36:20 +0800

    Remove useless trace log in NetUtils.java
    Signed-off-by: sunlisheng <sunlisheng@xiaomi.com>

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/net/NetUtils.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/net/NetUtils.java
@@ -833,8 +833,6 @@ public class NetUtils {
       Throwable t = ctor.newInstance(msg);
       return (T)(t.initCause(exception));
     } catch (Throwable e) {
-      LOG.trace("Unable to wrap exception of type {}: it has no (String) "
-          + "constructor", clazz, e);
       throw exception;
     }
   }
