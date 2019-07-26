ac03150d8cd480a81dc2a2d085e2f0403f46141e
Chen, Junjie <jimmyjchen@tencent.com>
2019-07-26 23:14:24 +0800

    HDDS-1839: Change topology sorting related logs in Pipeline from INFO to DEBUG. Contributed by Junjie Chen.
    This closes #1158.
    
    (cherry picked from commit c7c7a889a88dc37931c15286ca99ca41b08a3d36)

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/scm/pipeline/Pipeline.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/scm/pipeline/Pipeline.java
@@ -192,7 +192,7 @@ public final class Pipeline {
           }
         }
       }
-      LOG.info("Serialize pipeline {} with nodesInOrder{ }", id.toString(),
+      LOG.debug("Serialize pipeline {} with nodesInOrder{ }", id.toString(),
           nodes);
     }
     return builder.build();
@@ -339,7 +339,7 @@ public final class Pipeline {
             nodeIndex--;
           }
         }
-        LOG.info("Deserialize nodesInOrder {} in pipeline {}", nodesWithOrder,
+        LOG.debug("Deserialize nodesInOrder {} in pipeline {}", nodesWithOrder,
             id.toString());
         pipeline.setNodesInOrder(nodesWithOrder);
       } else if (nodesInOrder != null){
