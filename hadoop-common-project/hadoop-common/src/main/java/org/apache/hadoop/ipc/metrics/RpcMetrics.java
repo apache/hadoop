f3c2ad1f070a3f72c701d0774fe3a6ffbbb5582f
Akira Ajisaka <aajisaka@apache.org>
2019-07-29 17:37:26 -0700

    HADOOP-16435. RpcMetrics should not retained forever. Contributed by Zoltan Haindrich.
    (cherry picked from commit cbfa3f3e988b8a3f142abadfe4f18201b6438ac9)
    (cherry picked from commit 46c00aca2176400d1ba77f8af7756303b5826888)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/metrics/RpcMetrics.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/metrics/RpcMetrics.java
@@ -158,7 +158,9 @@ public class RpcMetrics {
    * Shutdown the instrumentation for the process
    */
   //@Override
-  public void shutdown() {}
+  public void shutdown() {
+    DefaultMetricsSystem.instance().unregisterSource(name);
+  }
 
   /**
    * Increment sent bytes by count
