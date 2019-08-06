08712a5268b18275e31fc62d37306fab9e2c5ed1
HUAN-PING SU <pingsutw@gmail.com>
2019-08-06 22:54:02 +0800

    HDDS-1919. Fix Javadoc in TestAuditParser

--- hadoop-ozone/tools/src/test/java/org/apache/hadoop/ozone/audit/parser/TestAuditParser.java
+++ hadoop-ozone/tools/src/test/java/org/apache/hadoop/ozone/audit/parser/TestAuditParser.java
@@ -42,7 +42,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Tests GenerateOzoneRequiredConfigurations.
+ * Tests TestAuditParser.
  */
 public class TestAuditParser {
   private static File outputBaseDir;
