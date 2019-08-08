e9780d273a226144804267ccf27a3238e142d3e0
Siyao Meng <50227127+smengcl@users.noreply.github.com>
2019-08-08 13:38:10 -0700

    HDDS-1829 On OM reload/restart OmMetrics#numKeys should be updated. Contributed by Siyao Meng.

--- hadoop-hdds/common/src/test/java/org/apache/hadoop/utils/db/TestRDBTableStore.java
+++ hadoop-hdds/common/src/test/java/org/apache/hadoop/utils/db/TestRDBTableStore.java
@@ -51,7 +51,8 @@ public class TestRDBTableStore {
       Arrays.asList(DFSUtil.bytes2String(RocksDB.DEFAULT_COLUMN_FAMILY),
           "First", "Second", "Third",
           "Fourth", "Fifth",
-          "Sixth", "Seventh");
+          "Sixth", "Seventh",
+          "Eighth");
   @Rule
   public TemporaryFolder folder = new TemporaryFolder();
   private RDBStore rdbStore = null;
@@ -245,6 +246,24 @@ public class TestRDBTableStore {
       byte[] invalidKey =
           RandomStringUtils.random(5).getBytes(StandardCharsets.UTF_8);
       Assert.assertFalse(testTable.isExist(invalidKey));
+    }
+  }
+
+  @Test
+  public void testCountEstimatedRowsInTable() throws Exception {
+    try (Table<byte[], byte[]> testTable = rdbStore.getTable("Eighth")) {
+      // Add a few keys
+      final int numKeys = 12345;
+      for (int i = 0; i < numKeys; i++) {
+        byte[] key =
+            RandomStringUtils.random(10).getBytes(StandardCharsets.UTF_8);
+        byte[] value =
+            RandomStringUtils.random(10).getBytes(StandardCharsets.UTF_8);
+        testTable.put(key, value);
+      }
+      long keyCount = testTable.getEstimatedKeyCount();
+      // The result should be larger than zero but not exceed(?) numKeys
+      Assert.assertTrue(keyCount > 0 && keyCount <= numKeys);
     }
   }
 }
