f4d061d403fc0d9b437862ca5aa5cef61bb5cfad
Siyao Meng <smeng@cloudera.com>
2019-08-14 07:24:22 -0700

    HDFS-14595. HDFS-11848 breaks API compatibility. Contributed by Siyao Meng.
    Signed-off-by: Wei-Chiu Chuang <weichiu@apache.org>
    Reviewed-by: Ayush Saxena <ayushsaxena@apache.org>

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DistributedFileSystem.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DistributedFileSystem.java
@@ -3380,6 +3380,12 @@ public class DistributedFileSystem extends FileSystem
     return dfs.listOpenFiles();
   }
 
+  @Deprecated
+  public RemoteIterator<OpenFileEntry> listOpenFiles(
+      EnumSet<OpenFilesType> openFilesTypes) throws IOException {
+    return dfs.listOpenFiles(openFilesTypes);
+  }
+
   public RemoteIterator<OpenFileEntry> listOpenFiles(
       EnumSet<OpenFilesType> openFilesTypes, String path) throws IOException {
     return dfs.listOpenFiles(openFilesTypes, path);
