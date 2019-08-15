9dc61c30769091dc2d3f11dda5ddcbbe6e5646cb
Aravindan Vijayan <avijayan@hortonworks.com>
2019-08-15 12:03:53 -0700

    HDDS-1903 : Use dynamic ports for SCM in TestSCMClientProtocolServer and TestSCMSecurityProtocolServer. (Commit 2)

--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/server/TestSCMSecurityProtocolServer.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/server/TestSCMSecurityProtocolServer.java
@@ -16,8 +16,8 @@
  */
 package org.apache.hadoop.hdds.scm.server;
 
-import static org.apache.hadoop.hdds.scm.ScmConfigKeys.OZONE_SCM_CLIENT_ADDRESS_KEY;
-import static org.apache.hadoop.hdds.scm.ScmConfigKeys.OZONE_SCM_CLIENT_BIND_HOST_DEFAULT;
+import static org.apache.hadoop.hdds.scm.ScmConfigKeys.OZONE_SCM_SECURITY_SERVICE_ADDRESS_KEY;
+import static org.apache.hadoop.hdds.scm.ScmConfigKeys.OZONE_SCM_SECURITY_SERVICE_BIND_HOST_DEFAULT;
 
 import java.net.ServerSocket;
 
@@ -25,6 +25,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
@@ -38,14 +39,19 @@ public class TestSCMSecurityProtocolServer {
 
   @Rule
   public Timeout timeout = new Timeout(1000 * 20);
+  private static int scmRpcSecurePort;
+
+  @BeforeClass
+  public static void setupClass() throws Exception {
+    scmRpcSecurePort = new ServerSocket(0).getLocalPort();
+  }
 
   @Before
   public void setUp() throws Exception {
-    int port = new ServerSocket(0).getLocalPort();
     config = new OzoneConfiguration();
-    config.set(OZONE_SCM_CLIENT_ADDRESS_KEY,
-        StringUtils.join(OZONE_SCM_CLIENT_BIND_HOST_DEFAULT, ":",
-            String.valueOf(port)));
+    config.set(OZONE_SCM_SECURITY_SERVICE_ADDRESS_KEY,
+        StringUtils.join(OZONE_SCM_SECURITY_SERVICE_BIND_HOST_DEFAULT,
+            ":", String.valueOf(scmRpcSecurePort)));
     securityProtocolServer = new SCMSecurityProtocolServer(config, null);
   }
 
