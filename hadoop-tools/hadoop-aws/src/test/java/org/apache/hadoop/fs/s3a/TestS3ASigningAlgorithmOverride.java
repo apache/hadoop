f4a2538665c0297f63ed3135b260abc77989cf3d
Saurav Verma <saurav.verma@zalando.de>
2019-08-19 13:56:47 +0200

    refactor test code

--- hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/TestS3ASigningAlgorithmOverride.java
+++ hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/TestS3ASigningAlgorithmOverride.java
@@ -20,6 +20,7 @@ package org.apache.hadoop.fs.s3a;
 
 import com.amazonaws.ClientConfiguration;
 import org.apache.hadoop.conf.Configuration;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Objects;
@@ -32,28 +33,21 @@ public class TestS3ASigningAlgorithmOverride extends AbstractS3ATestBase {
 
   @Override
   protected Configuration createConfiguration() {
+    // awsConf is instantiated here
     Configuration conf = super.createConfiguration();
     conf.set(Constants.SIGNING_ALGORITHM,
             S3ATestConstants.S3A_SIGNING_ALGORITHM);
-    LOG.debug("Inside createConfiguration...");
     return conf;
   }
 
   @Test
   public void testCustomSignerOverride() throws AssertionError {
-    LOG.debug("Inside createConfiguration...");
-    assertTrue(assertIsCustomSignerLoaded());
+    assertIsCustomSignerLoaded();
   }
 
-  private boolean assertIsCustomSignerLoaded() {
+  private void assertIsCustomSignerLoaded() {
     final ClientConfiguration awsConf = new ClientConfiguration();
-    LOG.debug("Inside assertIsCustomSignerLoaded...");
-    return assertEquals(awsConf.getSignerOverride(),
-            S3ATestConstants.S3A_SIGNING_ALGORITHM
-    );
-  }
-
-  private boolean assertEquals(String str1, String str2) {
-    return Objects.equals(str1, str2);
+    Assert.assertEquals(awsConf.getSignerOverride(),
+            S3ATestConstants.S3A_SIGNING_ALGORITHM);
   }
 }
