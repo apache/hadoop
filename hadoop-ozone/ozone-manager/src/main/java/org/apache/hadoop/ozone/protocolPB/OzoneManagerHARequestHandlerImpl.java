8ad077ec4053022a25f58f97e485b97f3da85995
Bharat Viswanadham <bharat@apache.org>
2019-08-19 17:41:16 -0700

    HDDS-1975. Implement OM RenewDelegationToken request to use Cache and DoubleBuffer.

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/protocolPB/OzoneManagerHARequestHandlerImpl.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/protocolPB/OzoneManagerHARequestHandlerImpl.java
@@ -75,6 +75,8 @@ public class OzoneManagerHARequestHandlerImpl
     case RemoveAcl:
     case SetAcl:
     case GetDelegationToken:
+    case CancelDelegationToken:
+    case RenewDelegationToken:
       //TODO: We don't need to pass transactionID, this will be removed when
       // complete write requests is changed to new model. And also we can
       // return OMClientResponse, then adding to doubleBuffer can be taken
