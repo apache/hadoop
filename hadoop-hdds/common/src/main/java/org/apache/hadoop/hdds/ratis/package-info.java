94546bb6e977189821353071b24b2a0b6f9e5a85
Aravindan Vijayan <avijayan@hortonworks.com>
2019-08-21 22:22:54 -0700

    HDDS-2008 : Wrong package for RatisHelper class in hadoop-hdds/common module.

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/ratis/package-info.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/ratis/package-info.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.ratis;
+package org.apache.hadoop.hdds.ratis;
 
 /**
  * This package contains classes related to Apache Ratis.
