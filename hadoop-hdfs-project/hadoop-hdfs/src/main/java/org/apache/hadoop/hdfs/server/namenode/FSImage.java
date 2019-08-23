d2bb764ffa65d6f6ec806c040814263b46eeb5b3
Chen Liang <cliang@apache.org>
2019-08-23 12:17:34 -0700

    HDFS-14674. [SBN read] Got an unexpected txid when tail editlog. Contributed by wangzhaohui.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSImage.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSImage.java
@@ -891,7 +891,8 @@ public class FSImage implements Closeable {
     StartupProgress prog = NameNode.getStartupProgress();
     prog.beginPhase(Phase.LOADING_EDITS);
     
-    long prevLastAppliedTxId = lastAppliedTxId;  
+    long prevLastAppliedTxId = lastAppliedTxId;
+    long remainingReadTxns = maxTxnsToRead;
     try {    
       FSEditLogLoader loader = new FSEditLogLoader(target, lastAppliedTxId);
       
@@ -908,8 +909,8 @@ public class FSImage implements Closeable {
               (lastAppliedTxId + 1) + logSuppressed);
         }
         try {
-          loader.loadFSEdits(editIn, lastAppliedTxId + 1, maxTxnsToRead,
-              startOpt, recovery);
+          remainingReadTxns -= loader.loadFSEdits(editIn, lastAppliedTxId + 1,
+                  remainingReadTxns, startOpt, recovery);
         } finally {
           // Update lastAppliedTxId even in case of error, since some ops may
           // have been successfully applied before the error.
@@ -919,6 +920,9 @@ public class FSImage implements Closeable {
         if (editIn.getLastTxId() != HdfsServerConstants.INVALID_TXID
             && recovery != null) {
           lastAppliedTxId = editIn.getLastTxId();
+        }
+        if (remainingReadTxns <= 0) {
+          break;
         }
       }
     } finally {
