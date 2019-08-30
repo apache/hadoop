0d405fb5c6562ebbfbe5091bd17b2d87585822c6
dineshchitlangia <dineshchitlangia@gmail.com>
2019-08-30 12:55:36 -0400

    HDDS-2014. Create Symmetric Key for GDPR (#1362)

---
+++ hadoop-ozone/common/src/test/java/org/apache/hadoop/ozone/security/TestGDPRSymmetricKey.java
@@ -0,0 +1,66 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership.  The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.hadoop.ozone.security;
+
+import org.apache.hadoop.ozone.OzoneConsts;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests GDPRSymmetricKey structure.
+ */
+public class TestGDPRSymmetricKey {
+
+  @Test
+  public void testKeyGenerationWithDefaults() throws Exception {
+    GDPRSymmetricKey gkey = new GDPRSymmetricKey();
+
+    Assert.assertTrue(gkey.getCipher().getAlgorithm()
+        .equalsIgnoreCase(OzoneConsts.GDPR_ALGORITHM_NAME));
+
+    gkey.getKeyDetails().forEach(
+        (k, v) -> Assert.assertTrue(v.length() > 0));
+  }
+
+  @Test
+  public void testKeyGenerationWithValidInput() throws Exception {
+    GDPRSymmetricKey gkey = new GDPRSymmetricKey(
+        "ApacheHadoopOzoneIsAnObjectStore",
+        OzoneConsts.GDPR_ALGORITHM_NAME);
+
+    Assert.assertTrue(gkey.getCipher().getAlgorithm()
+        .equalsIgnoreCase(OzoneConsts.GDPR_ALGORITHM_NAME));
+
+    gkey.getKeyDetails().forEach(
+        (k, v) -> Assert.assertTrue(v.length() > 0));
+  }
+
+  @Test
+  public void testKeyGenerationWithInvalidInput() throws Exception {
+    GDPRSymmetricKey gkey = null;
+    try{
+      gkey = new GDPRSymmetricKey("ozone",
+          OzoneConsts.GDPR_ALGORITHM_NAME);
+    } catch (IllegalArgumentException ex) {
+      Assert.assertTrue(ex.getMessage()
+          .equalsIgnoreCase("Secret must be exactly 32 characters"));
+      Assert.assertTrue(gkey == null);
+    }
+  }
+
+
+}
