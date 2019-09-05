3dc7e3d0235ab4b5d7cda9afd736ab3f3b18dbab
Roger Liu <roliu@microsoft.com>
2019-09-05 16:39:56 -0700

    reverted file to its original state in master

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/client/ConfiguredRMFailoverProxyProvider.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/client/ConfiguredRMFailoverProxyProvider.java
@@ -25,8 +25,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.conf.Configuration;
@@ -39,14 +39,14 @@ import org.apache.hadoop.yarn.conf.YarnConfiguration;
 @InterfaceStability.Unstable
 public class ConfiguredRMFailoverProxyProvider<T>
     implements RMFailoverProxyProvider<T> {
-  private static final Logger LOG =
-    LoggerFactory.getLogger(ConfiguredRMFailoverProxyProvider.class);
+  private static final Log LOG =
+      LogFactory.getLog(ConfiguredRMFailoverProxyProvider.class);
 
   private int currentProxyIndex = 0;
   Map<String, T> proxies = new HashMap<String, T>();
 
-  protected RMProxy<T> rmProxy;
-  protected Class<T> protocol;
+  private RMProxy<T> rmProxy;
+  private Class<T> protocol;
   protected YarnConfiguration conf;
   protected String[] rmServiceIds;
 
@@ -71,9 +71,15 @@ public class ConfiguredRMFailoverProxyProvider<T>
             YarnConfiguration.DEFAULT_CLIENT_FAILOVER_RETRIES_ON_SOCKET_TIMEOUTS));
   }
 
-  @Override
-  public Class<T> getInterface() {
-    return protocol;
+  private T getProxyInternal() {
+    try {
+      final InetSocketAddress rmAddress = rmProxy.getRMAddress(conf, protocol);
+      return RMProxy.getProxy(conf, protocol, rmAddress);
+    } catch (IOException ioe) {
+      LOG.error("Unable to create proxy to the ResourceManager " +
+          rmServiceIds[currentProxyIndex], ioe);
+      return null;
+    }
   }
 
   @Override
@@ -87,22 +93,16 @@ public class ConfiguredRMFailoverProxyProvider<T>
     return new ProxyInfo<T>(current, rmId);
   }
 
-  protected T getProxyInternal() {
-    try {
-      final InetSocketAddress rmAddress = rmProxy.getRMAddress(conf, protocol);
-      return rmProxy.getProxy(conf, protocol, rmAddress);
-    } catch (IOException ioe) {
-      LOG.error("Unable to create proxy to the ResourceManager " +
-          rmServiceIds[currentProxyIndex], ioe);
-      return null;
-    }
-  }
-
   @Override
   public synchronized void performFailover(T currentProxy) {
     currentProxyIndex = (currentProxyIndex + 1) % rmServiceIds.length;
     conf.set(YarnConfiguration.RM_HA_ID, rmServiceIds[currentProxyIndex]);
     LOG.info("Failing over to " + rmServiceIds[currentProxyIndex]);
+  }
+
+  @Override
+  public Class<T> getInterface() {
+    return protocol;
   }
 
   /**
