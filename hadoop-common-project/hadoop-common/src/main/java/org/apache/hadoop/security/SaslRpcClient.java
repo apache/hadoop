ea7639748f6f6885cd2014429b14894f766d2625
anaeimian <armannaeimian@gmail.com>
2019-09-06 16:31:50 -0400

    Avoiding logging Sasl message
    Based on a previous issue (HADOOP-11962), Sasl message should not be logged. Also, all of the instances of logging Sasl message have been removed except this one.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/SaslRpcClient.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/SaslRpcClient.java
@@ -449,9 +449,6 @@ public class SaslRpcClient {
 
   private void sendSaslMessage(OutputStream out, RpcSaslProto message)
       throws IOException {
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("Sending sasl message "+message);
-    }
     ResponseBuffer buf = new ResponseBuffer();
     saslHeader.writeDelimitedTo(buf);
     message.writeDelimitedTo(buf);
