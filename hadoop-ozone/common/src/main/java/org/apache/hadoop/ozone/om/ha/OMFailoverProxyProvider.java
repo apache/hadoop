9b6f2a00746f5ae55515ce6d402916ada66c0b1f
Siyao Meng <smeng@cloudera.com>
2019-09-09 15:00:34 -0700

    Remove unused import in OMFailoverProxyProvider
    Change-Id: I253c1c3d446392d4e83445c93f7643ab15e60560

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/ha/OMFailoverProxyProvider.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/ha/OMFailoverProxyProvider.java
@@ -45,7 +45,6 @@ import java.util.List;
 import java.util.Map;
 
 import static org.apache.hadoop.ozone.om.OMConfigKeys.OZONE_OM_ADDRESS_KEY;
-import static org.apache.hadoop.ozone.om.OMConfigKeys.OZONE_OM_SERVICE_IDS_KEY;
 
 /**
  * A failover proxy provider implementation which allows clients to configure
