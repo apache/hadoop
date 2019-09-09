0e88bcd8e6b43ed2b3ed2f1e4e68193562eb3b38
Jonathan Hung <jhung@linkedin.com>
2019-09-09 00:23:04 -0700

    YARN-9820. RM logs InvalidStateTransitionException when app is submitted. Contributed by Prabhu Joseph

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmapp/RMAppImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmapp/RMAppImpl.java
@@ -1036,7 +1036,7 @@ public class RMAppImpl implements RMApp, Recoverable {
             app.callerContext);
         appState.setApplicationTimeouts(app.getApplicationTimeouts());
         appState.setLaunchTime(event.getTimestamp());
-        app.rmContext.getStateStore().updateApplicationState(appState);
+        app.rmContext.getStateStore().updateApplicationState(appState, false);
         app.launchTime = event.getTimestamp();
       }
     }
