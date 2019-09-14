4126e67e31d9431477aa39903bb03c0350f9c389
Ayush Saxena <ayushsaxena@apache.org>
2019-09-14 10:12:17 +0530

    HDFS-14303. Addendum: check block directory logic not correct when there is only meta file, print no meaning warn log. Contributed by qiang Liu.

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/datanode/TestDirectoryScanner.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/datanode/TestDirectoryScanner.java
@@ -1132,8 +1132,9 @@ public class TestDirectoryScanner {
 
   @Test
   public void testDirectoryScannerInFederatedCluster() throws Exception {
+    HdfsConfiguration conf = new HdfsConfiguration(CONF);
     //Create Federated cluster with two nameservices and one DN
-    try (MiniDFSCluster cluster = new MiniDFSCluster.Builder(CONF)
+    try (MiniDFSCluster cluster = new MiniDFSCluster.Builder(conf)
         .nnTopology(MiniDFSNNTopology.simpleHAFederatedTopology(2))
         .numDataNodes(1).build()) {
       cluster.waitActive();
@@ -1150,7 +1151,7 @@ public class TestDirectoryScanner {
       int bp2Files = 2;
       writeFile(fs2, bp2Files);
       //Call the Directory scanner
-      scanner = new DirectoryScanner(dataNode, fds, CONF);
+      scanner = new DirectoryScanner(dataNode, fds, conf);
       scanner.setRetainDiffs(true);
       scanner.reconcile();
       //Check blocks in corresponding BP
