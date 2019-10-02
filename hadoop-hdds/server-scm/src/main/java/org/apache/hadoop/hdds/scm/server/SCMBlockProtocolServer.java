dc1e393302c1468ee84520f3eddda1cbe28f4a8e
S O'Donnell <sodonnell@cloudera.com>
2019-10-02 18:47:28 +0100

    Fixed another style issue (unused import) after removing the random logic

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/server/SCMBlockProtocolServer.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/server/SCMBlockProtocolServer.java
@@ -26,7 +26,6 @@ import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 import org.apache.hadoop.fs.CommonConfigurationKeys;
