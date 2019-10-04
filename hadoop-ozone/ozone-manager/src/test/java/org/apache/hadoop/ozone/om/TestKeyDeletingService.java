f99694a97ac939478a6bf9083cc2dca33fe47d1e
Elek, Márton <elek@users.noreply.github.com>
2019-10-04 18:48:49 +0200

    HDDS-2237. KeyDeletingService throws NPE if it's started too early (#1584)

--- hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/TestKeyDeletingService.java
+++ hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/TestKeyDeletingService.java
@@ -95,6 +95,7 @@ public class TestKeyDeletingService {
         new KeyManagerImpl(
             new ScmBlockLocationTestingClient(null, null, 0),
             metaMgr, conf, UUID.randomUUID().toString(), null);
+    keyManager.start(conf);
     final int keyCount = 100;
     createAndDeleteKeys(keyManager, keyCount, 1);
     KeyDeletingService keyDeletingService =
@@ -117,6 +118,7 @@ public class TestKeyDeletingService {
         new KeyManagerImpl(
             new ScmBlockLocationTestingClient(null, null, 1),
             metaMgr, conf, UUID.randomUUID().toString(), null);
+    keyManager.start(conf);
     final int keyCount = 100;
     createAndDeleteKeys(keyManager, keyCount, 1);
     KeyDeletingService keyDeletingService =
@@ -144,6 +146,7 @@ public class TestKeyDeletingService {
         new KeyManagerImpl(
             new ScmBlockLocationTestingClient(null, null, 1),
             metaMgr, conf, UUID.randomUUID().toString(), null);
+    keyManager.start(conf);
     final int keyCount = 100;
     createAndDeleteKeys(keyManager, keyCount, 0);
     KeyDeletingService keyDeletingService =
