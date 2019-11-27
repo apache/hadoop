aa9cd0a2d641bef91b3d9d4054d52e8b0ee61bc1
Sneha Vijayarajan <sneha.vijayarajan@gmail.com>
2019-11-27 15:07:27 -0800

    HADOOP-16660. ABFS: Make RetryCount in ExponentialRetryPolicy Configurable.
    Contributed by Sneha Vijayarajan.

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/AzureBlobFileSystemStore.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/AzureBlobFileSystemStore.java
@@ -1118,7 +1118,9 @@ public class AzureBlobFileSystemStore {
       tokenProvider = abfsConfiguration.getTokenProvider();
     }
 
-    this.client =  new AbfsClient(baseUrl, creds, abfsConfiguration, new ExponentialRetryPolicy(), tokenProvider, abfsPerfTracker);
+    this.client = new AbfsClient(baseUrl, creds, abfsConfiguration,
+        new ExponentialRetryPolicy(abfsConfiguration.getMaxIoRetries()),
+        tokenProvider, abfsPerfTracker);
   }
 
   private String getOctalNotation(FsPermission fsPermission) {
