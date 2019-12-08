b5235f1ed05f6506aa2c40825b2b647475a4d91a
Jonathan Hung <jhung@linkedin.com>
2019-12-08 15:52:17 -0800

    YARN-10012. Guaranteed and max capacity queue metrics for custom resources
    (cherry picked from commit 92bce918dc5d03560169642b71636800680a3292)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/MockNodes.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/MockNodes.java
@@ -36,6 +36,7 @@ import org.apache.hadoop.yarn.api.records.ResourceUtilization;
 import org.apache.hadoop.yarn.factories.RecordFactory;
 import org.apache.hadoop.yarn.factory.providers.RecordFactoryProvider;
 import org.apache.hadoop.yarn.nodelabels.CommonNodeLabelsManager;
+import org.apache.hadoop.yarn.resourcetypes.ResourceTypesTestHelper;
 import org.apache.hadoop.yarn.server.api.protocolrecords.NodeHeartbeatResponse;
 import org.apache.hadoop.yarn.server.api.records.OpportunisticContainersStatus;
 import org.apache.hadoop.yarn.server.resourcemanager.rmnode.RMNode;
@@ -91,6 +92,12 @@ public class MockNodes {
     rs.setMemorySize(mem);
     return rs;
   }
+
+  public static Resource newResource(long memory, int vCores,
+      Map<String, String> customResources) {
+    return ResourceTypesTestHelper.newResource(memory, vCores, customResources);
+  }
+
 
   public static Resource newUsedResource(Resource total) {
     Resource rs = recordFactory.newRecordInstance(Resource.class);
