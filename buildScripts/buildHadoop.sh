3ebb970c5f5e2d35a9ecfe3ef1f80714816364e6
AmithshaS <amithsha.s@flipkart.com>
2019-12-14 11:02:41 +0530

    find command to find pom.xml with snappy false and it will change to true

--- buildScripts/buildHadoop.sh
+++ buildScripts/buildHadoop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#sed -i "s/<require.snappy>false<\/require.snappy>/<require.snappy>true<\/require.snappy>/g" */*/pom.xml
+find . -name pom.xml -exec sed -i -e 's/<require.snappy>false<\/require.snappy>/<require.snappy>true<\/require.snappy>/g' {} \;
 mvn package -Pdist,native -Dyarn.arg="install --offline" -DskipTests -Drequire.snappy -Dmaven.javadoc.skip=true -U -X 
