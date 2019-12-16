5f17f9ff348c2307b3f190fc5ded111b372de93e
AmithshaS <amithsha.s@flipkart.com>
2019-12-16 16:16:14 +0530

    Upated the dependencies to install

--- buildScripts/installDependencies.sh
+++ buildScripts/installDependencies.sh
@@ -6,4 +6,4 @@ sudo apt-get update
 sudo apt-get install protobuf-compiler liblzo2-2 libc6 --allow-unauthenticated -y
 sudo apt-get install pkg-config  --yes --allow-unauthenticated
 sudo apt-get install --yes --allow-unauthenticated build-essential
-sudo apt-get install --yes --allow-unauthenticated  make  libsnappy-dev bzip2 libbz2-dev libjansson-dev zlib1g-dev libncurses5-dev libssl-dev
+sudo apt-get install --yes --allow-unauthenticated  make  libsnappy-dev bzip2 libbz2-dev libjansson-dev zlib1g-dev libncurses5-dev libssl-dev libfuse-dev fuse cyrus-dev
