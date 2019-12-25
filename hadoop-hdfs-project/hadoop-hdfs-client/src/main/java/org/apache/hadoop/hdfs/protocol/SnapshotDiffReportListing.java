328c522d491c0044dda700f8899bcbea6018e76d
csanivar <csanivar@yahoo-corp.jp>
2019-12-25 10:00:37 +0900

    Replace curator-shaded guava import with the standard one

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/protocol/SnapshotDiffReportListing.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/protocol/SnapshotDiffReportListing.java
@@ -20,7 +20,7 @@ package org.apache.hadoop.hdfs.protocol;
 import java.util.Collections;
 import java.util.List;
 
-import org.apache.curator.shaded.com.google.common.base.Preconditions;
+import com.google.common.base.Preconditions;
 
 import org.apache.hadoop.hdfs.DFSUtilClient;
 
