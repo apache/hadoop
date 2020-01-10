24e6a9e43a210cdecaa8e87926eef09c869988f9
Szilard Nemeth <snemeth@apache.org>
2020-01-10 21:14:07 +0100

    YARN-10067. Add dry-run feature to FS-CS converter tool. Contributed by Peter Bacsko

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/converter/TestFSConfigToCSConfigArgumentHandler.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/converter/TestFSConfigToCSConfigArgumentHandler.java
@@ -16,7 +16,14 @@
 
 package org.apache.hadoop.yarn.server.resourcemanager.scheduler.fair.converter;
 
-import com.google.common.collect.Lists;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -31,13 +38,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import com.google.common.collect.Lists;
 
 /**
  * Unit tests for FSConfigToCSConfigArgumentHandler.
@@ -53,12 +54,18 @@ public class TestFSConfigToCSConfigArgumentHandler {
 
   @Mock
   private FSConfigToCSConfigConverter mockConverter;
+
+  private DryRunResultHolder dryRunResultHolder;
+  private ConversionOptions conversionOptions;
+
   private FSConfigConverterTestCommons fsTestCommons;
 
   @Before
   public void setUp() throws IOException {
     fsTestCommons = new FSConfigConverterTestCommons();
     fsTestCommons.setUp();
+    dryRunResultHolder = new DryRunResultHolder();
+    conversionOptions = new ConversionOptions(dryRunResultHolder, false);
   }
 
   @After
@@ -80,7 +87,15 @@ public class TestFSConfigToCSConfigArgumentHandler {
 
 
   private FSConfigToCSConfigArgumentHandler createArgumentHandler() {
-    return new FSConfigToCSConfigArgumentHandler(mockConverter);
+    FSConfigToCSConfigArgumentHandler argumentHandler =
+        new FSConfigToCSConfigArgumentHandler();
+    argumentHandler.setConverterSupplier(this::getMockConverter);
+
+    return argumentHandler;
+  }
+
+  private FSConfigToCSConfigConverter getMockConverter() {
+    return mockConverter;
   }
 
   private static String[] getDefaultArgumentsAsArray() {
@@ -252,7 +267,7 @@ public class TestFSConfigToCSConfigArgumentHandler {
         ArgumentCaptor.forClass(FSConfigToCSConfigConverterParams.class);
 
     FSConfigToCSConfigArgumentHandler argumentHandler =
-        new FSConfigToCSConfigArgumentHandler(mockConverter);
+        createArgumentHandler();
 
     String[] args = getArgumentsAsArrayWithDefaults("-f",
         FSConfigConverterTestCommons.FS_ALLOC_FILE,
@@ -284,7 +299,7 @@ public class TestFSConfigToCSConfigArgumentHandler {
         ArgumentCaptor.forClass(FSConfigToCSConfigConverterParams.class);
 
     FSConfigToCSConfigArgumentHandler argumentHandler =
-        new FSConfigToCSConfigArgumentHandler(mockConverter);
+        createArgumentHandler();
 
     String[] args = getArgumentsAsArrayWithDefaults("-f",
         FSConfigConverterTestCommons.FS_ALLOC_FILE,
@@ -316,7 +331,7 @@ public class TestFSConfigToCSConfigArgumentHandler {
         ArgumentCaptor.forClass(FSConfigToCSConfigConverterParams.class);
 
     FSConfigToCSConfigArgumentHandler argumentHandler =
-        new FSConfigToCSConfigArgumentHandler(mockConverter);
+        createArgumentHandler();
 
     String[] args = getArgumentsAsArrayWithDefaults("-f",
         FSConfigConverterTestCommons.FS_ALLOC_FILE,
@@ -378,5 +393,55 @@ public class TestFSConfigToCSConfigArgumentHandler {
     assertEquals("Return value", -1, retVal);
     assertTrue("Error content missing", fsTestCommons.getErrContent()
         .toString().contains("Fatal error during FS config conversion"));
+  }
+
+  @Test
+  public void testDryRunWhenPreconditionExceptionOccurs() throws Exception {
+    testDryRunWithException(new PreconditionException("test"),
+        "Cannot start FS config conversion");
+  }
+
+  @Test
+  public void testDryRunWhenUnsupportedPropertyExceptionExceptionOccurs()
+      throws Exception {
+    testDryRunWithException(new UnsupportedPropertyException("test"),
+        "Unsupported property/setting encountered");
+  }
+
+  @Test
+  public void testDryRunWhenConversionExceptionExceptionOccurs()
+      throws Exception {
+    testDryRunWithException(new ConversionException("test"),
+        "Fatal error during FS config conversion");
+  }
+
+  @Test
+  public void testDryRunWhenIllegalArgumentExceptionExceptionOccurs()
+      throws Exception {
+    testDryRunWithException(new IllegalArgumentException("test"),
+        "Fatal error during FS config conversion");
+  }
+
+  private void testDryRunWithException(Exception exception,
+      String expectedErrorMessage) throws Exception {
+    setupFSConfigConversionFiles(true);
+
+    String[] args = getArgumentsAsArrayWithDefaults("-f",
+        FSConfigConverterTestCommons.FS_ALLOC_FILE,
+        "-r", FSConfigConverterTestCommons.CONVERSION_RULES_FILE, "-p",
+        "-d");
+    FSConfigToCSConfigArgumentHandler argumentHandler =
+        new FSConfigToCSConfigArgumentHandler(conversionOptions);
+    argumentHandler.setConverterSupplier(this::getMockConverter);
+
+    Mockito.doThrow(exception).when(mockConverter)
+      .convert(ArgumentMatchers.any(FSConfigToCSConfigConverterParams.class));
+
+    int retVal = argumentHandler.parseAndConvert(args);
+    assertEquals("Return value", -1, retVal);
+    assertEquals("Number of errors", 1, dryRunResultHolder.getErrors().size());
+    String error = dryRunResultHolder.getErrors().iterator().next();
+    assertTrue("Unexpected error message",
+        error.contains(expectedErrorMessage));
   }
 }
--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/converter/TestFSConfigToCSConfigRuleHandler.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/converter/TestFSConfigToCSConfigRuleHandler.java
@@ -30,6 +30,8 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Properties;
+
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -41,10 +43,26 @@ public class TestFSConfigToCSConfigRuleHandler {
   private static final String WARNING = "warning";
 
   private FSConfigToCSConfigRuleHandler ruleHandler;
+  private DryRunResultHolder dryRunResultHolder;
+
+  @Before
+  public void setup() {
+    dryRunResultHolder = new DryRunResultHolder();
+  }
+
+
+  private ConversionOptions createDryRunConversionOptions() {
+    return new ConversionOptions(dryRunResultHolder, true);
+  }
+
+  private ConversionOptions createDefaultConversionOptions() {
+    return new ConversionOptions(dryRunResultHolder, false);
+  }
 
   @Test
   public void testInitPropertyActionsToWarning() throws IOException {
-    ruleHandler = new FSConfigToCSConfigRuleHandler(new Properties());
+    ruleHandler = new FSConfigToCSConfigRuleHandler(new Properties(),
+        createDefaultConversionOptions());
 
     ruleHandler.handleChildQueueCount("test", 1);
     ruleHandler.handleDynamicMaxAssign();
@@ -69,7 +87,8 @@ public class TestFSConfigToCSConfigRuleHandler {
     rules.put(USER_MAX_APPS_DEFAULT, WARNING);
     rules.put(USER_MAX_RUNNING_APPS, WARNING);
 
-    ruleHandler = new FSConfigToCSConfigRuleHandler(rules);
+    ruleHandler = new FSConfigToCSConfigRuleHandler(rules,
+        createDefaultConversionOptions());
 
     ruleHandler.handleDynamicMaxAssign();
     ruleHandler.handleMaxCapacityPercentage("test");
@@ -94,7 +113,8 @@ public class TestFSConfigToCSConfigRuleHandler {
     rules.put(USER_MAX_RUNNING_APPS, ABORT);
     rules.put(MAX_CHILD_QUEUE_LIMIT, "1");
 
-    ruleHandler = new FSConfigToCSConfigRuleHandler(rules);
+    ruleHandler = new FSConfigToCSConfigRuleHandler(rules,
+        createDefaultConversionOptions());
 
     expectAbort(() -> ruleHandler.handleChildQueueCount("test", 2),
         ConversionException.class);
@@ -113,9 +133,44 @@ public class TestFSConfigToCSConfigRuleHandler {
     Properties rules = new Properties();
     rules.put(MAX_CHILD_QUEUE_LIMIT, "abc");
 
-    ruleHandler = new FSConfigToCSConfigRuleHandler(rules);
+    ruleHandler = new FSConfigToCSConfigRuleHandler(rules,
+        createDefaultConversionOptions());
 
     ruleHandler.handleChildQueueCount("test", 1);
+  }
+
+  @Test
+  public void testDryRunWarning() {
+    Properties rules = new Properties();
+
+    ruleHandler = new FSConfigToCSConfigRuleHandler(rules,
+        createDryRunConversionOptions());
+
+    ruleHandler.handleDynamicMaxAssign();
+    ruleHandler.handleMaxChildCapacity();
+
+    assertEquals("Number of warnings", 2,
+        dryRunResultHolder.getWarnings().size());
+    assertEquals("Number of errors", 0,
+        dryRunResultHolder.getErrors().size());
+  }
+
+  @Test
+  public void testDryRunError() {
+    Properties rules = new Properties();
+    rules.put(DYNAMIC_MAX_ASSIGN, ABORT);
+    rules.put(MAX_CHILD_CAPACITY, ABORT);
+
+    ruleHandler = new FSConfigToCSConfigRuleHandler(rules,
+        createDryRunConversionOptions());
+
+    ruleHandler.handleDynamicMaxAssign();
+    ruleHandler.handleMaxChildCapacity();
+
+    assertEquals("Number of warnings", 0,
+        dryRunResultHolder.getWarnings().size());
+    assertEquals("Number of errors", 2,
+        dryRunResultHolder.getErrors().size());
   }
 
   private void expectAbort(VoidCall call) {
