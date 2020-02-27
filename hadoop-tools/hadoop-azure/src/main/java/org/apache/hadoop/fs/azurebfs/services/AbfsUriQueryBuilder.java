f7a88ebde2f18e4a6925672f499fe212309d2f03
Sneha Vijayarajan <sneha.vijayarajan@gmail.com>
2020-02-27 11:40:19 +0530

    Revert "Fixing the Findbugs different way"
    This reverts commit dfd932131fd6192c9da4fb67a150e96fbd719bca.

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/services/AbfsUriQueryBuilder.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/services/AbfsUriQueryBuilder.java
@@ -42,10 +42,6 @@ public class AbfsUriQueryBuilder {
   }
 
   public void setSASToken(final String sasToken) {
-    if ((sasToken == null) || sasToken.isEmpty()) {
-      throw new UnsupportedOperationException("SASToken received is empty or null");
-    }
-
     this.sasToken = sasToken;
   }
 
