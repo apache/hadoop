030ae584f6a9f76dba20aeb9c4bbe2aa6ad6e399
pustota2009 <61382543+pustota2009@users.noreply.github.com>
2020-03-09 15:47:50 +0300

    Update TestShortCircuitCache.java

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/shortcircuit/TestShortCircuitCache.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/shortcircuit/TestShortCircuitCache.java
@@ -877,7 +877,7 @@ public class TestShortCircuitCache {
             return peerCache;
           });
 
-      Mockito.when(clientContext.getShortCircuitCache(0)).thenAnswer(
+      Mockito.when(clientContext.getShortCircuitCache(blk.getBlock().getBlockId())).thenAnswer(
           (Answer<ShortCircuitCache>) shortCircuitCacheCall -> {
             ShortCircuitCache cache = Mockito.mock(ShortCircuitCache.class);
             Mockito.when(cache.allocShmSlot(
