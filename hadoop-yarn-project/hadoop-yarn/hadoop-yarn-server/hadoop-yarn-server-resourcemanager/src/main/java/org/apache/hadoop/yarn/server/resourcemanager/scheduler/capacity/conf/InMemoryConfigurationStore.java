7abc6221a33b792429d4326ee01b5c5610a21a81
Szilard Nemeth <snemeth@apache.org>
2020-04-07 15:51:55 +0200

    YARN-10001. Add explanation of unimplemented methods in InMemoryConfigurationStore. Contributed by Siddharth Ahuja
    (cherry picked from commit 45362a9f4cbe512ee4cd6b7f65aa47d59fee612e)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/conf/InMemoryConfigurationStore.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/conf/InMemoryConfigurationStore.java
@@ -81,35 +81,71 @@ public class InMemoryConfigurationStore extends YarnConfigurationStore {
     return configVersion;
   }
 
+  /**
+   * Configuration mutations not logged (i.e. not persisted) but directly
+   * confirmed. As such, a list of persisted configuration mutations does not
+   * exist.
+   * @return null Configuration mutation list not applicable for this store.
+   */
   @Override
   public List<LogMutation> getConfirmedConfHistory(long fromId) {
     // Unimplemented.
     return null;
   }
 
+  /**
+   * Configuration mutations not logged (i.e. not persisted) but directly
+   * confirmed. As such, a list of persisted configuration mutations does not
+   * exist.
+   * @return null Configuration mutation list not applicable for this store.
+   */
   @Override
   protected LinkedList<LogMutation> getLogs() {
     // Unimplemented.
     return null;
   }
 
+  /**
+   * Configuration mutations applied directly in-memory. As such, there is no
+   * persistent configuration store.
+   * As there is no configuration store for versioning purposes,
+   * a conf store version is not applicable.
+   * @return null Conf store version not applicable for this store.
+   * @throws Exception
+   */
   @Override
   public Version getConfStoreVersion() throws Exception {
     // Does nothing.
     return null;
   }
 
+  /**
+   * Configuration mutations not logged (i.e. not persisted). As such, they are
+   * not persisted and not versioned. Hence, no version information to store.
+   * @throws Exception
+   */
   @Override
   public void storeVersion() throws Exception {
     // Does nothing.
   }
 
+  /**
+   * Configuration mutations not logged (i.e. not persisted). As such, they are
+   * not persisted and not versioned. Hence, a current version is not
+   * applicable.
+   * @return null A current version not applicable for this store.
+   */
   @Override
   public Version getCurrentVersion() {
     // Does nothing.
     return null;
   }
 
+  /**
+   * Configuration mutations not logged (i.e. not persisted). As such, they are
+   * not persisted and not versioned. Hence, version is always compatible,
+   * since it is in-memory.
+   */
   @Override
   public void checkVersion() {
     // Does nothing. (Version is always compatible since it's in memory)
