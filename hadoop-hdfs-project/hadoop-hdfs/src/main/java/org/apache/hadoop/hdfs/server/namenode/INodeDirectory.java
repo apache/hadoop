4ebe78496f8f28dc40fb232eb5c55ca68e9a178a
Mingliang Liu <liuml07@apache.org>
2020-04-22 13:31:02 -0700

    HDFS-15276. Concat on INodeRefernce fails with illegal state exception. Contributed by hemanthboyina

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/INodeDirectory.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/INodeDirectory.java
@@ -510,7 +510,7 @@ public class INodeDirectory extends INodeWithAdditionalFields
     }
 
     final INode removed = children.remove(i);
-    Preconditions.checkState(removed == child);
+    Preconditions.checkState(removed.equals(child));
     return true;
   }
 
