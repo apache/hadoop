5d36f969a3395e1a9f2977d57aee3f0f1b0e3521
李经纶 <lijinglun@xiaomi.com>
2020-05-01 15:27:00 +0800

    1. Fix checkstyle. 2. Fix unit test. 3. Add license. 4. Handle fs doesn't support concat. 5. Add documntation.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/shell/Concat.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/shell/Concat.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.fs.shell;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -12,16 +29,18 @@ import java.util.LinkedList;
 /**
  * Concat the given files.
  */
-@InterfaceAudience.Private @InterfaceStability.Unstable public class Concat
-    extends FsCommand {
+@InterfaceAudience.Private
+@InterfaceStability.Unstable
+public class Concat extends FsCommand {
   public static void registerCommands(CommandFactory factory) {
     factory.addClass(Concat.class, "-concat");
   }
 
   public static final String NAME = "concat";
   public static final String USAGE = "<target path> <src path> <src path> ...";
-  public static final String DESCRIPTION =
-      "Concatenate existing source files into the target file.";
+  public static final String DESCRIPTION = "Concatenate existing source files"
+      + " into the target file. Target file and source files should be in the"
+      + " same directory.";
   private static FileSystem tstFs; // test only.
 
   @Override
@@ -52,7 +71,12 @@ import java.util.LinkedList;
     if (tstFs != null) {
       fs = tstFs;
     }
-    fs.concat(target.path, srcArray);
+    try {
+      fs.concat(target.path, srcArray);
+    } catch (UnsupportedOperationException exception) {
+      throw new IOException("Dest filesystem '" + fs.getUri().getScheme()
+          + "' doesn't support concat.");
+    }
   }
 
   @VisibleForTesting
--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/shell/TestFsShellConcat.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/shell/TestFsShellConcat.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.fs.shell;
 
 import org.apache.hadoop.conf.Configuration;
@@ -11,9 +28,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import java.io.IOException;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.URI;
 import java.util.Random;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -25,10 +45,11 @@ import static org.junit.Assert.assertTrue;
  */
 public class TestFsShellConcat {
 
-  static Configuration conf;
-  static FsShell shell;
-  static LocalFileSystem lfs;
-  static Path testRootDir, dstPath;
+  private static Configuration conf;
+  private static FsShell shell;
+  private static LocalFileSystem lfs;
+  private static Path testRootDir;
+  private static Path dstPath;
 
   @Before
   public void before() throws IOException {
@@ -44,6 +65,7 @@ public class TestFsShellConcat {
     lfs.mkdirs(testRootDir);
     lfs.setWorkingDirectory(testRootDir);
     dstPath = new Path(testRootDir, "dstFile");
+    lfs.create(dstPath).close();
 
     Random random = new Random();
     for (int i = 0; i < 10; i++) {
@@ -64,13 +86,32 @@ public class TestFsShellConcat {
       return null;
     }).when(mockFs).concat(any(Path.class), any(Path[].class));
     Concat.setTstFs(mockFs);
-    shellRun(0, "-concat", dstPath.toString(), testRootDir+"/*");
+    shellRun(0, "-concat", dstPath.toString(), testRootDir+"/file-*");
 
     assertTrue(lfs.exists(dstPath));
     assertEquals(1, lfs.listStatus(testRootDir).length);
   }
 
-  private void shellRun(int n, String ... args) throws Exception {
+  @Test
+  public void testUnsupportedFs() throws Exception {
+    FileSystem mockFs = Mockito.mock(FileSystem.class);
+    Mockito.doThrow(
+        new UnsupportedOperationException("Mock unsupported exception."))
+        .when(mockFs).concat(any(Path.class), any(Path[].class));
+    Mockito.doAnswer(invocationOnMock -> new URI("mockfs:///")).when(mockFs)
+        .getUri();
+    Concat.setTstFs(mockFs);
+    PrintStream oldErr = System.err;
+    final ByteArrayOutputStream err = new ByteArrayOutputStream();
+    System.setErr(new PrintStream(err));
+    shellRun(1, "-concat", dstPath.toString(), testRootDir + "/file-*");
+    System.setErr(oldErr);
+    System.err.print(err.toString());
+    assertTrue(err.toString()
+        .contains("Dest filesystem 'mockfs' doesn't support concat"));
+  }
+
+  private void shellRun(int n, String... args) {
     assertEquals(n, shell.run(args));
   }
 
@@ -78,10 +119,21 @@ public class TestFsShellConcat {
    * Simple simulation of concat.
    */
   private void mockConcat(Path target, Path[] srcArray) throws IOException {
+    Path tmp = new Path(target.getParent(), target.getName() + ".bak");
+    lfs.rename(target, tmp);
     OutputStream out = lfs.create(target);
     try {
+      InputStream in = lfs.open(tmp);
+      try {
+        IOUtils.copyBytes(in, out, 1024);
+      } finally {
+        if (in != null) {
+          in.close();
+        }
+        lfs.delete(tmp, true);
+      }
       for (int i = 0; i < srcArray.length; i++) {
-        InputStream in = lfs.open(srcArray[i]);
+        in = lfs.open(srcArray[i]);
         try {
           IOUtils.copyBytes(in, out, 1024);
         } finally {
