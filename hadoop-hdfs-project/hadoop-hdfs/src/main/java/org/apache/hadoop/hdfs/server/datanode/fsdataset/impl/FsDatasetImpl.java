3c262f4fb7eb8bd764e3a7def19126b15ac8c5b7
Ayush Saxena <ayushsaxena@apache.org>
2020-06-03 12:47:15 +0530

    HDFS-11041. Unable to unregister FsDatasetState MBean if DataNode is shutdown twice. Contributed by Wei-Chiu Chuang.
    (cherry picked from commit e8cb2ae409bc1d62f23efef485d1c6f1ff21e86c)
    
    Change-Id: I9f04082d650628bc1b8b62dacaaf472f8a578742

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/FsDatasetImpl.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/FsDatasetImpl.java
@@ -2353,6 +2353,7 @@ class FsDatasetImpl implements FsDatasetSpi<FsVolumeImpl> {
 
     if (mbeanName != null) {
       MBeans.unregister(mbeanName);
+      mbeanName = null;
     }
     
     if (asyncDiskService != null) {
