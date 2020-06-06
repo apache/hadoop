1f27f7df06192737442f2ab25f4fc2dad34bda59
Uma Maheswara Rao G <umamahesh@apache.org>
2020-06-05 23:35:20 -0700

    Fixed test failures.

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/conf/TestCommonConfigurationFields.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/conf/TestCommonConfigurationFields.java
@@ -124,6 +124,25 @@ public class TestCommonConfigurationFields extends TestConfigurationFieldsBase {
     xmlPrefixToSkipCompare.add("fs.adl.");
     xmlPropsToSkipCompare.add("fs.AbstractFileSystem.adl.impl");
 
+    // ViewfsOverloadScheme target fs impl property keys are dynamically
+    // constructed and they are advanced props.
+    xmlPrefixToSkipCompare.add("fs.viewfs.overload.scheme.target.abfs.impl");
+    xmlPrefixToSkipCompare.add("fs.viewfs.overload.scheme.target.abfss.impl");
+    xmlPrefixToSkipCompare.add("fs.viewfs.overload.scheme.target.file.impl");
+    xmlPrefixToSkipCompare.add("fs.viewfs.overload.scheme.target.ftp.impl");
+    xmlPrefixToSkipCompare.add("fs.viewfs.overload.scheme.target.hdfs.impl");
+    xmlPrefixToSkipCompare.add("fs.viewfs.overload.scheme.target.http.impl");
+    xmlPrefixToSkipCompare.add("fs.viewfs.overload.scheme.target.https.impl");
+    xmlPrefixToSkipCompare.add("fs.viewfs.overload.scheme.target.o3fs.impl");
+    xmlPrefixToSkipCompare.add("fs.viewfs.overload.scheme.target.oss.impl");
+    xmlPrefixToSkipCompare.add("fs.viewfs.overload.scheme.target.s3a.impl");
+    xmlPrefixToSkipCompare
+        .add("fs.viewfs.overload.scheme.target.swebhdfs.impl");
+    xmlPrefixToSkipCompare
+        .add("fs.viewfs.overload.scheme.target.webhdfs.impl");
+    xmlPrefixToSkipCompare.add("fs.viewfs.overload.scheme.target.wasb.impl");
+    xmlPrefixToSkipCompare.add("fs.viewfs.overload.scheme.target.swift.impl");
+
     // Azure properties are in a different class
     // - org.apache.hadoop.fs.azure.AzureNativeFileSystemStore
     // - org.apache.hadoop.fs.azure.SASKeyGeneratorImpl
