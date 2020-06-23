8d7600ae6b7fadba1b4695666cf9287be021d481
He Xiaoqiao <hexiaoqiao@apache.org>
2020-06-23 16:13:43 +0800

    HADOOP-17068. Client fails forever when namenode ipaddr changed. Contributed by Sean Chow.
    (cherry picked from commit fa14e4bc001e28d9912e8d985d09bab75aedb87c)
    (cherry picked from commit 3a860e876e799ef44fca34e3cb19a89dbd4f6744)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Client.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Client.java
@@ -650,6 +650,7 @@ public class Client implements AutoCloseable {
     
     private synchronized void setupConnection(
         UserGroupInformation ticket) throws IOException {
+      LOG.debug("Setup connection to " + server.toString());
       short ioFailures = 0;
       short timeoutFailures = 0;
       while (true) {
@@ -712,8 +713,16 @@ public class Client implements AutoCloseable {
         } catch (IOException ie) {
           if (updateAddress()) {
             timeoutFailures = ioFailures = 0;
+            try {
+              // HADOOP-17068: when server changed, ignore the exception.
+              handleConnectionFailure(ioFailures++, ie);
+            } catch (IOException ioe) {
+              LOG.warn("Exception when handle ConnectionFailure: "
+                  + ioe.getMessage());
+            }
+          } else {
+            handleConnectionFailure(ioFailures++, ie);
           }
-          handleConnectionFailure(ioFailures++, ie);
         }
       }
     }
@@ -1279,7 +1288,7 @@ public class Client implements AutoCloseable {
           cleanupCalls();
         }
       } else {
-        // log the info
+        // Log the newest server information if update address.
         if (LOG.isDebugEnabled()) {
           LOG.debug("closing ipc connection to " + server + ": " +
               closeException.getMessage(),closeException);
