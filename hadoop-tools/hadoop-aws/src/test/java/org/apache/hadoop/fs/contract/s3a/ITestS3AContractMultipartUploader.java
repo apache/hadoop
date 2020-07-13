a51d72f0c63a8f356826619a8c1c8de0b372a9a6
Steve Loughran <stevel@cloudera.com>
2020-07-13 13:30:02 +0100

    HDFS-13934. Multipart uploaders to be created through FileSystem/FileContext.
    Contributed by Steve Loughran.
    
    Change-Id: Iebd34140c1a0aa71f44a3f4d0fee85f6bdf123a3

--- hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/contract/s3a/ITestS3AContractMultipartUploader.java
+++ hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/contract/s3a/ITestS3AContractMultipartUploader.java
@@ -15,25 +15,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.hadoop.fs.contract.s3a;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.contract.AbstractFSContract;
 import org.apache.hadoop.fs.contract.AbstractContractMultipartUploaderTest;
-import org.apache.hadoop.fs.contract.ContractTestUtils;
+import org.apache.hadoop.fs.contract.AbstractFSContract;
 import org.apache.hadoop.fs.s3a.S3AFileSystem;
-import org.apache.hadoop.fs.s3a.WriteOperationHelper;
 
-import static org.apache.hadoop.fs.s3a.S3ATestConstants.*;
-import static org.apache.hadoop.fs.s3a.S3ATestUtils.*;
+import static org.apache.hadoop.fs.contract.ContractTestUtils.skip;
+import static org.apache.hadoop.fs.s3a.S3ATestConstants.DEFAULT_SCALE_TESTS_ENABLED;
+import static org.apache.hadoop.fs.s3a.S3ATestConstants.KEY_HUGE_PARTITION_SIZE;
+import static org.apache.hadoop.fs.s3a.S3ATestConstants.KEY_SCALE_TESTS_ENABLED;
+import static org.apache.hadoop.fs.s3a.S3ATestConstants.SCALE_TEST_TIMEOUT_MILLIS;
+import static org.apache.hadoop.fs.s3a.S3ATestUtils.assume;
+import static org.apache.hadoop.fs.s3a.S3ATestUtils.getTestPropertyBool;
+import static org.apache.hadoop.fs.s3a.S3ATestUtils.getTestPropertyBytes;
+import static org.apache.hadoop.fs.s3a.S3ATestUtils.maybeEnableS3Guard;
 import static org.apache.hadoop.fs.s3a.scale.AbstractSTestS3AHugeFiles.DEFAULT_HUGE_PARTITION_SIZE;
 
 /**
  * Test MultipartUploader with S3A.
+ * <p></p>
  * Although not an S3A Scale test subclass, it uses the -Dscale option
  * to enable it, and partition size option to control the size of
  * parts uploaded.
@@ -41,14 +44,11 @@ import static org.apache.hadoop.fs.s3a.scale.AbstractSTestS3AHugeFiles.DEFAULT_H
 public class ITestS3AContractMultipartUploader extends
     AbstractContractMultipartUploaderTest {
 
-  private static final Logger LOG =
-      LoggerFactory.getLogger(ITestS3AContractMultipartUploader.class);
-
   private int partitionSize;
 
   /**
    * S3 requires a minimum part size of 5MB (except the last part).
-   * @return 5MB
+   * @return 5MB+ value
    */
   @Override
   protected int partSizeInBytes() {
@@ -127,36 +127,14 @@ public class ITestS3AContractMultipartUploader extends
   }
 
   /**
-   * Extend superclass teardown with actions to help clean up the S3 store,
-   * including aborting uploads under the test path.
-   */
-  @Override
-  public void teardown() throws Exception {
-    Path teardown = path("teardown").getParent();
-    S3AFileSystem fs = getFileSystem();
-    if (fs != null) {
-      WriteOperationHelper helper = fs.getWriteOperationHelper();
-      try {
-        LOG.info("Teardown: aborting outstanding uploads under {}", teardown);
-        int count = helper.abortMultipartUploadsUnderPath(
-            fs.pathToKey(teardown));
-        LOG.info("Found {} incomplete uploads", count);
-      } catch (Exception e) {
-        LOG.warn("Exeception in teardown", e);
-      }
-    }
-    super.teardown();
-  }
-
-  /**
    * S3 has no concept of directories, so this test does not apply.
    */
   public void testDirectoryInTheWay() throws Exception {
-    // no-op
+    skip("unsupported");
   }
 
   @Override
   public void testMultipartUploadReverseOrder() throws Exception {
-    ContractTestUtils.skip("skipped for speed");
+    skip("skipped for speed");
   }
 }
