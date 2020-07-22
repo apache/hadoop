5a68e20b31a48943a35ee8a19e756f69b3a9bc45
Masatake Iwasaki <iwasakims@apache.org>
2020-07-22 13:40:20 +0900

    HADOOP-17138. Fix spotbugs warnings surfaced after upgrade to 4.0.6. (#2155)
    (cherry picked from commit 1b29c9bfeee0035dd042357038b963843169d44c)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
@@ -3716,7 +3716,7 @@ public abstract class Server {
         if (count == null) {
           count = 1;
         } else {
-          count++;
+          count = count + 1;
         }
         userToConnectionsMap.put(user, count);
       }
@@ -3728,7 +3728,7 @@ public abstract class Server {
         if (count == null) {
           return;
         } else {
-          count--;
+          count = count - 1;
         }
         if (count == 0) {
           userToConnectionsMap.remove(user);
