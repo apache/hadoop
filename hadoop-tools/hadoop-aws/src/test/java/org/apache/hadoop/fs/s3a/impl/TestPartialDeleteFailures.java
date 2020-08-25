cc641534dcb39d304ecbd0820963465b85677ce2
Mukund Thakur <mthakur@cloudera.com>
2020-08-25 15:59:43 +0530

    HADOOP-17074. S3A Listing to be fully asynchronous. (#2207)
    Contributed by Mukund Thakur.

--- hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/impl/TestPartialDeleteFailures.java
+++ hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/impl/TestPartialDeleteFailures.java
@@ -29,6 +29,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -243,13 +244,14 @@ public class TestPartialDeleteFailures {
   private static class MinimalListingOperationCallbacks
           implements ListingOperationCallbacks {
     @Override
-    public S3ListResult listObjects(S3ListRequest request)
+    public CompletableFuture<S3ListResult> listObjectsAsync(
+            S3ListRequest request)
             throws IOException {
       return null;
     }
 
     @Override
-    public S3ListResult continueListObjects(
+    public CompletableFuture<S3ListResult> continueListObjectsAsync(
             S3ListRequest request,
             S3ListResult prevResult)
             throws IOException {
