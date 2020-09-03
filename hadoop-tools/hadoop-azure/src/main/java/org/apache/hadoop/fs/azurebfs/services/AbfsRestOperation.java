ac32ffcc9e2fc0599a37709d144647ffea6981b7
Mehakmeet Singh <mehakmeet.singh@cloudera.com>
2020-09-03 22:06:18 +0530

    HADOOP-17229. checkstyle fix

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/services/AbfsRestOperation.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/services/AbfsRestOperation.java
@@ -249,8 +249,8 @@ public class AbfsRestOperation {
       httpOperation.processResponse(buffer, bufferOffset, bufferLength);
       incrementCounter(AbfsStatistic.GET_RESPONSES, 1);
       //Only increment bytesReceived counter when the status code is 2XX.
-      if (httpOperation.getStatusCode() >= HttpURLConnection.HTTP_OK &&
-          httpOperation.getStatusCode() <= HttpURLConnection.HTTP_PARTIAL) {
+      if (httpOperation.getStatusCode() >= HttpURLConnection.HTTP_OK
+          && httpOperation.getStatusCode() <= HttpURLConnection.HTTP_PARTIAL) {
         incrementCounter(AbfsStatistic.BYTES_RECEIVED,
             httpOperation.getBytesReceived());
       }
