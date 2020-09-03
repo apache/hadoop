a33ba7b5031b4cf7b3c33544c33f6e4421348e28
Steve Loughran <stevel@cloudera.com>
2020-09-03 18:23:44 +0100

    HADOOP-17242. Add test cases for file and dir
    .. as suggested by Mukund
    
    Change-Id: I28eac9b10fdcf9a6ac87b6f17032afaf3c1f26a1

--- hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/ITestObjectListing.java
+++ hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/ITestObjectListing.java
@@ -18,13 +18,17 @@
 
 package org.apache.hadoop.fs.s3a;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.NoSuchElementException;
 
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.fs.contract.ContractTestUtils;
 
 import static org.apache.hadoop.test.LambdaTestUtils.intercept;
 
@@ -36,11 +40,10 @@ public class ITestObjectListing extends AbstractS3ATestBase {
   @Test
   public void testAsyncListEmptyPath() throws Throwable {
     Path path = methodPath();
+    String delimiter = "/";
     S3AFileSystem fs = getFileSystem();
     String pathKey = fs.pathToKey(path);
-    S3ListRequest r = fs.createListObjectsRequest(pathKey, "/");
-    RemoteIterator<S3ListResult> it = fs.getListing()
-        .createObjectListingIterator(path, r);
+    RemoteIterator<S3ListResult> it = listObjects(fs, path, delimiter, pathKey);
 
     // for any path on S3, this will return an empty value
     Assertions.assertThat(it.hasNext())
@@ -52,6 +55,18 @@ public class ITestObjectListing extends AbstractS3ATestBase {
 
     // invoke the result
     S3ListResult result = it.next();
+    assertEmptyResult(pathKey, result);
+
+    Assertions.assertThat(it.hasNext())
+        .describedAs("hasNext() after next() %s", it)
+        .isFalse();
+
+    // there are none left.
+    verifyNoMoreRecords(it);
+  }
+
+  protected void assertEmptyResult(final String pathKey,
+      final S3ListResult result) {
     Assertions.assertThat(result.getObjectSummaries())
         .describedAs("object summaries")
         .isEmpty();
@@ -61,13 +76,19 @@ public class ITestObjectListing extends AbstractS3ATestBase {
     Assertions.assertThat(result.representsEmptyDirectory(null, pathKey, null))
         .describedAs("does this represent an empty directory")
         .isFalse();
+  }
 
-    Assertions.assertThat(it.hasNext())
-        .describedAs("hasNext() after next() %s", it)
-        .isFalse();
-
-    // there are none left.
+  protected void verifyNoMoreRecords(final RemoteIterator<S3ListResult> it)
+      throws Exception {
     intercept(NoSuchElementException.class, () -> it.next());
+  }
+
+  protected RemoteIterator<S3ListResult> listObjects(final S3AFileSystem fs,
+      final Path path, final String delimiter, final String pathKey)
+      throws IOException {
+    S3ListRequest r = fs.createListObjectsRequest(pathKey, delimiter);
+    return fs.getListing()
+        .createObjectListingIterator(path, r);
   }
 
   @Test
@@ -75,14 +96,51 @@ public class ITestObjectListing extends AbstractS3ATestBase {
     describe("Get the list result without calling hasNext first");
     Path path = methodPath();
     S3AFileSystem fs = getFileSystem();
+    String pathToKey = fs.pathToKey(path);
+    RemoteIterator<S3ListResult> it = listObjects(fs, path, "/",
+        pathToKey);
+    // invoke the result
+    assertEmptyResult(pathToKey, it.next());
+    // there are none left.
+    verifyNoMoreRecords(it);
+  }
+
+  @Test
+  public void testListFile() throws Throwable {
+    describe("When you list a file you get the file back in the listing");
+    Path path = methodPath();
+    S3AFileSystem fs = getFileSystem();
+    ContractTestUtils.touch(fs, path);
     String pathKey = fs.pathToKey(path);
-    S3ListRequest r = fs.createListObjectsRequest(pathKey, "/");
-    RemoteIterator<S3ListResult> it = fs.getListing()
-        .createObjectListingIterator(path, r);
+    RemoteIterator<S3ListResult> it = listObjects(fs, path, "/", pathKey);
     // invoke the result
     S3ListResult result = it.next();
+    List<S3ObjectSummary> objectSummaries = result.getObjectSummaries();
+    Assertions.assertThat(objectSummaries)
+        .describedAs("object summaries")
+        .hasSize(1)
+        .allMatch(p -> p.getKey().equals(pathKey));
     // there are none left.
-    intercept(NoSuchElementException.class, () -> it.next());
+    verifyNoMoreRecords(it);
+  }
+
+  @Test
+  public void testListDirMarker() throws Throwable {
+    describe("Listing a dir marker");
+    Path path = methodPath();
+    S3AFileSystem fs = getFileSystem();
+    fs.mkdirs(path);
+    String pathKey = fs.pathToKey(path) + "/";
+    RemoteIterator<S3ListResult> it = listObjects(fs, path, "/", pathKey);
+    // invoke the result
+    S3ListResult result = it.next();
+    List<S3ObjectSummary> objectSummaries = result.getObjectSummaries();
+    Assertions.assertThat(objectSummaries)
+        .describedAs("object summaries")
+        .hasSize(1)
+        .allMatch(p -> p.getKey().equals(pathKey));
+    // there are none left.
+    verifyNoMoreRecords(it);
   }
 
 }
