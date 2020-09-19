c09a7256d4474850b4f9284c465685b0be12907b
Sumangala <f20150178g@alumni.bits-pilani.ac.in>
2020-09-19 06:27:27 -0700

    tests

--- hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/services/TestClientCorrelation.java
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/services/TestClientCorrelation.java
@@ -1,27 +1,33 @@
 package org.apache.hadoop.fs.azurebfs.services;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.*;
+//import static org.assertj.core.api.Assertions.assertTrue;
 import static org.hamcrest.Matchers.*;
 
-public class TestClientCorrelation {
-    private final AbfsClient abfsClient;
+import org.apache.hadoop.fs.azurebfs.constants.HttpHeaderConfigurations;
 
+import java.util.UUID;
+
+public class TestClientCorrelation {
+    private final AbfsHttpOperation op;
 
     public TestClientCorrelation() {
-        abfsClient = mock(AbfsClient.class);
+        op = mock(AbfsHttpOperation.class);
     }
 
     @Test
     public void verifyValidClientCorrelationId() {
 
         String clientCorrelationId = "valid-corr-id-123";
-        AbfsClient abfsClient = mock(AbfsClient.class);
-        AbfsHttpOperation op = abfsClient.getFilesystemProperties().getResult();
-        assertThat(op.getStatusCode(), greaterThan(400)).describedAs("Requests should not fail");
-        assertThat(op.getResponseHeader())
+        op.getConnection().setRequestProperty(HttpHeaderConfigurations.X_MS_CLIENT_REQUEST_ID,
+                clientCorrelationId + ":" + UUID.randomUUID().toString());
+        Assert.assertTrue("Requests should not fail", op.getStatusCode() > 400);
+        //assertThat("",op.getStatusCode(), greaterThan(400)).describedAs("Requests should not fail");
+        assertThat(op.getResponseHeader(HttpHeaderConfigurations.X_MS_REQUEST_ID))
                 .describedAs("Response header should contain client request header")
                 .contains(clientCorrelationId);
     }
@@ -30,20 +36,23 @@ public class TestClientCorrelation {
     public void verifyInvalidClientCorrelationId() {
 
         String clientCorrelationId = "thisIs!nval!d";
-        AbfsClient abfsClient = mock(AbfsClient.class);
-        AbfsHttpOperation op = abfsClient.getFilesystemProperties().getResult();
-        assertThat(op.getStatusCode(), greaterThan(400)).describedAs("Requests should not fail");
-        assertThat(op.getResponseHeader())
+        op.getConnection().setRequestProperty(HttpHeaderConfigurations.X_MS_CLIENT_REQUEST_ID,
+                clientCorrelationId + ":" + UUID.randomUUID().toString());
+        //assertTrue()
+        Assert.assertTrue("Requests should not fail",op.getStatusCode() > 400);
+        assertThat(op.getResponseHeader(HttpHeaderConfigurations.X_MS_REQUEST_ID))
                 .describedAs("Invalid correlation id should be converted to empty string")
                 .startsWith(":");
     }
 
+    @Test
     public void verifyEmptyClientCorrelationId() {
 
         String clientCorrelationId = "";
-        AbfsHttpOperation op = abfsClient.getFilesystemProperties().getResult();
-        assertThat(op.getStatusCode(), greaterThan(400)).describedAs("Requests should not fail");
-        assertThat(op.getResponseHeader())
+        op.getConnection().setRequestProperty(HttpHeaderConfigurations.X_MS_CLIENT_REQUEST_ID,
+                clientCorrelationId + ":" + UUID.randomUUID().toString());
+        Assert.assertTrue("Requests should not fail",op.getStatusCode() > 400);
+        assertThat(op.getResponseHeader(HttpHeaderConfigurations.X_MS_REQUEST_ID))
                 .describedAs("Should be empty").startsWith(":");
     }
 }
