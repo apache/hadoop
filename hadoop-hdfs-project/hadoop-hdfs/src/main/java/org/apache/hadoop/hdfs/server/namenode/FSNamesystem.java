7e4572e56a79c508a726fffc04b05c3c0607bb46
Kihwal Lee <kihwal@apache.org>
2020-10-16 11:17:34 -0500

    HDFS-15627. Log delete audits before collecting blocks.
    (cherry picked from commit 740a2c46353f8005dbed6f5bc15f21acfc4a6a23)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -3111,10 +3111,10 @@ public class FSNamesystem implements Namesystem, FSNamesystemMBean,
       writeUnlock(operationName);
     }
     getEditLog().logSync();
+    logAuditEvent(true, operationName, src);
     if (toRemovedBlocks != null) {
       removeBlocks(toRemovedBlocks); // Incremental deletion of blocks
     }
-    logAuditEvent(true, operationName, src);
     return ret;
   }
 
