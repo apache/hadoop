2e8cafac3b071fe5b943542827fd8a496b137fa9
Ayush Saxena <ayushsaxena@apache.org>
2020-10-19 10:48:47 +0530

    HDFS-14383. Compute datanode load based on StoragePolicy. Contributed by Ayush Saxena.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/DatanodeManager.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/DatanodeManager.java
@@ -24,6 +24,7 @@ import org.apache.hadoop.thirdparty.com.google.common.annotations.VisibleForTest
 import org.apache.hadoop.thirdparty.com.google.common.base.Preconditions;
 import org.apache.hadoop.thirdparty.com.google.common.net.InetAddresses;
 
+import org.apache.hadoop.fs.StorageType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.hadoop.HadoopIllegalArgumentException;
@@ -1977,6 +1978,11 @@ public class DatanodeManager {
           avgLoad = (double)xceivers/nodes;
         }
         return avgLoad;
+      }
+
+      @Override
+      public Map<StorageType, StorageTypeStats> getStorageTypeStats() {
+        return heartbeatManager.getStorageTypeStats();
       }
     };
   }
