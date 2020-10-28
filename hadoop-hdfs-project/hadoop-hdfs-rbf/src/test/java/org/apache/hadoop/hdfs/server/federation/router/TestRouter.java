49e5fc64ec61398e11c611691de44eac9f88a2e2
Akira Ajisaka <aajisaka@apache.org>
2020-10-28 17:19:58 +0900

    Revert "Fix parameter name in javadoc"
    This reverts commit 03898e8cfc7e622d9d6ce62defe788949401ae34.

--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/test/java/org/apache/hadoop/hdfs/server/federation/router/TestRouter.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/test/java/org/apache/hadoop/hdfs/server/federation/router/TestRouter.java
@@ -268,7 +268,7 @@ public class TestRouter {
   }
 
   /**
-   * Check the default value of dfs.federation.router.heartbeat.enable
+   * Check the default value of dfs.federation.router.namenode.heartbeat.enable
    * when it isn't explicitly defined.
    * @param enable value for dfs.federation.router.heartbeat.enable.
    */
