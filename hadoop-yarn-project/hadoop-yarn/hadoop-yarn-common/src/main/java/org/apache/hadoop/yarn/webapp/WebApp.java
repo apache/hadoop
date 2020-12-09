9e94e921a75e004e38bcb17b1558a137e806a59f
Akira Ajisaka <aajisaka@apache.org>
2020-12-09 20:29:33 +0900

    Revert "WIP. Use container v3 api"
    This reverts commit 2d71bec1b8ea8ebccd9b5d5ffc43ff0fa197f757.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/webapp/WebApp.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/webapp/WebApp.java
@@ -26,13 +26,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.google.inject.Singleton;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.http.HttpServer2;
 import org.apache.hadoop.yarn.webapp.view.RobotsTextPage;
-import org.glassfish.jersey.servlet.ServletContainer;
 import org.glassfish.jersey.servlet.ServletProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -184,19 +182,12 @@ public abstract class WebApp extends ServletModule {
 
       Map<String, String> params = new HashMap<String, String>();
       params.put(ServletProperties.FILTER_FORWARD_ON_404, "true");
-      Class<? extends Filter> filterClass = getWebAppFilterClass();
-      if (filterClass != null) {
-        filter("/*").through(getWebAppFilterClass(), params);
-      }
+      filter("/*").through(getWebAppFilterClass(), params);
     }
   }
 
   protected Class<? extends Filter> getWebAppFilterClass() {
-    return SingletonSevletContainer.class;
-  }
-
-  @Singleton
-  private static class SingletonSevletContainer extends ServletContainer {
+    return GuiceFilter.class;
   }
 
   /**
