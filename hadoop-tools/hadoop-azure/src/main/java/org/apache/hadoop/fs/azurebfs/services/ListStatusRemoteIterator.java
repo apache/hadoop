13ddf5274a425bf83c8b25176f53b9e10be03102
Bilahari T H <th.bilahari@gmail.com>
2020-12-30 16:33:16 +0530

    Making the server calls as a background activity

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/services/ListStatusRemoteIterator.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/services/ListStatusRemoteIterator.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hadoop.fs.azurebfs.services;
 
 import java.io.IOException;
@@ -9,48 +27,46 @@ import java.util.NoSuchElementException;
 import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.fs.azurebfs.AzureBlobFileSystemStore;
 
-import static org.apache.hadoop.fs.impl.FutureIOSupport.awaitFuture;
-
 public class ListStatusRemoteIterator implements RemoteIterator<FileStatus> {
+
+  private static final Logger LOG = LoggerFactory
+      .getLogger(ListStatusRemoteIterator.class);
 
   private static final boolean FETCH_ALL_FALSE = false;
 
   private final Path path;
   private final AzureBlobFileSystemStore abfsStore;
-  private final Queue<ListIterator<FileStatus>> iterators = new LinkedList<>();
+  private final Queue<ListIterator<FileStatus>> iteratorsQueue =
+      new LinkedList<>();
 
   private boolean firstRead = true;
-  private CompletableFuture<IOException> future;
   private String continuation;
-  private ListIterator<FileStatus> lsItr;
+  private ListIterator<FileStatus> currIterator;
+  private IOException ioException;
 
   public ListStatusRemoteIterator(final Path path,
       final AzureBlobFileSystemStore abfsStore) throws IOException {
     this.path = path;
     this.abfsStore = abfsStore;
-    fetchMoreFileStatusesAsync();
-    fetchMoreFileStatusesAsync();
-    lsItr = iterators.poll();
+    fetchAllAsync();
+    updateCurrentIterator();
   }
 
   @Override
   public boolean hasNext() throws IOException {
-    if (lsItr.hasNext()) {
+    if (currIterator.hasNext()) {
       return true;
     }
-    fetchMoreFileStatusesAsync();
-    if (!iterators.isEmpty()) {
-      lsItr = iterators.poll();
-      if (lsItr == null) {
-        return false;
-      }
-    }
-    return lsItr.hasNext();
+    updateCurrentIterator();
+    return currIterator.hasNext();
   }
 
   @Override
@@ -58,41 +74,52 @@ public class ListStatusRemoteIterator implements RemoteIterator<FileStatus> {
     if (!this.hasNext()) {
       throw new NoSuchElementException();
     }
-    return lsItr.next();
+    return currIterator.next();
   }
 
-  private void fetchMoreFileStatusesAsync() throws IOException {
-    forceFuture();
-    if (isIterationComplete()) {
-      return;
-    }
-    this.future = CompletableFuture.supplyAsync(() -> {
-      try {
-        List<FileStatus> fileStatuses = new ArrayList<>();
-        continuation = abfsStore
-            .listStatus(path, null, fileStatuses, FETCH_ALL_FALSE,
-                continuation);
-        if (fileStatuses != null && !fileStatuses.isEmpty()) {
-          iterators.add(fileStatuses.listIterator());
+  private void updateCurrentIterator() throws IOException {
+    synchronized (this) {
+      while (!isIterationComplete() && iteratorsQueue.isEmpty()) {
+        try {
+          this.wait();
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+          LOG.error("Thread got interrupted: {}", e);
         }
-        if (firstRead) {
-          firstRead = false;
-        }
-        return null;
-      } catch (IOException ex) {
-        return ex;
       }
-    });
+      if (!iteratorsQueue.isEmpty()) {
+        currIterator = iteratorsQueue.poll();
+      } else if (ioException != null) {
+        throw ioException;
+      }
+    }
   }
 
-  private void forceFuture() throws IOException {
-    if (future == null) {
-      return;
-    }
-    IOException ex = awaitFuture(future);
-    if (ex != null) {
-      throw ex;
-    }
+  private void fetchAllAsync() {
+    CompletableFuture.supplyAsync(() -> {
+      while (!isIterationComplete()) {
+        List<FileStatus> fileStatuses = new ArrayList<>();
+        try {
+          continuation = abfsStore
+              .listStatus(path, null, fileStatuses, FETCH_ALL_FALSE,
+                  continuation);
+        } catch (IOException e) {
+          ioException = e;
+          return null;
+        } finally {
+          if (firstRead) {
+            firstRead = false;
+          }
+        }
+        if (fileStatuses != null && !fileStatuses.isEmpty()) {
+          iteratorsQueue.add(fileStatuses.listIterator());
+          synchronized (this) {
+            this.notifyAll();
+          }
+        }
+      }
+      return null;
+    });
   }
 
   private boolean isIterationComplete() {
