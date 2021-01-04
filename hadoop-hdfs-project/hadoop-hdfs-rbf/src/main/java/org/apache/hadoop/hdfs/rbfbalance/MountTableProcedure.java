77299ae992b16066dd61e4fec9ff63b863ae2e21
Ayush Saxena <ayushsaxena@apache.org>
2021-01-05 00:05:03 +0530

    HDFS-15748. RBF: Move the router related part from hadoop-federation-balance module to hadoop-hdfs-rbf. Contributed by Jinglun.

--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/rbfbalance/MountTableProcedure.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/rbfbalance/MountTableProcedure.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.tools.fedbalance;
+package org.apache.hadoop.hdfs.rbfbalance;
 
 import org.apache.hadoop.thirdparty.com.google.common.annotations.VisibleForTesting;
 import org.apache.hadoop.conf.Configuration;
--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/rbfbalance/RouterFedBalance.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/rbfbalance/RouterFedBalance.java
@@ -15,57 +15,65 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.tools.fedbalance;
+package org.apache.hadoop.hdfs.rbfbalance;
 
-import org.apache.hadoop.thirdparty.com.google.common.annotations.VisibleForTesting;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.tools.fedbalance.procedure.BalanceProcedure;
+import org.apache.hadoop.hdfs.server.federation.resolver.MountTableManager;
+import org.apache.hadoop.hdfs.server.federation.router.RBFConfigKeys;
+import org.apache.hadoop.hdfs.server.federation.router.RouterClient;
+import org.apache.hadoop.hdfs.server.federation.store.records.MountTable;
+import org.apache.hadoop.net.NetUtils;
+import org.apache.hadoop.thirdparty.com.google.common.annotations.VisibleForTesting;
+import org.apache.hadoop.tools.fedbalance.FedBalanceConfigs;
+import org.apache.hadoop.tools.fedbalance.FedBalanceConfigs.TrashOption;
 import org.apache.hadoop.tools.fedbalance.procedure.BalanceJob;
+import org.apache.hadoop.tools.fedbalance.procedure.BalanceProcedure;
 import org.apache.hadoop.tools.fedbalance.procedure.BalanceProcedureScheduler;
+import org.apache.hadoop.tools.fedbalance.TrashProcedure;
+import org.apache.hadoop.tools.fedbalance.FedBalanceContext;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
+import static org.apache.hadoop.tools.fedbalance.FedBalance.FED_BALANCE_DEFAULT_XML;
+import static org.apache.hadoop.tools.fedbalance.FedBalance.FED_BALANCE_SITE_XML;
+import static org.apache.hadoop.tools.fedbalance.FedBalanceOptions.CLI_OPTIONS;
 import static org.apache.hadoop.tools.fedbalance.FedBalanceOptions.FORCE_CLOSE_OPEN;
 import static org.apache.hadoop.tools.fedbalance.FedBalanceOptions.MAP;
 import static org.apache.hadoop.tools.fedbalance.FedBalanceOptions.BANDWIDTH;
-import static org.apache.hadoop.tools.fedbalance.FedBalanceOptions.TRASH;
 import static org.apache.hadoop.tools.fedbalance.FedBalanceOptions.DELAY_DURATION;
-import static org.apache.hadoop.tools.fedbalance.FedBalanceOptions.CLI_OPTIONS;
 import static org.apache.hadoop.tools.fedbalance.FedBalanceOptions.DIFF_THRESHOLD;
-import static org.apache.hadoop.tools.fedbalance.FedBalanceConfigs.TrashOption;
+import static org.apache.hadoop.tools.fedbalance.FedBalanceOptions.TRASH;
 
 /**
- * Balance data from src cluster to dst cluster with distcp.
+ * Balance data in router-based federation cluster. From src sub-namespace to
+ * dst sub-namespace with distcp.
  *
  * 1. Move data from the source path to the destination path with distcp.
- * 2. Delete the source path to trash.
+ * 2. Update the the mount entry.
+ * 3. Delete the source path to trash.
  */
-public class FedBalance extends Configured implements Tool {
+public class RouterFedBalance extends Configured implements Tool {
 
   public static final Logger LOG =
-      LoggerFactory.getLogger(FedBalance.class);
+      LoggerFactory.getLogger(RouterFedBalance.class);
   private static final String SUBMIT_COMMAND = "submit";
   private static final String CONTINUE_COMMAND = "continue";
-  private static final String NO_MOUNT = "no-mount";
   private static final String DISTCP_PROCEDURE = "distcp-procedure";
+  private static final String MOUNT_TABLE_PROCEDURE = "mount-table-procedure";
   private static final String TRASH_PROCEDURE = "trash-procedure";
-
-  public static final String FED_BALANCE_DEFAULT_XML =
-      "hdfs-fedbalance-default.xml";
-  public static final String FED_BALANCE_SITE_XML = "hdfs-fedbalance-site.xml";
 
   /**
    * This class helps building the balance job.
@@ -78,7 +86,7 @@ public class FedBalance extends Configured implements Tool {
     /* Specify bandwidth per map in MB. */
     private int bandwidth = 10;
     /* Specify the trash behaviour of the source path. */
-    private TrashOption trashOpt = TrashOption.TRASH;
+    private FedBalanceConfigs.TrashOption trashOpt = TrashOption.TRASH;
     /* Specify the duration(millie seconds) when the procedure needs retry. */
     private long delayDuration = TimeUnit.SECONDS.toMillis(1);
     /* Specify the threshold of diff entries. */
@@ -157,21 +165,26 @@ public class FedBalance extends Configured implements Tool {
       if (dst.toUri().getAuthority() == null) {
         throw new IOException("The destination cluster must be specified.");
       }
-      Path src = new Path(inputSrc);
-      if (src.toUri().getAuthority() == null) {
-        throw new IOException("The source cluster must be specified.");
-      }
-      context = new FedBalanceContext.Builder(src, dst, NO_MOUNT, getConf())
-          .setForceCloseOpenFiles(forceCloseOpen).setUseMountReadOnly(false)
+      Path src = getSrcPath(inputSrc);
+      String mount = inputSrc;
+      context = new FedBalanceContext.Builder(src, dst, mount, getConf())
+          .setForceCloseOpenFiles(forceCloseOpen).setUseMountReadOnly(true)
           .setMapNum(map).setBandwidthLimit(bandwidth).setTrash(trashOpt)
-          .setDiffThreshold(diffThreshold).build();
+          .setDelayDuration(delayDuration).setDiffThreshold(diffThreshold)
+          .build();
 
       LOG.info(context.toString());
       // Construct the balance job.
       BalanceJob.Builder<BalanceProcedure> builder = new BalanceJob.Builder<>();
-      DistCpProcedure dcp =
-          new DistCpProcedure(DISTCP_PROCEDURE, null, delayDuration, context);
+      RouterDistCpProcedure dcp =
+          new RouterDistCpProcedure(DISTCP_PROCEDURE, null, delayDuration,
+              context);
       builder.nextProcedure(dcp);
+      MountTableProcedure mtp =
+          new MountTableProcedure(MOUNT_TABLE_PROCEDURE, null, delayDuration,
+              inputSrc, dst.toUri().getPath(), dst.toUri().getAuthority(),
+              getConf());
+      builder.nextProcedure(mtp);
       TrashProcedure tp =
           new TrashProcedure(TRASH_PROCEDURE, null, delayDuration, context);
       builder.nextProcedure(tp);
@@ -179,15 +192,14 @@ public class FedBalance extends Configured implements Tool {
     }
   }
 
-  public FedBalance() {
+  public RouterFedBalance() {
     super();
   }
 
   @Override
   public int run(String[] args) throws Exception {
     CommandLineParser parser = new GnuParser();
-    CommandLine command =
-        parser.parse(FedBalanceOptions.CLI_OPTIONS, args, true);
+    CommandLine command = parser.parse(CLI_OPTIONS, args, true);
     String[] leftOverArgs = command.getArgs();
     if (leftOverArgs == null || leftOverArgs.length < 1) {
       printUsage();
@@ -300,10 +312,38 @@ public class FedBalance extends Configured implements Tool {
     return 0;
   }
 
+  /**
+   * Get src uri from Router.
+   */
+  private Path getSrcPath(String fedPath) throws IOException {
+    String address = getConf().getTrimmed(
+        RBFConfigKeys.DFS_ROUTER_ADMIN_ADDRESS_KEY,
+        RBFConfigKeys.DFS_ROUTER_ADMIN_ADDRESS_DEFAULT);
+    InetSocketAddress routerSocket = NetUtils.createSocketAddr(address);
+    RouterClient rClient = new RouterClient(routerSocket, getConf());
+    try {
+      MountTableManager mountTable = rClient.getMountTableManager();
+      MountTable entry = MountTableProcedure.getMountEntry(fedPath, mountTable);
+      if (entry == null) {
+        throw new IllegalArgumentException(
+            "The mount point doesn't exist. path=" + fedPath);
+      } else if (entry.getDestinations().size() > 1) {
+        throw new IllegalArgumentException(
+            "The mount point has more than one destination. path=" + fedPath);
+      } else {
+        String ns = entry.getDestinations().get(0).getNameserviceId();
+        String path = entry.getDestinations().get(0).getDest();
+        return new Path("hdfs://" + ns + path);
+      }
+    } finally {
+      rClient.close();
+    }
+  }
+
   private void printUsage() {
     HelpFormatter formatter = new HelpFormatter();
     formatter.printHelp(
-        "fedbalance OPTIONS [submit|continue] <src> <target>\n\nOPTIONS",
+        "rbfbalance OPTIONS [submit|continue] <src> <target>\n\nOPTIONS",
         CLI_OPTIONS);
   }
 
@@ -323,19 +363,19 @@ public class FedBalance extends Configured implements Tool {
   }
 
   /**
-   * Main function of the FedBalance program. Parses the input arguments and
-   * invokes the FedBalance::run() method, via the ToolRunner.
-   * @param argv Command-line arguments sent to FedBalance.
+   * Main function of the RouterFedBalance program. Parses the input arguments
+   * and invokes the RouterFedBalance::run() method, via the ToolRunner.
+   * @param argv Command-line arguments sent to RouterFedBalance.
    */
   public static void main(String[] argv) {
     Configuration conf = getDefaultConf();
-    FedBalance fedBalance = new FedBalance();
+    RouterFedBalance fedBalance = new RouterFedBalance();
     fedBalance.setConf(conf);
     int exitCode;
     try {
       exitCode = ToolRunner.run(fedBalance, argv);
     } catch (Exception e) {
-      LOG.warn("Couldn't complete FedBalance operation.", e);
+      LOG.warn("Couldn't complete RouterFedBalance operation.", e);
       exitCode = -1;
     }
     System.exit(exitCode);
