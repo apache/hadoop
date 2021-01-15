d7a583a6935398f0c041ccdba823c0913af6f274
Takanobu Asanuma <tasanuma@apache.org>
2021-01-15 16:37:50 +0900

    revert Jenkinsfile

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/io/compress/TestCompressorDecompressor.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/io/compress/TestCompressorDecompressor.java
@@ -46,7 +46,7 @@ import org.apache.hadoop.thirdparty.com.google.common.collect.ImmutableSet;
  */
 public class TestCompressorDecompressor {
   
-  private static final Random rnd = new Random(1234567L);
+  private static final Random rnd = new Random(12345L);
   
   @Test
   public void testCompressorDecompressor() {
