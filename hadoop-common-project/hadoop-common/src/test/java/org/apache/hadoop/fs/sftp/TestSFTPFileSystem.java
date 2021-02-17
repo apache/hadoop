c485b39bd1dc5c21d294742cdfd06076f843ee95
Mikhail Pryakhin <m.pryahin@gmail.com>
2021-02-17 14:19:46 +0300

    HADOOP-17528. Making sure that re-entrant close calls are safe.

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/sftp/TestSFTPFileSystem.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/sftp/TestSFTPFileSystem.java
@@ -382,5 +382,7 @@ public class TestSFTPFileSystem {
     assertThat(fs.getConnectionPool().getLiveConnCount(), is(1));
     fs.close();
     assertThat(fs.getConnectionPool().getLiveConnCount(), is(0));
+    ///making sure that re-entrant close calls are safe
+    fs.close();
   }
 }
