173427d197142db8a3d935cc516d93f49b561a47
Wei-Chiu Chuang <weichiu@apache.org>
2021-02-22 18:13:06 +0800

    Revert "first commit (#2559)"
    This reverts commit 1b94569d893ca54ba86ad67340e2e9db27252e0f.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DirectoryScanner.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DirectoryScanner.java
@@ -506,7 +506,8 @@ public class DirectoryScanner implements Runnable {
         }
         // Block file and/or metadata file exists on the disk
         // Block exists in memory
-        if (info.getBlockFile() == null) {
+        if (info.getVolume().getStorageType() != StorageType.PROVIDED
+            && info.getBlockFile() == null) {
           // Block metadata file exits and block file is missing
           addDifference(diffRecord, statsRecord, info);
         } else if (info.getGenStamp() != memBlock.getGenerationStamp()
