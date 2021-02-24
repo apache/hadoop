7c839231922491d49b708ae8d6b2cf292176c96d
Renukaprasad C <48682981+prasad-acit@users.noreply.github.com>
2021-02-24 14:59:25 +0530

    HADOOP-17510. Hadoop prints sensitive Cookie information. (#2673)
    (cherry picked from commit 924fa87010ac193a8242d59b14f9fa9d17957fe5)
    (cherry picked from commit 9951ea9fa8427c134861f047acc164b5cee4eef0)

--- hadoop-common-project/hadoop-auth/src/main/java/org/apache/hadoop/security/authentication/client/AuthenticatedURL.java
+++ hadoop-common-project/hadoop-auth/src/main/java/org/apache/hadoop/security/authentication/client/AuthenticatedURL.java
@@ -150,7 +150,6 @@ public class AuthenticatedURL {
         cookieHeaders = new HashMap<>();
         cookieHeaders.put("Cookie", Arrays.asList(cookie.toString()));
       }
-      LOG.trace("Setting token value to {} ({})", authCookie, oldCookie);
     }
 
     private void setAuthCookieValue(String value) {
