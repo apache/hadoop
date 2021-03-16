47620f8821ed57706760beccbae94fd81573f922
Gautham B A <gautham.bangalore@gmail.com>
2021-03-16 21:36:51 +0530

    HDFS-15843. Make write cross-platform (#2710)

--- hadoop-hdfs-project/hadoop-hdfs-native-client/src/main/native/libhdfspp/lib/x-platform/c_api.cc
+++ hadoop-hdfs-project/hadoop-hdfs-native-client/src/main/native/libhdfspp/lib/x-platform/c_api.cc
@@ -16,26 +16,8 @@
  * limitations under the License.
  */
 
-#include "utils.h"
+#include "syscall.h"
 
-#include <filesystem>
-#include <string>
-#include <vector>
-
-std::string XPlatform::Utils::Basename(const std::string& file_path) {
-  if (file_path.empty()) {
-    return ".";
-  }
-
-  const std::filesystem::path path(file_path);
-  std::vector<std::string> parts;
-  for (const auto& part : std::filesystem::path(file_path)) {
-    parts.emplace_back(part.string());
-  }
-
-  /* Handle the case of trailing slash */
-  if (parts.back().empty()) {
-    parts.pop_back();
-  }
-  return parts.back();
+extern "C" int x_platform_syscall_write_to_stdout(const char* msg) {
+  return XPlatform::Syscall::WriteToStdout(msg) ? 1 : 0;
 }
