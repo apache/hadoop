02d578a9a1d2e084b609c24640c51a783a598464
Harunobu Daikoku <hd.11235813.revi1214@gmail.com>
2021-03-29 11:43:29 +0900

    HDFS-15900. RBF: empty blockpool id on dfsrouter caused by UNAVAILABLE NameNode.

--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/test/java/org/apache/hadoop/hdfs/server/federation/router/TestRouterRPCClientRetries.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/test/java/org/apache/hadoop/hdfs/server/federation/router/TestRouterRPCClientRetries.java
@@ -155,10 +155,10 @@ public class TestRouterRPCClientRetries {
     // Renew lease for the DFS client, it will succeed.
     routerProtocol.renewLease(client.getClientName());
 
-    // Verify the retry times, it will retry one time for ns0.
+    // Verify the retry times, it should succeed with no retry as long as at least one of the nameservices is ACTIVE.
     FederationRPCMetrics rpcMetrics = routerContext.getRouter()
         .getRpcServer().getRPCMetrics();
-    assertEquals(1, rpcMetrics.getProxyOpRetries());
+    assertEquals(0, rpcMetrics.getProxyOpRetries());
   }
 
   /**
