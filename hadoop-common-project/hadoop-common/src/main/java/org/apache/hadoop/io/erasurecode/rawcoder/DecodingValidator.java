78e228f24d8356db22d8ff3013a64724e18a2372
Wei-Chiu Chuang <weichiu@cloudera.com>
2021-04-09 11:20:19 +0800

    Fix import name
    Change-Id: I4f0bc6fc49f974d6d7fe87db3d3ac75e9b5513d0

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/erasurecode/rawcoder/DecodingValidator.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/erasurecode/rawcoder/DecodingValidator.java
@@ -17,8 +17,8 @@
  */
 package org.apache.hadoop.io.erasurecode.rawcoder;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.hadoop.classification.InterfaceAudience;
-import org.apache.hadoop.thirdparty.com.google.common.annotations.VisibleForTesting;
 import org.apache.hadoop.io.erasurecode.ECChunk;
 
 import java.io.IOException;
