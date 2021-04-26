90c6caf6505ea89c90ad59a52dc471ba6a24d102
Wei-Chiu Chuang <weichiu@apache.org>
2021-04-26 11:27:15 +0800

    Revert "HDFS-15624. fix the function of setting quota by storage type (#2377)"
    This reverts commit 394b9f7a5c42ffa38bf7c3a44e7d50a4d19414f9.
    
    Ref: HDFS-15995.
    Had to revert this commit, so we can commit HDFS-15566 (a critical bug preventing rolling upgrade to Hadoop 3.3)
    Will re-work this fix again later.

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/protocol/TestLayoutVersion.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/protocol/TestLayoutVersion.java
@@ -128,8 +128,7 @@ public class TestLayoutVersion {
         NameNodeLayoutVersion.Feature.APPEND_NEW_BLOCK,
         NameNodeLayoutVersion.Feature.QUOTA_BY_STORAGE_TYPE,
         NameNodeLayoutVersion.Feature.ERASURE_CODING,
-        NameNodeLayoutVersion.Feature.EXPANDED_STRING_TABLE,
-        NameNodeLayoutVersion.Feature.NVDIMM_SUPPORT);
+        NameNodeLayoutVersion.Feature.EXPANDED_STRING_TABLE);
     for (LayoutFeature f : compatibleFeatures) {
       assertEquals(String.format("Expected minimum compatible layout version " +
           "%d for feature %s.", baseLV, f), baseLV,
