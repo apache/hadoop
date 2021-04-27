f45365f2016b38fdef0cfdee00aea04e3cb6873b
litao <tomleescut@gmail.com>
2021-04-27 21:39:25 +0800

    HDFS-15975. Use LongAdder instead of AtomicLong for branch-3.3 (#2940)
    Signed-off-by: Takanobu Asanuma <tasanuma@apache.org>

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/FsDatasetImpl.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/FsDatasetImpl.java
@@ -2264,7 +2264,7 @@ class FsDatasetImpl implements FsDatasetSpi<FsVolumeImpl> {
         success = true;
       } finally {
         if (!success) {
-          cacheManager.numBlocksFailedToCache.incrementAndGet();
+          cacheManager.numBlocksFailedToCache.increment();
         }
       }
       blockFileName = info.getBlockURI().toString();
