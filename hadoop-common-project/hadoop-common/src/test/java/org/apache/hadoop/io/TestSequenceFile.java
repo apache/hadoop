467d626a75eb8065d9422844d2119c3f12d3dbf8
Kishen Das <kishen@cloudera.com>
2021-04-29 08:52:56 -0700

    Fix unit test

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/io/TestSequenceFile.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/io/TestSequenceFile.java
@@ -733,17 +733,23 @@ public class TestSequenceFile {
 
   @Test
   public void testSequenceFileWriter() throws Exception {
-    FileSystem fs = FileSystem.getLocal(conf);
+    Configuration conf = new Configuration();
+    // This test only works with Raw File System and not Local File System
+    FileSystem fs = FileSystem.getLocal(conf).getRaw();
     Path p = new Path(GenericTestUtils
       .getTempPath("testSequenceFileWriter.seq"));
     try(SequenceFile.Writer writer = SequenceFile.createWriter(
             fs, conf, p, LongWritable.class, Text.class)) {
-
+      Assertions.assertThat(writer.hasCapability
+        (StreamCapabilities.HSYNC)).isEqualTo(true);
+      Assertions.assertThat(writer.hasCapability(
+        StreamCapabilities.HFLUSH)).isEqualTo(true);
       LongWritable key = new LongWritable();
       key.set(1);
       Text value = new Text();
       value.set("value");
       writer.append(key, value);
+      writer.flush();
       writer.hflush();
       writer.hsync();
       Assertions.assertThat(fs.getFileStatus(p).getLen()).isGreaterThan(0);
