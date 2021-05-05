360660fc89e30906ea05005ecede3420477b8387
Borislav Iordanov <biordanov@apple.com>
2021-05-05 12:52:11 -0400

    fix issue with unconfigured SSL stores in HttpServer2

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/ssl/FileMonitoringTimerTask.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/ssl/FileMonitoringTimerTask.java
@@ -24,6 +24,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.TimerTask;
 import java.util.function.Consumer;
 
@@ -42,34 +46,53 @@ public class FileMonitoringTimerTask extends TimerTask {
   static final String PROCESS_ERROR_MESSAGE =
       "Could not process file change : ";
 
-  final private Path filePath;
+  final private List<Path> filePaths;
   final private Consumer<Path> onFileChange;
   final Consumer<Throwable> onChangeFailure;
-  private long lastProcessed;
+  private List<Long> lastProcessed;
+
+  /**
+   * See {@link #FileMonitoringTimerTask(List, Consumer, Consumer)}
+   *
+   * @param filePath The file to monitor.
+   * @param onFileChange What to do when the file changes.
+   * @param onChangeFailure What to do when <code>onFileChange</code> throws an exception.
+   */
+  public FileMonitoringTimerTask(Path filePath, Consumer<Path> onFileChange,
+                                 Consumer<Throwable> onChangeFailure) {
+    this(Collections.singletonList(filePath), onFileChange, onChangeFailure);
+  }
 
   /**
    * Create file monitoring task to be scheduled using a standard Java {@link java.util.Timer}
    * instance.
    *
-   * @param filePath The path to the file to monitor.
+   * @param filePaths The path to the file to monitor.
    * @param onFileChange The function to call when the file has changed.
    * @param onChangeFailure The function to call when an exception is thrown during the
    *                       file change processing.
    */
-  public FileMonitoringTimerTask(Path filePath, Consumer<Path> onFileChange,
-      Consumer<Throwable> onChangeFailure) {
-    Preconditions.checkNotNull(filePath, "path to monitor disk file is not set");
+  public FileMonitoringTimerTask(List<Path> filePaths, Consumer<Path> onFileChange,
+                                 Consumer<Throwable> onChangeFailure) {
+    Preconditions.checkNotNull(filePaths, "path to monitor disk file is not set");
     Preconditions.checkNotNull(onFileChange, "action to monitor disk file is not set");
 
-    this.filePath = filePath;
-    this.lastProcessed = filePath.toFile().lastModified();
+    this.filePaths = filePaths;
+    this.lastProcessed = new ArrayList<Long>();
+    this.filePaths.forEach(path -> this.lastProcessed.add(path.toFile().lastModified()));
     this.onFileChange = onFileChange;
     this.onChangeFailure = onChangeFailure;
   }
 
   @Override
   public void run() {
-    if (lastProcessed != filePath.toFile().lastModified()) {
+    int modified = -1;
+    for (int i = 0; i < filePaths.size() && modified < 0; i++) {
+      if (lastProcessed.get(i) != filePaths.get(i).toFile().lastModified())
+        modified = i;
+    }
+    if (modified > -1) {
+      Path filePath = filePaths.get(modified);
       try {
         onFileChange.accept(filePath);
       } catch (Throwable t) {
@@ -79,7 +102,7 @@ public class FileMonitoringTimerTask extends TimerTask {
           LOG.error(PROCESS_ERROR_MESSAGE + filePath.toString(), t);
         }
       }
-      lastProcessed = filePath.toFile().lastModified();
+      lastProcessed.set(modified, filePath.toFile().lastModified());
     }
   }
 }
