aa48086d7d7abd4fef4a037f684fe99cb68797fc
sumangala <f20150178g@alumni.bits-pilani.ac.in>
2021-05-12 15:53:14 +0530

    test fix

--- hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/services/TestGetContentSummary.java
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/services/TestGetContentSummary.java
@@ -47,119 +47,128 @@ public class TestGetContentSummary extends AbstractAbfsIntegrationTest {
   private static final int TEST_BUFFER_SIZE = 20;
   private static final int FILES_PER_DIRECTORY = 2;
   private static final int MAX_THREADS = 16;
-  private static final int NUM_FILES_FOR_LIST_MAX_TEST =
-      DEFAULT_AZURE_LIST_MAX_RESULTS + 10;
+  private static final int NUM_FILES_FOR_LIST_MAX_TEST = 10;
+//      DEFAULT_AZURE_LIST_MAX_RESULTS + 10;
+private static final int NUM_CONCURRENT_CALLS = 8;
 
   private final String[] directories = {"/testFolder",
+      "/testFolderII",
       "/testFolder/testFolder1",
       "/testFolder/testFolder2",
       "/testFolder/testFolder3",
-      "/testFolderII",
       "/testFolder/testFolder2/testFolder4",
       "/testFolder/testFolder2/testFolder5",
       "/testFolder/testFolder3/testFolder6",
-      "/testFolder/testFolder3/testFolder7",
-      "/testFolder/testFolder3/testFolder6/leafDir",
-      "/testFolderII/listMaxDir",
-      "/testFolderII/listMaxDir/" + DEFAULT_AZURE_LIST_MAX_RESULTS/2 + "_mid_folder"};
-  private final Path pathToFile = new Path("/testFolder/test1");
-  private final Path pathToListMaxDir = new Path("/testFolderII/listMaxDir");
-  private final Path pathToLeafDir =
-      new Path("/testFolder/testFolder3/testFolder6/leafDir");
-  private final Path pathToIntermediateDirWithFilesOnly = new Path(
-        "/testFolder/testFolder2/testFolder5");
-  private final Path pathToIntermediateDirWithFilesAndSubdirs = new Path(
-        "/testFolder/testFolder3");
-  private final String[] dirsWithNonEmptyFiles = {"/testFolder", "/testFolder/testFolder1",
-      "/testFolder/testFolder2/testFolder5", "/testFolder/testFolder3"};
+      "/testFolder/testFolder3/testFolder7"};
 
-  private final AzureBlobFileSystem fs = createFileSystem();
   private final byte[] b = new byte[TEST_BUFFER_SIZE];
 
   public TestGetContentSummary() throws Exception {
-    createDirectoryStructure();
     new Random().nextBytes(b);
   }
 
   @Test
   public void testFilesystemRoot()
-      throws IOException {
-    int fileCount =
-        (directories.length - 2) * FILES_PER_DIRECTORY + NUM_FILES_FOR_LIST_MAX_TEST;
+      throws IOException, ExecutionException, InterruptedException {
+    AzureBlobFileSystem fs = getFileSystem();
+    createDirectoryStructure();
+    int fileCount = directories.length * FILES_PER_DIRECTORY;
     ContentSummary contentSummary = fs.getContentSummary(new Path("/"));
     verifyContentSummary(contentSummary, directories.length, fileCount,
-        dirsWithNonEmptyFiles.length * FILES_PER_DIRECTORY * TEST_BUFFER_SIZE);
+        directories.length * TEST_BUFFER_SIZE);
   }
 
   @Test
   public void testFileContentSummary() throws IOException {
-    ContentSummary contentSummary = fs.getContentSummary(pathToFile);
+    AzureBlobFileSystem fs = getFileSystem();
+    fs.mkdirs(new Path("/testFolder"));
+    Path filePath = new Path("/testFolder/testFile");
+    fs.create(filePath);
+    FSDataOutputStream out = fs.append(filePath);
+    out.write(b);
+    out.close();
+    ContentSummary contentSummary = fs.getContentSummary(filePath);
     verifyContentSummary(contentSummary, 0, 1, TEST_BUFFER_SIZE);
   }
 
   @Test
   public void testLeafDir() throws IOException {
-    ContentSummary contentSummary = fs.getContentSummary(pathToLeafDir);
+    AzureBlobFileSystem fs = getFileSystem();
+    fs.mkdirs(new Path("/testFolder"));
+    fs.mkdirs(new Path("/testFolder/testFolder1"));
+    fs.mkdirs(new Path("/testFolder/testFolder2"));
+    Path leafDir = new Path("/testFolder/testFolder1/testFolder3");
+    fs.mkdirs(leafDir);
+    ContentSummary contentSummary = fs.getContentSummary(leafDir);
     verifyContentSummary(contentSummary, 0, 0, 0);
   }
 
   @Test
-  public void testIntermediateDirWithFilesOnly() throws IOException {
+  public void testIntermediateDirWithFilesOnly()
+      throws IOException, ExecutionException, InterruptedException {
+    AzureBlobFileSystem fs = getFileSystem();
+    fs.mkdirs(new Path("/testFolder"));
+    Path intermediateDir = new Path("/testFolder/testFolder1");
+    fs.mkdirs(intermediateDir);
+    populateDirWithFiles(intermediateDir, FILES_PER_DIRECTORY);
     ContentSummary contentSummary =
-        fs.getContentSummary(pathToIntermediateDirWithFilesOnly);
+        fs.getContentSummary(intermediateDir);
     verifyContentSummary(contentSummary, 0, FILES_PER_DIRECTORY,
-        TEST_BUFFER_SIZE * FILES_PER_DIRECTORY);
+        TEST_BUFFER_SIZE);
   }
 
   @Test
-  public void testIntermediateDirWithFilesAndSubdirs() throws IOException {
+  public void testIntermediateDirWithFilesAndSubdirs()
+      throws IOException, ExecutionException, InterruptedException {
+    AzureBlobFileSystem fs = getFileSystem();
+    fs.mkdirs(new Path("/testFolder"));
+    Path intermediateDir = new Path("/testFolder/testFolder1");
+    fs.mkdirs(intermediateDir);
+    populateDirWithFiles(intermediateDir, FILES_PER_DIRECTORY);
+    fs.mkdirs(new Path("/testFolder/testFolder1/testFolder3"));
     ContentSummary contentSummary =
-        fs.getContentSummary(pathToIntermediateDirWithFilesAndSubdirs);
-    verifyContentSummary(contentSummary, 3, 3 * FILES_PER_DIRECTORY,
-        TEST_BUFFER_SIZE * FILES_PER_DIRECTORY);
+        fs.getContentSummary(intermediateDir);
+    verifyContentSummary(contentSummary, 1, FILES_PER_DIRECTORY,
+        TEST_BUFFER_SIZE);
   }
 
   @Test
   public void testDirOverListMaxResultsItems()
-      throws IOException {
+      throws IOException, ExecutionException, InterruptedException {
+    AzureBlobFileSystem fs = getFileSystem();
+    fs.mkdirs(new Path("/testFolder"));
+    Path pathToListMaxDir = new Path("/testFolder/listMaxDir");
+    fs.mkdirs(pathToListMaxDir);
+    fs.mkdirs(new Path(pathToListMaxDir + "/testFolder2"));
+    populateDirWithFiles(pathToListMaxDir, NUM_FILES_FOR_LIST_MAX_TEST);
     verifyContentSummary(
         fs.getContentSummary(pathToListMaxDir), 1,
-        NUM_FILES_FOR_LIST_MAX_TEST + FILES_PER_DIRECTORY, 0);
+        NUM_FILES_FOR_LIST_MAX_TEST, TEST_BUFFER_SIZE);
   }
 
   @Test
   public void testInvalidPath() throws Exception {
+    AzureBlobFileSystem fs = getFileSystem();
     intercept(IOException.class, () -> fs.getContentSummary(new Path(
         "/nonExistentPath")));
   }
 
   @Test
   public void testConcurrentGetContentSummaryCalls()
-          throws InterruptedException, ExecutionException {
-    ExecutorService executorService = new ThreadPoolExecutor(1, MAX_THREADS,
-        5, TimeUnit.SECONDS, new SynchronousQueue<>());
+      throws InterruptedException, ExecutionException, IOException {
+    ExecutorService executorService = new ThreadPoolExecutor(1, MAX_THREADS, 5,
+        TimeUnit.SECONDS, new SynchronousQueue<>());
     ArrayList<Future<ContentSummary>> futures = new ArrayList<>();
-    for (String directory : directories) {
+    createDirectoryStructure();
+    for (int i = 0; i < NUM_CONCURRENT_CALLS; i++) {
       Future<ContentSummary> future = executorService.submit(
-              () -> fs.getContentSummary(new Path(directory)));
+          () -> getFileSystem().getContentSummary(new Path("/testFolder")));
       futures.add(future);
     }
-    int[][] dirCS = {{8, 8 * FILES_PER_DIRECTORY, 8 * TEST_BUFFER_SIZE},
-        {0, FILES_PER_DIRECTORY, 2 * TEST_BUFFER_SIZE},
-        {2, 3 * FILES_PER_DIRECTORY, 2 * TEST_BUFFER_SIZE},
-        {3, 3 * FILES_PER_DIRECTORY, 2 * TEST_BUFFER_SIZE},
-        {2, NUM_FILES_FOR_LIST_MAX_TEST + 2 * FILES_PER_DIRECTORY, 0},
-        {0, FILES_PER_DIRECTORY, 0},
-        {0, FILES_PER_DIRECTORY, FILES_PER_DIRECTORY * TEST_BUFFER_SIZE},
-        {1, FILES_PER_DIRECTORY, 0},
-        {0, FILES_PER_DIRECTORY, 0},
-        {0, 0, 0},
-        {1, NUM_FILES_FOR_LIST_MAX_TEST + 2, 0},
-        {0, FILES_PER_DIRECTORY, 0}};
-
-    for (int i=0; i<directories.length; i++) {
+    for (int i = 0; i < NUM_CONCURRENT_CALLS; i++) {
       ContentSummary contentSummary = futures.get(i).get();
-      verifyContentSummary(contentSummary, dirCS[i][0], dirCS[i][1], dirCS[i][2]);
+      verifyContentSummary(contentSummary, 7, 8 * FILES_PER_DIRECTORY,
+          8 * TEST_BUFFER_SIZE);
     }
     executorService.shutdown();
   }
@@ -178,26 +187,16 @@ public class TestGetContentSummary extends AbstractAbfsIntegrationTest {
 
   private void createDirectoryStructure()
       throws IOException, ExecutionException, InterruptedException {
+    AzureBlobFileSystem fs = getFileSystem();
     for (String directory : directories) {
       Path dirPath = new Path(directory);
       fs.mkdirs(dirPath);
-      if (!(dirPath.equals(pathToLeafDir) || dirPath
-          .equals(pathToListMaxDir))) {
-        populateDirWithFiles(dirPath, FILES_PER_DIRECTORY);
-      }
+      populateDirWithFiles(dirPath, FILES_PER_DIRECTORY);
     }
-    for (String dir : dirsWithNonEmptyFiles) {
-      for (int i = 0; i < FILES_PER_DIRECTORY; i++) {
-        FSDataOutputStream out = fs.append(new Path(dir + "/test" + i));
-        out.write(b);
-        out.close();
-      }
-    }
-    populateDirWithFiles(pathToListMaxDir, NUM_FILES_FOR_LIST_MAX_TEST);
   }
 
   private void populateDirWithFiles(Path directory, int numFiles)
-      throws ExecutionException, InterruptedException {
+      throws ExecutionException, InterruptedException, IOException {
     final List<Future<Void>> tasks = new ArrayList<>();
     ExecutorService es = Executors.newFixedThreadPool(10);
     for (int i = 0; i < numFiles; i++) {
@@ -210,6 +209,10 @@ public class TestGetContentSummary extends AbstractAbfsIntegrationTest {
     for (Future<Void> task : tasks) {
       task.get();
     }
+    FSDataOutputStream out = getFileSystem()
+        .append(new Path(directory + "/test0"));
+    out.write(b);
+    out.close();
     es.shutdownNow();
   }
 }
