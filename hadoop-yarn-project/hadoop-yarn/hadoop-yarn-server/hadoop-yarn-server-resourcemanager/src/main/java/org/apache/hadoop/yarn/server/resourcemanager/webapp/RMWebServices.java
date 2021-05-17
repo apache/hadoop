137e20cc9b927894251452e0a88e25c99e3f32c5
lujiefsi <lujiefsi@foxmail.com>
2021-05-16 23:44:38 -0500

    YARN-10555. Missing access check before getAppAttempts (#2608)
    Co-authored-by: lujie <lujie@foxmail.com>
    Signed-off-by: Akira Ajisaka <aajisaka@apache.org>
    (cherry picked from commit d92a25b790e5ad7d8e21fc3949cdd0f74d496b1b)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/webapp/RMWebServices.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/webapp/RMWebServices.java
@@ -1074,7 +1074,7 @@ public class RMWebServices extends WebServices implements RMWebServiceProtocol {
     AppAttemptsInfo appAttemptsInfo = new AppAttemptsInfo();
     for (RMAppAttempt attempt : app.getAppAttempts().values()) {
       AppAttemptInfo attemptInfo = new AppAttemptInfo(rm, attempt,
-          app.getUser(), hsr.getScheme() + "://");
+          hasAccess(app, hsr), app.getUser(), hsr.getScheme() + "://");
       appAttemptsInfo.add(attemptInfo);
     }
 
