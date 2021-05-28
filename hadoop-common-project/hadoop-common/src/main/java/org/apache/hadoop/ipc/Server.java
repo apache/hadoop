a357d4ce607eaefb4ee26ff157ce88074c3e55f2
Konstantin V Shvachko <shv@apache.org>
2021-05-27 18:44:27 -0700

    HDFS-16040. RpcQueueTime metric counts requeued calls as unique events. Contributed by Simbarashe Dzinamarira.
    (cherry picked from commit 8ce30f51f999c0a80db53a2a96b5be5505d4d151)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
@@ -2938,6 +2938,7 @@ public abstract class Server {
              */
             // Re-queue the call and continue
             requeueCall(call);
+            call = null;
             continue;
           }
           if (LOG.isDebugEnabled()) {
