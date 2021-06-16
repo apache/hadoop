ef6ecb7d8f7f3b2ebbd25440c1b701960b69b85e
zhengchenyu001 <zhengchenyu001@ke.com>
2021-06-16 16:04:52 +0800

    HDFS-16070. Addendum DataTransfer block storm when datanode's io is busy.

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/datanode/TestBusyIODataNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/datanode/TestBusyIODataNode.java
@@ -1,19 +1,18 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership.  The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package org.apache.hadoop.hdfs.server.datanode;
@@ -45,6 +44,7 @@ import org.apache.hadoop.hdfs.server.blockmanagement.DatanodeDescriptor;
 import org.apache.hadoop.hdfs.server.blockmanagement.NumberReplicas;
 import org.apache.hadoop.hdfs.server.namenode.FSNamesystem;
 import org.apache.hadoop.hdfs.server.namenode.INodeFile;
+import org.apache.hadoop.test.LambdaTestUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -76,10 +76,8 @@ public class TestBusyIODataNode {
         0, TimeUnit.MILLISECONDS);
     conf.setInt(DFSConfigKeys.DFS_REPLICATION_KEY, 1);
     conf.setInt(
-        DFSConfigKeys.DFS_NAMENODE_RECONSTRUCTION_PENDING_TIMEOUT_SEC_KEY,
-        1);
+        DFSConfigKeys.DFS_NAMENODE_RECONSTRUCTION_PENDING_TIMEOUT_SEC_KEY, 1);
     conf.setInt(DFSConfigKeys.DFS_NAMENODE_REDUNDANCY_INTERVAL_SECONDS_KEY, 1);
-    conf.setInt(DFSConfigKeys.DFS_BLOCKREPORT_INTERVAL_MSEC_KEY, 1000);
     conf.setInt(DFSConfigKeys.DFS_HEARTBEAT_INTERVAL_KEY, HEARTBEAT_INTERVAL);
     cluster = new MiniDFSCluster.Builder(conf).numDataNodes(2).build();
     cluster.waitActive();
@@ -95,55 +93,18 @@ public class TestBusyIODataNode {
     }
   }
 
-  static protected void writeFile(FileSystem fileSys, Path name, int repl)
-      throws IOException {
-    writeFile(fileSys, name, repl, 2);
-  }
-
-  static protected void writeFile(FileSystem fileSys, Path name, int repl,
-      int numOfBlocks) throws IOException {
-    writeFile(fileSys, name, repl, numOfBlocks, true);
-  }
-
-  static protected FSDataOutputStream writeFile(FileSystem fileSys, Path name,
-      int repl, int numOfBlocks, boolean completeFile)
+  public void writeFile(FileSystem fileSys, Path name, int repl)
       throws IOException {
     // create and write a file that contains two blocks of data
     FSDataOutputStream stm = fileSys.create(name, true, fileSys.getConf()
             .getInt(CommonConfigurationKeys.IO_FILE_BUFFER_SIZE_KEY, 4096),
         (short) repl, BLOCK_SIZE);
-    byte[] buffer = new byte[BLOCK_SIZE * numOfBlocks];
+    byte[] buffer = new byte[BLOCK_SIZE * 1];
     Random rand = new Random(SEED);
     rand.nextBytes(buffer);
     stm.write(buffer);
-    LOG.info("Created file " + name + " with " + repl + " replicas.");
-    if (completeFile) {
-      stm.close();
-      return null;
-    } else {
-      stm.flush();
-      // Do not close stream, return it
-      // so that it is not garbage collected
-      return stm;
-    }
-  }
-
-  /*
-   * Wait till node is fully decommissioned.
-   */
-  private void waitBlockMeetReplication(BlockInfo blockInfo, Short repl) {
-    boolean done = repl == blockInfo.numNodes();
-    while (!done) {
-      LOG.info("Waiting for repl change to " + repl + " current repl: "
-          + blockInfo.numNodes());
-      try {
-        Thread.sleep(HEARTBEAT_INTERVAL * 500);
-      } catch (InterruptedException e) {
-        // nothing
-      }
-      done = repl == blockInfo.numNodes();
-    }
-    LOG.info("block " + blockInfo + " meet the replication " + repl);
+    LOG.info("Created file {} with {} replicas.", name, repl);
+    stm.close();
   }
 
   /**
@@ -174,19 +135,25 @@ public class TestBusyIODataNode {
       if (datanode.getXferPort() != dn.getXferPort()) {
         continue;
       }
-      Set<ExtendedBlock> set = Collections
+      Set<ExtendedBlock> sleepSet = Collections
           .synchronizedSet(new HashSet<ExtendedBlock>() {
             @Override
             public boolean add(ExtendedBlock block) {
               boolean ret = super.add(block);
               try {
-                Thread.sleep(30000);
+                // need sleep more than DFS_NAMENODE_RECONSTRUCTION_PENDING_TIMEOUT_SEC_KEY
+                // + DFS_HEARTBEAT_INTERVAL_KEY + DFS_NAMENODE_REDUNDANCY_INTERVAL_SECONDS_KEY
+                // seconds, then we will make sure the pending reconstruction
+                // block is timeout, then trigger next DataTransfer.
+                Thread.sleep(8000);
               } catch (InterruptedException e) {
                 e.printStackTrace();
               }
               return ret;
             }
           });
+      // We set DataNode.transferringBlock to sleepSet so that we could simulate
+      //   that DataNode's DataTransfer will stuck.
       Field transferringBlock = DataNode.class
           .getDeclaredField("transferringBlock");
       transferringBlock.setAccessible(true);
@@ -194,8 +161,9 @@ public class TestBusyIODataNode {
       modifiers.setAccessible(true);
       modifiers.setInt(transferringBlock,
           transferringBlock.getModifiers() & ~Modifier.FINAL);
-      transferringBlock.set(dn, set);
+      transferringBlock.set(dn, sleepSet);
 
+      // Capture log so that we know we already avoid unnecessary data transfer.
       Field logger = DataNode.class.getDeclaredField("LOG");
       logger.setAccessible(true);
       modifiers = Field.class.getDeclaredField("modifiers");
@@ -206,12 +174,11 @@ public class TestBusyIODataNode {
 
     // 4. add block's replication to 2
     bm.setReplication((short) 1, (short) 2, firstBlock);
-    waitBlockMeetReplication(firstBlock, (short) 2);
+    LambdaTestUtils.await(10000, 500, () -> 2 == firstBlock.numNodes());
     replicas = bm.countNodes(firstBlock);
     Assert.assertEquals(replicas.liveReplicas(), 2);
 
-    // 5. sleep and verfiy the unnecessary transfer.
-    Thread.sleep(3000);
+    // 5. verfiy the unnecessary transfer.
     String blockPoolId = cluster.getNameNode().getNamesystem().getBlockPoolId();
     verify(log, atLeastOnce())
         .warn("Thread for transfer {} was already running，ignore this block.",
