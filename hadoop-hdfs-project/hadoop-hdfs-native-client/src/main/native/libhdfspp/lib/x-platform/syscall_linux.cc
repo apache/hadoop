81a1f268a9e171cec86483d6008173aabe76ff08
Gautham Banasandra <gautham.bangalore@gmail.com>
2021-06-21 19:59:35 +0530

    Add some formatting fixes
    * Fixed some formatting.

--- hadoop-hdfs-project/hadoop-hdfs-native-client/src/main/native/libhdfspp/lib/x-platform/syscall_linux.cc
+++ hadoop-hdfs-project/hadoop-hdfs-native-client/src/main/native/libhdfspp/lib/x-platform/syscall_linux.cc
@@ -70,4 +70,3 @@ int XPlatform::Syscall::CreateAndOpenTempFile(std::vector<char>& pattern) {
 bool XPlatform::Syscall::CloseFile(const int file_descriptor) {
   return close(file_descriptor) == 0;
 }
-
