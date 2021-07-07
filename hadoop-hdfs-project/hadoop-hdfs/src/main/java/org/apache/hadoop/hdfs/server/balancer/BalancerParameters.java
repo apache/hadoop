7581895ab49f9bde190706fd6687d788a5f98043
JiaguodongF <272436024@qq.com>
2021-07-07 16:43:05 +0800

    HDFS-16114. the balancer parameters print error (#3179)
    Co-authored-by: jiaguodong5 <jiaguodong5@jd.com>

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/balancer/BalancerParameters.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/balancer/BalancerParameters.java
@@ -116,8 +116,8 @@ final class BalancerParameters {
         + " max idle iteration = %s," + " #excluded nodes = %s,"
         + " #included nodes = %s," + " #source nodes = %s,"
         + " #blockpools = %s," + " run during upgrade = %s,"
-        + " hot block time interval = %s]"
-        + " sort top nodes = %s",
+        + " sort top nodes = %s,"
+        + " hot block time interval = %s]",
         Balancer.class.getSimpleName(), getClass().getSimpleName(), policy,
         threshold, maxIdleIteration, excludedNodes.size(),
         includedNodes.size(), sourceNodes.size(), blockpools.size(),
