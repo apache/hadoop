d7659fbc5e6883579e759c40a5faf6f000326c3a
Bobby Wang <wbo4958@gmail.com>
2021-07-28 07:02:49 +0800

    resolve comments

--- hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/TestS3AInputStreamRetry.java
+++ hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/TestS3AInputStreamRetry.java
@@ -129,12 +129,17 @@ public class TestS3AInputStreamRetry extends AbstractS3AMockTest {
         mockedS3ObjectIndex++;
         // open() -> lazySeek() -> reopen()
         //        -> getObject (mockedS3ObjectIndex=1) -> getObjectContent(objectInputStreamBad1)
-        // read() -> objectInputStreamBad1 throws exception -> onReadFailure -> reopen
-        //        -> getObject (mockedS3ObjectIndex=2) -> getObjectContent(objectInputStreamBad2)
-        //  -> retry -> wrappedStream.read -> objectInputStreamBad2 throws exception
-        //        -> onReadFailure -> reopen -> getObject (mockedS3ObjectIndex=3) throws exception
-        //        -> reopen throws exception
-        //  -> retry -> re-execute wrappedStream.read (we need to check if wrappedStream == null)
+        // read() -> objectInputStreamBad1 throws exception
+        //        -> onReadFailure -> close wrappedStream
+        //  -> retry(1) -> wrappedStream==null -> reopen -> getObject (mockedS3ObjectIndex=2)
+        //        -> getObjectContent(objectInputStreamBad2)-> objectInputStreamBad2
+        //        -> wrappedStream.read -> objectInputStreamBad2 throws exception
+        //        -> onReadFailure -> close wrappedStream
+        //  -> retry(2) -> wrappedStream==null -> reopen
+        //        -> getObject (mockedS3ObjectIndex=3) throws exception
+        //  -> retry(3) -> wrappedStream==null -> reopen -> getObject (mockedS3ObjectIndex=4)
+        //        -> getObjectContent(objectInputStreamGood)-> objectInputStreamGood
+        //        -> wrappedStream.read
         if (mockedS3ObjectIndex == 3) {
           throw new SdkClientException("Failed to get S3Object");
         }
