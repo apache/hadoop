f405c1f17dae5a88aca41c5ab631708444b74453
srinivasst <srinivasstxd@gmail.com>
2021-08-10 18:18:36 +0530

    Move AM container check to separate method.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmnode/RMNodeImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmnode/RMNodeImpl.java
@@ -1354,27 +1354,22 @@ public class RMNodeImpl implements RMNode, EventHandler<RMNodeEvent> {
       boolean isNodeDecommissioning =
           initialState.equals(NodeState.DECOMMISSIONING);
       if (isNodeDecommissioning) {
-        // This check solves the following race condition -
+        List<ApplicationId> keepAliveApps = statusEvent.getKeepAliveAppIds();
+        // hasScheduledAMContainers solves the following race condition -
         // 1. launch AM container on a node with 0 containers.
         // 2. gracefully decommission this node.
-        // 3. At the same time the node heartbeats to RM.
-        //    rmNode.runningApplications will be empty as this data structure is
-        //    updated later in this method. This will cause the node to be
+        // 3. Node heartbeats to RM. In StatusUpdateWhenHealthyTransition,
+        //    rmNode.runningApplications will be empty as it is updated after
+        //    call to RMNodeImpl.deactivateNode. This will cause the node to be
         //    deactivated even though container is running on it and hence kill
         //    all containers running on it.
         // In order to avoid such race conditions the ground truth is retrieved
         // from the scheduler before deactivating a DECOMMISSIONING node.
         // Only AM containers are considered as AM container reattempts can
         // cause application failures if max attempts is set to 1.
-        boolean hasLaunchedRMMasterContainers = rmNode.context.getScheduler()
-            .getSchedulerNode(rmNode.getNodeID())
-            .getCopiedListOfRunningContainers()
-            .stream().anyMatch(RMContainer::isAMContainer);
-
-        List<ApplicationId> keepAliveApps = statusEvent.getKeepAliveAppIds();
         if (rmNode.runningApplications.isEmpty() &&
             (keepAliveApps == null || keepAliveApps.isEmpty()) &&
-            !hasLaunchedRMMasterContainers) {
+            !hasScheduledAMContainers(rmNode)) {
           LOG.info("No containers running on " + rmNode.nodeId + ". "
               + "Attempting to deactivate decommissioning node.");
           RMNodeImpl.deactivateNode(rmNode, NodeState.DECOMMISSIONED);
@@ -1421,6 +1416,17 @@ public class RMNodeImpl implements RMNode, EventHandler<RMNodeEvent> {
       }
 
       return initialState;
+    }
+
+    /**
+     * Checks if the scheduler has scheduled any AMs on the given node.
+     * @return true if node has any AM scheduled on it.
+     */
+    private boolean hasScheduledAMContainers(RMNodeImpl rmNode) {
+      return rmNode.context.getScheduler()
+          .getSchedulerNode(rmNode.getNodeID())
+          .getCopiedListOfRunningContainers()
+          .stream().anyMatch(RMContainer::isAMContainer);
     }
   }
 
