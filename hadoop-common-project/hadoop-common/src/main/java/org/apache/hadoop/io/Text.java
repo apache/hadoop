c481bd8dbefeb27f09d3e45013770c59f4bdf0de
Peter Bacsko <pbacsko@cloudera.com>
2021-09-09 11:46:15 +0200

    [addendum] HADOOP-17901. Added clarification comment
    Change-Id: I72a40d8e4f9978d84618f586199916edd9939e78

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/Text.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/Text.java
@@ -300,6 +300,8 @@ public class Text extends BinaryComparable
    */
   private boolean ensureCapacity(final int capacity) {
     if (bytes.length < capacity) {
+      // Try to expand  the backing array by the factor of 1.5x  
+      // (by taking the current size + diving it by half)
       int targetSize = Math.max(capacity, bytes.length + (bytes.length >> 1));
       bytes = new byte[targetSize];
       return true;
