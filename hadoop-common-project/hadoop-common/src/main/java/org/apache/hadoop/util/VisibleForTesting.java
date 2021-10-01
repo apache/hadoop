53929537a26bebf13e36ef0eccb9a1ef037c5e8f
Viraj Jasani <vjasani@apache.org>
2021-10-01 15:40:27 +0530

    removing redundant comments

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/VisibleForTesting.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/VisibleForTesting.java
@@ -21,12 +21,6 @@ package org.apache.hadoop.util;
 /**
  * Annotates a program element that exists, or is more widely visible than
  * otherwise necessary, only for use in test code.
- *
- * <p><b>Do not use this interface</b> for public or protected declarations:
- * it is a fig leaf for bad design, and it does not prevent anyone from using
- * the declaration---and experience has shown that they will.
- * If the method breaks the encapsulation of its class, then its internal
- * representation will be hard to change.
  */
 public @interface VisibleForTesting {
 }
