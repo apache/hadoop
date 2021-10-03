0090084350cf3e0eeafe8b6a6fb14927846e4ca0
Viraj Jasani <vjasani@apache.org>
2021-10-03 23:59:26 +0530

    HADOOP-17950. Provide replacement for deprecated APIs of commons-io IOUtils

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/ProviderUtils.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/ProviderUtils.java
@@ -23,6 +23,7 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 import org.apache.hadoop.thirdparty.com.google.common.annotations.VisibleForTesting;
 import org.apache.commons.io.IOUtils;
@@ -224,7 +225,8 @@ public final class ProviderUtils {
           throw new IOException("Password file does not exist");
         }
         try (InputStream is = pwdFile.openStream()) {
-          pass = IOUtils.toString(is).trim().toCharArray();
+          pass = IOUtils.toString(is, Charset.defaultCharset()).trim()
+              .toCharArray();
         }
       }
     }
