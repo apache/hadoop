967d6caea93963f45d122aa70be98c4d46c79b91
Viraj Jasani <vjasani@apache.org>
2021-10-05 19:24:48 +0530

    HADOOP-17947. Additional element types for VisibleForTesting

--- hadoop-common-project/hadoop-annotations/src/main/java/org/apache/hadoop/classification/VisibleForTesting.java
+++ hadoop-common-project/hadoop-annotations/src/main/java/org/apache/hadoop/classification/VisibleForTesting.java
@@ -37,7 +37,8 @@ import java.lang.annotation.Target;
  * they MUST be re-scoped as public/limited private.
  */
 @Retention(RetentionPolicy.CLASS)
-@Target({ ElementType.TYPE, ElementType.METHOD })
+@Target({ ElementType.TYPE, ElementType.METHOD, ElementType.FIELD,
+    ElementType.CONSTRUCTOR })
 @Documented
 public @interface VisibleForTesting {
 }
