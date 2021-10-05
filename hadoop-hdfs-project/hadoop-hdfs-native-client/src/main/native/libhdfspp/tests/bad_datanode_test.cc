8f660971437e161edde20ac80626407edca8ed4e
Gautham Banasandra <gautham.bangalore@gmail.com>
2021-10-05 07:42:22 +0530

    Trigger CI

--- hadoop-hdfs-project/hadoop-hdfs-native-client/src/main/native/libhdfspp/tests/bad_datanode_test.cc
+++ hadoop-hdfs-project/hadoop-hdfs-native-client/src/main/native/libhdfspp/tests/bad_datanode_test.cc
@@ -341,6 +341,7 @@ TEST(BadDataNodeTest, InternalError) {
   ASSERT_TRUE(tracker->IsBadNode(failing_dn));
 }
 
+
 int main(int argc, char *argv[]) {
   // The following line must be executed to initialize Google Mock
   // (and Google Test) before running the tests.
