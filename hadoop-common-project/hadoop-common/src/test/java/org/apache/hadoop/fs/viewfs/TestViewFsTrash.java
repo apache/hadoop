d62702b02f4f2199dab0633bfd6c6665676d5a69
Xing Lin <xinglin@linkedin.com>
2021-10-13 07:54:55 -0700

    checkStyle: removed 3 unused imports

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/viewfs/TestViewFsTrash.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/viewfs/TestViewFsTrash.java
@@ -17,16 +17,11 @@
  */
 package org.apache.hadoop.fs.viewfs;
 
-
-import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileSystemTestHelper;
 import org.apache.hadoop.fs.FsConstants;
-import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.RawLocalFileSystem;
 import org.apache.hadoop.fs.TestTrash;
 import org.junit.After;
 import org.junit.Before;
