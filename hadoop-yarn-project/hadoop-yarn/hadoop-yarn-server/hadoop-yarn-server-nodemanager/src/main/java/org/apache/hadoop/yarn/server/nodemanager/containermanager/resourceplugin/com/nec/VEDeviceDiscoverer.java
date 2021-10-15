a81b57b2412ebc1b20a72b800467c329f792c796
lujie <lujie@ict.ac.cn>
2021-10-15 16:32:24 +0800

    fix format

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/resourceplugin/com/nec/VEDeviceDiscoverer.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/resourceplugin/com/nec/VEDeviceDiscoverer.java
@@ -60,7 +60,7 @@ class VEDeviceDiscoverer {
   public Set<Device> getDevicesFromPath(String path) throws IOException {
     MutableInt counter = new MutableInt(0);
     try (Stream<Path> stream = Files.walk(Paths.get(path), 1)) {
-        return stream.filter(p -> p.toFile().getName().startsWith("veslot"))
+      return stream.filter(p -> p.toFile().getName().startsWith("veslot"))
             .map(p -> toDevice(p, counter))
             .collect(Collectors.toSet());
     }
