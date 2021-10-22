f4d253faebebbff7ae4580b2c56fdd42bde965e4
9uapaw <gyorandr@gmail.com>
2021-10-22 13:59:03 +0200

    YARN-10930. Clean tests and fix review feedbacks

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/conf/QueueCapacityConfigParser.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/conf/QueueCapacityConfigParser.java
@@ -49,6 +49,7 @@ public class QueueCapacityConfigParser {
 
   private static final Pattern RESOURCE_PATTERN = Pattern.compile(RESOURCE_REGEX);
   private static final Pattern UNIFORM_PATTERN = Pattern.compile(UNIFORM_REGEX);
+  public static final String FLOAT_DIGIT_REGEX = "[0-9.]";
 
   private final List<Parser> parsers = new ArrayList<>();
 
@@ -79,6 +80,8 @@ public class QueueCapacityConfigParser {
     if (capacityString == null) {
       return new QueueCapacityVector();
     }
+    // Trim all spaces from capacity string
+    capacityString = capacityString.replaceAll(" ", "");
 
     for (Parser parser : parsers) {
       Matcher matcher = parser.regex.matcher(capacityString);
@@ -164,12 +167,11 @@ public class QueueCapacityConfigParser {
     QueueCapacityType capacityType = QueueCapacityType.ABSOLUTE;
 
     // Extract suffix from a value e.g. for 6w extract w
-    String suffix = resourceValue.replaceAll("[0-9]", "");
+    String suffix = resourceValue.replaceAll(FLOAT_DIGIT_REGEX, "");
     if (!resourceValue.endsWith(suffix)) {
       return;
     }
 
-    String cleanResourceName = resourceName.replaceAll(" ", "");
     float parsedResourceValue = Float.parseFloat(resourceValue.substring(
         0, resourceValue.length() - suffix.length()));
     float convertedValue = parsedResourceValue;
@@ -185,7 +187,7 @@ public class QueueCapacityConfigParser {
       }
     }
 
-    resource.setResource(cleanResourceName, convertedValue, capacityType);
+    resource.setResource(resourceName, convertedValue, capacityType);
   }
 
   /**
