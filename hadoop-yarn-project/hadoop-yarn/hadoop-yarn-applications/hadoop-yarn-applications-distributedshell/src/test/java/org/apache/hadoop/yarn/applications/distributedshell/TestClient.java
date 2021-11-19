b7458cffbe5f3555026aaec583574c950418c79a
Ashutosh Gupta <ashutosh.gupta@st.niituniversity.in>
2021-11-20 00:52:52 +0530

    formating code as hadoop project code formatter guidance

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-applications/hadoop-yarn-applications-distributedshell/src/test/java/org/apache/hadoop/yarn/applications/distributedshell/TestClient.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-applications/hadoop-yarn-applications-distributedshell/src/test/java/org/apache/hadoop/yarn/applications/distributedshell/TestClient.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,17 +29,17 @@ import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestClient {
-    @Test
-    public void testParseResourcesString() {
-        // Setup
-        final Map<String, Long> expectedResult = new HashMap<>();
-        expectedResult.put("memory-mb", 3072L);
-        expectedResult.put("vcores", 1L);
+  @Test
+  public void testParseResourcesString() {
+    // Setup
+    final Map<String, Long> expectedResult = new HashMap<>();
+    expectedResult.put("memory-mb", 3072L);
+    expectedResult.put("vcores", 1L);
 
-        // Run the test
-        final Map<String, Long> result = Client.parseResourcesString("[memory-mb=3072,vcores=1]");
+    // Run the test
+    final Map<String, Long> result = Client.parseResourcesString("[memory-mb=3072,vcores=1]");
 
-        // Verify the results
-        assertEquals(expectedResult, result);
-    }
+    // Verify the results
+    assertEquals(expectedResult, result);
+  }
 }
