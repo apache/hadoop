ad97c93b220636787ce84163c391370b039fd4b2
functioner <oierwhz@gmail.com>
2021-12-03 16:28:57 -0500

    add testIOEOnListenerAccept

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
@@ -1325,7 +1325,7 @@ public abstract class Server {
   }
 
   /** Listens on the socket. Creates jobs for the handler threads*/
-  private class Listener extends Thread {
+  protected class Listener extends Thread {
     
     private ServerSocketChannel acceptChannel = null; //the accept channel
     private Selector selector = null; //the selector that we use for the server
@@ -1530,16 +1530,20 @@ public abstract class Server {
     InetSocketAddress getAddress() {
       return (InetSocketAddress)acceptChannel.socket().getLocalSocketAddress();
     }
-    
+
+    protected void configureSocketChannel(SocketChannel channel) throws IOException {
+      channel.configureBlocking(false);
+      channel.socket().setTcpNoDelay(tcpNoDelay);
+      channel.socket().setKeepAlive(true);
+    }
+
     void doAccept(SelectionKey key) throws InterruptedException, IOException,  OutOfMemoryError {
       ServerSocketChannel server = (ServerSocketChannel) key.channel();
       SocketChannel channel;
       while ((channel = server.accept()) != null) {
 
         try {
-          channel.configureBlocking(false);
-          channel.socket().setTcpNoDelay(tcpNoDelay);
-          channel.socket().setKeepAlive(true);
+          configureSocketChannel(channel);
         } catch (IOException e) {
           LOG.warn("Error in an accepted SocketChannel", e);
           try {
@@ -3260,7 +3264,7 @@ public abstract class Server {
     this.negotiateResponse = buildNegotiateResponse(enabledAuthMethods);
     
     // Start the listener here and let it bind to the port
-    listener = new Listener(port);
+    listener = createListener(port);
     // set the server port to the default listener port.
     this.port = listener.getAddress().getPort();
     connectionManager = new ConnectionManager();
@@ -3291,6 +3295,10 @@ public abstract class Server {
         HealthCheckFailedException.class);
   }
 
+  protected Listener createListener(int port) throws IOException {
+    return new Listener(port);
+  }
+
   public synchronized void addAuxiliaryListener(int auxiliaryPort)
       throws IOException {
     if (auxiliaryListenerMap == null) {
@@ -3300,7 +3308,7 @@ public abstract class Server {
       throw new IOException(
           "There is already a listener binding to: " + auxiliaryPort);
     }
-    Listener newListener = new Listener(auxiliaryPort);
+    Listener newListener = createListener(auxiliaryPort);
     newListener.setIsAuxiliary();
 
     // in the case of port = 0, the listener would be on a != 0 port.
