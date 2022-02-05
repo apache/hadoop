57e7fa00fa5968eb109ff95f266dc93bd9547a7d
bensonlin321 <bensonlin321@gmail.com>
2022-02-05 12:12:35 -0800

    refactor: remove unused import

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/test/java/org/apache/hadoop/yarn/webapp/view/TestHeaderBlock.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/test/java/org/apache/hadoop/yarn/webapp/view/TestHeaderBlock.java
@@ -24,7 +24,6 @@ import java.io.PrintWriter;
 import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.Map;
-import java.util.HashMap;
 
 import org.apache.hadoop.yarn.webapp.test.WebAppTests;
 
