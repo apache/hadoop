92b9fbad9c32741e843a9b2659b8e4461e7baa5e
Monthon Klongklaew <monthonk@amazon.com>
2022-02-14 11:37:54 +0000

    HADOOP-13294. Change error message to the console when trying to delete root directory

--- hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/ITestS3AFsShell.java
+++ hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/ITestS3AFsShell.java
@@ -84,7 +84,7 @@ public class ITestS3AFsShell extends AbstractS3ATestBase {
 
     assertNotEquals("Should not be able to delete root directory", 0,
         fsShell.run(new String[] {"-rm", "-r", "-f", fs.getUri().toString() + "/"}));
-    assertTrue(err.toString().contains("Input/output error"));
+    assertTrue(err.toString().contains("S3A: Cannot delete the root directory"));
     err.reset();
   }
 
