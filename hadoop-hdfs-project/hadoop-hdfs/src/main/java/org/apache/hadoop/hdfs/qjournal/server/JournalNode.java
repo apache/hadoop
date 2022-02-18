ce4be63aafe16360ee6dffc5c236aed9ee74eafa
Viraj Jasani <vjasani@apache.org>
2022-02-19 00:30:49 +0530

    HADOOP-18131. Upgrade maven enforcer plugin and relevant dependencies

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/qjournal/server/JournalNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/qjournal/server/JournalNode.java
@@ -62,6 +62,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 /**
