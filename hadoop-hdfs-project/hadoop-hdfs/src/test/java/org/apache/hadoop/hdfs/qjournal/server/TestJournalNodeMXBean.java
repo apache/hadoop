17c215b442b482a0ba00c9cf69043562d2e25d9e
Viraj Jasani <vjasani@apache.org>
2022-02-20 11:26:45 +0530

    HDFS-16461. Expose JournalNode storage info in the jmx metrics

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/qjournal/server/TestJournalNodeMXBean.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/qjournal/server/TestJournalNodeMXBean.java
@@ -44,6 +44,7 @@ public class TestJournalNodeMXBean {
   
   private static final String NAMESERVICE = "ns1";
   private static final int NUM_JN = 1;
+  private static final int NS_ID = 12345;
   
   private MiniJournalCluster jCluster;
   private JournalNode jn;
@@ -80,7 +81,7 @@ public class TestJournalNodeMXBean {
     assertFalse(journalStatus.contains(NAMESERVICE));
 
     // format the journal ns1
-    final NamespaceInfo FAKE_NSINFO = new NamespaceInfo(12345, "mycluster",
+    final NamespaceInfo FAKE_NSINFO = new NamespaceInfo(NS_ID, "mycluster",
         "my-bp", 0L);
     jn.getOrCreateJournal(NAMESERVICE).format(FAKE_NSINFO, false);
 
@@ -109,6 +110,11 @@ public class TestJournalNodeMXBean {
     assertEquals(jn.getJNStartedTimeInMillis(), startTime);
     String version = (String) mbs.getAttribute(mxbeanName, "Version");
     assertEquals(jn.getVersion(), version);
+    String[] journalStorageInfos = (String[]) mbs.getAttribute(mxbeanName, "StorageInfos");
+    assertEquals(jn.getStorageInfos().size(), journalStorageInfos.length);
+    assertTrue(journalStorageInfos[1].contains("cid=mycluster"));
+    assertTrue(journalStorageInfos[1].contains("c=0"));
+    assertTrue(journalStorageInfos[1].contains("nsid=" + NS_ID));
 
     // restart journal node without formatting
     jCluster = new MiniJournalCluster.Builder(new Configuration()).format(false)
