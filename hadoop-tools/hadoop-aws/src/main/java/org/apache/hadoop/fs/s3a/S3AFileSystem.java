708a0ce21bc1bd6164ff650e9104de70fe3f1dbb
Steve Loughran <stevel@cloudera.com>
2022-03-22 13:20:37 +0000

    HADOOP-13704. Optimized S3A getContentSummary()
    Optimize the scan for s3 by performing a deep tree listing,
    inferring directory counts from the paths returned.
    
    Contributed by Ahmar Suhail.
    
    Change-Id: I26ffa8c6f65fd11c68a88d6e2243b0eac6ffd024

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java
@@ -3266,9 +3266,9 @@ public class S3AFileSystem extends FileSystem implements StreamCapabilities,
     }
 
     @Override
-    public RemoteIterator<S3AFileStatus> listStatusIterator(final Path path)
-        throws IOException {
-      return S3AFileSystem.this.innerListStatus(path);
+    public RemoteIterator<S3ALocatedFileStatus> listFilesIterator(final Path path,
+        final boolean recursive) throws IOException {
+      return S3AFileSystem.this.innerListFiles(path, recursive, Listing.ACCEPT_ALL_BUT_S3N, null);
     }
   }
 
