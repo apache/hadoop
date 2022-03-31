20483f6dc7bba5572acec7262658393504dc314a
Xing Lin <linxingnku@gmail.com>
2022-03-31 14:59:09 -0700

    HADOOP-18169. getDelegationTokens in ViewFs should also fetch the token from fallback FS (#4094)
    Signed-off-by: Owen O'Malley <oomalley@linkedin.com>

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/viewfs/ViewFs.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/viewfs/ViewFs.java
@@ -746,6 +746,17 @@ public class ViewFs extends AbstractFileSystem {
         result.addAll(tokens);
       }
     }
+
+    // Add tokens from fallback FS
+    if (this.fsState.getRootFallbackLink() != null) {
+      AbstractFileSystem rootFallbackFs =
+          this.fsState.getRootFallbackLink().getTargetFileSystem();
+      List<Token<?>> tokens = rootFallbackFs.getDelegationTokens(renewer);
+      if (tokens != null) {
+        result.addAll(tokens);
+      }
+    }
+
     return result;
   }
 
