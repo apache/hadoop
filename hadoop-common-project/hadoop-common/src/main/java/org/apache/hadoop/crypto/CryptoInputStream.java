8b9b12d1c580a83b0bbce83dde8c94e8cbe79a9d
Renukaprasad C <48682981+prasad-acit@users.noreply.github.com>
2022-04-24 23:49:12 +0530

    HDFS-16551. CryptoInputStream#close() should be syncronized (#4227)
    (cherry picked from commit 5f259ef7fd9dd03aab38cbe07899d34b3ca917dd)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/crypto/CryptoInputStream.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/crypto/CryptoInputStream.java
@@ -313,7 +313,7 @@ public class CryptoInputStream extends FilterInputStream implements
   }
   
   @Override
-  public void close() throws IOException {
+  public synchronized void close() throws IOException {
     if (closed) {
       return;
     }
