74d63284b8fcb779450e75d0581d83f0261ad05b
Anmol Asrani <anmolasrani@microsoft.com>
2022-07-25 11:56:39 +0530

    Test case added

--- hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/TestAzureBlobFileSystem.java
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/TestAzureBlobFileSystem.java
@@ -19,26 +19,22 @@
 package org.apache.hadoop.fs.azurebfs;
 
 import org.junit.Test;
-import org.apache.hadoop.fs.azurebfs.AzureBlobFileSystem;
 import org.mockito.Mockito;
 import org.apache.hadoop.fs.FileSystem;
-import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.azurebfs.constants.AbfsHttpConstants;
 import org.apache.hadoop.fs.Path;
 import org.junit.Assert;
-import org.apache.hadoop.fs.azurebfs.AzureBlobFileSystem;
 import static org.apache.hadoop.fs.azurebfs.constants.ConfigurationKeys.AZURE_CREATE_REMOTE_FILESYSTEM_DURING_INITIALIZATION;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import org.apache.hadoop.test.LambdaTestUtils;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import org.apache.hadoop.fs.azurebfs.AzureBlobFileSystemStore.AzureBlobFileSystemStoreBuilder;
 import java.nio.file.AccessDeniedException;
 import org.apache.hadoop.fs.azurebfs.utils.TracingContext;
 
-public class TestAzureBlobFileSystem extends AbstractAbfsIntegrationTest{
-
+public class TestAzureBlobFileSystem extends AbstractAbfsIntegrationTest {
   public TestAzureBlobFileSystem() throws Exception {
   }
 
@@ -47,21 +43,33 @@ public class TestAzureBlobFileSystem extends AbstractAbfsIntegrationTest{
     AzureBlobFileSystem fs = new AzureBlobFileSystem();
     Configuration conf = this.getRawConfiguration();
     conf.setBoolean(AZURE_CREATE_REMOTE_FILESYSTEM_DURING_INITIALIZATION, true);
-    fs.initialize(FileSystem.getDefaultUri(conf),conf);
-    Assert.assertNotNull(fs.getFileStatus(new Path(AbfsHttpConstants.ROOT_PATH)));
-    AzureBlobFileSystemStore abfsStore = Mockito.mock(AzureBlobFileSystemStore.class);
-    AzureBlobFileSystem azureBlobFileSystem = Mockito.spy(AzureBlobFileSystem.class);
-//    doThrow(new AccessDeniedException("Not permissible")).when(abfsStore).
-//        getFileStatus(any(Path.class), any(TracingContext.class));
-//    TestAzureBlobFileSystem.setAbfsStore(abfsStore, azureBlobFileSystem);
-    doReturn(abfsStore).when(azureBlobFileSystem).getAzureBlobFileSystemStore(any(AzureBlobFileSystemStoreBuilder.class));
-    doReturn(this.getConfiguration()).when(abfsStore).getAbfsConfiguration();
+
+    //Create a file system
+    fs.initialize(FileSystem.getDefaultUri(conf), conf);
+
+    //Since fileSystem is created, getFileStatus should not return null
+    Assert.assertNotNull(
+        fs.getFileStatus(new Path(AbfsHttpConstants.ROOT_PATH)));
+
+    //Throw exception for getFileStatus and see if it gets caught correctly
+    AzureBlobFileSystemStore abfsStore = Mockito.mock(
+        AzureBlobFileSystemStore.class);
+    AzureBlobFileSystem azureBlobFileSystem = Mockito.spy(getFileSystem());
+    doReturn(abfsStore).when(azureBlobFileSystem)
+        .getAzureBlobFileSystemStore(
+            any(AzureBlobFileSystemStoreBuilder.class));
+    AbfsConfiguration abfsConfiguration = Mockito.mock(AbfsConfiguration.class);
+    Mockito.doReturn(true)
+        .when(abfsConfiguration)
+        .getCreateRemoteFileSystemDuringInitialization();
+    Mockito.doReturn(this.getConfiguration().getClientCorrelationId())
+        .when(abfsConfiguration)
+        .getClientCorrelationId();
+    Mockito.doReturn(abfsConfiguration).when(abfsStore).getAbfsConfiguration();
     doThrow(new AccessDeniedException("Not permissible")).when(abfsStore).
         getFileStatus(any(Path.class), any(TracingContext.class));
-    azureBlobFileSystem.initialize(FileSystem.getDefaultUri(conf),conf);
-  }
-
-  public static void setAbfsStore(AzureBlobFileSystemStore abfsStore, AzureBlobFileSystem fs) {
-    Mockito.doReturn(abfsStore).when(fs).getAbfsStore();
+    LambdaTestUtils.intercept(AccessDeniedException.class, () -> {
+      azureBlobFileSystem.initialize(FileSystem.getDefaultUri(conf), conf);
+    });
   }
 }
