0eefb19e003b934c7314abbfced5203f959148d0
slfan1989 <louj1988@@>
2022-08-05 07:43:58 -0700

    YARN-10885. Fix CheckStyle.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-common/src/main/java/org/apache/hadoop/yarn/server/federation/utils/FederationStateStoreFacade.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-common/src/main/java/org/apache/hadoop/yarn/server/federation/utils/FederationStateStoreFacade.java
@@ -21,7 +21,6 @@ package org.apache.hadoop.yarn.server.federation.utils;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 import javax.cache.Cache;
