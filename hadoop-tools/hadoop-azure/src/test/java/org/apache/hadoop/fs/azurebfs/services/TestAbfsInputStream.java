a0d3ef90ef1bc03d8a24475b0ed81728b32aeeda
Anmol Asrani <anmolasrani@microsoft.com>
2022-08-12 19:35:42 +0530

    Checkstyle fixes

--- hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/services/TestAbfsInputStream.java
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/services/TestAbfsInputStream.java
@@ -112,7 +112,8 @@ public class TestAbfsInputStream extends
         null,
         FORWARD_SLASH + fileName,
         THREE_KB,
-        inputStreamContext.withReadBufferSize(ONE_KB).withReadAheadQueueDepth(10).withReadAheadBlockSize(ONE_KB).withReadFooterMetrics(new AbfsReadFooterMetrics()),
+        inputStreamContext.withReadBufferSize(ONE_KB).withReadAheadQueueDepth(10).withReadAheadBlockSize(ONE_KB).
+            withReadFooterMetrics(new AbfsReadFooterMetrics()),
         "eTag",
         getTestTracingContext(null, false));
 
