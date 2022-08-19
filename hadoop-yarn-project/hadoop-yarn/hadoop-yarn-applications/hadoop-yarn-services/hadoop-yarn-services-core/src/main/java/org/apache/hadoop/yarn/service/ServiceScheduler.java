ca537e832540218d06a877554df4ac155bc95021
slfan1989 <louj1988@@>
2022-08-19 16:49:53 +0800

    YARN-11240. Fix CodeStyle.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-applications/hadoop-yarn-services/hadoop-yarn-services-core/src/main/java/org/apache/hadoop/yarn/service/ServiceScheduler.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-applications/hadoop-yarn-services/hadoop-yarn-services-core/src/main/java/org/apache/hadoop/yarn/service/ServiceScheduler.java
@@ -1139,7 +1139,7 @@ public class ServiceScheduler extends CompositeService {
         LOG.info("YARN sysfs synchronized.");
       }
     } catch (IOException | URISyntaxException | InterruptedException e) {
-      LOG.error("Fail to sync service spec: {}", e.getMessage());
+      LOG.error("Fail to sync service spec: {}", e.getMessage(), e);
     }
   }
 }
