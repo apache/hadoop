fd2b1b2fad9fe2258b9cb29838f3b459eec0702f
tomglliu <tomglliu@tencent.com>
2022-08-29 17:09:02 +0800

    DN should notify NN when client requests a missing block

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/FsDatasetImpl.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/FsDatasetImpl.java
@@ -243,8 +243,13 @@ class FsDatasetImpl implements FsDatasetSpi<FsVolumeImpl> {
   @Override // FsDatasetSpi
   public LengthInputStream getMetaDataInputStream(ExtendedBlock b)
       throws IOException {
-    File meta = FsDatasetUtil.getMetaFile(getBlockFile(b), b.getGenerationStamp());
     FsVolumeSpi volume = null;
+    File meta = null;
+    try {
+      meta = FsDatasetUtil.getMetaFile(getBlockFile(b), b.getGenerationStamp());
+    } catch (IOException e) {
+      throw new FileNotFoundException("BlockId " + b.getBlockName() + " is not valid.");
+    }
 
     if (meta == null || !meta.exists()) {
       return null;
