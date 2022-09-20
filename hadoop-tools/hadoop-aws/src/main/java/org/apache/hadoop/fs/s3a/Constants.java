271acdd29e8dd9e24df5615e922fdd85477d853c
Mukund Thakur <mthakur@cloudera.com>
2022-09-20 14:05:52 -0500

    HADOOP-18347. S3A Vectored IO to use bounded thread pool
    part of HADOOP-18103.
    Also introducing a config fs.s3a.vectored.active.ranged.reads
    to configure the maximum number of number of range reads a
    single input stream can have active (downloading, or queued)
    to the central FileSystem instance's pool of queued operations.
    This stops a single stream overloading the shared thread pool.

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/Constants.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/Constants.java
@@ -1207,6 +1207,24 @@ public final class Constants {
   public static final int DEFAULT_AWS_S3_VECTOR_READS_MAX_MERGED_READ_SIZE = 1253376; //1M
 
   /**
+   * Maximum number of range reads a single input stream can have
+   * active (downloading, or queued) to the central FileSystem
+   * instance's pool of queued operations.
+   * This stops a single stream overloading the shared thread pool.
+   * {@value}
+   * <p>
+   * Default is {@link #DEFAULT_AWS_S3_VECTOR_ACTIVE_RANGE_READS}
+   */
+  public static final String AWS_S3_VECTOR_ACTIVE_RANGE_READS =
+          "fs.s3a.vectored.active.ranged.reads";
+
+  /**
+   * Limit of queued range data download operations during vectored
+   * read. Value: {@value}
+   */
+  public static final int DEFAULT_AWS_S3_VECTOR_ACTIVE_RANGE_READS = 4;
+
+  /**
    * Prefix of auth classes in AWS SDK V1.
    */
   public static final String AWS_AUTH_CLASS_PREFIX = "com.amazonaws.auth";
