ceec19e61a5eea6af749881429da1fa470d0dfc6
Kidd5368 <57645247+Kidd53685368@users.noreply.github.com>
2022-09-23 11:27:56 +0800

    HDFS-16776 Erasure Coding: The length of targets should be checked when DN gets a reconstruction task (#4901)
    (cherry picked from commit 9a29075f915173e24c77cf8aea2908da0aa328e3)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/erasurecode/StripedReconstructor.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/erasurecode/StripedReconstructor.java
@@ -277,6 +277,10 @@ abstract class StripedReconstructor {
     return decoder;
   }
 
+  int getNumLiveBlocks(){
+    return liveBitSet.cardinality();
+  }
+
   void cleanup() {
     if (decoder != null) {
       decoder.release();
