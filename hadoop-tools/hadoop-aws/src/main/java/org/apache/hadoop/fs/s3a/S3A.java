fd1aced2d3b7feb4e303bfd2023e735eb95d075c
Steve Loughran <stevel@cloudera.com>
2022-10-04 16:17:33 +0100

    HADOOP-18476. Abfs and S3A FileContext bindings to close wrapped filesystems in finalizer
    Not a fan of finalize, especially as FileContext delete on exit seems trouble.
    The class holds strong references so finalize() won't be called, ever.
    
    Ignoring that, and avoiding something major like making everything closeable,
    the abfs and s3a bindings should close their wrapped FS in close.
    
    Change-Id: I7cfc66df02a74407e896a11ecc9704175236648c

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3A.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3A.java
@@ -33,11 +33,12 @@ import java.net.URISyntaxException;
  */
 @InterfaceAudience.Public
 @InterfaceStability.Evolving
-public class S3A extends DelegateToFileSystem{
+public class S3A extends DelegateToFileSystem {
 
   public S3A(URI theUri, Configuration conf)
-          throws IOException, URISyntaxException {
-    super(theUri, new S3AFileSystem(), conf, "s3a", false);
+      throws IOException, URISyntaxException {
+    super(theUri, new S3AFileSystem(), conf,
+      "s3a", false);
   }
 
   @Override
@@ -53,5 +54,14 @@ public class S3A extends DelegateToFileSystem{
     sb.append("; fsImpl=").append(fsImpl);
     sb.append('}');
     return sb.toString();
+  }
+
+  /**
+   * Close the file system; the FileContext API doesn't have an explicit close.
+   */
+  @Override
+  protected void finalize() throws Throwable {
+    fsImpl.close();
+    super.finalize();
   }
 }
