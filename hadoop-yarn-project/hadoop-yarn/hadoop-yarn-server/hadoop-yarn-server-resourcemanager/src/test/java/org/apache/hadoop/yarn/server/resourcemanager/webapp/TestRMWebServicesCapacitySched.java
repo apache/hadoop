bfce21ee08f2faef795354ac3615eb2a393bd158
PJ Fanning <pjfanning@users.noreply.github.com>
2022-10-13 18:19:19 +0100

    YARN-11330. use secure XML parsers (#4981)
    Move construction of XML parsers in YARN
    modules to using the locked-down parser factory
    of HADOOP-18469.
    
    One exception: GpuDeviceInformationParser still supports DTD resolution;
    all other features are disabled.
    
    Contributed by P J Fanning

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/webapp/TestRMWebServicesCapacitySched.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/webapp/TestRMWebServicesCapacitySched.java
@@ -50,6 +50,7 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.http.JettyUtils;
+import org.apache.hadoop.util.XMLUtils;
 import org.apache.hadoop.yarn.api.records.Resource;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.server.resourcemanager.MockRM;
@@ -315,7 +316,7 @@ public class TestRMWebServicesCapacitySched extends JerseyTestBase {
     DOMSource domSource = new DOMSource(document);
     StringWriter writer = new StringWriter();
     StreamResult result = new StreamResult(writer);
-    TransformerFactory tf = TransformerFactory.newInstance();
+    TransformerFactory tf = XMLUtils.newSecureTransformerFactory();
     Transformer transformer = tf.newTransformer();
     transformer.setOutputProperty(OutputKeys.INDENT, "yes");
     transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
@@ -324,7 +325,7 @@ public class TestRMWebServicesCapacitySched extends JerseyTestBase {
   }
 
   public static Document loadDocument(String xml) throws Exception {
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilderFactory factory = XMLUtils.newSecureDocumentBuilderFactory();
     DocumentBuilder builder = factory.newDocumentBuilder();
     InputSource is = new InputSource(new StringReader(xml));
     return builder.parse(is);
