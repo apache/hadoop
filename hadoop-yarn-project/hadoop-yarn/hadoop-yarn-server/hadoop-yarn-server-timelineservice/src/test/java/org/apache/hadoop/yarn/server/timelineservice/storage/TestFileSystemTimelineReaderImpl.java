2e3f91bdf501d693a6899398221d39bd10c584bb
slfan1989 <55643692+slfan1989@users.noreply.github.com>
2022-10-13 14:40:54 +0800

    HADOOP-18360. Update commons-csv from 1.0 to 1.9.0. (#4928). Contributed by fanshilun.
    Signed-off-by: Ayush Saxena <ayushsaxena@apache.org>

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timelineservice/src/test/java/org/apache/hadoop/yarn/server/timelineservice/storage/TestFileSystemTimelineReaderImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timelineservice/src/test/java/org/apache/hadoop/yarn/server/timelineservice/storage/TestFileSystemTimelineReaderImpl.java
@@ -74,7 +74,7 @@ public class TestFileSystemTimelineReaderImpl {
     loadEntityData(rootDir);
     // Create app flow mapping file.
     CSVFormat format =
-        CSVFormat.DEFAULT.withHeader("APP", "USER", "FLOW", "FLOWRUN");
+        CSVFormat.Builder.create().setHeader("APP", "USER", "FLOW", "FLOWRUN").build();
     String appFlowMappingFile = rootDir + File.separator + "entities" +
         File.separator + "cluster1" + File.separator +
         FileSystemTimelineReaderImpl.APP_FLOW_MAPPING_FILE;
