2593ab5f28efd2ae960e21445c677bcf3ad71d5a
PJ Fanning <pjfanning@users.noreply.github.com>
2022-10-20 17:37:56 +0100

    HADOOP-15983. Use jersey-json that is built to use jackson2 ((#3988)
    Moves from com.sun.jersey 1.19 to the artifact
    com.github.pjfanning:jersey-json:1.20
    
    This allows jackson 1 to be removed from the classpath.
    
    Contains
    
    * HADOOP-16908. Prune Jackson 1 from the codebase and restrict
       its usage for future
    * HADOOP-18219. Fix shaded client test failure
    
    These are needed for the HADOOP-15983 changes to build.
    
    Contributed by PJ Fanning.

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/oauth2/AzureADAuthenticator.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/oauth2/AzureADAuthenticator.java
@@ -29,10 +29,10 @@ import java.util.Date;
 import java.util.Hashtable;
 import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import org.apache.hadoop.thirdparty.com.google.common.base.Preconditions;
-import org.codehaus.jackson.JsonFactory;
-import org.codehaus.jackson.JsonParser;
-import org.codehaus.jackson.JsonToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
