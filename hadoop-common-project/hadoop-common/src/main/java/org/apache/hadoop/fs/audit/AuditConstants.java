7c8b3f18ed72454141c88986477447781e18387e
Ankit Saurabh <sauraank@amazon.co.uk>
2022-10-27 11:43:20 +0100

    Removed extra spaces

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/audit/AuditConstants.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/audit/AuditConstants.java
@@ -91,7 +91,7 @@ public final class AuditConstants {
   public static final String PARAM_PROCESS = "ps";
 
   /**
-   * Header: Range for GET request data : {@value }.
+   * Header: Range for GET request data : {@value}.
    */
   public static final String PARAM_RANGE = "r";
 
