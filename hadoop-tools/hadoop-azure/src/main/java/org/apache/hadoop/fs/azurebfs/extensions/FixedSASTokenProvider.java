087c6b740eb6dd41d7b2634646c33168d70f4826
sreeb-msft <sreeb@microsoft.com>
2022-11-15 11:15:39 +0530

    Added new FixedTokenProvider class to read fixed SAS

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/extensions/FixedSASTokenProvider.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/extensions/FixedSASTokenProvider.java
@@ -1,2 +1,28 @@
-package org.apache.hadoop.fs.azurebfs.extensions;public class FixedSASTokenProvider {
+package org.apache.hadoop.fs.azurebfs.extensions;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.azurebfs.constants.ConfigurationKeys;
+import org.apache.hadoop.fs.azurebfs.contracts.exceptions.InvalidConfigurationValueException;
+import org.apache.hadoop.security.AccessControlException;
+
+import java.io.IOException;
+
+public class FixedSASTokenProvider implements SASTokenProvider{
+    Configuration configuration;
+    String accountName;
+
+    @Override
+    public void initialize(Configuration configuration, String accountName) throws IOException {
+        this.configuration = configuration;
+        this.accountName = accountName;
+    }
+
+    @Override
+    public String getSASToken(String account, String fileSystem, String path, String operation) throws IOException {
+        String fixedToken = configuration.get(ConfigurationKeys.FS_AZURE_SAS_FIXED_TOKEN, null);
+        if (fixedToken == null)
+            throw new InvalidConfigurationValueException("The fixed SAS Token configuration value is invalid.");
+        else
+            return fixedToken;
+    }
 }
