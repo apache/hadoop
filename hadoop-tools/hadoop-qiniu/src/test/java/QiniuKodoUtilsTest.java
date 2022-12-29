9ed73deeb47e412073f409ca82ff8c39afa2db2a
zzq <i@zhangzqs.cn>
2022-12-29 17:31:56 +0800

    Complete QiniuKodoUtilsTest.java

--- hadoop-tools/hadoop-qiniu/src/test/java/QiniuKodoUtilsTest.java
+++ hadoop-tools/hadoop-qiniu/src/test/java/QiniuKodoUtilsTest.java
@@ -1,20 +1,47 @@
 import org.apache.hadoop.fs.Path;
 import org.junit.Test;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import static org.apache.hadoop.fs.qiniu.kodo.QiniuKodoUtils.*;
 import static org.junit.Assert.*;
 
 public class QiniuKodoUtilsTest {
-
-
     @Test
-    public void testIsKeyDir() {
-        boolean is = isKeyDir("user/yangsen/");
-        assertTrue(is);
+    public void testPathToKey() {
+        assertEquals("p1/c1", pathToKey(new Path("qiniu://bucket/user/user1"), new Path("/p1/c1")));
+        assertEquals("user/user1/p1/c1", pathToKey(new Path("qiniu://bucket/user/user1"), new Path("./p1/c1")));
+        assertEquals("user/user1/p1/c1", pathToKey(new Path("qiniu://bucket/user/user1"), new Path("p1/c1")));
     }
 
     @Test
-    public void testPathToKey() {
-        assertEquals("p1/c1", pathToKey(new Path(""), new Path("/p1/c1")));
+    public void testKeyToPath() throws URISyntaxException {
+        URI uri = new URI("qiniu://bucket/user/user1");
+        assertEquals(new Path("qiniu://bucket/p1/c1"), keyToPath(uri, new Path("qiniu://bucket/user/user1"), "p1/c1"));
+        assertEquals(new Path("qiniu://bucket/p1/c1"), keyToPath(uri, new Path("/bucket/user/user1"), "p1/c1"));
+    }
+
+    @Test
+    public void testKeyToFileKey() {
+        assertNull(keyToFileKey(null));
+        assertEquals("a", keyToFileKey("a"));
+        assertEquals("a", keyToFileKey("a/"));
+    }
+
+    @Test
+    public void testKeyToDirKey() {
+        assertNull(keyToDirKey(null));
+        assertEquals("", keyToDirKey(""));
+        assertEquals("a/", keyToDirKey("a/"));
+        assertEquals("a/", keyToDirKey("a"));
+    }
+
+    @Test
+    public void testIsKeyDir() {
+        assertFalse(isKeyDir(null));
+        assertFalse(isKeyDir(""));
+        assertFalse(isKeyDir("user/user1"));
+        assertTrue(isKeyDir("user/user1/"));
     }
 }
