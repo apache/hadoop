211293adca038c9de0be557573639c60ff27c26b
zzq <i@zhangzqs.cn>
2022-12-30 14:24:28 +0800

    add QiniuKodoClientTest class

---
+++ hadoop-tools/hadoop-qiniu/src/test/java/org/apache/hadoop/fs/qinu/kodo/QiniuKodoClientTest.java
@@ -0,0 +1,52 @@
+package org.apache.hadoop.fs.qinu.kodo;
+
+import com.qiniu.storage.Region;
+import com.qiniu.util.Auth;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.qiniu.kodo.Constants;
+import org.apache.hadoop.fs.qiniu.kodo.QiniuKodoClient;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class QiniuKodoClientTest {
+    QiniuKodoClient client;
+    @Before
+    public void setup() {
+        Configuration conf = new Configuration();
+        conf.addResource("contract-test-options.xml");
+
+        com.qiniu.storage.Configuration qiniuConfig = new com.qiniu.storage.Configuration();
+        qiniuConfig.region = Region.autoRegion();
+
+        String accessKey = conf.get(Constants.QINIU_PARAMETER_ACCESS_KEY);
+        String secretKey = conf.get(Constants.QINIU_PARAMETER_SECRET_KEY);
+        Auth auth = Auth.create(accessKey, secretKey);
+        String bucket = URI.create(conf.get("fs.contract.test.fs.qiniu")).getHost();
+        client = new QiniuKodoClient(auth, qiniuConfig, bucket);
+    }
+
+    @Test
+    public void testMkdir() throws IOException {
+        client.makeEmptyObject("aaab/");
+    }
+
+    @Test
+    public void testListStatus() throws IOException {
+        client.listStatus("", false).forEach((e)->{
+            System.out.println(e.key);
+        });
+    }
+
+    @Test
+    public void testGetFileStatus() throws IOException {
+        System.out.println(client.getFileStatus("user"));
+    }
+
+    @Test
+    public void testDeleteKeys() throws IOException {
+        client.deleteKeys("");
+    }
+}
