37c82b173320792929ae4d3d4f653aa8c6409fa0
zzq <i@zhangzqs.cn>
2023-01-09 14:08:21 +0800

    fix test class QiniuKodoFileSystemTest

--- hadoop-tools/hadoop-qiniu/src/test/java/org/apache/hadoop/fs/qinu/kodo/QiniuKodoFileSystemTest.java
+++ hadoop-tools/hadoop-qiniu/src/test/java/org/apache/hadoop/fs/qinu/kodo/QiniuKodoFileSystemTest.java
@@ -21,10 +21,10 @@ public class QiniuKodoFileSystemTest {
         conf.addResource("core-site.xml");
         conf.addResource("contract-test-options.xml");
 
-        conf.setIfUnset("fs.contract.test.fs.qiniu", "kodo://qshell-hadoop");
+        conf.setIfUnset("fs.contract.test.fs.kodo", "kodo://qshell-hadoop");
 
         fs = new QiniuKodoFileSystem();
-        fs.initialize(URI.create(conf.get("fs.contract.test.fs.qiniu")), conf);
+        fs.initialize(URI.create(conf.get("fs.contract.test.fs.kodo")), conf);
     }
 
     @Test
@@ -69,7 +69,7 @@ public class QiniuKodoFileSystemTest {
 
     @Test
     public void testWrite() throws IOException {
-        FSDataOutputStream os = fs.create(new Path("/zzq123"));
+        FSDataOutputStream os = fs.create(new Path("/user/zzq123.txt"));
         String s = "HelloWorld Qiniu";
         os.writeUTF(s);
         os.close();
