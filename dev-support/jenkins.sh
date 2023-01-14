a5599ebe1f31a8559c770128aeff76bae89f4bbb
Gautham Banasandra <gautham.bangalore@gmail.com>
2023-01-14 21:44:39 +0530

    Delete /f/yetus/m2

--- dev-support/jenkins.sh
+++ dev-support/jenkins.sh
@@ -119,6 +119,7 @@ function run_ci() {
     rm -rf "${PATCHDIR:?}"
   fi
   mkdir -p "${PATCHDIR}"
+  rm -rf "/f/yetus/m2"
 
   # if given a JIRA issue, process it. If CHANGE_URL is set
   # (e.g., Github Branch Source plugin), process it.
