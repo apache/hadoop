cd8b3898044df19712e3ebd38eb14ec9f0f62fe7
zzq <i@zhangzqs.cn>
2023-01-16 15:25:52 +0800

    refactor SourceDataFetcher's fetch method

--- hadoop-tools/hadoop-qiniu/src/main/java/org/apache/hadoop/fs/qiniu/kodo/download/QiniuKodoSourceDataFetcher.java
+++ hadoop-tools/hadoop-qiniu/src/main/java/org/apache/hadoop/fs/qiniu/kodo/download/QiniuKodoSourceDataFetcher.java
@@ -1,11 +1,13 @@
 package org.apache.hadoop.fs.qiniu.kodo.download;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.fs.qiniu.kodo.QiniuKodoClient;
 import org.apache.hadoop.fs.qiniu.kodo.blockcache.DataFetcherBlockReader;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 public class QiniuKodoSourceDataFetcher extends DataFetcherBlockReader {
     private final QiniuKodoClient client;
@@ -16,18 +18,12 @@ public class QiniuKodoSourceDataFetcher extends DataFetcherBlockReader {
         this.client = client;
     }
 
-    private static final byte[] buffer = new byte[8*1024];
-
     @Override
     public byte[] fetch(String key, long offset, int size) {
         try(InputStream is = client.fetch(key, offset, size)) {
-            ByteArrayOutputStream out = new ByteArrayOutputStream(size);
-
-            int sz;
-            while ((sz = is.read(buffer)) != -1) {
-                out.write(buffer, 0, sz);
-            }
-            return out.toByteArray();
+            byte[] buf = new byte[size];
+            int cnt = IOUtils.read(is, buf);
+            return Arrays.copyOf(buf, cnt);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
