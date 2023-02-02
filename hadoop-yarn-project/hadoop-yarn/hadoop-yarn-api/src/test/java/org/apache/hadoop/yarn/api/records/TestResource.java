c44f675bfa664ea764115da867948a2c63d17c32
slfan1989 <louj1988@@>
2023-02-02 15:26:21 +0800

    YARN-11426. Fix CheckStyle.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/test/java/org/apache/hadoop/yarn/api/records/TestResource.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/test/java/org/apache/hadoop/yarn/api/records/TestResource.java
@@ -42,4 +42,70 @@ class TestResource {
         "Cast to Integer.MAX_VALUE if the long is greater than "
             + "Integer.MAX_VALUE");
   }
+
+  @Test
+  public void testResourceFormatted() {
+    // We set 10MB
+    String expectedResult1 = "<memory:10 MB, vCores:1>";
+    MockResource capability1 = new MockResource();
+    capability1.setMemory(10);
+    capability1.setVirtualCores(1);
+    assertEquals(capability1.toFormattedString(), expectedResult1);
+
+    // We set 1024 MB = 1GB
+    String expectedResult2 = "<memory:1 GB, vCores:1>";
+    MockResource capability2 = new MockResource();
+    capability2.setMemory(1024);
+    capability2.setVirtualCores(1);
+    assertEquals(capability2.toFormattedString(), expectedResult2);
+
+    // We set 1024 * 1024 MB = 1024 GB = 1TB
+    String expectedResult3 = "<memory:1 TB, vCores:1>";
+    MockResource capability3 = new MockResource();
+    capability3.setMemory(1024 * 1024);
+    capability3.setVirtualCores(1);
+    assertEquals(capability3.toFormattedString(), expectedResult3);
+
+    // We set 1024 * 1024 * 1024 MB = 1024 * 1024 GB = 1 * 1024 TB = 1 PB
+    String expectedResult4 = "<memory:1 PB, vCores:1>";
+    MockResource capability4 = new MockResource();
+    capability4.setMemory(1024 * 1024 * 1024);
+    capability4.setVirtualCores(1);
+    assertEquals(capability4.toFormattedString(), expectedResult4);
+  }
+
+  class MockResource extends Resource {
+
+    public MockResource(){
+      this.resources = new ResourceInformation[0];
+    }
+
+    int memory;
+    int virtualCores;
+
+    @Override
+    public int getMemory() {
+      return memory;
+    }
+
+    @Override
+    public void setMemory(int pMemory) {
+      this.memory = pMemory;
+    }
+
+    @Override
+    public int getVirtualCores() {
+      return virtualCores;
+    }
+
+    @Override
+    public void setVirtualCores(int pVCores) {
+      this.virtualCores = pVCores;
+    }
+
+    @Override
+    public long getMemorySize() {
+      return memory;
+    }
+  }
 }
