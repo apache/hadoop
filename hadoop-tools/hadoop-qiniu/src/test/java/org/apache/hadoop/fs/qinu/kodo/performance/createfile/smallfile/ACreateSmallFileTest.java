ae2925988f8a1722ff1f4b1bcf144841c185e70e
zzq <i@zhangzqs.cn>
2023-02-02 17:16:45 +0800

    rename executorService to service

--- hadoop-tools/hadoop-qiniu/src/test/java/org/apache/hadoop/fs/qinu/kodo/performance/createfile/smallfile/ACreateSmallFileTest.java
+++ hadoop-tools/hadoop-qiniu/src/test/java/org/apache/hadoop/fs/qinu/kodo/performance/createfile/smallfile/ACreateSmallFileTest.java
@@ -6,19 +6,17 @@ import org.apache.hadoop.fs.qinu.kodo.performance.QiniuKodoPerformanceBaseTest;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public abstract class ACreateSmallFileTest extends QiniuKodoPerformanceBaseTest {
     abstract protected int files();
 
     @Override
-    protected long testImpl(String testDir, FileSystem fs, ExecutorService executorService) throws Exception {
+    protected long testImpl(String testDir, FileSystem fs, ExecutorService service) throws Exception {
         long ms = System.currentTimeMillis();
 
         for (int i = 0; i < files(); i++) {
             final Path p = new Path(testDir + "/" + i);
-            executorService.submit(() -> {
+            service.submit(() -> {
                 try {
                     fs.create(p).close();
                 } catch (IOException e) {
@@ -26,10 +24,8 @@ public abstract class ACreateSmallFileTest extends QiniuKodoPerformanceBaseTest 
                 }
             });
         }
-        executorService.shutdown();
-        if (!executorService.awaitTermination(1, TimeUnit.MINUTES)) {
-            throw new TimeoutException("More than 1 minute");
-        }
+        service.shutdown();
+        awaitAllExecutors(service);
         return System.currentTimeMillis() - ms;
     }
 }
