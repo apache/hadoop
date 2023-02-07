ff048fce66b6d06f1e896740423b091596a58309
gardenia <colm.dougan@gmail.com>
2023-02-07 12:01:57 +0000

    HADOOP-18621. Resource leak in CryptoOutputStream.close() (#5347)
    When closing we need to wrap the flush() in a try .. finally, otherwise
    when flush throws it will stop completion of the remainder of the
    close activities and in particular the close of the underlying wrapped
    stream object resulting in a resource leak.
    
    Contributed by Colm Dougan

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/crypto/CryptoOutputStream.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/crypto/CryptoOutputStream.java
@@ -242,12 +242,15 @@ public class CryptoOutputStream extends FilterOutputStream implements
       return;
     }
     try {
-      flush();
-      if (closeOutputStream) {
-        super.close();
-        codec.close();
+      try {
+        flush();
+      } finally {
+        if (closeOutputStream) {
+          super.close();
+          codec.close();
+        }
+        freeBuffers();
       }
-      freeBuffers();
     } finally {
       closed = true;
     }
