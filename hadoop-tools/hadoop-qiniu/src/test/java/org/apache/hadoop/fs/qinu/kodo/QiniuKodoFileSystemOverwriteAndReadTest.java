1e47a19903ddc9ec0c59254848cb92d7a6a7bf1c
zzq <i@zhangzqs.cn>
2023-02-08 14:13:43 +0800

    add QiniuKodoFileSystemOverwriteAndReadTest s3a

--- hadoop-tools/hadoop-qiniu/src/test/java/org/apache/hadoop/fs/qinu/kodo/QiniuKodoFileSystemOverwriteAndReadTest.java
+++ hadoop-tools/hadoop-qiniu/src/test/java/org/apache/hadoop/fs/qinu/kodo/QiniuKodoFileSystemOverwriteAndReadTest.java
@@ -3,7 +3,6 @@ package org.apache.hadoop.fs.qinu.kodo;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystemContractBaseTest;
 import org.apache.hadoop.fs.qiniu.kodo.QiniuKodoFileSystem;
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +12,8 @@ import java.net.URI;
 public class QiniuKodoFileSystemOverwriteAndReadTest extends FileSystemContractBaseTest {
     private static final Logger LOG = LoggerFactory.getLogger(QiniuKodoFileSystemOverwriteAndReadTest.class);
 
-    @Before
-    public void setup() throws Exception {
+    @Test
+    public void testOverWriteAndReadByKodo() throws Exception {
         Configuration conf = new Configuration();
         conf.addResource("core-site.xml");
         conf.addResource("contract-test-options.xml");
@@ -22,10 +21,27 @@ public class QiniuKodoFileSystemOverwriteAndReadTest extends FileSystemContractB
         fs = new QiniuKodoFileSystem();
         fs.initialize(URI.create(conf.get("fs.contract.test.fs.kodo")), conf);
 
+        int success = 0;
+        while (true) {
+            try {
+                testOverWriteAndRead();
+                success++;
+            } catch (Throwable e) {
+                LOG.error("Success: " + success + " Error: ", e);
+                break;
+            }
+        }
     }
 
     @Test
-    public void testOverWriteAndRead1() throws Exception {
+    public void testOverWriteAndReadByS3A() throws Exception {
+        Configuration conf = new Configuration();
+        conf.addResource("core-site.xml");
+        conf.addResource("contract-test-options.xml");
+
+        fs = new QiniuKodoFileSystem();
+        fs.initialize(URI.create(conf.get("fs.contract.test.fs.kodo")), conf);
+
         int success = 0;
         while (true) {
             try {
