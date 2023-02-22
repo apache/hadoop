18db7e3b7249960d09bb82476943a35d684e53cf
zzq <i@zhangzqs.cn>
2023-02-22 17:11:47 +0800

    add rename large dir performance test

---
+++ hadoop-tools/hadoop-qiniu/src/test/java/org/apache/hadoop/fs/qinu/kodo/performance/rename/largedir/RenameLargeDirTest.java
@@ -0,0 +1,33 @@
+package org.apache.hadoop.fs.qinu.kodo.performance.rename.largedir;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.qinu.kodo.performance.QiniuKodoPerformanceBaseTest;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RenameLargeDirTest extends QiniuKodoPerformanceBaseTest {
+    @Override
+    protected ExecutorService buildExecutorService() {
+        return Executors.newSingleThreadExecutor();
+    }
+
+    @Override
+    protected long testImpl(String testDir, FileSystem fs, ExecutorService service) throws Exception {
+        String oldPath = testDir + "/oldDir";
+        String newPath = testDir + "/newDir";
+        getPrepareHelper().prepareLargeDir(oldPath, 10000);
+        service.submit(() -> {
+            try {
+                fs.rename(new Path(oldPath), new Path(newPath));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        long ms = System.currentTimeMillis();
+        awaitAllExecutors(service);
+        return System.currentTimeMillis() - ms;
+    }
+}
