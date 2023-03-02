a7c1fad0c9a675195579c971962ddd32e5d9fc51
ZanderXu <zanderxu@apache.org>
2023-03-02 08:18:38 +0800

    HDFS-16923. [SBN read] getlisting RPC to observer will throw NPE if path does not exist (#5400)
    Signed-off-by: Erik Krogen <xkrogen@apache.org>
    
    (cherry picked from commit 6bd24448154fcd3ab9099d7783cc7f7f76c61e08)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -4090,7 +4090,7 @@ public class FSNamesystem implements Namesystem, FSNamesystemMBean,
       logAuditEvent(false, operationName, src);
       throw e;
     }
-    if (needLocation && isObserver()) {
+    if (dl != null && needLocation && isObserver()) {
       for (HdfsFileStatus fs : dl.getPartialListing()) {
         if (fs instanceof HdfsLocatedFileStatus) {
           LocatedBlocks lbs = ((HdfsLocatedFileStatus) fs).getLocatedBlocks();
