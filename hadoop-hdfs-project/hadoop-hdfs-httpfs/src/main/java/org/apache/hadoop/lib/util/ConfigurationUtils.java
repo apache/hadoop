1201158df1c58ec0f44d58da613ca9a49dc36eaa
OpsHubTechUser <99649109+OpsHubTech@users.noreply.github.com>
2023-03-15 19:58:33 +0530

    Update ConfigurationUtils.java

--- hadoop-hdfs-project/hadoop-hdfs-httpfs/src/main/java/org/apache/hadoop/lib/util/ConfigurationUtils.java
+++ hadoop-hdfs-project/hadoop-hdfs-httpfs/src/main/java/org/apache/hadoop/lib/util/ConfigurationUtils.java
@@ -73,7 +73,7 @@ public abstract class ConfigurationUtils {
     for (Map.Entry<String, String> entry : conf) {
       resolved.set(entry.getKey(), conf.get(entry.getKey()));
     }
-    // Testing MR pipeline final testings change
+    // Testing MR pipeline final testings change 
     return resolved;
   }
 
