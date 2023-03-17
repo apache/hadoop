fa723ae83916af6e814923eca53a5fef046d33aa
slfan1989 <55643692+slfan1989@users.noreply.github.com>
2023-03-18 02:59:45 +0800

    YARN-11445. [Federation] Add getClusterInfo, getClusterUserInfo REST APIs for Router. (#5472)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-router/src/test/java/org/apache/hadoop/yarn/server/router/webapp/TestFederationInterceptorREST.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-router/src/test/java/org/apache/hadoop/yarn/server/router/webapp/TestFederationInterceptorREST.java
@@ -73,10 +73,13 @@ import org.apache.hadoop.yarn.server.federation.store.records.GetApplicationHome
 import org.apache.hadoop.yarn.server.federation.store.records.ApplicationHomeSubCluster;
 import org.apache.hadoop.yarn.server.federation.utils.FederationStateStoreFacade;
 import org.apache.hadoop.yarn.server.federation.utils.FederationStateStoreTestUtil;
+import org.apache.hadoop.yarn.server.resourcemanager.MockRM;
 import org.apache.hadoop.yarn.server.resourcemanager.webapp.dao.AppInfo;
 import org.apache.hadoop.yarn.server.resourcemanager.webapp.dao.AppState;
 import org.apache.hadoop.yarn.server.resourcemanager.webapp.dao.ApplicationSubmissionContextInfo;
 import org.apache.hadoop.yarn.server.resourcemanager.webapp.dao.AppsInfo;
+import org.apache.hadoop.yarn.server.resourcemanager.webapp.dao.ClusterInfo;
+import org.apache.hadoop.yarn.server.resourcemanager.webapp.dao.ClusterUserInfo;
 import org.apache.hadoop.yarn.server.resourcemanager.webapp.dao.ClusterMetricsInfo;
 import org.apache.hadoop.yarn.server.resourcemanager.webapp.dao.NewApplication;
 import org.apache.hadoop.yarn.server.resourcemanager.webapp.dao.NodeInfo;
@@ -128,9 +131,12 @@ import org.apache.hadoop.yarn.server.webapp.dao.ContainersInfo;
 import org.apache.hadoop.yarn.server.router.webapp.dao.FederationRMQueueAclInfo;
 import org.apache.hadoop.yarn.server.router.webapp.dao.FederationBulkActivitiesInfo;
 import org.apache.hadoop.yarn.server.router.webapp.dao.FederationSchedulerTypeInfo;
+import org.apache.hadoop.yarn.server.router.webapp.dao.FederationClusterInfo;
+import org.apache.hadoop.yarn.server.router.webapp.dao.FederationClusterUserInfo;
 import org.apache.hadoop.yarn.util.LRUCacheHashMap;
 import org.apache.hadoop.yarn.util.MonotonicClock;
 import org.apache.hadoop.yarn.util.Times;
+import org.apache.hadoop.yarn.util.YarnVersionInfo;
 import org.apache.hadoop.yarn.webapp.BadRequestException;
 import org.apache.hadoop.yarn.webapp.util.WebAppUtils;
 import org.junit.Assert;
@@ -2126,5 +2132,87 @@ public class TestFederationInterceptorREST extends BaseRouterWebServicesTest {
     oldNodeLabels1.add("A1");
     LambdaTestUtils.intercept(YarnRuntimeException.class, "removeFromClusterNodeLabels Error",
         () -> interceptor.removeFromClusterNodeLabels(oldNodeLabels1, null));
+  }
+
+  @Test
+  public void testGetClusterUserInfo() {
+    String requestUserName = "test-user";
+    HttpServletRequest hsr = mock(HttpServletRequest.class);
+    when(hsr.getRemoteUser()).thenReturn(requestUserName);
+    ClusterUserInfo clusterUserInfo = interceptor.getClusterUserInfo(hsr);
+
+    Assert.assertNotNull(clusterUserInfo);
+    Assert.assertTrue(clusterUserInfo instanceof FederationClusterUserInfo);
+
+    FederationClusterUserInfo federationClusterUserInfo =
+        (FederationClusterUserInfo) clusterUserInfo;
+
+    List<ClusterUserInfo> fedClusterUserInfoList = federationClusterUserInfo.getList();
+    Assert.assertNotNull(fedClusterUserInfoList);
+    Assert.assertEquals(4, fedClusterUserInfoList.size());
+
+    List<String> subClusterIds = subClusters.stream().map(
+        subClusterId -> subClusterId.getId()).collect(Collectors.toList());
+    MockRM mockRM = interceptor.getMockRM();
+
+    for (ClusterUserInfo fedClusterUserInfo : fedClusterUserInfoList) {
+      // Check subClusterId
+      String subClusterId = fedClusterUserInfo.getSubClusterId();
+      Assert.assertNotNull(subClusterId);
+      Assert.assertTrue(subClusterIds.contains(subClusterId));
+
+      // Check requestedUser
+      String requestedUser = fedClusterUserInfo.getRequestedUser();
+      Assert.assertNotNull(requestedUser);
+      Assert.assertEquals(requestUserName, requestedUser);
+
+      // Check rmLoginUser
+      String rmLoginUser = fedClusterUserInfo.getRmLoginUser();
+      Assert.assertNotNull(rmLoginUser);
+      Assert.assertEquals(mockRM.getRMLoginUser(), rmLoginUser);
+    }
+  }
+
+  @Test
+  public void testGetClusterInfo() {
+    ClusterInfo clusterInfos = interceptor.getClusterInfo();
+    Assert.assertNotNull(clusterInfos);
+    Assert.assertTrue(clusterInfos instanceof FederationClusterInfo);
+
+    FederationClusterInfo federationClusterInfos =
+        (FederationClusterInfo) (clusterInfos);
+
+    List<ClusterInfo> fedClusterInfosList = federationClusterInfos.getList();
+    Assert.assertNotNull(fedClusterInfosList);
+    Assert.assertEquals(4, fedClusterInfosList.size());
+
+    List<String> subClusterIds = subClusters.stream().map(
+        subClusterId -> subClusterId.getId()).collect(Collectors.toList());
+
+    MockRM mockRM = interceptor.getMockRM();
+    String yarnVersion = YarnVersionInfo.getVersion();
+
+    for (ClusterInfo clusterInfo : fedClusterInfosList) {
+      String subClusterId = clusterInfo.getSubClusterId();
+      // Check subClusterId
+      Assert.assertTrue(subClusterIds.contains(subClusterId));
+
+      // Check state
+      String clusterState = mockRM.getServiceState().toString();
+      Assert.assertEquals(clusterState, clusterInfo.getState());
+
+      // Check rmStateStoreName
+      String rmStateStoreName =
+          mockRM.getRMContext().getStateStore().getClass().getName();
+      Assert.assertEquals(rmStateStoreName, clusterInfo.getRMStateStore());
+
+      // Check RM Version
+      Assert.assertEquals(yarnVersion, clusterInfo.getRMVersion());
+
+      // Check haZooKeeperConnectionState
+      String rmHAZookeeperConnectionState = mockRM.getRMContext().getHAZookeeperConnectionState();
+      Assert.assertEquals(rmHAZookeeperConnectionState,
+          clusterInfo.getHAZookeeperConnectionState());
+    }
   }
 }
