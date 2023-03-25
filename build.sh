c464e3069eb17eb240e5dd2d40eeaca107b9da03
Ayush Saxena <ayushsaxena@apache.org>
2023-03-25 18:22:32 +0530

    HADOOP-18681. Upgrade hadoop3 docker scripts to use 3.3.5.

--- build.sh
+++ build.sh
@@ -17,11 +17,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
 mkdir -p build
-if [ ! -d "$DIR/build/apache-rat-0.13" ]; then
-	curl -LSs https://dlcdn.apache.org/creadur/apache-rat-0.13/apache-rat-0.13-bin.tar.gz -o "$DIR/build/apache-rat.tar.gz"
+if [ ! -d "$DIR/build/apache-rat-0.15" ]; then
+	curl -LSs https://dlcdn.apache.org/creadur/apache-rat-0.15/apache-rat-0.15-bin.tar.gz -o "$DIR/build/apache-rat.tar.gz"
 	cd $DIR/build
 	tar zvxf apache-rat.tar.gz
 	cd -
 fi
-java -jar $DIR/build/apache-rat-0.13/apache-rat-0.13.jar $DIR -e public -e apache-rat-0.13 -e .git -e .gitignore
+java -jar $DIR/build/apache-rat-0.15/apache-rat-0.15.jar $DIR -e public -e apache-rat-0.15 -e .git -e .gitignore
 docker build -t apache/hadoop:3 .
