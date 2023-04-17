31fdb98aade5ad76d81fa68e437e9ab2d40b0674
Chris Bevard <cbevard1@gmail.com>
2023-04-17 10:40:19 -0400

    downgrade to flush() instead of no-op.

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3ABlockOutputStream.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3ABlockOutputStream.java
@@ -726,7 +726,7 @@ class S3ABlockOutputStream extends OutputStream implements
   /**
    * Shared processing of Syncable operation reporting/downgrade.
    */
-  private void handleSyncableInvocation() {
+  private void handleSyncableInvocation() throws IOException {
     final UnsupportedOperationException ex
         = new UnsupportedOperationException(E_NOT_SYNCABLE);
     if (!downgradeSyncableExceptions) {
@@ -738,6 +738,7 @@ class S3ABlockOutputStream extends OutputStream implements
         key);
     // and log at debug
     LOG.debug("Downgrading Syncable call", ex);
+    flush();
   }
 
   @Override
