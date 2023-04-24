4180f381d0bfffc64d202dbc00b7b07d2c154d05
Pranav Saxena <pranavsaxena@microsoft.com>
2023-04-24 00:27:56 -0700

    shell script name change for parallel thread

--- hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/ITestAzcopyHelper.java
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/ITestAzcopyHelper.java
@@ -123,18 +123,18 @@ public class ITestAzcopyHelper {
         String configuredFixedToken = configuration.get(FS_AZURE_SAS_FIXED_TOKEN, null);
         if (configuredFixedToken != null) {
             if (isFile) {
-                createFileCreationScript(azcopyDirPath, "createFile.sh", azcopyDirPath, configuredFixedToken, url);
+                createFileCreationScript(azcopyDirPath, "createFile" + Thread.currentThread().getName() + ".sh", azcopyDirPath, configuredFixedToken, url);
             } else {
-                createFolderCreationScript(azcopyDirPath, "createFolder.sh", azcopyDirPath, configuredFixedToken, url);
+                createFolderCreationScript(azcopyDirPath, "createFolder" + Thread.currentThread().getName() + ".sh", azcopyDirPath, configuredFixedToken, url);
             }
         } else {
             throw new Exception("The SAS token provided is null");
         }
         String path;
         if (isFile) {
-            path = azcopyDirPath + "/createFile.sh";
+            path = azcopyDirPath + "/createFile" + Thread.currentThread().getName() + ".sh";
         } else {
-            path = azcopyDirPath + "/createFolder.sh";
+            path = azcopyDirPath + "/createFolder" + Thread.currentThread().getName() + ".sh";
         }
         try {
             ProcessBuilder pb = new ProcessBuilder(path);
