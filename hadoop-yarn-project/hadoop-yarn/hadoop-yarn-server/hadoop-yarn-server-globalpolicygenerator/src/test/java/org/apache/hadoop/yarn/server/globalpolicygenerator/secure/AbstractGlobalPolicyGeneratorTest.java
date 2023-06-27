f3dcebb5ea52ac7d9f0cde36f7319786f9a1f915
slfan1989 <louj1988@@>
2023-06-27 18:34:24 +0800

    YARN-11090. Fix CheckStyle.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-globalpolicygenerator/src/test/java/org/apache/hadoop/yarn/server/globalpolicygenerator/secure/AbstractGlobalPolicyGeneratorTest.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-globalpolicygenerator/src/test/java/org/apache/hadoop/yarn/server/globalpolicygenerator/secure/AbstractGlobalPolicyGeneratorTest.java
@@ -20,7 +20,15 @@ package org.apache.hadoop.yarn.server.globalpolicygenerator.secure;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.CommonConfigurationKeysPublic;
+import org.apache.hadoop.metrics2.lib.DefaultMetricsSystem;
 import org.apache.hadoop.minikdc.MiniKdc;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.server.federation.store.impl.MemoryFederationStateStore;
+import org.apache.hadoop.yarn.server.federation.utils.FederationStateStoreFacade;
+import org.apache.hadoop.yarn.server.globalpolicygenerator.GlobalPolicyGenerator;
+import org.junit.BeforeClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,6 +36,7 @@ import java.io.File;
 import java.util.Properties;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public abstract class AbstractGlobalPolicyGeneratorTest {
@@ -56,6 +65,26 @@ public abstract class AbstractGlobalPolicyGeneratorTest {
   private static File routerKeytab;
   private static File kdcWorkDir;
   private static Configuration conf;
+  private GlobalPolicyGenerator gpg;
+
+  @BeforeClass
+  public static void beforeSecureRouterTestClass() throws Exception {
+    // Sets up the KDC and Principals.
+    setupKDCAndPrincipals();
+
+    // Init YarnConfiguration
+    conf = new YarnConfiguration();
+
+    // Enable Kerberos authentication configuration
+    conf.setBoolean(CommonConfigurationKeysPublic.HADOOP_SECURITY_AUTHORIZATION, true);
+    conf.set(CommonConfigurationKeysPublic.HADOOP_SECURITY_AUTHENTICATION, KERBEROS);
+
+    // Router Kerberos KeyTab configuration
+    conf.set(YarnConfiguration.GPG_PRINCIPAL, GPG_LOCALHOST_REALM);
+    conf.set(YarnConfiguration.GPG_KEYTAB, routerKeytab.getAbsolutePath());
+
+    DefaultMetricsSystem.setMiniClusterMode(true);
+  }
 
   /**
    * Sets up the KDC and Principals.
@@ -96,5 +125,53 @@ public abstract class AbstractGlobalPolicyGeneratorTest {
         principal + "/localhost",
         principal + "/127.0.0.1");
     return keytab;
+  }
+
+  /**
+   * Start the router in safe mode.
+   *
+   * @throws Exception an error occurred.
+   */
+  public synchronized void startSecureGPG() {
+    assertNull("GPG is already running", gpg);
+    MemoryFederationStateStore stateStore = new MemoryFederationStateStore();
+    stateStore.init(conf);
+    FederationStateStoreFacade.getInstance().reinitialize(stateStore, conf);
+    UserGroupInformation.setConfiguration(conf);
+    gpg = new GlobalPolicyGenerator();
+    gpg.init(conf);
+    gpg.start();
+  }
+
+  /**
+   * Shut down the KDC service.
+   *
+   * @throws Exception an error occurred.
+   */
+  public static void teardownKDC() throws Exception {
+    if (kdc != null) {
+      kdc.stop();
+      kdc = null;
+    }
+  }
+
+  public GlobalPolicyGenerator getGpg() {
+    return gpg;
+  }
+
+  public static MiniKdc getKdc() {
+    return kdc;
+  }
+
+  /**
+   * Stop the router in safe mode.
+   *
+   * @throws Exception an error occurred.
+   */
+  protected synchronized void stopSecureRouter() throws Exception {
+    if (gpg != null) {
+      gpg.stop();
+      gpg = null;
+    }
   }
 }
