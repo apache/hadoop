1cb2e10ec182e105ad91506392ec49e87d447a88
zhanghaobo <hfutzhanghb@163.com>
2023-07-25 18:31:27 +0800

    HDFS-17123. Sort datanodeStorages when generating StorageBlockReport[] in method BPServiceActor#blockReport for future convenience

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/datanode/extdataset/ExternalDatasetImpl.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/datanode/extdataset/ExternalDatasetImpl.java
@@ -195,7 +195,7 @@ public class ExternalDatasetImpl implements FsDatasetSpi<ExternalVolumeImpl> {
   @Override
   public Map<DatanodeStorage, BlockListAsLongs> getBlockReports(String bpid) {
     final Map<DatanodeStorage, BlockListAsLongs> result =
-	new HashMap<DatanodeStorage, BlockListAsLongs>();
+	new TreeMap<DatanodeStorage, BlockListAsLongs>();
 
     result.put(storage, BlockListAsLongs.EMPTY);
     return result;
