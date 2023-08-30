ad7ca4e35806d29ea40e1ccd3f0fab69ef6a1b46
Steve Loughran <stevel@cloudera.com>
2023-08-30 11:25:57 +0100

    HADOOP-18679. Bulk delete: define semantics better
    Including option "fs.option.bulkdelete.background" to indicate
    this is a background cleanup and so can be lower priority (somehow)
    
    Change-Id: Idb55ebf2a6664fb23e3dbacd3e0ade45cb4936e1

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/BulkDelete.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/BulkDelete.java
@@ -27,6 +27,8 @@ import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.fs.statistics.IOStatistics;
 import org.apache.hadoop.fs.statistics.IOStatisticsSource;
 
+import static org.apache.hadoop.fs.statistics.IOStatisticsLogging.ioStatisticsToPrettyString;
+
 /**
  * Interface for bulk file delete operations.
  * <p>
@@ -37,6 +39,13 @@ import org.apache.hadoop.fs.statistics.IOStatisticsSource;
  * A remote iterator provides the list of paths to delete; all must be under
  * the base path.
  * <p>
+ * There is no guarantee order of execution.
+ * Implementations may shuffle paths before posting requests.
+ * <p>
+ * Callers MUST have no expectation that parent directories will exist after the
+ * operation completes; if an object store needs to explicitly look for and create
+ * directory markers, that step will be omitted.
+ * <p>
  * The iterator may be a {@code Closeable} and if so, it will be closed on
  * completion, irrespective of the outcome.
  * <p>
@@ -46,11 +55,30 @@ import org.apache.hadoop.fs.statistics.IOStatisticsSource;
  * If the iterator's methods raise any exception, the delete will fail fast;
  * no more files will be submitted for deletion.
  * <p>
+ * The {@link #OPT_BACKGROUND} boolean option is a hint to prioritise other work
+ * over the delete; use it for background cleanup, compaction etc.
+ * <p>
+ * no guarantee of page size being greater than 1, or even constant through
+ * the entire operation. The {@link #OPT_PAGE_SIZE} option can be used as
+ * a hint.
+ * Most object stores may have a maximum page size; if a larger size is requested,
+ * you will always get something at or below that limit (i.e. it is not an
+ * error to ask for more than the limit)
+ * <p>
+ * Be aware that on some stores (AWS S3) each object listed in a bulk delete counts
+ * against the write IOPS limit; large page sizes are counterproductive here.
+ * @see <a href="https://issues.apache.org/jira/browse/HADOOP-16823">HADOOP-16823.Large DeleteObject requests are their own Thundering Herd</a>
+ * <p>
+ * Progress callback: the callback may come from any thread, further work may or may
+ * not be blocked during the callback's processing in application code.
+ * <p>
  * If a bulk delete call fails, then the next progress report will include
  * information about the failure; the callback can decide whether to continue
  * or not. The default callback is {@link #FAIL_FAST}, which will trigger
  * a fast failure.
- *
+ * <p>
+ * After a progress callback requests abort, active operations MAY continue.
+ * The {@link ProgressReport#aborting} flag indicates this.
  */
 @InterfaceAudience.Public
 @InterfaceStability.Unstable
@@ -98,6 +126,14 @@ public interface BulkDelete {
   String OPT_PAGE_SIZE = "fs.option.bulkdelete.page.size";
 
   /**
+   * Is this a background operation?
+   * If so, a lower write rate may be used so that it doesn't interfere
+   * with higher priority workloads -such as through rate limiting
+   * and/or the use of smaller page sizes.
+   */
+  String OPT_BACKGROUND = "fs.option.bulkdelete.background";
+
+  /**
    * Callback for progress; allows for a delete
    * to be aborted (best effort).
    * There are no guarantees as to which thread this will be called from.
@@ -113,7 +149,7 @@ public interface BulkDelete {
   }
 
   /**
-   * Progress update data.
+   * Delete progress report.
    */
   class ProgressReport {
 
@@ -139,14 +175,24 @@ public interface BulkDelete {
      */
     private final IOException exception;
 
+    /**
+     * Has an abort been requested from a previous progress report?
+     * If a progress report is delivered with this flag, it indicates
+     * that the abort has been requested, and that this report is
+     * from a page which was submitted before the abort request.
+     */
+    private final boolean aborting;
+
     public ProgressReport(final int deleteCount,
         final List<Path> successes,
         final List<Path> failures,
-        final IOException exception) {
+        final IOException exception,
+        final boolean aborting) {
       this.deleteCount = deleteCount;
       this.successes = successes;
       this.failures = failures;
       this.exception = exception;
+      this.aborting = aborting;
     }
 
     public int getDeleteCount() {
@@ -160,6 +206,14 @@ public interface BulkDelete {
     public List<Path> getFailures() {
       return failures;
     }
+
+    public IOException getException() {
+      return exception;
+    }
+
+    public boolean isAborting() {
+      return aborting;
+    }
   }
 
   /**
@@ -169,8 +223,14 @@ public interface BulkDelete {
 
     /**
      * Did the operation succeed?
+     * That is: delete all files without any failures?
      */
     private final boolean successful;
+
+    /**
+     * Wast the operation aborted?
+     */
+    private final boolean aborted;
 
     /**
      * An exception covering at least one of the failures
@@ -181,20 +241,18 @@ public interface BulkDelete {
     /**
      * Number of files deleted.
      */
-    private final int deleteCount;
+    private final int deleted;
+
+    /**
+     * Number of files which failed to delete.
+     */
+    private final int failures;
 
     /**
      * Number of delete pages submitted to the store.
      */
     private final int pageCount;
 
-    /**
-     * List of files which failed to delete.
-     * This may be empty, but will never be null.
-     * If the operation failed fast, it will not
-     * include those files which were not submitted
-     */
-    private final List<Path> failures;
 
     /**
      * IO Statistics.
@@ -204,15 +262,17 @@ public interface BulkDelete {
     private final IOStatistics iostats;
 
     public Outcome(final boolean successful,
+        final boolean aborted,
         final IOException exception,
-        final int deleteCount,
+        final int deleted,
+        final int failures,
         final int pageCount,
-        final List<Path> failures,
         final IOStatistics iostats) {
 
       this.successful = successful;
+      this.aborted = aborted;
       this.exception = exception;
-      this.deleteCount = deleteCount;
+      this.deleted = deleted;
       this.pageCount = pageCount;
       this.failures = failures;
       this.iostats = iostats;
@@ -226,11 +286,11 @@ public interface BulkDelete {
       return exception;
     }
 
-    public int getDeleteCount() {
-      return deleteCount;
+    public int getDeleted() {
+      return deleted;
     }
 
-    public List<Path> getFailures() {
+    public int getFailures() {
       return failures;
     }
 
@@ -243,9 +303,9 @@ public interface BulkDelete {
     public String toString() {
       return "Outcome{" +
           "successful=" + successful +
-          ", deleteCount=" + deleteCount +
+          ", deleteCount=" + deleted +
           ", failures=" + failures +
-          ", iostats=" + iostats +
+          ", iostats=" + ioStatisticsToPrettyString(iostats) +
           '}';
     }
   }
