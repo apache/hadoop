a45e3f0b88e0c8423ee3d2e7281e8410d5016ae5
yijut2 <52221089+yijut2@users.noreply.github.com>
2023-12-07 00:34:44 -0600

    Update TestDFSClientCache.java

--- hadoop-hdfs-project/hadoop-hdfs-nfs/src/test/java/org/apache/hadoop/hdfs/nfs/nfs3/TestDFSClientCache.java
+++ hadoop-hdfs-project/hadoop-hdfs-nfs/src/test/java/org/apache/hadoop/hdfs/nfs/nfs3/TestDFSClientCache.java
@@ -31,7 +31,7 @@ import org.apache.hadoop.hdfs.DFSClient;
 import org.apache.hadoop.hdfs.nfs.conf.NfsConfiguration;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.junit.Test;
-import org.junit.AfterClass;
+import org.junit.After;
 
 public class TestDFSClientCache {
   @After
