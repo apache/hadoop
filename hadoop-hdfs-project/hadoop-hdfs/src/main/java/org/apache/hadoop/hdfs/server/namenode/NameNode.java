7afcc6df75e3a3ca17eba450d77442829e4bbe7a
zzccctv <zzccctv@163.com>
2023-12-29 21:25:05 +0800

    Delete invalid code logic in namenode format (#6323)
    Signed-off-by: Takanobu Asanuma <tasanuma@apache.org>
    (cherry picked from commit 9f76fba6a44bdf281bc8a8874c03301eca73aafb)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
@@ -1253,9 +1253,6 @@ public class NameNode extends ReconfigurableBase implements
     
     Collection<URI> nameDirsToFormat = FSNamesystem.getNamespaceDirs(conf);
     List<URI> sharedDirs = FSNamesystem.getSharedEditsDirs(conf);
-    List<URI> dirsToPrompt = new ArrayList<URI>();
-    dirsToPrompt.addAll(nameDirsToFormat);
-    dirsToPrompt.addAll(sharedDirs);
     List<URI> editDirsToFormat = 
                  FSNamesystem.getNamespaceEditsDirs(conf);
 
@@ -1298,9 +1295,7 @@ public class NameNode extends ReconfigurableBase implements
       LOG.warn("Encountered exception during format", ioe);
       throw ioe;
     } finally {
-      if (fsImage != null) {
-        fsImage.close();
-      }
+      fsImage.close();
       if (fsn != null) {
         fsn.close();
       }
