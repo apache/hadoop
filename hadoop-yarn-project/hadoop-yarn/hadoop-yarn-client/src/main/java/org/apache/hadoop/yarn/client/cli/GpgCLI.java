462f6aab177809b19634ea4609e8d021ada22dd1
slfan1989 <slfan1989@apache.org>
2023-12-31 22:35:03 +0800

    YARN-11638. [GPG] GPG Support CLI.

---
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-client/src/main/java/org/apache/hadoop/yarn/client/cli/GpgCLI.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.yarn.client.cli;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class GpgCLI extends Configured implements Tool {
+
+  public GpgCLI() {
+    super();
+  }
+
+  public GpgCLI(Configuration conf) {
+    super(conf);
+  }
+
+  private static void printHelp() {
+    StringBuilder summary = new StringBuilder();
+    summary.append("router-admin is the command to execute " +
+        "YARN Federation administrative commands.\n");
+    StringBuilder helpBuilder = new StringBuilder();
+    System.out.println(summary);
+    helpBuilder.append("   -help [cmd]: Displays help for the given command or all commands" +
+        " if none is specified.");
+    System.out.println(helpBuilder);
+    System.out.println();
+    ToolRunner.printGenericCommandUsage(System.out);
+  }
+
+  @Override
+  public int run(String[] args) throws Exception {
+    return 0;
+  }
+}
