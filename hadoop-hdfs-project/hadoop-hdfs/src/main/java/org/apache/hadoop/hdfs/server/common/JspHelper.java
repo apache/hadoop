1eab4744774cb8325c1622734d27b247de27fbc6
顾鹏 <gupeng@gupengdeMacBook-Pro.local>
2024-01-10 11:26:48 +0800

    fix blanks eol

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/common/JspHelper.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/common/JspHelper.java
@@ -115,7 +115,7 @@ public class JspHelper {
     String remoteUser = null;
    
     if (UserGroupInformation.isSecurityEnabled()) {
-      // If using RemoteUser GetJournalEditServlet.isValidRequestor, it returns false due to 
+      // If using RemoteUser GetJournalEditServlet.isValidRequestor, it returns false due to
       // inability to verify FQDN for more info refer HDFS-17276.
       if (request.getUserPrincipal() != null) {
         remoteUser = request.getUserPrincipal().getName();
