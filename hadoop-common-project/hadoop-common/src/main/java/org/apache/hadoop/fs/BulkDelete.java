0787a7c09fa64697bd644b569cd0c892dcd6072d
Steve Loughran <stevel@cloudera.com>
2024-01-24 16:29:37 +0000

    HADOOP-18679. Add API for bulk/paged object deletion
    A more minimal design that is easier to use and implement.
    
    Caller creates a BulkOperation; they get the page size of
    it and then submit batches to delete of less than that size.
    
    The outcome of each call contains a list of failures.
    
    S3A implementation to show how straightforward it is.
    
    Even with the single entry page size, it is still more
    efficient to use this as it doesn't try to recreate a
    parent dir or perform any probes to see if it is a directory:
    it maps straight to a DELETE call.
    
    Change-Id: Ibe8737e7933fe03d39070e7138a710b50c3d60c2

---
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/BulkDelete.java
@@ -0,0 +1,143 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.fs;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.hadoop.classification.InterfaceAudience;
+import org.apache.hadoop.classification.InterfaceStability;
+import org.apache.hadoop.fs.statistics.IOStatisticsSource;
+
+/**
+ * API for bulk deletion of objects/files,
+ * <i>but not directories</i>.
+ * After use, call {@code close()} to release any resources and
+ * to guarantee store IOStatistics are updated.
+ * <p>
+ * Callers MUST have no expectation that parent directories will exist after the
+ * operation completes; if an object store needs to explicitly look for and create
+ * directory markers, that step will be omitted.
+ * <p>
+ * <p>
+ * Be aware that on some stores (AWS S3) each object listed in a bulk delete counts
+ * against the write IOPS limit; large page sizes are counterproductive here.
+ * @see <a href="https://issues.apache.org/jira/browse/HADOOP-16823">HADOOP-16823.
+ *  Large DeleteObject requests are their own Thundering Herd</a>
+ * <p>
+ */
+@InterfaceAudience.Public
+@InterfaceStability.Unstable
+public interface BulkDelete extends IOStatisticsSource, Closeable {
+
+  /**
+   * The maximum number of objects/files to delete in a single request.
+   * @return a number greater than or equal to zero.
+   */
+  int pageSize();
+
+  /**
+   * Base path of a bulk delete operation.
+   * All paths submitted in {@link #bulkDelete(List)} must be under this path.
+   */
+  Path basePath();
+
+  /**
+   * Delete a list of files/objects.
+   * <ul>
+   *   <li>Files must be under the path provided in {@link #basePath()}.</li>
+   *   <li>The size of the list must be equal to or less than the page size
+   *       declared in {@link #pageSize()}.</li>
+   *   <li>Directories are not supported; the outcome of attempting to delete
+   *       directories is undefined (ignored; undetected, listed as failures...).</li>
+   *   <li>The operation is not atomic.</li>
+   *   <li>The operation is treated as idempotent: network failures may
+   *        trigger resubmission of the request -any new objects created under a
+   *        path in the list may then be deleted.</li>
+   *    <li>There is no guarantee that any parent directories exist after this call.
+   *    </li>
+   * </ul>
+   * @param paths list of paths which must be absolute and under the base path.
+   * provided in {@link #basePath()}.
+   * @throws IOException IO problems including networking, authentication and more.
+   * @throws IllegalArgumentException if a path argument is invalid.
+   */
+  BulkDeleteOutcome bulkDelete(List<Path> paths)
+      throws IOException, IllegalArgumentException;
+
+  /**
+   * The outcome: a list of paths which failed to delete.
+   * An empty list means all files were successfully deleted.
+   * There are no guarantees about the ordering of the list.
+   * Reasons for failures are not provided.
+   * File Not Found is not a failure.
+   */
+  class BulkDeleteOutcome {
+
+    /**
+     * List of paths which failed to delete.
+     */
+    private final List<BulkDeleteOutcomeElement> failures;
+
+    /**
+     * Constructor.
+     * @param failures list of failures. This must be non-null.
+     */
+    public BulkDeleteOutcome(final List<BulkDeleteOutcomeElement> failures) {
+      this.failures = failures;
+    }
+
+    /**
+     * Get the list of failures.
+     * @return a possibly empty list of failures.
+     */
+    public List<BulkDeleteOutcomeElement> getFailures() {
+      return failures;
+    }
+  }
+
+  class BulkDeleteOutcomeElement {
+    private final Path path;
+    private final String error;
+    private final Exception exception;
+
+    public BulkDeleteOutcomeElement(
+        final Path path,
+        final String error,
+        final Exception exception) {
+      this.path = path;
+      this.error = error;
+      this.exception = exception;
+    }
+
+    public Path getPath() {
+      return path;
+    }
+
+    public String getError() {
+      return error;
+    }
+
+    public Exception getException() {
+      return exception;
+    }
+  }
+
+}
