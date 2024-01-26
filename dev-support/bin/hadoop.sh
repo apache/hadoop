9b1ccced227a1b5c4be6d0f5ea367872c52703a9
Gautham Banasandra <gautham.bangalore@gmail.com>
2024-01-26 20:11:50 +0530

    Skip running unit tests on Windows

--- dev-support/bin/hadoop.sh
+++ dev-support/bin/hadoop.sh
@@ -387,6 +387,11 @@ function personality_modules
       fi
     ;;
     unit)
+      if [[ "$IS_WINDOWS" && "$IS_WINDOWS" == 1 ]]; then
+        echo "Won't run unit tests for Windows in pre-commit CI"
+        return
+      fi
+
       extra="-Dsurefire.rerunFailingTestsCount=2"
       if [[ "${BUILDMODE}" = full ]]; then
         ordering=mvnsrc
