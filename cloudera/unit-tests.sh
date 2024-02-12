89cf7f9854e4bbb11ef6a70bebf49ada0dc25428
Tamas Domok <tdomok@cloudera.com>
2024-02-12 09:43:05 +0100

    COMPX-16060. Run sonar with JDK17.
    Change-Id: I7328231e50a9b77ebf77babc82fd52caba1bca41
    (cherry picked from commit ef88b7c71b021487cdeb084481f12ec7ce8b47f3)
    (cherry picked from commit 83111f3e8ca9a0ae4ade3d6eaf2c57cdfc8530de)

--- cloudera/unit-tests.sh
+++ cloudera/unit-tests.sh
@@ -18,6 +18,9 @@ SONAR_LOGIN="${SONAR_LOGIN:-}"
 SONAR_PROJECT_KEY="${SONAR_PROJECT_KEY:-}"
 SONAR_PROJECT_NAME="${SONAR_PROJECT_NAME:-}"
 
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
 mvn -s "$MAVEN_SETTINGS" -B -e -Pclover -f "${SCRIPT_DIR}/../pom.xml" clean install -DskipTests -DskipShade \
     --projects '!hadoop-client-modules/hadoop-client-check-invariants,!hadoop-client-modules/hadoop-client-check-test-invariants'
 
@@ -27,6 +30,8 @@ mvn -s "$MAVEN_SETTINGS" -B -e -Pclover -f "$POM_FILE" clover:aggregate clover:c
 
 if [ -z ${SKIP_SONAR+x} ]; then
     if [ -n "$SONAR_LOGIN" ]; then
+        export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
+        export PATH=$JAVA_HOME/bin:$PATH
         mvn -s "$MAVEN_SETTINGS" -B -e -Pclover -f "$POM_FILE" sonar:sonar -Dsonar.clover.reportPath=./target/clover/clover.xml \
             -Dsonar.host.url="$SONAR_URL" -Dsonar.login="$SONAR_LOGIN" -Dsonar.projectKey="$SONAR_PROJECT_KEY" -Dsonar.projectName="$SONAR_PROJECT_NAME"
     fi
