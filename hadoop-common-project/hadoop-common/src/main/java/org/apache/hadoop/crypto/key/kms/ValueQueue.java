e34b9982c1f12024e4af32dbc2e321403a795abd
余良 <yul165@chinaunicom.cn>
2024-03-13 17:47:04 +0800

    HADOOP-19052.Hadoop use Shell command to get the count of the hard link which takes a lot of time

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/crypto/key/kms/ValueQueue.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/crypto/key/kms/ValueQueue.java
@@ -328,8 +328,9 @@ public class ValueQueue <E> {
           keyQueues.getAllPresent(Arrays.asList(keyName));
       if (map.get(keyName) == null) {
         return 0;
+      }else {
+        return map.get(keyName).size();
       }
-      return map.get(keyName).size();
     } finally {
       readUnlock(keyName);
     }
