9d426f04dfb12d4fbac151c8bae3a7b99e4b8360
Haiyang.Hu <huhaiyang926@126.com>
2024-03-29 16:54:29 +0800

    HDFS-17448. Enhance the stability of the unit test TestDiskBalancerCommand

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/diskbalancer/command/TestDiskBalancerCommand.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/diskbalancer/command/TestDiskBalancerCommand.java
@@ -834,8 +834,10 @@ public class TestDiskBalancerCommand {
           .getIpcPort(), dataNode2.getIpcPort());
       final String cmdLine = String.format("hdfs diskbalancer %s", queryArg);
       List<String> outputs = runCommand(cmdLine);
-      assertThat(outputs.get(1), containsString("localhost:" + dataNode1.getIpcPort()));
-      assertThat(outputs.get(6), containsString("localhost:" + dataNode2.getIpcPort()));
+      assertTrue(outputs.get(1).contains("localhost:" + dataNode1.getIpcPort())
+          || outputs.get(6).contains("localhost:" + dataNode1.getIpcPort()));
+      assertTrue(outputs.get(1).contains("localhost:" + dataNode2.getIpcPort())
+          || outputs.get(6).contains("localhost:" + dataNode2.getIpcPort()));
     } finally {
       miniDFSCluster.shutdown();
     }
