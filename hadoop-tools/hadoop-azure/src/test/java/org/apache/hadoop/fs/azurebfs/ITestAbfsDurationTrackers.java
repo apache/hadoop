caf6c566973049b8eb9e29278a15d2a2e38af6f1
Pranav Saxena <pranavsaxena@microsoft.com>
2024-04-18 06:43:03 -0700

    Revert "made ITestAbfsDurationTrackers run sequential; assertion revert revert"
    This reverts commit f4fef333ab2a287e6c20910f0eaa9bac4c7f8dba.

--- hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/ITestAbfsDurationTrackers.java
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/ITestAbfsDurationTrackers.java
@@ -102,12 +102,9 @@ public class ITestAbfsDurationTrackers extends AbstractAbfsIntegrationTest {
    */
   private void assertDurationTracker(IOStatistics ioStatistics) {
     for (AbfsStatistic abfsStatistic : HTTP_DURATION_TRACKER_LIST) {
-      AbstractDoubleAssert doubleAssert = Assertions.assertThat(
-              lookupMeanStatistic(ioStatistics,
-                  abfsStatistic.getStatName()
-                      + StoreStatisticNames.SUFFIX_MEAN).mean())
-          .describedAs(
-              "The DurationTracker Named " + abfsStatistic.getStatName()
+      AbstractDoubleAssert doubleAssert = Assertions.assertThat(lookupMeanStatistic(ioStatistics,
+          abfsStatistic.getStatName() + StoreStatisticNames.SUFFIX_MEAN).mean())
+          .describedAs("The DurationTracker Named " + abfsStatistic.getStatName()
                   + " Doesn't match the expected value.");
       if (abfsStatistic == HTTP_HEAD_REQUEST
           && getConfiguration().getInputStreamLazyOptimizationEnabled()) {
@@ -116,5 +113,4 @@ public class ITestAbfsDurationTrackers extends AbstractAbfsIntegrationTest {
         doubleAssert.isGreaterThan(0.0);
       }
     }
-  }
 }
