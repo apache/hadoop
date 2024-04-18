e7cd9a31a1c6b8de6f389e6c07c98a66567a9723
Pranav Saxena <pranavsaxena@microsoft.com>
2024-04-18 06:47:13 -0700

    greater than equal to comparison

--- hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/ITestAbfsDurationTrackers.java
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/ITestAbfsDurationTrackers.java
@@ -105,8 +105,21 @@ public class ITestAbfsDurationTrackers extends AbstractAbfsIntegrationTest {
       AbstractDoubleAssert doubleAssert = Assertions.assertThat(lookupMeanStatistic(ioStatistics,
           abfsStatistic.getStatName() + StoreStatisticNames.SUFFIX_MEAN).mean())
           .describedAs("The DurationTracker Named " + abfsStatistic.getStatName()
-                  + " Doesn't match the expected value.")
-          .isGreaterThan(0.0);
+                  + " Doesn't match the expected value.");
+      if (abfsStatistic == HTTP_HEAD_REQUEST && getConfiguration().getInputStreamLazyOptimizationEnabled()) {
+        /*
+         * In an environment where this is the only test running, there would be no
+         * head call for the fileSystem as the inputStream would be lazily opened.
+         *
+         * But, in an environment where there are multiple tests running in parallel
+         * using same fileSystem instance, there could be few head calls from those tests.
+         *
+         * Hence, asserting for greater than or equal to 0.0.
+         */
+        doubleAssert.isGreaterThanOrEqualTo(0.0);
+      } else {
+        doubleAssert.isGreaterThan(0.0);
+      }
       }
     }
 }
