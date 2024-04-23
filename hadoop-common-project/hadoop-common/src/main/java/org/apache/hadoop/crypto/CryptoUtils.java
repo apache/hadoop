07cebad3573d47d09d790c3eed3f1faaed75900d
Tsz-Wo Nicholas Sze <szetszwo@apache.org>
2024-04-23 08:55:19 -0700

    Address review comments.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/crypto/CryptoUtils.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/crypto/CryptoUtils.java
@@ -19,6 +19,8 @@ package org.apache.hadoop.crypto;
 
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.CommonConfigurationKeysPublic;
+import org.apache.hadoop.fs.store.LogExactlyOnce;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,46 +28,54 @@ import java.lang.reflect.Field;
 import java.security.Provider;
 import java.security.Security;
 
-import static org.apache.hadoop.fs.CommonConfigurationKeysPublic.HADOOP_SECURITY_CRYPTO_JCE_PROVIDER_ADD_DEFAULT;
-import static org.apache.hadoop.fs.CommonConfigurationKeysPublic.HADOOP_SECURITY_CRYPTO_JCE_PROVIDER_ADD_KEY;
-import static org.apache.hadoop.fs.CommonConfigurationKeysPublic.HADOOP_SECURITY_CRYPTO_JCE_PROVIDER_KEY;
-
+/** Utility methods for the crypto related features. */
 @InterfaceAudience.Private
 public class CryptoUtils {
   static final Logger LOG = LoggerFactory.getLogger(CryptoUtils.class);
+  private static final LogExactlyOnce LOG_FAILED_TO_LOAD_CLASS = new LogExactlyOnce(LOG);
+  private static final LogExactlyOnce LOG_FAILED_TO_GET_FIELD = new LogExactlyOnce(LOG);
+  private static final LogExactlyOnce LOG_FAILED_TO_ADD_PROVIDER = new LogExactlyOnce(LOG);
 
   private static final String BOUNCY_CASTLE_PROVIDER_CLASS
       = "org.bouncycastle.jce.provider.BouncyCastleProvider";
   private static final String PROVIDER_NAME_FIELD = "PROVIDER_NAME";
 
-  private static void addProvider(String provider) {
-    if (provider == null || provider.isEmpty()) {
-      return;
-    }
-    try {
-      // For backward compatible, try to auto-add BOUNCY_CASTLE_PROVIDER_CLASS.
-      final Class<?> clazz = Class.forName(BOUNCY_CASTLE_PROVIDER_CLASS);
-      final Field provider_name = clazz.getField("PROVIDER_NAME");
-      if (provider.equals(provider_name.get(null))) {
-        Security.addProvider((Provider) clazz.getConstructor().newInstance());
-        LOG.debug("Successfully added security provider {}", provider);
+  /**
+   * Get the security provider value specified in
+   * {@link CommonConfigurationKeysPublic#HADOOP_SECURITY_CRYPTO_JCE_PROVIDER_KEY}
+   * from the given conf.
+   *
+   * @param conf the configuration
+   * @return the configured provider, if there is any; otherwise, return an empty string.
+   */
+  public static String getJceProvider(Configuration conf) {
+    final String provider = conf.getTrimmed(
+        CommonConfigurationKeysPublic.HADOOP_SECURITY_CRYPTO_JCE_PROVIDER_KEY, "");
+    final boolean autoAdd = conf.getBoolean(
+        CommonConfigurationKeysPublic.HADOOP_SECURITY_CRYPTO_JCE_PROVIDER_AUTO_ADD_KEY,
+        CommonConfigurationKeysPublic.HADOOP_SECURITY_CRYPTO_JCE_PROVIDER_AUTO_ADD_DEFAULT);
+
+    // For backward compatible, auto-add BOUNCY_CASTLE_PROVIDER_CLASS.
+    if (autoAdd && !provider.isEmpty()) {
+      try {
+        // Use reflection in order to avoid statically loading the class.
+        final Class<?> clazz = Class.forName(BOUNCY_CASTLE_PROVIDER_CLASS);
+        final Field provider_name = clazz.getField("PROVIDER_NAME");
+        if (provider.equals(provider_name.get(null))) {
+          Security.addProvider((Provider) clazz.getConstructor().newInstance());
+          LOG.debug("Successfully added security provider {}", provider);
+        }
+      } catch (ClassNotFoundException e) {
+        LOG_FAILED_TO_LOAD_CLASS.warn("Failed to load " + BOUNCY_CASTLE_PROVIDER_CLASS, e);
+      } catch (NoSuchFieldException e) {
+        LOG_FAILED_TO_GET_FIELD.warn("Failed to get field " + PROVIDER_NAME_FIELD
+            + " from class " + BOUNCY_CASTLE_PROVIDER_CLASS, e);
+      } catch (Exception e) {
+        LOG_FAILED_TO_ADD_PROVIDER.warn("Failed to add security provider for {}", provider, e);
       }
-    } catch (ClassNotFoundException e) {
-      LOG.warn("Failed to load " + BOUNCY_CASTLE_PROVIDER_CLASS, e);
-    } catch (NoSuchFieldException e) {
-      LOG.warn("Failed to get field " + PROVIDER_NAME_FIELD
-          + " from class " + BOUNCY_CASTLE_PROVIDER_CLASS, e);
-    } catch (Exception e) {
-      LOG.warn("Failed to add security provider for {}", provider, e);
     }
+    return provider;
   }
 
-  public static String getJceProvider(Configuration conf) {
-    final String jceProvider = conf.get(HADOOP_SECURITY_CRYPTO_JCE_PROVIDER_KEY);
-    if (conf.getBoolean(HADOOP_SECURITY_CRYPTO_JCE_PROVIDER_ADD_KEY,
-        HADOOP_SECURITY_CRYPTO_JCE_PROVIDER_ADD_DEFAULT)) {
-      CryptoUtils.addProvider(jceProvider);
-    }
-    return jceProvider;
-  }
+  private CryptoUtils() {}
 }
