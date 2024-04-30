83e6105a7e05d9ba12f516bd05f3b8c508872750
Peter Szucs <116345192+p-szucs@users.noreply.github.com>
2024-04-30 11:25:16 +0200

    COMPX-16794. YARN-11685. Create a config to enable/disable cgroup v2 functionality (#6770)
    (cherry picked from commit 910cb6b887c73641d8eadd79249dfaa852edd809)
    Change-Id: I7edb942dd6412809d3af32446bfec2d4ba1d2870
    (cherry picked from commit 2e066b248393a8030319b64846a6dc57fe48727c)
    (cherry picked from commit 5551a34cbefd11b8308cc8744853db141c6e988b)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/conf/YarnConfiguration.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/conf/YarnConfiguration.java
@@ -2815,6 +2815,14 @@ public class YarnConfiguration extends Configuration {
       20;
 
   /**
+   * Boolean indicating whether cgroup v2 is enabled.
+   */
+  public static final String NM_LINUX_CONTAINER_CGROUPS_V2_ENABLED =
+      NM_PREFIX + "linux-container-executor.cgroups.v2.enabled";
+
+  public static final boolean DEFAULT_NM_LINUX_CONTAINER_CGROUPS_V2_ENABLED = false;
+
+  /**
    * Indicates if memory and CPU limits will be set for the Windows Job
    * Object for the containers launched by the default container executor.
    */
