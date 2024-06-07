225106cb5a7ca96867d8e2397292833e56a51357
Jian Zhang <1361320460@qq.com>
2024-06-07 16:45:58 +0800

    warp IOException & add javadoc

--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/router/async/AsyncForEachRun.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/router/async/AsyncForEachRun.java
@@ -22,6 +22,49 @@ import java.util.Iterator;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 
+/**
+ * The AsyncForEachRun class is part of the asynchronous operation utilities
+ * within the Hadoop Distributed File System (HDFS) Federation router.
+ * It provides the functionality to perform asynchronous operations on each
+ * element of an Iterator, applying a given async function and then applying
+ * a final transformation function to the results.
+ *
+ * <p>This class is designed to work with other asynchronous interfaces and
+ * utility classes to enable complex asynchronous workflows. It allows for
+ * non-blocking execution of tasks, which can improve the performance and
+ * responsiveness of HDFS operations.</p>
+ *
+ * <p>The class implements the AsyncRun interface, which means it can be used
+ * in asynchronous task chains. It maintains an Iterator of elements to
+ * process, an asyncFunction to apply to each element, and a final
+ * transformation function (thenApply) to produce the final result.</p>
+ *
+ * <p>The run method initiates the asynchronous operation, and the doOnce
+ * method recursively applies the asyncFunction to each element and handles
+ * the results. If the satisfy flag is set, the operation is completed
+ * with the current result.</p>
+ *
+ * <p>AsyncForEachRun is used to implement the following semantics:</p>
+ * <pre>
+ * {@code
+ * for (I element : elements) {
+ *     T res = asyncFunction(element);
+ *     R result = thenApply(element, res);
+ *     if (satisfyCondition(res, result)) {
+ *         break;
+ *     }
+ * }
+ * return result;
+ * }
+ * </pre>
+ *
+ * @param <I> the type of the elements being iterated over
+ * @param <T> the type of the intermediate result from the asyncFunction
+ * @param <R> the type of the final result after applying the thenApply function
+ * @see AsyncRun
+ * @see AsyncApplyFunction
+ * @see BiFunction
+ */
 public class AsyncForEachRun<I, T, R> implements AsyncRun<R> {
 
   private boolean satisfy = false;
@@ -41,6 +84,23 @@ public class AsyncForEachRun<I, T, R> implements AsyncRun<R> {
     setCurCompletableFuture(result);
   }
 
+  /**
+   * Performs a single iteration of the asynchronous for-each operation.
+   *
+   * <p>This method is called to process each element of the iterator provided to
+   * the {@link AsyncForEachRun} constructor. It applies the asynchronous function to
+   * the current element, then applies the 'then' function to the result. If the
+   * 'satisfy' condition is met, the iteration is halted, and the current result is
+   * used to complete the future. This method is recursive, so it will continue to
+   * call itself for the next elements until the iterator is exhausted or the satisfy
+   * condition is true.</p>
+   *
+   * @param ret the initial or current result to be passed into the 'then' function
+   * @throws IOException if an I/O error occurs while applying the asynchronous function
+   * @see #forEach(Iterator)
+   * @see #asyncDo(AsyncApplyFunction)
+   * @see #then(BiFunction)
+   */
   private void doOnce(R ret) throws IOException {
     if (!iterator.hasNext()) {
       result.complete(ret);
@@ -53,7 +113,7 @@ public class AsyncForEachRun<I, T, R> implements AsyncRun<R> {
       try {
         r = thenApply.apply(AsyncForEachRun.this, t);
       } catch (IOException e) {
-        result.completeExceptionally(e);
+        result.completeExceptionally(new CompletionException(e));
         return null;
       }
       if (satisfy) {
@@ -70,10 +130,38 @@ public class AsyncForEachRun<I, T, R> implements AsyncRun<R> {
         result.completeExceptionally(e.getCause()));
   }
 
+  /**
+   * Retrieves the current element being processed in the asynchronous for-each loop.
+   *
+   * <p>This method provides access to the element that is currently being
+   * operated on within the asynchronous iteration. It can be useful for
+   * inspection, logging, or other purposes that require knowledge of the
+   * current state of the iteration.</p>
+   *
+   * @return the current element of type {@code I} being processed in the iterator.
+   * @see #forEach(Iterator)
+   * @see #run()
+   */
   public I getNow() {
     return now;
   }
 
+  /**
+   * Triggers the termination of the current asynchronous iteration.
+   *
+   * <p>This method is used to break out of the asynchronous for-each loop
+   * prematurely. It sets a flag that indicates the iteration should be
+   * terminated at the earliest opportunity. This is particularly useful when
+   * the processing logic determines that further iteration is unnecessary
+   * or when a specific condition has been met.</p>
+   *
+   * <p>Once this method is called, the next time the loop is about to process
+   * a new element, it will check the flag and cease operation, allowing the
+   * application to move on to the next step or complete the task.</p>
+   *
+   * @see #getNow()
+   * @see #run()
+   */
   public void breakNow() {
     satisfy = true;
   }
--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/router/async/AsyncRun.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/router/async/AsyncRun.java
@@ -20,11 +20,51 @@ package org.apache.hadoop.hdfs.server.federation.router.async;
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * The AsyncRun interface represents an asynchronous operation that can be
+ * executed in the context of the Hadoop Distributed File System (HDFS)
+ * Federation router. Implementations of this interface are responsible for
+ * performing a task and completing a {@link CompletableFuture} with the
+ * result of the operation.
+ *
+ * <p>
+ * The {@code run} method of this interface is intended to be used in an
+ * asynchronous workflow, where the operation may involve I/O tasks or
+ * other long-running processes. By implementing this interface, classes
+ * can define custom asynchronous behavior that can be chained with other
+ * asynchronous operations using utility methods provided by the
+ * {@link AsyncUtil} class.</p>
+ *
+ * <p>
+ * For example, an implementation of AsyncRun could perform a non-blocking
+ * read or write operation to HDFS, and upon completion, it could use
+ * AsyncUtil methods to handle the result or propagate any exceptions that
+ * occurred during the operation.</p>
+ *
+ * @param <R> the type of the result produced by the asynchronous operation
+ * @see AsyncUtil
+ */
 @FunctionalInterface
 public interface AsyncRun<R> extends Async<R> {
 
+  /**
+   * Executes the asynchronous operation represented by this AsyncRun instance.
+   * This method is expected to perform the operation and, upon completion,
+   * complete the current thread's {@link CompletableFuture} with the result.
+   *
+   * @throws IOException if an I/O error occurs during the execution of the operation
+   */
   void run() throws IOException;
 
+  /**
+   * Provides an asynchronous version of the {@code run} method, which returns a
+   * {@link CompletableFuture} representing the result of the operation.
+   * This method is typically used in an asynchronous workflow to initiate the
+   * operation without waiting for its completion.
+   *
+   * @return a CompletableFuture that completes with the result of the operation
+   * @throws IOException if an I/O error occurs during the initiation of the operation
+   */
   default CompletableFuture<R> async() throws IOException {
     run();
     CompletableFuture<R> completableFuture = getCurCompletableFuture();
