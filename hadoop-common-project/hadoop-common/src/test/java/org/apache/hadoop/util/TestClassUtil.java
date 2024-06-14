ece23d1e189378e5b91e38d1d0ece8d6f1aab43b
Viraj Jasani <vjasani@apache.org>
2024-06-13 23:37:31 -0700

    addressing comments - find jar path for assertj

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/util/TestClassUtil.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/util/TestClassUtil.java
@@ -21,38 +21,44 @@ package org.apache.hadoop.util;
 import java.io.File;
 
 import org.apache.hadoop.fs.viewfs.ViewFileSystem;
-import org.apache.log4j.Logger;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class TestClassUtil {
+
   @Test(timeout=10000)
   public void testFindContainingJar() {
-    String containingJar = ClassUtil.findContainingJar(Logger.class);
-    assertThat(containingJar)
-        .describedAs("Containing jar for %s", Logger.class)
+    String containingJar = ClassUtil.findContainingJar(Assertions.class);
+    Assertions
+        .assertThat(containingJar)
+        .describedAs("Containing jar for %s", Assertions.class)
         .isNotNull();
     File jarFile = new File(containingJar);
-    assertThat(jarFile)
+    Assertions
+        .assertThat(jarFile)
         .describedAs("Containing jar %s", jarFile)
         .exists();
-    assertThat(jarFile.getName())
+    Assertions
+        .assertThat(jarFile.getName())
         .describedAs("Containing jar name %s", jarFile.getName())
-        .matches("reload4j.*[.]jar");
+        .matches("assertj-core.*[.]jar");
   }
 
   @Test(timeout = 10000)
   public void testFindContainingClass() {
     String classFileLocation = ClassUtil.findClassLocation(ViewFileSystem.class);
-    assertThat(classFileLocation)
+    Assertions
+        .assertThat(classFileLocation)
         .describedAs("Class path for %s", ViewFileSystem.class)
         .isNotNull();
     File classFile = new File(classFileLocation);
-    assertThat(classFile)
+    Assertions
+        .assertThat(classFile)
         .describedAs("Containing class file %s", classFile)
         .exists();
-    assertThat(classFile.getName())
+    Assertions
+        .assertThat(classFile.getName())
         .describedAs("Containing class file name %s", classFile.getName())
         .matches("ViewFileSystem.class");
   }
