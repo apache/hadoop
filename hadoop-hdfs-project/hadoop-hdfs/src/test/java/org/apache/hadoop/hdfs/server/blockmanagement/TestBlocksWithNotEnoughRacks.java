72de19092562929747f762f17a27eecfc8e72cfd
Vico.Wu <583424568@qq.com>
2024-07-04 22:41:25 +0800

    fix compile error in testcase

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/blockmanagement/TestBlocksWithNotEnoughRacks.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/blockmanagement/TestBlocksWithNotEnoughRacks.java
@@ -600,7 +600,7 @@ public class TestBlocksWithNotEnoughRacks {
     }
   }
 
-  static BlockReconstructionWork generateReconstruction(
+  static BlockReconstructionWork generateReconstructionForBlock(
       FSNamesystem fsn, BlockInfo block, int priority) {
     fsn.writeLock();
     try {
