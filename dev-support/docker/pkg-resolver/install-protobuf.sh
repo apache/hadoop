92c6ebc42cf2476c157a9ec98fb8c8d180151091
Ayush Saxena <ayushsaxena@apache.org>
2024-08-21 01:54:40 +0530

    Test

--- dev-support/docker/pkg-resolver/install-protobuf.sh
+++ dev-support/docker/pkg-resolver/install-protobuf.sh
@@ -27,7 +27,7 @@ if [ $? -eq 1 ]; then
   exit 1
 fi
 
-default_version="3.21.12"
+default_version="3.25.3"
 version_to_install=$default_version
 if [ -n "$2" ]; then
   version_to_install="$2"
@@ -38,11 +38,11 @@ if [ "$version_to_install" != "3.21.12" ]; then
   version_to_install=$default_version
 fi
 
-if [ "$version_to_install" == "3.21.12" ]; then
+if [ "$version_to_install" == "3.25.3" ]; then
   # hadolint ignore=DL3003
   mkdir -p /opt/protobuf-src &&
     curl -L -s -S \
-      https://github.com/protocolbuffers/protobuf/archive/refs/tags/v3.21.12.tar.gz \
+      https://github.com/protocolbuffers/protobuf/releases/download/v25.3/protobuf-25.3.tar.gz \
       -o /opt/protobuf.tar.gz &&
     tar xzf /opt/protobuf.tar.gz --strip-components 1 -C /opt/protobuf-src &&
     cd /opt/protobuf-src &&
