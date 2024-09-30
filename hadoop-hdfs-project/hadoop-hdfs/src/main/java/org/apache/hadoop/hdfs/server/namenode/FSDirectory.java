1f6194173e2a701b4ca49cf38e2b060c6be846fa
Felix Nguyen <kokonguyen191@gmail.com>
2024-09-30 10:31:12 +0800

    HDFS-17632. Fix javadoc

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSDirectory.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSDirectory.java
@@ -2065,7 +2065,10 @@ public class FSDirectory implements Closeable {
     }
   }
 
-  /** Should only be used for tests to reset to any value */
+  /**
+   * Should only be used for tests to reset to any value.
+   * @param newValue new value to set to
+   */
   @VisibleForTesting
   public void resetLastInodeIdWithoutChecking(long newValue) {
     inodeId.setCurrentValue(newValue);
