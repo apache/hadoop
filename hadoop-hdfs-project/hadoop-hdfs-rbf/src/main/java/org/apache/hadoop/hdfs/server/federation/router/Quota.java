9ad73e547b3a2076b911fee4739b3c1faaafe263
hfutatzhanghb <hfutzhanghb@163.com>
2024-11-25 14:41:24 +0800

    HDFS-17659. [ARR]Router Quota supports asynchronous rpc. (#7157). Contributed by hfutatzhanghb.
    Reviewed-by: Jian Zhang <keepromise@apache.org>

--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/router/Quota.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/router/Quota.java
@@ -213,9 +213,9 @@ public class Quota {
    * method will do some additional filtering.
    * @param path Federation path.
    * @return List of valid quota remote locations.
-   * @throws IOException
+   * @throws IOException If the location for this path cannot be determined.
    */
-  private List<RemoteLocation> getValidQuotaLocations(String path)
+  protected List<RemoteLocation> getValidQuotaLocations(String path)
       throws IOException {
     final List<RemoteLocation> locations = getQuotaRemoteLocations(path);
 
@@ -359,7 +359,7 @@ public class Quota {
    * federation path.
    * @param path Federation path.
    * @return List of quota remote locations.
-   * @throws IOException
+   * @throws IOException If the location for this path cannot be determined.
    */
   private List<RemoteLocation> getQuotaRemoteLocations(String path)
       throws IOException {
