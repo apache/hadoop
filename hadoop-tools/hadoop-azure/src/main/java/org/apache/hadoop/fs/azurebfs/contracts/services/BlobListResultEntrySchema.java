f4d9b53997d24c318d050b2ef22c3fc53b0929d8
Anuj Modi <anujmodi2011@gmail.com>
2024-12-05 07:00:30 -0800

    Build Check

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/contracts/services/BlobListResultEntrySchema.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/contracts/services/BlobListResultEntrySchema.java
@@ -1,21 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.apache.hadoop.fs.azurebfs.contracts.services;
 
 import java.util.HashMap;
@@ -38,6 +20,7 @@ public class BlobListResultEntrySchema implements ListResultEntrySchema {
   private String owner;
   private String group;
   private String permission;
+  private String acl;
   private Long contentLength = 0L;
   private String copyId;
   private String copyStatus;
@@ -191,6 +174,10 @@ public class BlobListResultEntrySchema implements ListResultEntrySchema {
     this.permission = permission;
   }
 
+  public void setAcl(final String acl) {
+    this.acl = acl;
+  }
+
   public void setContentLength(final Long contentLength) {
     this.contentLength = contentLength;
   }
@@ -225,5 +212,9 @@ public class BlobListResultEntrySchema implements ListResultEntrySchema {
 
   public void addMetadata(final String key, final String value) {
     this.metadata.put(key, value);
+  }
+
+  public String getAcl() {
+    return acl;
   }
 }
