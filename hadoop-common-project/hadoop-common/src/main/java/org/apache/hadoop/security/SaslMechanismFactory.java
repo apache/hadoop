90fe047d3934f839741ef76e7c1e72da3572b235
Tsz-Wo Nicholas Sze <szetszwo@apache.org>
2024-12-10 14:07:55 -0800

    CDPD-90798: Change back to env having a higher precedence than conf.
    (cherry picked from commit e8388b257776cc8fa4fd15970229d91a005365b0)
    Change-Id: I4f51e964aee5bc75e28ae94124badbbc31d77001

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/SaslMechanismFactory.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/SaslMechanismFactory.java
@@ -43,14 +43,14 @@ public final class SaslMechanismFactory {
     LOG.debug("{} = {} (env)", SASL_MECHANISM_ENV, envValue);
 
     // conf
-    final Configuration conf = new Configuration();
+    final Configuration conf = new Configuration(false);
     final String confValue = conf.get(HADOOP_SECURITY_SASL_MECHANISM_KEY,
         HADOOP_SECURITY_SASL_MECHANISM_DEFAULT);
     LOG.debug("{} = {} (conf)", HADOOP_SECURITY_SASL_MECHANISM_KEY, confValue);
 
-    // conf has a higher precedence than env
-    mechanism = confValue != null ? confValue
-        : envValue != null ? envValue
+    // env has a higher precedence than conf
+    mechanism = envValue != null ? envValue
+        : confValue != null ? confValue
         : HADOOP_SECURITY_SASL_MECHANISM_DEFAULT;
     LOG.debug("SASL_MECHANISM = {} (effective)", mechanism);
     return mechanism;
@@ -70,4 +70,8 @@ public final class SaslMechanismFactory {
   }
 
   private SaslMechanismFactory() {}
+
+  public static void main(String[] args) {
+    System.out.println("SASL_MECHANISM = " + getMechanism());
+  }
 }
