81374dc7733b7a542c46c438ee10ced49934d84b
zhanghaobo <hfutzhanghb@163.com>
2025-01-16 20:42:54 +0800

    fix blanks eol

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DatanodeUtil.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DatanodeUtil.java
@@ -39,7 +39,7 @@ public class DatanodeUtil {
   public static final String DISK_ERROR = "Possible disk error: ";
 
   private static final String SEP = System.getProperty("file.separator");
-  
+
   private static final long MASK = 0x1F;
 
   /** Get the cause of an I/O exception if caused by a possible disk error
