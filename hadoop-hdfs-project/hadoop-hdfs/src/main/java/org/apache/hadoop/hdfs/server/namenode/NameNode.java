349acbd511d9ae6e3c42344151c52d8faeb6a165
Aditya Raj Verma <arvstd222@gmail.com>
2025-01-24 11:36:32 +0000

    Backported feature HDFS-16547 to dissallow observer transition if namenode is in safemode

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
@@ -1903,6 +1903,9 @@ public class NameNode extends ReconfigurableBase implements
   synchronized void transitionToObserver()
       throws ServiceFailedException, AccessControlException {
     namesystem.checkSuperuserPrivilege();
+    if (notBecomeActiveInSafemode && isInSafeMode()) {
+      throw new ServiceFailedException(getRole() + " still not leave safemode");
+    }
     if (!haEnabled) {
       throw new ServiceFailedException("HA for namenode is not enabled");
     }
