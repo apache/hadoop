61fbb733ee0e5147668ad1365e1f89054e45f619
Anuj Modi <anujmodi2011@gmail.com>
2025-02-02 22:39:08 -0800

    Test Fix

--- hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/services/ITestAbfsClientHandler.java
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/services/ITestAbfsClientHandler.java
@@ -42,9 +42,15 @@ public class ITestAbfsClientHandler extends AbstractAbfsIntegrationTest {
   public void testAbfsClientHandlerInitialization() throws Exception {
     AzureBlobFileSystem fs = getFileSystem();
     AbfsClientHandler clientHandler = fs.getAbfsStore().getClientHandler();
-    Assertions.assertThat(clientHandler.getClient()).isInstanceOf(AbfsDfsClient.class);
+    if (getAbfsServiceType() == AbfsServiceType.DFS) {
+      Assertions.assertThat(clientHandler.getClient()).isInstanceOf(AbfsDfsClient.class);
+      Assertions.assertThat(clientHandler.getIngressClient()).isInstanceOf(AbfsDfsClient.class);
+    } else {
+      Assertions.assertThat(clientHandler.getClient()).isInstanceOf(AbfsBlobClient.class);
+      Assertions.assertThat(clientHandler.getIngressClient()).isInstanceOf(AbfsBlobClient.class);
+    }
     Assertions.assertThat(clientHandler.getClient(AbfsServiceType.DFS)).isInstanceOf(AbfsDfsClient.class);
     Assertions.assertThat(clientHandler.getClient(AbfsServiceType.BLOB)).isInstanceOf(AbfsBlobClient.class);
-    Assertions.assertThat(clientHandler.getIngressClient()).isInstanceOf(AbfsDfsClient.class);
+
   }
 }
