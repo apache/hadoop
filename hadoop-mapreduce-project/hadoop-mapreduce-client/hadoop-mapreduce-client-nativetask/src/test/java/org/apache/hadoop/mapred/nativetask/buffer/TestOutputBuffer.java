17a1a095bd72ab455235a69cbab1b59461454928
slfan1989 <55643692+slfan1989@users.noreply.github.com>
2025-02-06 05:10:57 +0800

    MAPREDUCE-7415. [JDK17] Upgrade Junit 4 to 5 in hadoop-mapreduce-client-nativetask. (#7349)
    Co-authored-by: Chris Nauroth <cnauroth@apache.org>
    Reviewed-by: Chris Nauroth <cnauroth@apache.org>
    Signed-off-by: Shilun Fan <slfan1989@apache.org>

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-nativetask/src/test/java/org/apache/hadoop/mapred/nativetask/buffer/TestOutputBuffer.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-nativetask/src/test/java/org/apache/hadoop/mapred/nativetask/buffer/TestOutputBuffer.java
@@ -17,7 +17,7 @@
  */
 package org.apache.hadoop.mapred.nativetask.buffer;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
