cb3fafddbfa4a7470fa4165986235883396cba7d
zhanghaobo <hfutzhanghb@163.com>
2025-02-19 16:34:07 +0800

    HDFS-17729. Inconsistent mtime in the results of -stat and -ls command due to different TimeZone.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/shell/Stat.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/shell/Stat.java
@@ -73,7 +73,6 @@ class Stat extends FsCommand {
   protected final SimpleDateFormat timeFmt;
   {
     timeFmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    timeFmt.setTimeZone(TimeZone.getTimeZone("UTC"));
   }
 
   // default format string
