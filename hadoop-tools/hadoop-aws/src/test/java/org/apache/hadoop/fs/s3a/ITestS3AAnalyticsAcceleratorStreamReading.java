425edc07ca7ec4ed530b67096337f44d81c9f40e
Ahmar Suhail <ahmarsu@amazon.co.uk>
2025-03-05 13:03:32 +0000

    upgrades AAL version to 1.0.0

--- hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/ITestS3AAnalyticsAcceleratorStreamReading.java
+++ hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/ITestS3AAnalyticsAcceleratorStreamReading.java
@@ -64,8 +64,6 @@ import static org.apache.hadoop.test.LambdaTestUtils.intercept;
 public class ITestS3AAnalyticsAcceleratorStreamReading extends AbstractS3ATestBase {
 
   private static final String PHYSICAL_IO_PREFIX = "physicalio";
-  private static final String LOGICAL_IO_PREFIX = "logicalio";
-
 
   private Path externalTestFile;
 
@@ -174,35 +172,6 @@ public class ITestS3AAnalyticsAcceleratorStreamReading extends AbstractS3ATestBa
     }
 
     verifyStatisticCounterValue(ioStats, STREAM_READ_ANALYTICS_OPENED, 1);
-  }
-
-  @Test
-  public void testConnectorFrameworkConfigurable() {
-    describe("Verify S3 connector framework reads configuration");
-
-    Configuration conf = new Configuration(getConfiguration());
-
-    //Disable Predictive Prefetching
-    conf.set(ANALYTICS_ACCELERATOR_CONFIGURATION_PREFIX +
-        "." + LOGICAL_IO_PREFIX + ".prefetching.mode", "all");
-
-    //Set Blobstore Capacity
-    conf.setInt(ANALYTICS_ACCELERATOR_CONFIGURATION_PREFIX +
-        "." + PHYSICAL_IO_PREFIX + ".blobstore.capacity", 1);
-
-    ConnectorConfiguration connectorConfiguration =
-        new ConnectorConfiguration(conf, ANALYTICS_ACCELERATOR_CONFIGURATION_PREFIX);
-
-    S3SeekableInputStreamConfiguration configuration =
-        S3SeekableInputStreamConfiguration.fromConfiguration(connectorConfiguration);
-
-    Assertions.assertThat(configuration.getLogicalIOConfiguration().getPrefetchingMode())
-            .as("AnalyticsStream configuration is not set to expected value")
-            .isSameAs(PrefetchMode.ALL);
-
-    Assertions.assertThat(configuration.getPhysicalIOConfiguration().getBlobStoreCapacity())
-            .as("AnalyticsStream configuration is not set to expected value")
-            .isEqualTo(1);
   }
 
   @Test
