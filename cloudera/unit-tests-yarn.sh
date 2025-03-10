d890d3d97e4c91f83652d19763bbda415f112982
Hean Chhinling <heanchhinling@admins-MacBook-Pro.local>
2025-03-10 11:19:30 +0100

    COMPX-19445: change the SonarQube project name to include UT-cdh_main for Unit Testing

--- cloudera/unit-tests-yarn.sh
+++ cloudera/unit-tests-yarn.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2021 Cloudera, Inc. All rights reserved.
 
 POM_FILE=hadoop-yarn-project/pom.xml \
-SONAR_PROJECT_KEY=Hadoop-YARN-cdh_main \
-SONAR_PROJECT_NAME="Hadoop YARN cdh_main" \
+SONAR_PROJECT_KEY=Hadoop-YARN-UT-cdh_main \
+SONAR_PROJECT_NAME="Hadoop YARN UT-cdh_main" \
 SONAR_TOKEN="sqp_bc50a870273f14e119cf704845b5af400e3bbc08" \
 cloudera/unit-tests.sh --projects '!hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timelineservice-hbase-tests' "$@"
