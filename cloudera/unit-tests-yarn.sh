23a8c82f154f4ce0a79c59f1c75da4e885ce0132
ChhinlingHean <chhinling.hean@cloudera.com>
2025-03-12 08:12:20 +0100

    COMPX-19445: Update the SonarQube project token for Hadoop YARN UT-cdh_main (#277)
    Co-authored-by: Hean Chhinling <heanchhinling@admins-MacBook-Pro.local>

--- cloudera/unit-tests-yarn.sh
+++ cloudera/unit-tests-yarn.sh
@@ -4,5 +4,5 @@
 POM_FILE=hadoop-yarn-project/pom.xml \
 SONAR_PROJECT_KEY=Hadoop-YARN-UT-cdh_main \
 SONAR_PROJECT_NAME="Hadoop YARN UT-cdh_main" \
-SONAR_TOKEN="sqp_bc50a870273f14e119cf704845b5af400e3bbc08" \
+SONAR_TOKEN="sqp_50dce89c318b11761da0df13b55dec734d9a5a60" \
 cloudera/unit-tests.sh --projects '!hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timelineservice-hbase-tests' "$@"
