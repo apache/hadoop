b6fb9359725b8727c0bd1ae9e86096423a5095c5
slfan1989 <slfan1989@apache.org>
2025-03-27 09:12:15 +0800

    YARN-11266. Fix CheckStyle.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/TestClientRMService.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/TestClientRMService.java
@@ -711,7 +711,7 @@ public class TestClientRMService {
     assertThrows(ApplicationNotFoundException.class, () -> {
       RMContext rmContext = mock(RMContext.class);
       when(rmContext.getRMApps()).thenReturn(
-        new ConcurrentHashMap<ApplicationId, RMApp>());
+          new ConcurrentHashMap<ApplicationId, RMApp>());
       ClientRMService rmService = new ClientRMService(rmContext, null, null,
           null, null, null);
       ApplicationId applicationId =
