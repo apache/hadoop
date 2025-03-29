f09b946cd84d837717ad417c71edfe32bd9fedce
slfan1989 <slfan1989@apache.org>
2025-03-29 17:06:05 +0800

    HADOOP-19439. Upgrade JUnit from 4 to 5 in hadoop-rumen.

--- hadoop-tools/hadoop-rumen/src/test/java/org/apache/hadoop/tools/rumen/TestHistograms.java
+++ hadoop-tools/hadoop-rumen/src/test/java/org/apache/hadoop/tools/rumen/TestHistograms.java
@@ -34,10 +34,12 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
-import org.junit.Ignore;
-import org.junit.Test;
-import static org.junit.Assert.*;
-@Ignore
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@Disabled
 public class TestHistograms {
 
   /**
@@ -71,7 +73,7 @@ public class TestHistograms {
       if (fileName.startsWith("input")) {
         String testName = fileName.substring("input".length());
         Path goldFilePath = new Path(rootInputFile, "gold"+testName);
-        assertTrue("Gold file dies not exist", lfs.exists(goldFilePath));
+        assertTrue(lfs.exists(goldFilePath), "Gold file dies not exist");
         LoggedDiscreteCDF newResult = histogramFileToCDF(filePath, lfs);
         System.out.println("Testing a Histogram for " + fileName);
         FSDataInputStream goldStream = lfs.open(goldFilePath);
@@ -107,11 +109,11 @@ public class TestHistograms {
     List<Long> typeProbeData = new HistogramRawTestData().getData();
 
     assertTrue(
-        "The data attribute of a jackson-reconstructed HistogramRawTestData "
+    
+       measurements.getClass() == typeProbeData.getClass(), "The data attribute of a jackson-reconstructed HistogramRawTestData "
             + " should be a " + typeProbeData.getClass().getName()
             + ", like a virgin HistogramRawTestData, but it's a "
-            + measurements.getClass().getName(),
-        measurements.getClass() == typeProbeData.getClass());
+            + measurements.getClass().getName());
 
     for (int j = 0; j < measurements.size(); ++j) {
       hist.enter(measurements.get(j));
