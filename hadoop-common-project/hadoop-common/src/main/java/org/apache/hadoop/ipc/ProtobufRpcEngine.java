6126c3e7abf65873e3f5b79b710299359ce583b7
zhanghaobo <hfutzhanghb@163.com>
2025-03-31 14:21:02 +0800

    HADOOP-19521. Fix time unit mismatch in method updateDeferredMetrics.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/ProtobufRpcEngine.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/ProtobufRpcEngine.java
@@ -415,7 +415,7 @@ public class ProtobufRpcEngine implements RpcEngine {
         long deltaNanos = Time.monotonicNowNanos() - call.getStartHandleTimestampNanos();
         updateProcessingDetails(call, deltaNanos);
         call.setDeferredResponse(RpcWritable.wrap(message));
-        server.updateDeferredMetrics(call, methodName, deltaNanos);
+        server.updateDeferredMetrics(call, methodName, TimeUnit.NANOSECONDS.toMillis(deltaNanos));
       }
 
       @Override
@@ -424,7 +424,7 @@ public class ProtobufRpcEngine implements RpcEngine {
         updateProcessingDetails(call, deltaNanos);
         call.setDeferredError(t);
         String detailedMetricsName = t.getClass().getSimpleName();
-        server.updateDeferredMetrics(call, detailedMetricsName, deltaNanos);
+        server.updateDeferredMetrics(call, detailedMetricsName, TimeUnit.NANOSECONDS.toMillis(deltaNanos));
       }
     }
 
