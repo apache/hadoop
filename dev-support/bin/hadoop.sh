029cd588a350001661bb32c3dc4ecd8e8213279a
Istvan Toth <stoty@apache.org>
2025-04-03 19:57:35 +0200

    add -Dsurefire.failIfNoSpecifiedTests=false in hadoop.sh

--- dev-support/bin/hadoop.sh
+++ dev-support/bin/hadoop.sh
@@ -619,7 +619,7 @@ function shadedclient_rebuild
   echo_and_redirect "${logfile}" \
     "${MAVEN}" "${MAVEN_ARGS[@]}" verify -fae --batch-mode -am \
       "${modules[@]}" \
-      -DskipShade -Dtest=NoUnitTests -Dmaven.javadoc.skip=true -Dcheckstyle.skip=true \
+      -DskipShade -Dtest=NoUnitTests -Dsurefire.failIfNoSpecifiedTests=false -Dmaven.javadoc.skip=true -Dcheckstyle.skip=true \
       -Dspotbugs.skip=true ${extra[*]}
 
   count=$("${GREP}" -c '\[ERROR\]' "${logfile}")
