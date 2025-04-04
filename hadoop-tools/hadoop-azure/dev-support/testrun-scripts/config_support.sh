82c7c026e869d7bee6944f30f7547b459015ef83
Manika Joshi <manikajoshi500@gmail.com>
2025-04-03 21:58:25 -0700

    comment suggestions

--- hadoop-tools/hadoop-azure/dev-support/testrun-scripts/config_support.sh
+++ hadoop-tools/hadoop-azure/dev-support/testrun-scripts/config_support.sh
@@ -2,6 +2,21 @@
 
 set -eo pipefail
 
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 FILE=$1
 
 if [ ! -f "$FILE" ]; then
@@ -19,8 +34,8 @@ cp "$FILE" "$OUTPUT_FILE"
 
 contactTeamMsg="For any queries or support, kindly reach out to us at 'askabfs@microsoft.com'."
 endpoint=".dfs."
-printf "Select 'HNS' if you're migrating to ABFS driver with Hierarchical Namespace enabled account,
-            or 'Non-HNS' if you're migrating with Non-Hierarchical Namespace (FNS) account. \n"
+printf "Select 'HNS' if you're migrating to ABFS driver for Hierarchical Namespace enabled account,
+          or 'Non-HNS' if you're migrating to ABFS driver for Non-Hierarchical Namespace (FNS) account. \n"
 printf "WARNING: Please ensure the correct option is chosen as it will affect the configuration changes made to the file. \n"
 printf "If you are unsure, follow the instructions below to check from Azure Portal: \n"
 printf "* Go to the Azure Portal and navigate to your storage account. \n"
@@ -88,17 +103,19 @@ obsolete_configs_list=(
     "storage.client.logging" #fs.azure.storage.client.logging
     "storage.emulator.account.name" #fs.azure.storage.emulator.account.name
     "storage.timeout" #fs.azure.storage.timeout
+    "enable.append.support" #fs.azure.enable.append.support
 )
 
 # Stop the script if any unsupported config is found
 for key in "${unsupported_configs_list[@]}"; do
     if grep -q "$key" "$OUTPUT_FILE"; then
-        echo "FAILURE: Remove the following configuration from file and rerun: '$key'"
+        echo "Remove the following configuration from file and rerun: '$key'"
         failure=true
     fi
 done
 
 if [ "$failure" = true ]; then
+     echo "FAILURE: Unsupported Config Found"
     echo "$contactTeamMsg"
     echo "Exiting..."
     exit 1
--- hadoop-tools/hadoop-azure/src/site/markdown/wasbToAbfsMigration.md
+++ hadoop-tools/hadoop-azure/src/site/markdown/wasbToAbfsMigration.md
@@ -1,4 +1,18 @@
-# WASB to ABFS Configuration Conversion Script 
+<!---
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License. See accompanying LICENSE file.
+-->
+
+# WASB to ABFS Configuration Conversion Script
 
 To support customer onboard for migration from WASB to ABFS driver, we've
 introduced a script to help with the configuration changes required
