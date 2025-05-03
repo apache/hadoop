3a9fae56cfb639dc3f4c15db13fb231b025faa91
slfan1989 <slfan1989@apache.org>
2025-05-03 10:41:27 +0800

    HADOOP-19415. [JDK17] Upgrade JUnit from 4 to 5 in hadoop-common Part9.

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/FileSystemTestWrapper.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/FileSystemTestWrapper.java
@@ -29,7 +29,7 @@ import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.security.AccessControlException;
 import org.apache.hadoop.util.Progressable;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 
 /**
  * Helper class for unit tests.
@@ -170,29 +170,29 @@ public final class FileSystemTestWrapper extends FSTestWrapper {
   public void checkFileStatus(String path, fileType expectedType)
       throws IOException {
     FileStatus s = fs.getFileStatus(new Path(path));
-    Assert.assertNotNull(s);
+    Assertions.assertNotNull(s);
     if (expectedType == fileType.isDir) {
-      Assert.assertTrue(s.isDirectory());
+      Assertions.assertTrue(s.isDirectory());
     } else if (expectedType == fileType.isFile) {
-      Assert.assertTrue(s.isFile());
+      Assertions.assertTrue(s.isFile());
     } else if (expectedType == fileType.isSymlink) {
-      Assert.assertTrue(s.isSymlink());
+      Assertions.assertTrue(s.isSymlink());
     }
-    Assert.assertEquals(fs.makeQualified(new Path(path)), s.getPath());
+    Assertions.assertEquals(fs.makeQualified(new Path(path)), s.getPath());
   }
 
   public void checkFileLinkStatus(String path, fileType expectedType)
       throws IOException {
     FileStatus s = fs.getFileLinkStatus(new Path(path));
-    Assert.assertNotNull(s);
+    Assertions.assertNotNull(s);
     if (expectedType == fileType.isDir) {
-      Assert.assertTrue(s.isDirectory());
+      Assertions.assertTrue(s.isDirectory());
     } else if (expectedType == fileType.isFile) {
-      Assert.assertTrue(s.isFile());
+      Assertions.assertTrue(s.isFile());
     } else if (expectedType == fileType.isSymlink) {
-      Assert.assertTrue(s.isSymlink());
+      Assertions.assertTrue(s.isSymlink());
     }
-    Assert.assertEquals(fs.makeQualified(new Path(path)), s.getPath());
+    Assertions.assertEquals(fs.makeQualified(new Path(path)), s.getPath());
   }
 
   //
--- hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azure/ITestNativeAzureFileSystemContractEmulator.java
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azure/ITestNativeAzureFileSystemContractEmulator.java
@@ -23,10 +23,10 @@ import static org.junit.Assume.assumeNotNull;
 import org.apache.hadoop.fs.FileSystemContractBaseTest;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.azure.integration.AzureTestUtils;
+import org.apache.hadoop.test.TestName;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.rules.TestName;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 /**
  * Run the {@code FileSystemContractBaseTest} tests against the emulator
@@ -36,14 +36,14 @@ public class ITestNativeAzureFileSystemContractEmulator extends
   private AzureBlobStorageTestAccount testAccount;
   private Path basePath;
 
-  @Rule
+  @RegisterExtension
   public TestName methodName = new TestName();
 
   private void nameThread() {
     Thread.currentThread().setName("JUnit-" + methodName.getMethodName());
   }
 
-  @Before
+  @BeforeEach
   public void setUp() throws Exception {
     nameThread();
     testAccount = AzureBlobStorageTestAccount.createForEmulator();
