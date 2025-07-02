720e37317f93c7f850eeef4d629c41ae79f0c30f
Arunkumar Chacko <arunchacko@google.com>
2025-07-02 17:58:18 +0000

    HADOOP-19343. Add support for append(), compose(), concat()
    Closes #7773
    
    Signed-off-by: Chris Nauroth <cnauroth@apache.org>

--- hadoop-tools/hadoop-gcp/src/test/java/org/apache/hadoop/fs/gs/contract/ITestGoogleContractConcat.java
+++ hadoop-tools/hadoop-gcp/src/test/java/org/apache/hadoop/fs/gs/contract/ITestGoogleContractConcat.java
@@ -17,12 +17,12 @@
  */
 
 package org.apache.hadoop.fs.gs.contract;
-
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.contract.AbstractContractMkdirTest;
+import org.apache.hadoop.fs.contract.AbstractContractConcatTest;
 import org.apache.hadoop.fs.contract.AbstractFSContract;
 
-public class ITestGoogleContractMkdir extends AbstractContractMkdirTest {
+/** GCS contract tests covering file concat. */
+public class ITestGoogleContractConcat extends AbstractContractConcatTest {
   @Override
   protected AbstractFSContract createContract(Configuration conf) {
     return new GoogleContract(conf);
