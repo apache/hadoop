0a0f4e197ef6432862eb88674d59e8bcbdc65914
mattkduran <19656092+mattkduran@users.noreply.github.com>
2025-07-16 13:13:52 -0700

    Add in cleanup method

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/services/AbfsClient.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/services/AbfsClient.java
@@ -363,6 +363,9 @@ public abstract class AbfsClient implements Closeable {
     if (abfsApacheHttpClient != null) {
       abfsApacheHttpClient.close();
     }
+    if (intercept != null) {
+      IOUtils.cleanupWithLogger(LOG, intercept);
+    }
     if (tokenProvider instanceof Closeable) {
       IOUtils.cleanupWithLogger(LOG,
           (Closeable) tokenProvider);
