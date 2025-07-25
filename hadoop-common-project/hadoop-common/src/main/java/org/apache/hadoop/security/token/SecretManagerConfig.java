b02c20254ed02b0802bb849fda7299e25ef3eb88
K0K0V0K <bkosztolnik@cloudera.com>
2025-07-25 07:45:41 +0200

    HADOOP-19639. SecretManager configuration at runtime
    - fix style

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/token/SecretManagerConfig.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/token/SecretManagerConfig.java
@@ -34,7 +34,8 @@ import java.security.NoSuchAlgorithmException;
  * and message authentication code (MAC) generation using specified algorithms and key lengths.
  * <p>
  * This class supports static access to the selected cryptographic algorithm and key length,
- * and provides methods to create configured {@link javax.crypto.KeyGenerator} and {@link javax.crypto.Mac} instances.
+ * and provides methods to create configured {@link javax.crypto.KeyGenerator}
+ * and {@link javax.crypto.Mac} instances.
  * The configuration is initialized statically from a provided {@link Configuration} object.
  * <p>
  * The {@link SecretManager} has some static method, so static configuration is required
@@ -43,12 +44,15 @@ import java.security.NoSuchAlgorithmException;
 @InterfaceStability.Evolving
 public class SecretManagerConfig {
   private static final Logger LOG = LoggerFactory.getLogger(SecretManagerConfig.class);
-  private static String SELECTED_ALGORITHM;
-  private static int SELECTED_LENGTH;
-  private static boolean INITIALIZED;
+  private static String selectedAlgorithm;
+  private static int selectedLength;
+  private static boolean initialized;
 
   static {
     update(new Configuration());
+  }
+
+  private SecretManagerConfig() {
   }
 
   /**
@@ -60,18 +64,18 @@ public class SecretManagerConfig {
    * @param conf the configuration object containing cryptographic settings
    */
   public static synchronized void update(Configuration conf) {
-    if (INITIALIZED) {
+    if (initialized) {
       LOG.warn(
-          "Keygen or Mac was already initialized with older configuration, those will not be updated");
+        "Keygen or Mac was already initialized with older config, those will not be updated");
     }
-    SELECTED_ALGORITHM = conf.get(
-        CommonConfigurationKeysPublic.HADOOP_SECURITY_SECRET_MANAGER_KEY_GENERATOR_ALGORITHM_KEY,
-        CommonConfigurationKeysPublic.HADOOP_SECURITY_SECRET_MANAGER_KEY_GENERATOR_ALGORITHM_DEFAULT);
-    LOG.debug("Selected hash algorithm: {}", SELECTED_ALGORITHM);
-    SELECTED_LENGTH =
-        conf.getInt(CommonConfigurationKeysPublic.HADOOP_SECURITY_SECRET_MANAGER_KEY_LENGTH_KEY,
-            CommonConfigurationKeysPublic.HADOOP_SECURITY_SECRET_MANAGER_KEY_LENGTH_DEFAULT);
-    LOG.debug("Selected hash key length: {}", SELECTED_LENGTH);
+    selectedAlgorithm = conf.get(
+      CommonConfigurationKeysPublic.HADOOP_SECURITY_SECRET_MANAGER_KEY_GENERATOR_ALGORITHM_KEY,
+      CommonConfigurationKeysPublic.HADOOP_SECURITY_SECRET_MANAGER_KEY_GENERATOR_ALGORITHM_DEFAULT);
+    LOG.debug("Selected hash algorithm: {}", selectedAlgorithm);
+    selectedLength = conf.getInt(
+      CommonConfigurationKeysPublic.HADOOP_SECURITY_SECRET_MANAGER_KEY_LENGTH_KEY,
+      CommonConfigurationKeysPublic.HADOOP_SECURITY_SECRET_MANAGER_KEY_LENGTH_DEFAULT);
+    LOG.debug("Selected hash key length: {}", selectedLength);
   }
 
   /**
@@ -80,7 +84,7 @@ public class SecretManagerConfig {
    * @return the name of the selected algorithm
    */
   public static synchronized String getSelectedAlgorithm() {
-    return SELECTED_ALGORITHM;
+    return selectedAlgorithm;
   }
 
   /**
@@ -89,7 +93,7 @@ public class SecretManagerConfig {
    * @return the selected key length
    */
   public static synchronized int getSelectedLength() {
-    return SELECTED_LENGTH;
+    return selectedLength;
   }
 
   /**
@@ -100,14 +104,14 @@ public class SecretManagerConfig {
    * @throws IllegalArgumentException if the specified algorithm is not available
    */
   public static synchronized KeyGenerator createKeyGenerator() {
-    LOG.debug("Creating key generator instance {}, {}", SELECTED_ALGORITHM, SELECTED_LENGTH);
-    INITIALIZED = true;
+    LOG.debug("Creating key generator instance {}, {}", selectedAlgorithm, selectedLength);
+    initialized = true;
     try {
-      KeyGenerator keyGen = KeyGenerator.getInstance(SELECTED_ALGORITHM);
-      keyGen.init(SELECTED_LENGTH);
+      KeyGenerator keyGen = KeyGenerator.getInstance(selectedAlgorithm);
+      keyGen.init(selectedLength);
       return keyGen;
     } catch (NoSuchAlgorithmException nsa) {
-      throw new IllegalArgumentException("Can't find " + SELECTED_ALGORITHM, nsa);
+      throw new IllegalArgumentException("Can't find " + selectedAlgorithm, nsa);
     }
   }
 
@@ -118,12 +122,12 @@ public class SecretManagerConfig {
    * @throws IllegalArgumentException if the specified algorithm is not available
    */
   public static synchronized Mac createMac() {
-    LOG.debug("Creating mac instance {}", SELECTED_ALGORITHM);
-    INITIALIZED = true;
+    LOG.debug("Creating mac instance {}", selectedAlgorithm);
+    initialized = true;
     try {
-      return Mac.getInstance(SELECTED_ALGORITHM);
+      return Mac.getInstance(selectedAlgorithm);
     } catch (NoSuchAlgorithmException nsa) {
-      throw new IllegalArgumentException("Can't find " + SELECTED_ALGORITHM, nsa);
+      throw new IllegalArgumentException("Can't find " + selectedAlgorithm, nsa);
     }
   }
 }
