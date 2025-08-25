e1ca635de86907bced4ccad61435921029972f8b
Tom McCormick <tmccormi@linkedin.com>
2025-08-25 17:32:18 -0400

    fix checkstyle

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/AuthorizationContext.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/AuthorizationContext.java
@@ -21,19 +21,19 @@ package org.apache.hadoop.security;
  * Utility for managing a thread-local authorization header for RPC calls.
  */
 public final class AuthorizationContext {
-    private static final ThreadLocal<byte[]> AUTH_HEADER = new ThreadLocal<>();
+  private static final ThreadLocal<byte[]> AUTH_HEADER = new ThreadLocal<>();
 
-    private AuthorizationContext() {}
+  private AuthorizationContext() {}
 
-    public static void setCurrentAuthorizationHeader(byte[] header) {
-        AUTH_HEADER.set(header);
-    }
+  public static void setCurrentAuthorizationHeader(byte[] header) {
+    AUTH_HEADER.set(header);
+  }
 
-    public static byte[] getCurrentAuthorizationHeader() {
-        return AUTH_HEADER.get();
-    }
+  public static byte[] getCurrentAuthorizationHeader() {
+    return AUTH_HEADER.get();
+  }
 
-    public static void clear() {
-        AUTH_HEADER.remove();
-    }
+  public static void clear() {
+    AUTH_HEADER.remove();
+  }
 }
