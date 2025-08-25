b24978a655f381c074bd8173622cf510e0d2a89a
zheng-weihao <zheng-weihao@outlook.com>
2025-08-25 14:16:38 +0800

    YARN-11856. DOWNLOADING resources unlock and cleanup is interrupted when killing a container that is localizing
    Signed-off-by: zheng-weihao <zheng-weihao@outlook.com>

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/localizer/ResourceLocalizationService.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/localizer/ResourceLocalizationService.java
@@ -1294,8 +1294,12 @@ public class ResourceLocalizationService extends CompositeService
           // On error, report failure to Container and signal ABORT
           // Notify resource of failed localization
           ContainerId cId = context.getContainerId();
-          dispatcher.getEventHandler().handle(new ContainerResourceFailedEvent(
-              cId, null, exception.getMessage()));
+          try {
+              dispatcher.getEventHandler().handle(new ContainerResourceFailedEvent(
+                      cId, null, exception.getMessage()));
+          } catch (Exception | FSError e) {
+              LOG.info("Failed to send container resource failed event for " + cId, e);
+          }
         }
         List<Path> paths = new ArrayList<Path>();
         for (LocalizerResourceRequestEvent event : scheduled.values()) {
