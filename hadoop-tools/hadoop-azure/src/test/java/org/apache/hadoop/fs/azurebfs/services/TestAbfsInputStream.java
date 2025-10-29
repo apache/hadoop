ebab278b5eb297775e3e821dfcbf3c7c1b227f7d
Manika Joshi <manikajoshi500@gmail.com>
2025-10-29 01:11:15 -0700

    test addition

--- hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/services/TestAbfsInputStream.java
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/services/TestAbfsInputStream.java
@@ -19,8 +19,11 @@
 package org.apache.hadoop.fs.azurebfs.services;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -58,6 +61,7 @@ import org.apache.hadoop.fs.impl.OpenFileParameters;
 import static org.apache.hadoop.fs.azurebfs.constants.AbfsHttpConstants.COLON;
 import static org.apache.hadoop.fs.azurebfs.constants.AbfsHttpConstants.EMPTY_STRING;
 import static org.apache.hadoop.fs.azurebfs.constants.AbfsHttpConstants.SPLIT_NO_LIMIT;
+import static org.apache.hadoop.fs.azurebfs.constants.ConfigurationKeys.FS_AZURE_ENABLE_REQUEST_PRIORITY_FOR_PREFETCH;
 import static org.apache.hadoop.fs.azurebfs.constants.ReadType.DIRECT_READ;
 import static org.apache.hadoop.fs.azurebfs.constants.ReadType.FOOTER_READ;
 import static org.apache.hadoop.fs.azurebfs.constants.ReadType.MISSEDCACHE_READ;
@@ -66,8 +70,12 @@ import static org.apache.hadoop.fs.azurebfs.constants.ReadType.PREFETCH_READ;
 import static org.apache.hadoop.fs.azurebfs.constants.ReadType.SMALLFILE_READ;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.nullable;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
@@ -104,6 +112,9 @@ public class TestAbfsInputStream extends
   private static final int POSITION_INDEX = 9;
   private static final int OPERATION_INDEX = 6;
   private static final int READTYPE_INDEX = 11;
+  private static final String PREFETCH_TRAFFIC_PRIORITY_HEADER = "x-ms-request-priority";
+  private static final String PREFETCH_TRAFFIC_PRIORITY_HEADER_VALUE = "7";
+
 
   @AfterEach
   @Override
@@ -894,6 +905,90 @@ public class TestAbfsInputStream extends
     Path testPath = createTestFile(fs, fileSize);
     readFile(fs, testPath, fileSize);
     assertReadTypeInClientRequestId(fs, numOfReadCalls, totalReadCalls, readType);
+  }
+
+  /*
+   * Test to verify that both conditions of prefetch read and respective config
+   * enabled needs to be true for the priority header to be added
+   */
+  @Test
+  public void testPrefetchReadAddsPriorityHeaderWithDifferentConfigs()
+      throws Exception {
+    Configuration configuration1 = new Configuration(getRawConfiguration());
+    configuration1.set(FS_AZURE_ENABLE_REQUEST_PRIORITY_FOR_PREFETCH, "true");
+
+    Configuration configuration2 = new Configuration(getRawConfiguration());
+    //use the default value for the config: false
+    configuration2.unset(FS_AZURE_ENABLE_REQUEST_PRIORITY_FOR_PREFETCH);
+
+    TracingContext tracingContext1 = mock(TracingContext.class);
+    when(tracingContext1.getReadType()).thenReturn(PREFETCH_READ);
+
+    //Prefetch Read with config enabled
+    executePrefetchReadTest(tracingContext1, configuration1, true);
+    //Prefetch Read with config disabled
+    executePrefetchReadTest(tracingContext1, configuration2, false);
+
+    when(tracingContext1.getReadType()).thenReturn(DIRECT_READ);
+
+    //Non-prefetch read with config disabled
+    executePrefetchReadTest(tracingContext1, configuration2, false);
+    //Non-prefetch read with config enabled
+    executePrefetchReadTest(tracingContext1, configuration1, false);
+  }
+
+  private void executePrefetchReadTest(TracingContext tracingContext,
+      Configuration rawConfig,
+      boolean shouldHaveHeader) throws Exception {
+    AzureBlobFileSystem azureFs = (AzureBlobFileSystem) FileSystem.newInstance(
+        rawConfig);
+    AzureBlobFileSystemStore store = Mockito.spy(azureFs.getAbfsStore());
+
+    AbfsClient abfsClient = Mockito.spy(store.getClient());
+    Mockito.doReturn(abfsClient).when(store).getClient();
+
+    List<AbfsHttpHeader> headersList = new ArrayList<>();
+
+    doAnswer(invocation -> {
+      AbfsRestOperation realOp
+          = (AbfsRestOperation) invocation.callRealMethod();
+      AbfsRestOperation spiedOp = spy(realOp);
+
+      headersList.addAll(spiedOp.getRequestHeaders());
+
+      doNothing().when(spiedOp).execute(any(TracingContext.class));
+      return spiedOp;
+    })
+        .when(abfsClient)
+        .getAbfsRestOperation(
+            any(AbfsRestOperationType.class),
+            anyString(),
+            any(URL.class),
+            anyList(),
+            any(byte[].class),
+            anyInt(),
+            anyInt(),
+            nullable(String.class)
+        );
+
+    abfsClient.read(
+        "dummy-path", 0L, new byte[1], 0, 1,
+        "etag", "leaseId", null, tracingContext);
+
+
+    if (shouldHaveHeader) {
+      assertThat(headersList)
+          .anySatisfy(header -> {
+            assertThat(header.getName()).isEqualTo(
+                PREFETCH_TRAFFIC_PRIORITY_HEADER);
+            assertThat(header.getValue()).isEqualTo(
+                PREFETCH_TRAFFIC_PRIORITY_HEADER_VALUE);
+          });
+    } else {
+      assertThat(headersList)
+          .noneSatisfy(header -> assertThat(header.getName()).isEqualTo(
+              PREFETCH_TRAFFIC_PRIORITY_HEADER));
+    }
   }
 
   private Path createTestFile(AzureBlobFileSystem fs, int fileSize) throws Exception {
