447a536f86b2e7701834fbd47aca29c6b5b52f89
K0K0V0K <bkosztolnik@cloudera.com>
2025-11-21 12:22:52 +0100

    COMPX-24309. Upgrade github yarn smoke UT to use JDK17
    - remove auto UT for jdk 17 upgrade feature branch cause it is closed
    - upgrade cdh_main to use jdk 17

--- cloudera/unit-tests-yarn-smoke.sh
+++ cloudera/unit-tests-yarn-smoke.sh
@@ -6,5 +6,5 @@ set -eux -o pipefail
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 MAVEN_SETTINGS="${MAVEN_SETTINGS:-"${SCRIPT_DIR}/settings.xml"}"
 
-mvn -s "$MAVEN_SETTINGS" -B -f "${SCRIPT_DIR}/../pom.xml" clean install -T 16 -DskipTests -DskipShade
-mvn -s "$MAVEN_SETTINGS" -B -f hadoop-yarn-project/pom.xml test --fail-never --projects 'hadoop-yarn/hadoop-yarn-common, hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-common, hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager, hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager, hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests'
+mvn -Dmaven.repo.local=$SCRIPT_DIR -s "$MAVEN_SETTINGS" -B -f "${SCRIPT_DIR}/../pom.xml" clean install -T 16 -DskipTests -DskipShade
+mvn -Dmaven.repo.local=$SCRIPT_DIR -s "$MAVEN_SETTINGS" -B -f hadoop-yarn-project/pom.xml test --fail-never --projects 'hadoop-yarn/hadoop-yarn-common, hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-common, hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager, hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager, hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests'
