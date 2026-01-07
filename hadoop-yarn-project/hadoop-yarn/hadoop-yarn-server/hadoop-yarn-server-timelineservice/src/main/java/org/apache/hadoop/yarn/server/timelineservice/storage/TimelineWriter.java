665cbcb6e7c1348312df1c6ebabdcfd4eb89f9a6
Steve Loughran <stevel@cloudera.com>
2026-01-07 15:25:22 +0000

    Revert "YARN-11916. FileSystemTimelineReaderImpl vulnerable to race conditions (#8164)"
    This reverts commit 0dc79ad5d9aa980fe6720e2f9d027f926d4c069a.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timelineservice/src/main/java/org/apache/hadoop/yarn/server/timelineservice/storage/TimelineWriter.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-timelineservice/src/main/java/org/apache/hadoop/yarn/server/timelineservice/storage/TimelineWriter.java
@@ -73,8 +73,6 @@ public interface TimelineWriter extends Service {
    *
    * Any errors occurring for individual write request objects will be reported
    * in the response.
-   *<p>
-   * This is not invoked anywhere, tested and all implementations return null.
    *
    * @param data
    *          a {@link TimelineEntity} object
@@ -82,7 +80,7 @@ public interface TimelineWriter extends Service {
    *          value.
    * @param track Specifies the track or dimension along which aggregation would
    *     occur. Includes USER, FLOW, QUEUE, etc.
-   * @return a {@link TimelineWriteResponse} object. All implementations return null.
+   * @return a {@link TimelineWriteResponse} object.
    * @throws IOException if there is any exception encountered while aggregating
    *     entities to the backend storage.
    */
