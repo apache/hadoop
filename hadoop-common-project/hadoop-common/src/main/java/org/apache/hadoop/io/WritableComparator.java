cf758f7904db27a5aa2c9201515f8466d1ce7fac
Cheng Pan <chengpan@apache.org>
2026-02-09 16:37:50 +0800

    fix end offset

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/WritableComparator.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/WritableComparator.java
@@ -230,7 +230,7 @@ public class WritableComparator implements RawComparator, Configurable {
    */
   public static int compareBytes(byte[] b1, int s1, int l1,
                                  byte[] b2, int s2, int l2) {
-    return Arrays.compareUnsigned(b1, s1, l1, b2, s2, l2);
+    return Arrays.compareUnsigned(b1, s1, s1 + l1, b2, s2, s2 + l2);
   }
 
   /**
