435243da80d22315bd736071055513bcf286875e
Cheng Pan <chengpan@apache.org>
2026-02-10 21:56:02 +0800

    disable SC2086 for DOCKER_INTERACTIVE_RUN

--- start-build-env.sh
+++ start-build-env.sh
@@ -110,6 +110,7 @@ DOCKER_INTERACTIVE_RUN=${DOCKER_INTERACTIVE_RUN-"-i -t"}
 # within the container and use the result on your normal
 # system.  And this also is a significant speedup in subsequent
 # builds because the dependencies are downloaded only once.
+# shellcheck disable=SC2086
 docker run "${DOCKER_PLATFORM_ARGS[@]}" --rm=true ${DOCKER_INTERACTIVE_RUN} \
   -v "${PWD}:${DOCKER_HOME_DIR}/hadoop${V_OPTS:-}" \
   -w "${DOCKER_HOME_DIR}/hadoop" \
