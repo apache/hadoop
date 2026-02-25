582929158d21579bb9ebc1d62a9be0f7b6213ead
LiuGuH <444506464@qq.com>
2026-02-25 16:48:10 +0800

    fix asflicense and checkstyle

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/TestNNUpdateStorageVersionWhenInterrupt.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/TestNNUpdateStorageVersionWhenInterrupt.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hdfs.server.namenode;
 
 import java.io.File;
@@ -20,10 +37,10 @@ import org.apache.hadoop.test.GenericTestUtils;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestNNUpdateStorageVersionWhenInterrupt {
-  static String nnDir =
+  private static final String nnDir =
       GenericTestUtils.getTestDir("dfs").getAbsolutePath() + File.separator + "namenode";
-  static NNStorage nnStorage;
-  static GenericTestUtils.LogCapturer nnStorageLog;
+  private static NNStorage nnStorage;
+  private static GenericTestUtils.LogCapturer nnStorageLog;
 
   @BeforeAll
   public static void setUp() throws IOException, URISyntaxException {
@@ -45,7 +62,7 @@ public class TestNNUpdateStorageVersionWhenInterrupt {
   @Test
   public void test()
       throws IOException, URISyntaxException, InterruptedException, TimeoutException {
-    Thread thread = new updateVersionFileThread(nnStorage);
+    Thread thread = new UpdateVersionFileThread(nnStorage);
     assertEquals(1, nnStorage.getNumStorageDirs());
 
     thread.start();
@@ -57,10 +74,10 @@ public class TestNNUpdateStorageVersionWhenInterrupt {
     assertEquals(1, nnStorage.getNumStorageDirs());
   }
 
-  private static class updateVersionFileThread extends Thread {
+  private static class UpdateVersionFileThread extends Thread {
     NNStorage nnStorage;
 
-    public updateVersionFileThread(NNStorage nnStorage) {
+    UpdateVersionFileThread(NNStorage nnStorage) {
       this.nnStorage = nnStorage;
     }
 
