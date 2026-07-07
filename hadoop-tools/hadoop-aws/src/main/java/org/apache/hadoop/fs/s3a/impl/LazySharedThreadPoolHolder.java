85c233724020e27ca9e5ef69fec2fe6de1d592a1
Konstantin Bereznyakov <konstantinb@users.noreply.github.com>
2026-07-07 14:29:21 -0700

    HADOOP-19862: S3A: introduce configurable shared thread pool for AWS SDK clients (#8426)
    * S3A: introduce configurable shared thread pool for AWS SDK clients
    * missed the need to upgrade new test code to JUnit 5
    * addressing addiitional PR feedback (mainly test code)

---
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/impl/LazySharedThreadPoolHolder.java
@@ -0,0 +1,142 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.fs.s3a.impl;
+
+import java.util.Optional;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.hadoop.classification.InterfaceAudience;
+import org.apache.hadoop.classification.InterfaceStability;
+import org.apache.hadoop.classification.VisibleForTesting;
+import org.apache.hadoop.conf.Configuration;
+
+import static org.apache.hadoop.fs.s3a.Constants.AWS_CLIENT_SHARED_THREADPOOL_KEEPALIVE_DEFAULT;
+import static org.apache.hadoop.fs.s3a.Constants.AWS_CLIENT_SHARED_THREADPOOL_SIZE_DEFAULT;
+import static org.apache.hadoop.fs.s3a.S3AUtils.intOption;
+
+/**
+ * Holder for a lazily initialized shared ScheduledExecutorService.
+ */
+@InterfaceAudience.Private
+@InterfaceStability.Unstable
+public class LazySharedThreadPoolHolder {
+
+  private static final Logger LOG =
+      LoggerFactory.getLogger(LazySharedThreadPoolHolder.class);
+
+  private final String enabledKey;
+  private final String sizeKey;
+  private final String keepAliveKey;
+  private final String namePrefix;
+
+  private Optional<ScheduledExecutorService> executor;
+
+  /**
+   * Create a holder for a lazy shared thread pool.
+   * @param enabledKey config key to enable the shared pool
+   * @param sizeKey config key for pool size
+   * @param keepAliveKey config key for thread keep-alive in seconds
+   * @param namePrefix thread name prefix for debugging
+   */
+  public LazySharedThreadPoolHolder(String enabledKey, String sizeKey,
+      String keepAliveKey, String namePrefix) {
+    this.enabledKey = enabledKey;
+    this.sizeKey = sizeKey;
+    this.keepAliveKey = keepAliveKey;
+    this.namePrefix = namePrefix;
+  }
+
+  /**
+   * Get the config key controlling whether the shared pool is enabled.
+   * This is for testing only.
+   * @return the enabled config key
+   */
+  @VisibleForTesting
+  public String getEnabledKey() {
+    return enabledKey;
+  }
+
+  /**
+   * Get the config key controlling the pool size.
+   * This is for testing only.
+   * @return the size config key
+   */
+  @VisibleForTesting
+  public String getSizeKey() {
+    return sizeKey;
+  }
+
+  /**
+   * Get the config key controlling the thread keep-alive.
+   * This is for testing only.
+   * @return the keep-alive config key
+   */
+  @VisibleForTesting
+  public String getKeepAliveKey() {
+    return keepAliveKey;
+  }
+
+  /**
+   * Get the shared executor, creating it on first call if enabled.
+   * @param conf configuration
+   * @return the executor, or null if not enabled
+   */
+  public synchronized ScheduledExecutorService get(Configuration conf) {
+    if (executor == null) {
+      if (conf.getBoolean(enabledKey, false)) {
+        int poolSize = intOption(conf, sizeKey,
+            AWS_CLIENT_SHARED_THREADPOOL_SIZE_DEFAULT, 1);
+        int keepAlive = intOption(conf, keepAliveKey,
+            AWS_CLIENT_SHARED_THREADPOOL_KEEPALIVE_DEFAULT, 1);
+        executor = Optional.of(createScheduledExecutor(namePrefix, poolSize, keepAlive));
+      } else {
+        executor = Optional.empty();
+      }
+    }
+    return executor.orElse(null);
+  }
+
+  /**
+   * Create a scheduled executor with idle thread timeout.
+   * @param namePrefix thread name prefix for debugging
+   * @param poolSize core pool size
+   * @param keepAliveSeconds keepalive time in seconds
+   * @return the executor
+   */
+  @VisibleForTesting
+  public static ScheduledExecutorService createScheduledExecutor(
+      String namePrefix, int poolSize, int keepAliveSeconds) {
+    ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(poolSize,
+        runnable -> {
+          Thread t = new Thread(runnable, namePrefix);
+          t.setDaemon(true);
+          return t;
+        });
+    pool.setKeepAliveTime(keepAliveSeconds, TimeUnit.SECONDS);
+    pool.allowCoreThreadTimeOut(true);
+    LOG.debug("Created shared executor '{}' with pool size {} and keepalive {}s",
+        namePrefix, poolSize, keepAliveSeconds);
+    return pool;
+  }
+}
