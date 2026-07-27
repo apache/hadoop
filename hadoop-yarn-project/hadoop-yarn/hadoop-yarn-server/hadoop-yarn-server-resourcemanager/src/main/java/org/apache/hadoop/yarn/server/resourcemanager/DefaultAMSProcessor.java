673fc22b512a0545ebabb4fcb6949bb71191ffb5
Ryu Kobayashi <beter.max@gmail.com>
2026-07-27 17:43:41 +0900

    YARN-11964: Clamp negative available resources in AllocateResponse to zero (#8523) Contributed by Ryu Kobayashi
    Clamp negative scheduler headroom to zero when DefaultAMSProcessor constructs the AllocateResponse. Keeping the change at the response boundary avoids changing the semantics of general Resource operations.
    
    Preserve null resource limits for compatibility and add a regression test covering negative memory and vCore headroom.
    
    Reviewed-by: Cheng Pan <chengpan@apache.org>
    Signed-off-by: Shilun Fan <slfan1989@apache.org>

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/DefaultAMSProcessor.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/DefaultAMSProcessor.java
@@ -331,7 +331,9 @@ final class DefaultAMSProcessor implements ApplicationMasterServiceProcessor {
 
     response.setCompletedContainersStatuses(appAttempt
         .pullJustFinishedContainers());
-    response.setAvailableResources(allocation.getResourceLimit());
+    Resource resourceLimit = allocation.getResourceLimit();
+    response.setAvailableResources(resourceLimit == null ? null :
+        Resources.componentwiseMax(resourceLimit, Resources.none()));
 
     QueueMetrics queueMetrics =
         this.rmContext.getScheduler().getRootQueueMetrics();
