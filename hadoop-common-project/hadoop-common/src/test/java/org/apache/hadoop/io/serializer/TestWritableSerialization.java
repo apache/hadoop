e12e8de63f6bc8044ddd1c26c20821f44eb5d57f
Steve Loughran <stevel@apache.org>
2026-07-31 15:37:28 +0100

    HADOOP-19932. Prune obsolete classes in org.apache.hadoop.io.serializer (#8585) (#8642)
    This cuts out obsolete classes from org.apache.hadoop.io.serializer
    
    - JavaComparator
    - JavaSerialization
    - DeserializerComparator
    
    These were all tagged as experimental or limited private(hdfs/mapreduce), or Unstable.
    
    "Wake up, time to die!"
    
    Contains content generated by Claude.ai
    
    ea15fed5-5bdd-4ab0-a603-9c9189281fe2
    
    (cherry picked from commit fcac5e7096a59914527c6d6010bd703b1b8cd67c)

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/io/serializer/TestWritableSerialization.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/io/serializer/TestWritableSerialization.java
@@ -18,10 +18,6 @@
 
 package org.apache.hadoop.io.serializer;
 
-import java.io.Serializable;
-
-import org.apache.hadoop.io.DataInputBuffer;
-import org.apache.hadoop.io.DataOutputBuffer;
 import static org.apache.hadoop.io.TestGenericWritable.CONF_TEST_KEY;
 import static org.apache.hadoop.io.TestGenericWritable.CONF_TEST_VALUE;
 
@@ -29,7 +25,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.TestGenericWritable.Baz;
 import org.apache.hadoop.io.TestGenericWritable.FooGenericWritable;
-import org.apache.hadoop.io.WritableComparator;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -60,43 +55,6 @@ public class TestWritableSerialization {
     Baz result = SerializationTestUtil.testSerialization(conf, baz);
     assertEquals(baz, result);
     assertNotNull(result.getConf());
-  }
-
-  @Test
-  @SuppressWarnings({"rawtypes", "unchecked"})
-  public void testWritableComparatorJavaSerialization() throws Exception {
-    Serialization ser = new JavaSerialization();
-
-    Serializer<TestWC> serializer = ser.getSerializer(TestWC.class);
-    DataOutputBuffer dob = new DataOutputBuffer();
-    serializer.open(dob);
-    TestWC orig = new TestWC(0);
-    serializer.serialize(orig);
-    serializer.close();
-
-    Deserializer<TestWC> deserializer = ser.getDeserializer(TestWC.class);
-    DataInputBuffer dib = new DataInputBuffer();
-    dib.reset(dob.getData(), 0, dob.getLength());
-    deserializer.open(dib);
-    TestWC deser = deserializer.deserialize(null);
-    deserializer.close();
-    assertEquals(orig, deser);
-  }
-
-  static class TestWC extends WritableComparator implements Serializable {
-    static final long serialVersionUID = 0x4344;
-    final int val;
-    TestWC() { this(7); }
-    TestWC(int val) { this.val = val; }
-    @Override
-    public boolean equals(Object o) {
-      if (o instanceof TestWC) {
-        return ((TestWC)o).val == val;
-      }
-      return false;
-    }
-    @Override
-    public int hashCode() { return val; }
   }
 
 }
